04f044f4386aa83a6b521e65ce7ac680363a1952
Jeff Haynie <jhaynie@gmail.com>
2009-10-14 09:24:53 -0700

    prevent default project from complaining in JS lint

--- support/all/index.js
+++ support/all/index.js
@@ -2,7 +2,7 @@ window.onload = function()
 {
 	document.getElementById('version').innerHTML = Titanium.version;
 	document.getElementById('phone').innerHTML = Titanium.Platform.name + ' '+Titanium.Platform.version;
-	document.getElementById('b').onclick = function() { Titanium.Media.vibrate() };
+	document.getElementById('b').onclick = function() { Titanium.Media.vibrate(); };
 
 	Titanium.Gesture.addEventListener('shake',function(){
 		var alerty = Titanium.UI.createAlertDialog();
