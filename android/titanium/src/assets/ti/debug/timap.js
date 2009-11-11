9520e69bc1ff69cf70ebf95de31cbbca441ace7d
donthorp <don@donthorp.net>
2009-11-10 18:17:27 -0600

    Titanium.Geolocation.reverseGeocoder is implemented in the maps module

--- android/titanium/src/assets/ti/debug/timap.js
+++ android/titanium/src/assets/ti/debug/timap.js
@@ -4,6 +4,7 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+// requires Ti, UI, Geolocation
 
 Ti.UI.MAP_VIEW_STANDARD = 0;
 Ti.UI.MAP_VIEW_SATELLITE = 1;
@@ -14,34 +15,34 @@ Ti.UI.MapView =
 	_module : "TitaniumMap",
 
 	setCenterCoordinate : function(coordinate) {
-
+		Ti.Method.dispatch(this._module, "setCenterCoordinate", coordinate);
 	},
 
 	setRegion : function(region) {
-
+		Ti.Method.dispatch(this._module, "setRegion", region);
 	},
 
 	setType : function(type) {
-
+		Ti.Method.dispatch(this._module, "setType", type);
 	},
 
 	setZoomEnabled : function(enabled) {
-
+		Ti.Method.dipatch(this._module,"setZoomEnabled", enabled);
 	},
 
 	setScrollEnabled : function(enabled) {
-
+		Ti.Method.dipatch(this._module,"setScrollEnabled", enabled);
 	}
 };
 
-Ti.UI.prototype.createMapView = function(options) {
+Ti.UI.createMapView = function(options) {
 	if (Ti.isUndefined(options)) {
 		options = {};
 	}
 
 };
 
-Ti.Geolocation.prototype.reverseGeocoder = function(coordinate, location) {
-
+Ti.Geolocation.reverseGeocoder = function(coordinate, location) {
+	return Ti.Method.dispatch("TitaniumMap", "reverseGeocoder", coordinate, registerOneShot(this, location));
 };
 
