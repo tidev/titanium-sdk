376ba31bf75c1d8970cb2038e4d0059ca9c94ec9
Jeff Haynie <jhaynie@appcelerator.com>
2010-02-03 20:13:51 -0800

    initial removal of old files from 0.8

--- iphone/NuZip/objc/NuZip.h
+++
@@ -1,9 +0,0 @@
-
-
-#import <Foundation/Foundation.h>
-
-@interface NuZip : NSObject {}
-
-+ (BOOL) unzip: (NSString *) inputPath toFolder: (NSString *) outputPath password:(NSString *) passwordString error:(NSError **) error;
-
-@end
