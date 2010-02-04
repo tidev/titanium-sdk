069a925450eb1efd8506275e1a36872d01acfd40
Jeff Haynie <jhaynie@appcelerator.com>
2010-02-04 10:54:40 -0800

    fixed so you can now build with ticore from remote repo

--- support/all/app.js
+++ support/all/app.js
@@ -1,2 +1,4 @@
 var w = Ti.UI.createWindow({backgroundColor:'red'});
+var v = Ti.UI.createLabel({text:Ti.version,color:'white',width:'auto',font:{fontSize:72,fontWeight:'bold'}});
+w.add(v);
 w.open();
