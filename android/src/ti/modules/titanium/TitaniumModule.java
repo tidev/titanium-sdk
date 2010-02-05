595a0ef9485fa474c69988ffba9f96041da35ba3
Marshall Culpepper <mculpepper@appcelerator.com>
2010-02-04 21:47:39 -0600

    implemented set/clearTimeout, set/clearInterval, refactored KrollCallback.call to accept an array of Objects (for functions taking more than one argument), the old style TiDict syntax is now on callWithProperties. removed a cast in KrollMethod.call to KrollObject ("thisObj" wasn't being used in this context anyway)

--- android/src/ti/modules/titanium/TitaniumModule.java
+++ android/src/ti/modules/titanium/TitaniumModule.java
@@ -1,10 +1,15 @@
 package ti.modules.titanium;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.appcelerator.titanium.TiContext;
 import org.appcelerator.titanium.TiDict;
 import org.appcelerator.titanium.TiModule;
+import org.appcelerator.titanium.kroll.KrollCallback;
 import org.appcelerator.titanium.util.Log;
 
 public class TitaniumModule
@@ -38,5 +43,66 @@ public class TitaniumModule
 				Log.e(LCAT, "Error while evaluating: " + filename, e);
 			}
 		}
+	}
+	
+	private HashMap<Integer, Timer> timers = new HashMap<Integer, Timer>();
+	private int currentTimerId;
+	
+	public int setTimeout(Object fn, long timeout, final Object[] args)
+		throws IllegalArgumentException
+	{
+		// TODO: we should handle evaluatable code eventually too..
+		Log.d(LCAT, "got setTimeout @" + new Date().getTime());
+		if (fn instanceof KrollCallback) {
+			final KrollCallback callback = (KrollCallback) fn;
+			Timer timer = new Timer();
+			final int timerId = currentTimerId++;
+
+			timers.put(timerId, timer);
+			timer.schedule(new TimerTask(){
+				@Override
+				public void run() {
+					Log.d(LCAT, "calling timer " + timerId + " @" + new Date().getTime());
+					callback.call(args);
+					timers.remove(timerId);
+				}
+			}, timeout);
+			return timerId;
+		}
+		else throw new IllegalArgumentException("Don't know how to call callback of type: " + fn.getClass().getName());
+	}
+	
+	public void clearTimeout(int timerId) {
+		if (timers.containsKey(timerId)) {
+			Timer timer = timers.remove(timerId);
+			timer.cancel();
+		}
+	}
+	
+	public int setInterval(Object fn, long timeout, final Object[] args)
+		throws IllegalArgumentException
+	{
+		// TODO: we should handle evaluatable code eventually too..
+		Log.d(LCAT, "got setInterval @" + new Date().getTime());
+		if (fn instanceof KrollCallback) {
+			final KrollCallback callback = (KrollCallback) fn;
+			Timer timer = new Timer();
+			final int timerId = currentTimerId++;
+	
+			timers.put(timerId, timer);
+			timer.schedule(new TimerTask(){
+				@Override
+				public void run() {
+					Log.d(LCAT, "calling interval timer " + timerId + " @" + new Date().getTime());
+					callback.call(args);
+				}
+			}, timeout, timeout);
+			return timerId;
+		}
+		else throw new IllegalArgumentException("Don't know how to call callback of type: " + fn.getClass().getName());
+	}
+	
+	public void clearInterval(int timerId) {
+		clearTimeout(timerId);
 	}
 }
