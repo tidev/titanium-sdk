f4a8c6bfaef72f11f78250516e15ed4bf29765ee
Jeff Haynie <jhaynie@appcelerator.com>
2010-02-06 21:49:49 -0800

    removed unused symbol

--- support/iphone/dependscompiler.py
+++ support/iphone/dependscompiler.py
@@ -15,15 +15,6 @@ import jspacker
 ignoreFiles = ['.gitignore', '.cvsignore','.DS_Store'];
 ignoreDirs = ['.git','.svn', 'CVS'];
 
-HEADER = """/**
- * Appcelerator Titanium Mobile
- * This is generated code. Do not modify. Your changes will be lost.
- * Generated code is Copyright (c) 2009-2010 by Appcelerator, Inc.
- * All Rights Reserved.
- */
-#import <Foundation/Foundation.h>
-"""
-
 import_depends = []
 imports_map = {}
 
