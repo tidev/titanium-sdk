2d54b9f0d2c248865c2fc7d4ed991bbd52cb2938
Nolan Wright <nwright@appcelerator.com>
2010-02-08 11:04:21 -0800

    more tests

---
+++ demos/KitchenSink/Resources/examples/photo_gallery_save.js
@@ -0,0 +1,34 @@
+Titanium.Media.showCamera({
+
+	success:function(event)
+	{
+		var cropRect = event.cropRect;
+		var image = event.media;
+		var thumbnail = event.thumbnail;
+		
+		Titanium.Media.saveToPhotoGallery(image);
+	},
+	cancel:function()
+	{
+
+	},
+	error:function(error)
+	{
+		// create alert
+		var a = Titanium.UI.createAlertDialog({title:'Camera'});
+
+		// set message
+		if (error.code == Titanium.Media.NO_CAMERA)
+		{
+			a.setMessage('Device does not have video recording capabilities');
+		}
+		else
+		{
+			a.setMessage('Unexpected error: ' + error.code);
+		}
+
+		// show alert
+		a.show();
+	},
+	allowImageEditing:true,
+});
