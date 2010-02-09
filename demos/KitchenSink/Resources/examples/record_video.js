5b11987d94a06ec4d8fc3725e372c701383e9f5d
Nolan Wright <nwright@appcelerator.com>
2010-02-08 16:21:31 -0800

    more tests

--- demos/KitchenSink/Resources/examples/record_video.js
+++ demos/KitchenSink/Resources/examples/record_video.js
@@ -1,58 +1,70 @@
 var win = Titanium.UI.currentWindow;
 
-
-
-Titanium.Media.showCamera({
-
-	success:function(event)
-	{
-		for (v in event)
-		{
-			Ti.API.info('v ' + v + ' event[v] ' + event[v]);
-		}
-		var video = event.media;
-		var thumbnail = event.thumbnail;
-	
-		var f = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory,'mymovie.mov');
-		f.write(video);
-
-		// put in timeout - strange threading issues from doing this in callback
-		setTimeout(function()
-		{
-			var activeMovie = Titanium.Media.createVideoPlayer({
-				backgroundColor:'#111',
-				movieControlMode:Titanium.Media.VIDEO_CONTROL_DEFAULT,
-				scalingMode:Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
-				contentURL:f.nativePath
-			});
-			activeMovie.play();
-		
-		},500);
-	
-	},
-	cancel:function()
-	{
-
-	},
-	error:function(error)
-	{
-		// create alert
-		var a = Titanium.UI.createAlertDialog({title:'Video'});
-
-		// set message
-		if (error.code == Titanium.Media.NO_VIDEO)
-		{
-			a.setMessage('Device does not have video recording capabilities');
-		}
-		else
-		{
-			a.setMessage('Unexpected error: ' + error.code);
-		}
-
-		// show alert
-		a.show();
-	},
-	mediaTypes: Titanium.Media.MEDIA_TYPE_VIDEO,
-	videoMaximumDuration:10000,
-	videoQuality:Titanium.Media.QUALITY_HIGH
+var b = Titanium.UI.createButton({
+	title:'Record Movie',
+	width:200,
+	height:40,
+	top:20
 });
+win.add(b);
+
+b.addEventListener('click', function()
+{
+	if (b.title == 'Play Movie')
+	{
+		var activeMovie = Titanium.Media.createVideoPlayer({
+			backgroundColor:'#111',
+			movieControlMode:Titanium.Media.VIDEO_CONTROL_DEFAULT,
+			scalingMode:Titanium.Media.VIDEO_SCALING_ASPECT_FILL,
+			contentURL:movieFile.nativePath
+		});
+		activeMovie.play();
+
+		activeMovie.addEventListener('complete', function()
+		{
+			movieFile.deleteFile();
+			b.title = 'Record Movie';
+		});
+	}
+	else
+	{
+		Titanium.Media.showCamera({
+
+			success:function(event)
+			{
+				var video = event.media;
+				movieFile = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory,'mymovie.mov');
+				movieFile.write(video);
+				b.title = 'Play Movie';
+			},
+			cancel:function()
+			{
+
+			},
+			error:function(error)
+			{
+				// create alert
+				var a = Titanium.UI.createAlertDialog({title:'Video'});
+
+				// set message
+				if (error.code == Titanium.Media.NO_VIDEO)
+				{
+					a.setMessage('Device does not have video recording capabilities');
+				}
+				else
+				{
+					a.setMessage('Unexpected error: ' + error.code);
+				}
+
+				// show alert
+				a.show();
+			},
+			mediaTypes: Titanium.Media.MEDIA_TYPE_VIDEO,
+			videoMaximumDuration:10000,
+			videoQuality:Titanium.Media.QUALITY_HIGH
+		});
+	
+	}
+	
+});
+
