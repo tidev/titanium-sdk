a6f4fcf12bf83b5e47e9d773174d2651d0a3055b
Nolan Wright <nwright@appcelerator.com>
2010-02-09 16:00:18 -0800

    more tests

--- demos/KitchenSink/Resources/app.js
+++ demos/KitchenSink/Resources/app.js
@@ -1,3 +1,5 @@
+//Titanium.include('my_js_include.js', 'my_js_include_2.js', 'local_include.js');
+
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
--- demos/KitchenSink/Resources/examples/js_include.js
+++ demos/KitchenSink/Resources/examples/js_include.js
@@ -1,3 +1,3 @@
-Titanium.include('../my_js_include.js', '../my_js_include_2.js');
+Titanium.include('../my_js_include.js', '../my_js_include_2.js', 'local_include.js');
 
-alert('first name: ' + myFirstName + ' last name: ' + myLastName);
+alert('first name: ' + myFirstName + ' middle name: ' + myMiddleName +' last name: ' + myLastName);
