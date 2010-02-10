a89ad6dcc28bc75689e2408a9b5329730556e7d1
Marshall Culpepper <mculpepper@appcelerator.com>
2010-02-10 11:36:16 -0800

    initial implementation of XHR for android. make sure KrollObjects that contain a TiProxy are asked for dynamic properties before we delegate back to Rhino. added support for the "image" blob property on ImageView (per KitchenSink)

--- android/src/org/appcelerator/titanium/kroll/KrollCallback.java
+++ android/src/org/appcelerator/titanium/kroll/KrollCallback.java
@@ -33,7 +33,13 @@ public class KrollCallback
 		call(new Object[] { data });
 	}
 	
-	public void call(Object[] args) {
+	public void call()
+	{
+		call(new Object[0]);
+	}
+	
+	public void call(Object[] args)
+	{
 		if (args == null) args = new Object[0];
 		final Object[] fArgs = args;
 		
@@ -47,7 +53,7 @@ public class KrollCallback
 						Object jsArg = KrollObject.fromNative(fArgs[i], kroll);
 						jsArgs[i] = jsArg;
 					}
-					method.call(ctx, thisObj, thisObj.getPrototype(), jsArgs);
+					method.call(ctx, thisObj, thisObj, jsArgs);
 				} catch (Throwable e) {
 					Log.e(LCAT, "ERROR: " + e.getMessage(), e);
 					//Context.throwAsScriptRuntimeEx(e);
