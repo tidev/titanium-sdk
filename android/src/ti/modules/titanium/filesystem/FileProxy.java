2857deccaaf9da2778bacc5bbe24adf13777a3dd
Marshall Culpepper <mculpepper@appcelerator.com>
2010-02-10 18:59:47 -0800

    added file object support to the "image" property of ImageView

--- android/src/ti/modules/titanium/filesystem/FileProxy.java
+++ android/src/ti/modules/titanium/filesystem/FileProxy.java
@@ -1,6 +1,9 @@
 package ti.modules.titanium.filesystem;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 
 import org.appcelerator.titanium.TiBlob;
@@ -14,16 +17,35 @@ public class FileProxy extends TiProxy
 
 	String path;
 	TiBaseFile tbf; // The base file object.
-
+	
+	public static <T>
+	String join(final Collection<T> objs, final String delimiter) {
+	    if (objs == null || objs.isEmpty())
+	        return "";
+	    Iterator<T> iter = objs.iterator();
+	    // remove the following two lines, if you expect the Collection will behave well
+	    if (!iter.hasNext())
+	        return "";
+	    StringBuffer buffer = new StringBuffer(String.valueOf(iter.next()));
+	    while (iter.hasNext())
+	        buffer.append(delimiter).append(String.valueOf(iter.next()));
+	    return buffer.toString();
+	}
+	
 	public FileProxy(TiContext tiContext, String[] parts) {
 		super(tiContext);
-
-		tbf = TiFileFactory.createTitaniumFile(tiContext, parts, false);
+		
+		String path = getTiContext().resolveUrl(join(Arrays.asList(parts), "/"));
+		tbf = TiFileFactory.createTitaniumFile(tiContext, new String[] { path }, false);
 	}
 
 	private FileProxy(TiContext tiContext, TiBaseFile tbf) {
 		super(tiContext);
 		this.tbf = tbf;
+	}
+	
+	public TiBaseFile getBaseFile() {
+		return tbf;
 	}
 
 	public boolean isFile() {
