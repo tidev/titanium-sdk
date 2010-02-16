b16644f5a31fcdb06f32273a72726a37ca01609d
donthorp <don@donthorp.net>
2010-02-16 09:58:58 -0800

    DisplayCaps

--- android/src/ti/modules/titanium/platform/PlatformModule.java
+++ android/src/ti/modules/titanium/platform/PlatformModule.java
@@ -3,13 +3,23 @@ package ti.modules.titanium.platform;
 import org.appcelerator.titanium.TiContext;
 import org.appcelerator.titanium.TiModule;
 
-public class PlatformModule extends TiModule {
+public class PlatformModule extends TiModule
+{
+
+	protected DisplayCapsProxy displayCaps;
 
 	public PlatformModule(TiContext context) {
 		super(context);
 	}
-	
+
 	public String getName() {
 		return "android";
+	}
+
+	public DisplayCapsProxy getDisplayCaps() {
+		if (displayCaps == null) {
+			displayCaps = new DisplayCapsProxy(getTiContext());
+		}
+		return displayCaps;
 	}
 }
