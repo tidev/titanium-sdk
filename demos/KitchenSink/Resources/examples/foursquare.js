17f25896e4e3872377e79108ca4684a2d2dc9d87
Jeff Haynie <jhaynie@appcelerator.com>
2010-02-25 18:45:45 -0800

    added new Utils module to house stuff that doesn't fit elsewhere... added Foursquare API example

---
+++ demos/KitchenSink/Resources/examples/foursquare.js
@@ -0,0 +1,120 @@
+var win = Titanium.UI.currentWindow;
+
+var username = Ti.UI.createTextField({
+	autocapitalization:Titanium.UI.TEXT_AUTOCAPITALIZATION_NONE,
+	width:300,
+	top:10,
+	height:35,
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+	hintText:'Foursquare Username'
+});
+win.add(username);
+
+var password = Ti.UI.createTextField({
+	autocapitalization:Titanium.UI.TEXT_AUTOCAPITALIZATION_NONE,
+	width:300,
+	top:55,
+	height:35,
+	borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED,
+	passwordMask:true,
+	hintText:'Foursquare Password'
+});
+win.add(password);
+
+var button = Titanium.UI.createButton({
+	title:'Get Venues Nearby',
+	top:100,
+	width:300,
+	height:40
+});
+win.add(button);
+
+var navActInd = Titanium.UI.createActivityIndicator();
+win.setRightNavButton(navActInd);
+
+button.addEventListener('click', function()
+{
+	label.text = "Determining your location...";
+	navActInd.show();
+	password.blur();
+	Ti.API.info("starting geo");
+	Titanium.Geolocation.getCurrentPosition(function(e)
+	{
+		Ti.API.info("received geo response");
+		if (e.error)
+		{
+			alert(e.error);
+			return;
+		}
+
+		var longitude = e.coords.longitude;
+		var latitude = e.coords.latitude;
+
+		label.text = "You are at: "+longitude+"\n"+latitude+"\n\nFinding venues...";
+		
+		var xhr = Titanium.Network.createHTTPClient();
+		xhr.onerror = function(e)
+		{
+			Ti.API.info("ERROR " + e.error);
+			navActInd.hide();
+			alert(e.error);
+		}
+		xhr.onload = function()
+		{
+			label.hide();
+//			Ti.API.info("foursquare response was "+this.responseText);
+			
+			var resp =  eval('('+this.responseText+')');
+			var venues = resp.groups[0].venues;
+			for (var i=0;i<venues.length;i++)
+			{
+				status.text += venues[i].name+'\n\n';
+
+			}
+			scrollView.add(status);
+			
+			navActInd.hide();
+		};
+		// open the client and encode our URL
+		xhr.open('GET','http://api.foursquare.com/v1/venues.json?geolat='+latitude+'&geolong='+longitude);
+		// base64 encode our Authorization header
+		xhr.setRequestHeader('Authorization','Basic '+Ti.Utils.base64encode(username.value+':'+password.value));
+
+		// send the data
+		xhr.send();
+		Ti.API.info("sending foursquare API request for "+latitude+","+longitude);
+	});
+});
+var scrollView = Titanium.UI.createScrollView({
+	top:150,
+	contentHeight:'auto',
+	contentWidth:'auto',
+	backgroundColor:'#13386c',
+	width:300,
+	height:200,
+	borderRadius:10
+});
+win.add(scrollView);
+
+var label = Titanium.UI.createLabel({
+	text:'Please login',
+	font:{fontSize:18},
+	color:'white',
+	width:250,
+	height:'auto',
+	textAlign:'center'
+});
+scrollView.add(label);
+
+var status = Titanium.UI.createLabel({
+	font:{fontSize:18},
+	color:'white',
+	width:250,
+	height:'auto',
+	top:20,
+	text:'',
+	textAlign:'center'
+});
+
+
+
---
+++ iphone/Classes/UtilsModule.m
@@ -0,0 +1,74 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+#import "UtilsModule.h"
+#import "TiUtils.h"
+#import "Base64Transcoder.h"
+#import <CommonCrypto/CommonDigest.h>
+
+@implementation UtilsModule
+
+#pragma mark Public API
+
+-(NSString*)base64encode:(id)args
+{
+	ENSURE_SINGLE_ARG(args,NSString);
+
+	const char *data = [args UTF8String];
+	size_t len = [args length];
+	
+	size_t outsize = EstimateBas64EncodedDataSize(len);
+	char *base64Result = malloc(sizeof(char)*outsize);
+    size_t theResultLength = outsize;
+	
+    bool result = Base64EncodeData(data, len, base64Result, &theResultLength);
+	if (result)
+	{
+		NSData *theData = [NSData dataWithBytes:base64Result length:theResultLength];
+		free(base64Result);
+		return [[[NSString alloc] initWithData:theData encoding:NSUTF8StringEncoding] autorelease];
+	}    
+	free(base64Result);
+	return nil;
+}
+
+-(NSString*)base64decode:(id)args
+{
+	ENSURE_SINGLE_ARG(args,NSString);
+	
+	const char *data = [args UTF8String];
+	size_t len = [args length];
+	
+	size_t outsize = EstimateBas64DecodedDataSize(len);
+	char *base64Result = malloc(sizeof(char)*outsize);
+    size_t theResultLength = outsize;
+	
+    bool result = Base64DecodeData(data, len, base64Result, &theResultLength);
+	if (result)
+	{
+		NSData *theData = [NSData dataWithBytes:base64Result length:theResultLength];
+		free(base64Result);
+		return [[[NSString alloc] initWithData:theData encoding:NSUTF8StringEncoding] autorelease];
+	}    
+	free(base64Result);
+	return nil;
+}
+
+-(NSString*)md5HexDigest:(id)args
+{
+	ENSURE_SINGLE_ARG(args,NSString);
+	const char* str = [args UTF8String];
+	unsigned char result[CC_MD5_DIGEST_LENGTH];
+	CC_MD5(str, strlen(str), result);
+	
+	return [NSString stringWithFormat:
+			@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
+			result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
+			result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]
+			];
+}
+@end
