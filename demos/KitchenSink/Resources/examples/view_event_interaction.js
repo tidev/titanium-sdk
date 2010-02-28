660ec39e0f613cf31c55debfaf8366248216fcef
Nolan Wright <nwright@appcelerator.com>
2010-02-28 13:12:35 -0800

    more tests

--- demos/KitchenSink/Resources/examples/view_event_interaction.js
+++ demos/KitchenSink/Resources/examples/view_event_interaction.js
@@ -33,13 +33,41 @@ view2.add(label);
 win.add(view2);
 win.add(view1);
 
+var l = Ti.UI.createLabel({
+	text:'click on box',
+	width:300,
+	height:'auto',
+	top:10,
+	font:{fontSize:13},
+});
+win.add(l);
+var l2 = Ti.UI.createLabel({
+	text:'click on label',
+	width:300,
+	height:'auto',
+	top:25,
+	font:{fontSize:13},
+});
+win.add(l2);
 view2.addEventListener('click',function()
 {
-	alert("You were able to click on the view. Good!");
+	l.text = "You were able to click on the box. Good!";
+
+	setTimeout(function()
+	{
+		l.text = "click on box";
+		
+	},1000);
+	
 });
 
 label.addEventListener('click',function()
 {
-	alert("You were able to click on the label. Good!");
+	l2.text = "You were able to click on the label. Good!";
+
+	setTimeout(function()
+	{
+		l2.text = "click on label";
+	},1000);
 });
 
