b788b3f19981c43a29cde5a34765782880f3f54c
donthorp <don@donthorp.net>
2010-03-01 13:39:45 -0600

    Added Gesture module so webview tests will display. Have not integrated eventing.

---
+++ android/modules/gesture/src/ti/modules/titanium/gesture/GestureModule.java
@@ -0,0 +1,11 @@
+package ti.modules.titanium.gesture;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiModule;
+
+public class GestureModule extends TiModule {
+
+	public GestureModule(TiContext tiContext) {
+		super(tiContext);
+	}
+}
