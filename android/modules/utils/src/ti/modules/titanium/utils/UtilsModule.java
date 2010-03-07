438be11e61476ed14ca79adc8daf30986b10b7df
donthorp <don@donthorp.net>
2010-03-06 17:59:49 -0800

    Added stub utils module

---
+++ android/modules/utils/src/ti/modules/titanium/utils/UtilsModule.java
@@ -0,0 +1,11 @@
+package ti.modules.titanium.utils;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiModule;
+
+public class UtilsModule extends TiModule {
+
+	public UtilsModule(TiContext tiContext) {
+		super(tiContext);
+	}
+}
