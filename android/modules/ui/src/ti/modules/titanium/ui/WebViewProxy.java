8e20e1ebdffae803268f0e7bc8922c8bd9877a81
donthorp <don@donthorp.net>
2010-03-08 12:29:44 -0800

    Some webview fixes

--- android/modules/ui/src/ti/modules/titanium/ui/WebViewProxy.java
+++ android/modules/ui/src/ti/modules/titanium/ui/WebViewProxy.java
@@ -19,51 +19,26 @@ import android.os.Message;
 public class WebViewProxy extends ViewProxy
 	implements Handler.Callback
 {
-	
+
 	private static final int MSG_FIRST_ID = ViewProxy.MSG_LAST_ID + 1;
 
-	private static final int MSG_SET_URL = MSG_FIRST_ID + 100;
-	private static final int MSG_SET_HTML = MSG_FIRST_ID + 101;
-	private static final int MSG_EVAL_JS = MSG_FIRST_ID + 102;
+	private static final int MSG_EVAL_JS = MSG_FIRST_ID + 100;
 	protected static final int MSG_LAST_ID = MSG_FIRST_ID + 999;
-	
+
 	public WebViewProxy(TiContext context, Object[] args) {
 		super(context, args);
 	}
-	
+
 	@Override
 	public TiUIView createView(Activity activity) {
 		return new TiUIWebView(this);
 	}
-	
+
 	public TiUIWebView getWebView() {
 		return (TiUIWebView)getView(getTiContext().getActivity());
 	}
-	
-	public void setUrl(String url)
-	{
-		if (getTiContext().isUIThread()) {
-			getWebView().setUrl(url);
-		} else {
-			AsyncResult result = new AsyncResult(getTiContext().getActivity());
-			Message msg = getUIHandler().obtainMessage(MSG_SET_URL, result);
-			msg.obj = url;
-			msg.sendToTarget();
-		}
-	}
-	
-	public void setHtml(String html)
-	{
-		if (getTiContext().isUIThread()) {
-			getWebView().setHtml(html);
-		} else {
-			AsyncResult result = new AsyncResult(getTiContext().getActivity());
-			Message msg = getUIHandler().obtainMessage(MSG_SET_HTML, result);
-			msg.obj = html;
-			msg.sendToTarget();
-		}
-	}
-	
+
+
 	public Object evalJS(String code) {
 		if (getTiContext().isUIThread()) {
 			return getWebView().getJSValue(code);
@@ -75,16 +50,10 @@ public class WebViewProxy extends ViewProxy
 			return result.getResult();
 		}
 	}
-	
+
 	@Override
 	public boolean handleMessage(Message msg) {
 		switch (msg.what) {
-			case MSG_SET_URL:
-				getWebView().setUrl((String)msg.obj);
-				return true;
-			case MSG_SET_HTML:
-				getWebView().setHtml((String)msg.obj);
-				return true;
 			case MSG_EVAL_JS:
 				AsyncResult result = (AsyncResult)msg.obj;
 				String value = getWebView().getJSValue((String)result.getArg());
