fba852ee1713e9d6ac8170e1d7a9352f34968896
Blain Hamon <blain@Millenium-Falcon.local>
2010-03-08 20:46:59 -0800

    Masked images ahoy

--- iphone/Terrordom/rawtest.js
+++ iphone/Terrordom/rawtest.js
@@ -1,103 +1,38 @@
 var win = Titanium.UI.currentWindow;
-win.backgroundColor = '#fff';
+win.backgroundImage = 'Default.png';
 
-var view1 = Ti.UI.createView({
-	backgroundColor:'transparent'
-});
+var imgWidth = (273/2);
+var imgHeight = (613/2);
 
-var b1 = Titanium.UI.createButton({
-	backgroundImage:'ktswbutton.png',
-	top:10,
-    left:25,
-    width:75,
-    height:75
-});
+var bodyTone = Ti.UI.createMaskedImage({mask:'body_mask.png',
+	top:50,left:10,height:imgHeight,width:imgWidth,
+	tint:'white'
+	});
 
-var b2 = Titanium.UI.createButton({
-	backgroundImage:'campusmapbutton.png',
-	top:10,
-    left:120,
-    width:75,
-    height:75
-});
+var bodyOutline = Ti.UI.createMaskedImage({mask:'body_lines.png',
+	top:50,left:10,height:imgHeight,width:imgWidth,
+	tint:'black'
+	});
 
-var b3 = Titanium.UI.createButton({
-	backgroundImage:'directorybutton.png',
-	top:10,
-    left:215,
-    width:75,
-    height:75
-});
+var head = Ti.UI.createMaskedImage({mask:'head_mask.png',
+	top:-10,left:25,height:120,width:80,
+	});
 
-var b4 = Titanium.UI.createButton({
-	backgroundImage:'newsbutton.png',
-	top:105,
-    left:25,
-    width:75,
-    height:75
-});
+win.add(bodyTone);
+win.add(bodyOutline);
+win.add(head);
 
-var b5 = Titanium.UI.createButton({
-    backgroundImage:'sportsbutton.png',
-    top:105,
-    left:120,
-    width:75,
-    height:75
-});
+var changeHead = Ti.UI.createButton({width:60,top:10,right:10,height:30,title:'Use Pic...'});
+win.add(changeHead);
 
-view1.add(b1);
-view1.add(b2);
-view1.add(b3);
-view1.add(b4);
-view1.add(b5);
-
-var view2 = Ti.UI.createView({
-	backgroundColor:'transparent'
-});
-
-var b10 = Titanium.UI.createButton({
-	backgroundImage:'dininghallsbutton.png',
-	top:10,
-    left:25,
-    width:75,
-    height:75
-});
-
-var b11 = Titanium.UI.createButton({
-	backgroundImage:'bobcattubebutton.png',
-	top:10,
-    left:120,
-    width:75,
-    height:75
-});
-
-var b12 = Titanium.UI.createButton({
-	backgroundImage:'audiobutton.png',
-	top:10,
-    left:215,
-    width:75,
-    height:75
-});
-
-view2.add(b10);
-view2.add(b11);
-view2.add(b12);
-
-var scrollView = Titanium.UI.createScrollableView({
-	views:[view1,view2],
-	showPagingControl:true,
-	pagingControlHeight:30,
-    pagingControlColor:'#660000'
-});
-
-win.add(scrollView);
-
-b1.addEventListener('click', function()
-{
-    var win2 = Titanium.UI.createWindow({
-        url:'ktsw.js',
-        barColor:'#660000',
-        title:'KTSW 89.9 FM'
-    });
-    Titanium.UI.currentTab.open(win2,{animated:true});
+changeHead.addEventListener('click',function(e){
+	Titanium.Media.openPhotoGallery({
+	success:function(event)
+	{
+		var image = event.media;
+		// set image view
+		head.image = image;
+	},
+	allowImageEditing:true,
+	});
 });
