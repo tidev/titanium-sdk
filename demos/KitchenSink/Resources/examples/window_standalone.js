a21ad09113e577cf7b10462ce3de855cd831e8f8
Nolan Wright <nwright@appcelerator.com>
2010-03-17 15:24:46 -0700

    more tests

--- demos/KitchenSink/Resources/examples/window_standalone.js
+++ demos/KitchenSink/Resources/examples/window_standalone.js
@@ -5,6 +5,12 @@
 //
 
 var win = Titanium.UI.currentWindow;
+
+win.orientationModes = [
+	Titanium.UI.PORTRAIT,
+	Titanium.UI.LANDSCAPE_LEFT,
+	Titanium.UI.LANDSCAPE_RIGHT,
+];
 win.addEventListener('focus', function()
 {
 	Ti.API.info('FOCUSED EVENT RECEIVED')
