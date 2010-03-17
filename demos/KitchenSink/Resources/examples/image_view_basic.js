d5c15bf4f31b29aaba4c853d7ad78debdf7df62e
Nolan Wright <nwright@appcelerator.com>
2010-03-17 14:19:03 -0700

    more test

--- demos/KitchenSink/Resources/examples/image_view_basic.js
+++ demos/KitchenSink/Resources/examples/image_view_basic.js
@@ -1,7 +1,7 @@
 var win = Titanium.UI.currentWindow;
 
 var imageView = Titanium.UI.createImageView({
-	url:'../images/cloud.png',
+	url:'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png',
 	width:261,
 	height:178,
 	top:20
@@ -23,7 +23,7 @@ function clicker()
 {
 	Titanium.UI.createAlertDialog({title:'Image View', message:'You clicked me!'}).show();
 	l.text = "Try again. You shouldn't get alert and the image should be different";
-	imageView.url = 'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png';
+	imageView.url = '../images/cloud.png';
 	imageView.removeEventListener('click',clicker);
 }
 
