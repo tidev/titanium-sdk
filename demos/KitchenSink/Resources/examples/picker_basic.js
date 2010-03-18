709d8965d79594af50c6bb9a192f2f37effa9bfd
Nolan Wright <nwright@appcelerator.com>
2010-03-18 16:51:02 -0700

    test change

--- demos/KitchenSink/Resources/examples/picker_basic.js
+++ demos/KitchenSink/Resources/examples/picker_basic.js
@@ -6,7 +6,7 @@ var picker = Ti.UI.createPicker();
 var data = [];
 data[0]=Ti.UI.createPickerRow({title:'Bananas',custom_item:'b'});
 data[1]=Ti.UI.createPickerRow({title:'Strawberries',custom_item:'s'});
-data[2]=Ti.UI.createPickerRow({title:'Mangos',custom_item:'m', selected:true});
+data[2]=Ti.UI.createPickerRow({title:'Mangos',custom_item:'m'});
 data[3]=Ti.UI.createPickerRow({title:'Grapes',custom_item:'g'});
 
 picker.add(data);
