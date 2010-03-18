5cc396a0d54b4938d72bfb6c268d5d6ca6187512
donthorp <don@donthorp.net>
2010-03-18 18:07:10 -0500

    Added support for popup on uncaught JS error

--- android/titanium/src/org/appcelerator/titanium/TiContext.java
+++ android/titanium/src/org/appcelerator/titanium/TiContext.java
@@ -12,11 +12,11 @@ import java.lang.ref.WeakReference;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.appcelerator.titanium.bridge.OnEventListenerChange;
@@ -29,18 +29,28 @@ import org.appcelerator.titanium.util.TiActivitySupport;
 import org.appcelerator.titanium.util.TiConfig;
 import org.appcelerator.titanium.util.TiFileHelper;
 import org.appcelerator.titanium.util.TiFileHelper2;
+import org.mozilla.javascript.ErrorReporter;
+import org.mozilla.javascript.EvaluatorException;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
+import android.os.Process;
 import android.os.RemoteException;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class TiContext implements TiEvaluator, ITiMenuDispatcherListener
+public class TiContext implements TiEvaluator, ITiMenuDispatcherListener, ErrorReporter
 {
 	private static final String LCAT = "TiContext";
 	private static final boolean DBG = TiConfig.LOGD;
@@ -661,6 +671,26 @@ public class TiContext implements TiEvaluator, ITiMenuDispatcherListener
 		}
 	}
 
+
+	@Override
+	public void error(String message, String sourceName, int line, String lineSource, int lineOffset)
+	{
+		doRhinoDialog("Error", message, sourceName, line, lineSource, lineOffset);
+	}
+
+	@Override
+	public EvaluatorException runtimeError(String message, String sourceName, int line, String lineSource, int lineOffset)
+	{
+		doRhinoDialog("Runtime Error", message, sourceName, line, lineSource, lineOffset);
+		return null;
+	}
+
+	@Override
+	public void warning(String message, String sourceName, int line, String lineSource, int lineOffset)
+	{
+		doRhinoDialog("Warning", message, sourceName, line, lineSource, lineOffset);
+	}
+
 	public static TiContext createTiContext(Activity activity, TiDict preload, String baseUrl)
 	{
 		TiContext tic = new TiContext(activity, baseUrl);
@@ -669,4 +699,96 @@ public class TiContext implements TiEvaluator, ITiMenuDispatcherListener
 		tic.setJSContext(krollBridge);
 		return tic;
 	}
+
+	private void doRhinoDialog(final String title, final String message, final String sourceName, final int line,
+			final String lineSource, final int lineOffset)
+	{
+		final Semaphore s = new Semaphore(0);
+		final Activity activity = getActivity();
+
+		activity.runOnUiThread(new Runnable(){
+
+			@Override
+			public void run()
+			{
+				OnClickListener listener = new OnClickListener() {
+
+					public void onClick(DialogInterface dialog, int which) {
+						Process.killProcess(Process.myPid());
+					}
+				};
+
+				FrameLayout layout = new FrameLayout(activity);
+				layout.setBackgroundColor(Color.rgb(128, 0, 0));
+
+				LinearLayout vlayout = new LinearLayout(activity);
+				vlayout.setOrientation(LinearLayout.VERTICAL);
+				vlayout.setPadding(10, 10, 10, 10);
+
+				layout.addView(vlayout);
+
+				TextView sourceInfoView = new TextView(activity);
+				sourceInfoView.setBackgroundColor(Color.WHITE);
+				sourceInfoView.setTextColor(Color.BLACK);
+				sourceInfoView.setPadding(4, 5, 4, 0);
+				sourceInfoView.setText("[" + line + "," + lineOffset + "] " + sourceName);
+
+				TextView messageView = new TextView(activity);
+				messageView.setBackgroundColor(Color.WHITE);
+				messageView.setTextColor(Color.BLACK);
+				messageView.setPadding(4, 5, 4, 0);
+				messageView.setText(message);
+
+				TextView sourceView = new TextView(activity);
+				sourceView.setBackgroundColor(Color.WHITE);
+				sourceView.setTextColor(Color.BLACK);
+				sourceView.setPadding(4, 5, 4, 0);
+				sourceView.setText(lineSource);
+
+				TextView infoLabel = new TextView(activity);
+				infoLabel.setText("Location: ");
+				infoLabel.setTextColor(Color.WHITE);
+				infoLabel.setTextScaleX(1.5f);
+
+				TextView messageLabel = new TextView(activity);
+				messageLabel.setText("Message: ");
+				messageLabel.setTextColor(Color.WHITE);
+				messageLabel.setTextScaleX(1.5f);
+
+				TextView sourceLabel = new TextView(activity);
+				sourceLabel.setText("Source: ");
+				sourceLabel.setTextColor(Color.WHITE);
+				sourceLabel.setTextScaleX(1.5f);
+
+				vlayout.addView(infoLabel);
+				vlayout.addView(sourceInfoView);
+				vlayout.addView(messageLabel);
+				vlayout.addView(messageView);
+				vlayout.addView(sourceLabel);
+				vlayout.addView(sourceView);
+
+		        new AlertDialog.Builder(activity)
+		        .setTitle(title)
+		        .setView(layout)
+		        .setPositiveButton("Kill",listener)
+		        .setNeutralButton("Continue", new OnClickListener(){
+					@Override
+					public void onClick(DialogInterface arg0, int arg1) {
+						s.release();
+					}})
+		        .setCancelable(false)
+		        .create()
+		        .show();
+
+			}
+		});
+
+        try {
+        	s.acquire();
+        } catch (InterruptedException e) {
+        	// Ignore
+        }
+	}
+
+
 }
