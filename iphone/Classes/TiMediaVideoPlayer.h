928211e9fbab91f8053482f5c6660653fbf517a6
Jeff Haynie <jhaynie@appcelerator.com>
2010-03-25 22:39:42 -0700

    fixed doc, fix simulator crash issues on leopard, video player changes related to ipad

---
+++ iphone/Classes/TiMediaVideoPlayer.h
@@ -0,0 +1,21 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+#import "TiUIView.h"
+#import <MediaPlayer/MediaPlayer.h>
+
+#ifdef IPAD
+
+@interface TiMediaVideoPlayer : TiUIView {
+@private
+	MPMoviePlayerController *controller;
+}
+
+-(id)initWithPlayer:(MPMoviePlayerController*)controller;
+
+@end
+
+#endif
--- iphone/Classes/TiMediaVideoPlayerProxy.h
+++ iphone/Classes/TiMediaVideoPlayerProxy.h
@@ -5,31 +5,42 @@
  * Please see the LICENSE included with this distribution for details.
  */
 #import <MediaPlayer/MediaPlayer.h>
-#import "TiProxy.h"
+#import "TiViewProxy.h"
 #import "TiColor.h"
 #import "TiFile.h"
 
-@interface TiMediaVideoPlayerProxy : TiProxy {
+@interface TiMediaVideoPlayerProxy : TiViewProxy {
 @private
 	NSURL *url;
 	MPMoviePlayerController *movie;
 	TiColor *backgroundColor;
 	MPMovieScalingMode scalingMode;
 	MPMovieControlMode movieControlMode;
+	MPMovieControlStyle movieControlStyle;
 	NSTimeInterval initialPlaybackTime;
 	BOOL playing;
 	NSMutableArray *views;
 	TiFile *tempFile;
+#ifdef IPAD
+	KrollCallback *thumbnailCallback;
+#endif
 }
 
 @property(nonatomic,readwrite,assign) id url;
 @property(nonatomic,readwrite,assign) TiColor* backgroundColor;
 @property(nonatomic,readwrite,assign) NSNumber* scalingMode;
-@property(nonatomic,readwrite,assign) NSNumber* movieControlMode;
 @property(nonatomic,readwrite,assign) NSNumber* initialPlaybackTime;
 @property(nonatomic,readonly) NSNumber* playing;
 
 -(void)add:(id)proxy;
 -(void)remove:(id)proxy;
+
+
+// this was deprecated in 3.2 but kept around for older devices
+@property(nonatomic,readwrite,assign) NSNumber* movieControlMode;
+
+// introduced in 3.2
+@property(nonatomic,readwrite,assign) NSNumber* movieControlStyle;
+
 
 @end
