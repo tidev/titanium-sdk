5ec50c1e91db6f759205363b0e7daadcf7439ffe
Bill Dawson <bill@billdawson.com>
2010-04-27 13:02:18 +0200

    New classes for picker -- still doesn't work however

---
+++ android/modules/ui/src/ti/modules/titanium/ui/PickerColumnProxy.java
@@ -0,0 +1,13 @@
+package ti.modules.titanium.ui;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiProxy;
+
+public class PickerColumnProxy extends TiProxy {
+
+	public PickerColumnProxy(TiContext tiContext) {
+		super(tiContext);
+		// TODO Auto-generated constructor stub
+	}
+
+}
