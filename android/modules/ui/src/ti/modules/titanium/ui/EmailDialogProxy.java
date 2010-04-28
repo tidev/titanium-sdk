3f8c12cf06bc0c832fd12b2253cf2d4c972a3305
Bill Dawson <bill@billdawson.com>
2010-04-28 09:15:26 +0200

    Continue work on Picker (not functional) and begin work on EmailDialogProxy (not functional

--- android/modules/ui/src/ti/modules/titanium/ui/EmailDialogProxy.java
+++ android/modules/ui/src/ti/modules/titanium/ui/EmailDialogProxy.java
@@ -4,38 +4,43 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+
 package ti.modules.titanium.ui;
 
 import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiProxy;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.util.Log;
-import org.appcelerator.titanium.util.TiConfig;
 import org.appcelerator.titanium.view.TiUIView;
 
 import android.app.Activity;
 
-public class PickerRowProxy extends TiViewProxy {
+public class EmailDialogProxy extends TiViewProxy {
 	
-	private static final String LCAT = "PickerRowProxy";
-	private static final boolean DBG = TiConfig.LOGD;
-
-	public PickerRowProxy(TiContext tiContext, Object[] args) {
+	public EmailDialogProxy(TiContext tiContext, Object[] args) {
 		super(tiContext, args);
-		if (DBG) {
-			Log.d(LCAT, "Creating a pickerrowproxy with args length " + args.length);
-			if (args.length>0){
-				Object o = args[0];
-				Log.d(LCAT, "first arg type is " + o.getClass().getName());
-			}
-		}	
-		
-		
+		// TODO Auto-generated constructor stub
 	}
+
+	public static int CANCELLED = 0;
+	public static int SAVED = 1;
+	public static int SENT = 2;
+	public static int FAILED = 3;
+	
+	private String subject;
+
+	
+	public void open(){
+		Log.d("EmailDialogProxy", "opening, with subject " + this.getDynamicValue("subject"));
+	}
+	
 
 	@Override
 	public TiUIView createView(Activity activity) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
 
 }
