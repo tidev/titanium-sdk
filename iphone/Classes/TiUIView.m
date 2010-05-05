04f29aa89ffc4b9afe88e7a59dce3459f2297973
Stephen Tramer <sptramer@gmail.com>
2010-05-04 18:13:04 -0700

    Fixes event problems involving tables & propagation.

--- iphone/Classes/TiUIView.m
+++ iphone/Classes/TiUIView.m
@@ -844,9 +844,13 @@ DEFINE_EXCEPTIONS
     return [super hitTest:point withEvent:event];
 }
 
+// TODO: Take a very close look at event handling.
+// It's kind of broken for tables right now, but there are a couple
+// hacks to get around it.
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event 
 {
 	UITouch *touch = [touches anyObject];
+    BOOL clickEvent = NO;
 	
 	if (handlesSwipes)
 	{
@@ -880,10 +884,13 @@ DEFINE_EXCEPTIONS
 		{
 			[proxy fireEvent:@"touchstart" withObject:evt propagate:YES];
 		}
-		
+        
+        // Click handling is special; don't propagate if we have a delegate,
+        // but DO invoke the touch delegate at the end.
 		if ([touch tapCount] == 1 && [proxy _hasListeners:@"click"])
 		{
-			[proxy fireEvent:@"click" withObject:evt propagate:YES];
+			[proxy fireEvent:@"click" withObject:evt propagate:(touchDelegate==nil)];
+            clickEvent = YES;
 		}
 		else if ([touch tapCount] == 2 && [proxy _hasListeners:@"dblclick"])
 		{
@@ -891,7 +898,7 @@ DEFINE_EXCEPTIONS
 		}
 	}
 	
-	if (touchDelegate!=nil)
+	if (touchDelegate!=nil && clickEvent)
 	{
 		[touchDelegate touchesBegan:touches withEvent:event];
 	}
