b254adf5e07f6f1eadd5f4ef8ba912f1756035be
Stephen Tramer <sptramer@gmail.com>
2010-05-09 15:25:44 -0700

    Updated table view touch test with workaround.

---
+++ demos/KitchenSink_iPad/Resources/examples/xhr_filedownload.js
@@ -0,0 +1,129 @@
+var win = Titanium.UI.currentWindow;
+
+var ind=Titanium.UI.createProgressBar({
+	width:200,
+	height:50,
+	min:0,
+	max:1,
+	value:0,
+	style:Titanium.UI.iPhone.ProgressBarStyle.PLAIN,
+	top:10,
+	message:'Downloading ' + (Ti.Platform.name == 'android' ? 'PNG' : 'PDF') + ' File',
+	font:{fontSize:12, fontWeight:'bold'},
+	color:'#888'
+});
+
+win.add(ind);
+ind.show();
+
+
+var b1 = Titanium.UI.createButton({
+	title:'Set Web View (url)',
+	height:40,
+	width:200,
+	top:70
+});
+win.add(b1);
+var c = null;
+b1.addEventListener('click', function()
+{
+	c = Titanium.Network.createHTTPClient();
+	
+	c.setTimeout(10000);
+	c.onload = function()
+	{
+		Ti.API.info('IN ONLOAD ');
+
+		var filename = Titanium.Platform.name == 'android' ? 'test.png' : 'test.pdf';
+		var f = Titanium.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory,filename);
+		f.write(this.responseData);
+		var wv = Ti.UI.createWebView({
+			url:f.nativePath,
+			bottom:0,
+			left:0,
+			right:0,
+			top:170
+		});
+		win.add(wv);
+	};
+	c.ondatastream = function(e)
+	{
+		ind.value = e.progress ;
+		Ti.API.info('ONDATASTREAM1 - PROGRESS: ' + e.progress);
+	};
+	c.error = function(e)
+	{
+		Ti.UI.createAlertDialog({title:'XHR', message:'Error: ' + e.error}).show();
+	};
+	
+	// open the client
+	if (Titanium.Platform.name == 'android') {
+		//android's WebView doesn't support embedded PDF content
+		c.open('GET', 'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png');
+	} else {
+		c.open('GET','http://www.appcelerator.com/assets/The_iPad_App_Wave.pdf');
+	}
+
+	// send the data
+	c.send();
+	
+});
+
+
+var b2 = Titanium.UI.createButton({
+	title:'Set Web View (data)',
+	height:40,
+	width:200,
+	top:120
+});
+b2.addEventListener('click', function()
+{
+	c = Titanium.Network.createHTTPClient();
+
+	c.onload = function()
+	{
+		var wv = Ti.UI.createWebView({
+			data:this.responseData,
+			bottom:0,
+			left:0,
+			right:0,
+			top:170
+		});
+		win.add(wv);
+	};
+	c.ondatastream = function(e)
+	{
+		ind.value = e.progress ;
+		Ti.API.info('ONDATASTREAM2 - PROGRESS: ' + e.progress);
+	};
+
+	// open the client
+	if (Titanium.Platform.name == 'android') {
+		//android's WebView doesn't support embedded PDF content
+		c.open('GET', 'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png');
+	} else {
+		c.open('GET','http://www.appcelerator.com/assets/The_iPad_App_Wave.pdf');
+	}
+
+	// send the data
+	c.send();
+	
+});
+win.add(b2);
+
+var abort = Titanium.UI.createButton({
+	title:'Abort',
+	height:40,
+	width:200,
+	top:170
+});
+win.add(abort);
+abort.addEventListener('click', function()
+{
+	c.abort();
+	
+	alert("request aborted");
+	
+	c = Titanium.Network.createHTTPClient();
+	ind.value = 0;
+});
