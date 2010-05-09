d34c87ac0b60e27f46352a422b75433f4b35d930
Stephen Tramer <sptramer@gmail.com>
2010-05-09 15:26:57 -0700

    Fix for last bad commit.

--- demos/KitchenSink/Resources/examples/table_view_touch.js
+++ demos/KitchenSink/Resources/examples/table_view_touch.js
@@ -3,7 +3,6 @@ var data = [];
 
 for (var x=0;x<4;x++)
 {
-	var view = Ti.UI.createView();
 	var label = Ti.UI.createLabel({
 		text:'Row Label ' + x,
 		height:'auto',
@@ -12,8 +11,7 @@ for (var x=0;x<4;x++)
 		left:10
 	});
 	var row = Ti.UI.createTableViewRow({height:50});
-	view.add(label);
-	row.add(view);
+	row.add(label);
 	data.push(row);
 }
 
@@ -24,12 +22,12 @@ var tableview = Titanium.UI.createTableView({
 
 tableview.addEventListener('touchstart', function(e)
 {
-	e.row.children[0].children[0].color = '#fff';
+	e.row.children[0].color = '#fff';
 });
 
 tableview.addEventListener('touchend', function(e)
 {
-	e.row.children[0].children[0].color = '#336699';
+	e.row.children[0].color = '#336699';
 });
 
 
