c1f2d72ba904f7ac6fedca25806a651744d08405
Jeff Haynie <jhaynie@appcelerator.com>
2010-05-23 16:39:57 -0700

    start of updated module SDK

---
+++ support/module/iphone/iphone.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+#
+# iPhone Module Project Create Script
+#
+import os,sys,shutil
+
+#
+# this class is created after all template files (if any) in
+# this directory are copied to allow the project creation 
+# script to complete the install
+# 
+class iphone(object):
+	def __init__(self,project_dir,config):
+		pass
+		
