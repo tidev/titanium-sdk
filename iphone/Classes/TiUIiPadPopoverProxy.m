16ce2339fac494cfb1308d7a46cdb18fb50795e8
Jeff Haynie <jhaynie@appcelerator.com>
2010-06-06 14:38:44 -0700

    fixed ipad build issue related to popover

--- iphone/Classes/TiUIiPadPopoverProxy.m
+++ iphone/Classes/TiUIiPadPopoverProxy.m
@@ -4,8 +4,7 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-#ifdef USE_TI_UIIPADPOPOVER
-
+#if defined(USE_TI_UIIPADPOPOVER) || defined(USE_TI_UIIPADSPLITWINDOW)
 
 #import "TiUIiPadPopoverProxy.h"
 #import "TiUIiPadPopover.h"
