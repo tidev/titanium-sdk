d5c9219d644c0abd967afb8207e145c466d7ace7
Nolan Wright <nwright@appcelerator.com>
2010-06-14 21:43:05 -0700

    merge

--- demos/KitchenSink/build/iphone/main.m
+++ demos/KitchenSink/build/iphone/main.m
@@ -8,7 +8,7 @@
 #define STRING(x) _QUOTEME(x)
 
 NSString * const TI_APPLICATION_DEPLOYTYPE = @"development";
-NSString * const TI_APPLICATION_ID = @"com.appcelerator.titanium";
+NSString * const TI_APPLICATION_ID = @"com.nolanwright.kitchensink";
 NSString * const TI_APPLICATION_PUBLISHER = @"nwright";
 NSString * const TI_APPLICATION_URL = @"appcelerator.com";
 NSString * const TI_APPLICATION_NAME = @"KitchenSink";
@@ -19,7 +19,7 @@ NSString * const TI_APPLICATION_GUID = @"6fe33f33fd1f4e95a06d2d217170866d";
 BOOL const TI_APPLICATION_ANALYTICS = true;
 
 #ifdef TARGET_IPHONE_SIMULATOR
-NSString * const TI_APPLICATION_RESOURCE_DIR = @"/Users/jhaynie/work/titanium_mobile/demos/KitchenSink/Resources";
+NSString * const TI_APPLICATION_RESOURCE_DIR = @"/Users/nwright/Appcelerator/TIDEV/titanium_mobile/demos/KitchenSink/Resources";
 #endif
 
 int main(int argc, char *argv[]) {
