7cdb184e850ed7c10c81de65ea49b67fa29987d3
Blain Hamon <blain@Millenium-Falcon.local>
2010-06-23 18:40:28 -0700

    Closes #966 AGAIN. This time, it makes sure that not only is remove from superview on the main thread, but so is the view release, just in case the superview wasn't there to retain.

--- iphone/Classes/TiViewProxy.m
+++ iphone/Classes/TiViewProxy.m
@@ -710,7 +710,8 @@
 		view.proxy = nil;
 		// must be on main thread
 		[view performSelectorOnMainThread:@selector(removeFromSuperview) withObject:nil waitUntilDone:NO];
-		RELEASE_TO_NIL(view);
+		[view performSelectorOnMainThread:@selector(release) withObject:nil waitUntilDone:NO];
+		view = nil;
 	}
 	
 	[self lockChildrenForWriting];
