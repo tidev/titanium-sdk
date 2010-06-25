16c2ed9cfc4abc39b70ff4ba097eee0b76380203
Jeff Haynie <jhaynie@appcelerator.com>
2010-06-25 13:13:49 -0700

    XHR progress check for both upload and download

--- iphone/Classes/TiNetworkHTTPClientProxy.m
+++ iphone/Classes/TiNetworkHTTPClientProxy.m
@@ -242,7 +242,7 @@ extern NSString * const TI_APPLICATION_DEPLOYTYPE;
 	}
 	if (onload!=nil && state==NetworkClientStateDone && connected)
 	{
-		if (ondatastream)
+		if (ondatastream && downloadProgress>0)
 		{
 			CGFloat progress = (CGFloat)((CGFloat)downloadProgress/(CGFloat)downloadLength);
 			if (progress < 1.0)
@@ -253,6 +253,20 @@ extern NSString * const TI_APPLICATION_DEPLOYTYPE;
 				TiNetworkHTTPClientResultProxy *thisPointer = [[TiNetworkHTTPClientResultProxy alloc] initWithDelegate:self];
 				NSDictionary *event = [NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:progress] forKey:@"progress"];
 				[self _fireEventToListener:@"datastream" withObject:event listener:ondatastream thisObject:thisPointer];
+				[thisPointer release];
+			}
+		}
+		else if (onsendstream && uploadProgress>0)
+		{
+			CGFloat progress = (CGFloat)((CGFloat)uploadProgress/(CGFloat)uploadLength);
+			if (progress < 1.0)
+			{
+				// seems to be a problem in ASI where we'll get .999999 but never 1.0
+				// so we need to synthesize this
+				progress = 1.0;
+				TiNetworkHTTPClientResultProxy *thisPointer = [[TiNetworkHTTPClientResultProxy alloc] initWithDelegate:self];
+				NSDictionary *event = [NSDictionary dictionaryWithObject:[NSNumber numberWithFloat:progress] forKey:@"progress"];
+				[self _fireEventToListener:@"sendstream" withObject:event listener:onsendstream thisObject:thisPointer];
 				[thisPointer release];
 			}
 		}
