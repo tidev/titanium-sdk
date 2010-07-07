5e32d8b073b6f3c4e6516d0776be78fa67d9db48
Blain Hamon <blain@Millenium-Falcon.local>
2010-07-06 17:06:05 -0700

    Reverting to the previous functionality of using base url in web view, so that HTML5 video works again.

--- iphone/Classes/TiUIWebView.m
+++ iphone/Classes/TiUIWebView.m
@@ -369,6 +369,10 @@ NSString * const kTitaniumJavascript = @"Ti.App={};Ti.API={};Ti.App._listeners={
 	[[self webview] setScalesPageToFit:scaling];
 }
 
+#ifndef USE_BASE_URL
+#define USE_BASE_URL	1
+#endif
+
 -(void)setUrl_:(id)args
 {
 	RELEASE_TO_NIL(url);
@@ -400,6 +404,9 @@ NSString * const kTitaniumJavascript = @"Ti.App={};Ti.API={};Ti.App._listeners={
 		NSString *textEncodingName = @"utf-8";
 		NSString *path = [url path];
 		NSError *error = nil;
+#if USE_BASE_URL
+		NSURL *baseURL = [[url retain] autorelease];
+#endif
 		
 		// first check to see if we're attempting to load a file from the 
 		// filesystem and if so, and it exists, use that 
@@ -491,7 +498,11 @@ NSString * const kTitaniumJavascript = @"Ti.App={};Ti.API={};Ti.App._listeners={
 		if (html!=nil)
 		{
 			//Because local HTML may rely on JS that's stored in the app: schema, we must kee the url in the app: format.
+#if USE_BASE_URL
+			[self loadHTML:html encoding:encoding textEncodingName:textEncodingName mimeType:mimeType baseURL:baseURL];
+#else
 			[self loadHTML:html encoding:encoding textEncodingName:textEncodingName mimeType:mimeType baseURL:url];
+#endif
 		}
 		else 
 		{
