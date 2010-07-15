a1a4a1e0faf0060b283f6665698709f3e60d9d67
Nolan Wright <nwright@appcelerator.com>
2010-07-15 09:18:10 -0700

    test change

--- demos/KitchenSink/Resources/examples/table_view_noscroll.js
+++ demos/KitchenSink/Resources/examples/table_view_noscroll.js
@@ -1,6 +1,12 @@
 var w = Ti.UI.currentWindow;
+var search = Titanium.UI.createSearchBar({
+	showCancel:false,
+	hintText:'search'
+});
+
 var tv = Ti.UI.createTableView({
-	scrollable:false
+	scrollable:false,
+	search:search
 });
 w.add(tv);
 
