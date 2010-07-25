e545951d732649870c99b18fba304c125cf3705c
donthorp <don@donthorp.net>
2010-07-24 21:51:49 -0500

    Intent Proxy Skeleton

---
+++ android/modules/android/src/ti/modules/titanium/android/AndroidModule.java
@@ -0,0 +1,36 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package ti.modules.titanium.android;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiDict;
+import org.appcelerator.titanium.TiModule;
+
+public class AndroidModule extends TiModule
+{
+	private static final String LCAT = "TiAndroid";
+
+	private static TiDict constants;
+
+
+	public AndroidModule(TiContext tiContext) {
+		super(tiContext);
+	}
+
+
+	@Override
+	public TiDict getConstants()
+	{
+		if (constants == null) {
+			constants = new TiDict();
+
+		}
+
+		return constants;
+	}
+
+}
---
+++ android/modules/android/src/ti/modules/titanium/android/IntentProxy.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ti.modules.titanium.android;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiProxy;
+
+public class IntentProxy extends TiProxy 
+{
+	private static final String LCAT = "TiIntent";
+	
+	public IntentProxy(TiContext tiContext) 
+	{
+		super(tiContext);
+	}
+}
