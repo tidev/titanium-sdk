2b0621944e3c4ac73ae2ba5ab777f19a2d7f5a29
marshall <mculpepper@appcelerator.com>
2010-07-30 16:32:13 -0500

    support for querying calendar by ID, rudimentary createEvent support from calendar, added toDate conversion in TiConvert (and cleaned up space formatting)

--- android/modules/calendar/src/ti/modules/titanium/android/calendar/CalendarProxy.java
+++ android/modules/calendar/src/ti/modules/titanium/android/calendar/CalendarProxy.java
@@ -5,10 +5,11 @@ import java.util.Calendar;
 import java.util.Date;
 
 import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiDict;
 import org.appcelerator.titanium.TiProxy;
+import org.appcelerator.titanium.util.TiConvert;
 
 import android.content.ContentResolver;
-import android.content.ContentUris;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
@@ -63,6 +64,67 @@ public class CalendarProxy extends TiProxy {
 		long date2 = date1 + DateUtils.YEAR_IN_MILLIS;
 		ArrayList<EventProxy> events = EventProxy.queryEventsBetweenDates(getTiContext(), date1, date2, this);
 		return events.toArray(new EventProxy[events.size()]);
+	}
+	
+	public EventProxy[] getEventsInMonth(int year, int month) {
+		Calendar firstOfTheMonth = Calendar.getInstance();
+		firstOfTheMonth.clear();
+		firstOfTheMonth.set(year, month, 1);
+		Calendar lastOfTheMonth = Calendar.getInstance();
+		lastOfTheMonth.clear();
+		lastOfTheMonth.set(year, month, 1, 23, 59, 59);
+		
+		int lastDay = lastOfTheMonth.getActualMaximum(Calendar.DAY_OF_MONTH);
+		lastOfTheMonth.set(Calendar.DAY_OF_MONTH, lastDay);
+		
+		long date1 = firstOfTheMonth.getTimeInMillis();
+		long date2 = lastOfTheMonth.getTimeInMillis();
+		
+		ArrayList<EventProxy> events = EventProxy.queryEventsBetweenDates(getTiContext(), date1, date2, this);
+		return events.toArray(new EventProxy[events.size()]);
+	}
+	
+	public EventProxy[] getEventsInDate(int year, int month, int day) {
+		Calendar beginningOfDay = Calendar.getInstance();
+		beginningOfDay.clear();
+		beginningOfDay.set(year, month, day, 0, 0, 0);
+		Calendar endOfDay = Calendar.getInstance();
+		endOfDay.clear();
+		endOfDay.set(year, month, day, 23, 59, 59);
+		
+		long date1 = beginningOfDay.getTimeInMillis();
+		long date2 = endOfDay.getTimeInMillis();
+		
+		ArrayList<EventProxy> events = EventProxy.queryEventsBetweenDates(getTiContext(), date1, date2, this);
+		return events.toArray(new EventProxy[events.size()]);
+	}
+	
+	public EventProxy[] getEventsBetweenDates(Date date1, Date date2) {
+		ArrayList<EventProxy> events = EventProxy.queryEventsBetweenDates(getTiContext(), date1.getTime(), date2.getTime(), this);
+		return events.toArray(new EventProxy[events.size()]);
+	}
+	
+	public EventProxy createEvent(TiDict data) {
+		String title = TiConvert.toString(data, "title");
+		String description = null;
+		Date begin = null, end = null;
+		boolean allDay = false;
+		
+		if (data.containsKey("description")) {
+			description = TiConvert.toString(data, "description");
+		}
+		if (data.containsKey("begin")) {
+			begin = TiConvert.toDate(data, "begin");
+		}
+		if (data.containsKey("end")) {
+			end = TiConvert.toDate(data, "end");
+		}
+		if (data.containsKey("allDay")) {
+			allDay = TiConvert.toBoolean(data, "allDay");
+		}
+		
+		return EventProxy.createEvent(getTiContext(), this,
+			title, description, begin, end, allDay);
 	}
 	
 	public String getName() {
