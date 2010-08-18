7441b9ea78b7d94b630388fe2715377d1cb78cb4
Bill Dawson <bill@billdawson.com>
2010-08-18 20:14:48 +0200

    Add missing copyright notice (cherry picked from commit 0169910d6a2221e71b34bdd526922d899cdfacba)

--- android/modules/ui/src/ti/modules/titanium/ui/widget/picker/FormatNumericWheelAdapter.java
+++ android/modules/ui/src/ti/modules/titanium/ui/widget/picker/FormatNumericWheelAdapter.java
@@ -1,3 +1,10 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
 package ti.modules.titanium.ui.widget.picker;
 
 import java.text.NumberFormat;
