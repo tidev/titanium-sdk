f8eb5d283402cf8073fa679380ff9bedc318cf34
Blain Hamon <blain@Millenium-Falcon.local>
2010-09-02 14:33:10 -0700

    Cleaning out old unused code

--- iphone/Classes/TiRootController.h
+++ iphone/Classes/TiRootController.h
@@ -21,8 +21,6 @@
 -(void)setBackgroundColor:(UIColor*)color;
 -(void)setBackgroundImage:(UIImage*) backgroundImage;
 
--(void)refreshOrientationModesIfNeeded:(TiWindowProxy *)oldCurrentWindow;
-
 - (void)willHideViewController:(UIViewController *)focusedViewController animated:(BOOL)animated;
 - (void)didHideViewController:(UIViewController *)focusedViewController animated:(BOOL)animated;
 - (void)willShowViewController:(UIViewController *)focusedViewController animated:(BOOL)animated;
