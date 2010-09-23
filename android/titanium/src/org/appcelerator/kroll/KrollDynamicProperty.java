53bd3e0174c63d8b8d9be2be2cdf889ef564be34
marshall <mculpepper@appcelerator.com>
2010-09-23 15:42:46 -0500

    code cleanup in KrollDynamicProperty

--- android/titanium/src/org/appcelerator/kroll/KrollDynamicProperty.java
+++ android/titanium/src/org/appcelerator/kroll/KrollDynamicProperty.java
@@ -25,59 +25,17 @@ public abstract class KrollDynamicProperty implements KrollProperty {
 	//protected boolean getMethodHasInvocation, setMethodHasInvocation;
 	protected KrollNativeConverter nativeConverter;
 	protected KrollJavascriptConverter javascriptConverter;
-	protected KrollDefaultValueProvider[] getDefaultProviders;
-	protected KrollDefaultValueProvider[] setDefaultProviders;
 	
 	public KrollDynamicProperty(KrollProxy proxy, String name, boolean get, boolean set, boolean retain) {
 		this.name = name;
 		this.get = get;
 		this.set = set;
 		this.proxy = proxy;
-		
-		/*if (get && getMethodName != null) {
-			getMethod = KrollReflectionUtils.getMethod(proxy.getClass(), getMethodName);
-			if (getMethod != null) {
-				getMethodHasInvocation = getMethod.getParameterTypes().length > 0 && getMethod.getParameterTypes()[0].equals(KrollInvocation.class);
-			}
-		}
-		if (set && setMethodName != null) {
-			setMethod = KrollReflectionUtils.getMethod(proxy.getClass(), setMethodName);
-			if (setMethod != null) {
-				setMethodHasInvocation = setMethod.getParameterTypes().length > 0 && setMethod.getParameterTypes()[0].equals(KrollInvocation.class);
-			}
-		}*/
 		this.retain = retain;
 	}
 	
-	/*protected KrollDefaultValueProvider getDefaultValueProvider(String method, int argIndex) {
-		KrollDefaultValueProvider[] providers = method == getMethod ? getDefaultProviders : setDefaultProviders;
-		if (providers == null || providers.length <= argIndex || providers[argIndex] == null) {
-			return KrollConverter.getInstance();
-		}
-		
-		return providers[argIndex];
-	}*/
-	
 	protected Object safeInvoke(KrollInvocation invocation, String method, Object value) {
 		try {
-			/*Class<?>[] paramTypes = method.getParameterTypes();
-			if (args.length < paramTypes.length) {
-				Object newArgs[] = new Object[paramTypes.length];
-				System.arraycopy(args, 0, newArgs, 0, args.length);
-				
-				// Append default values onto the end for dynamic getters/setters that have optional arguments
-				for (int i = args.length; i < paramTypes.length; i++) {
-					KrollDefaultValueProvider provider = getDefaultValueProvider(method, i);
-					newArgs[i] = provider.getDefaultValue(paramTypes[i]);
-				}
-				args = newArgs;
-			} else if (args.length > paramTypes.length) {
-				// cut off the remaining args
-				Object newArgs[] = new Object[paramTypes.length];
-				System.arraycopy(args, 0, newArgs, 0, paramTypes.length);
-				args = newArgs;
-			}*/
-			
 			if (!runOnUiThread) {
 				if (method.equals(GET)) {
 					return dynamicGet(invocation);
@@ -129,11 +87,7 @@ public abstract class KrollDynamicProperty implements KrollProperty {
 	@Override
 	public Object get(KrollInvocation invocation, String name) {
 		if (supportsGet(name)) {
-			//if (getMethodHasInvocation) {
-				return safeInvoke(invocation, GET, null);
-			/*} else {
-				return safeInvoke(invocation, GET);
-			}*/
+			return safeInvoke(invocation, GET, null);
 		}
 		
 		return nativeConverter.convertNative(invocation, proxy.getProperty(name));
@@ -147,12 +101,7 @@ public abstract class KrollDynamicProperty implements KrollProperty {
 			if (retain) {
 				proxy.setProperty(name, value);
 			}
-			//if (setMethodHasInvocation) {
-				safeInvoke(invocation, SET, value);
-			/*} else {
-				safeInvoke(invocation, SET, value);
-			}*/
-			
+			safeInvoke(invocation, SET, value);
 		} else {
 			Object convertedValue = javascriptConverter.convertJavascript(invocation, value, Object.class);
 			proxy.setProperty(name, convertedValue, true);
@@ -181,13 +130,5 @@ public abstract class KrollDynamicProperty implements KrollProperty {
 
 	public void setJavascriptConverter(KrollJavascriptConverter javascriptConverter) {
 		this.javascriptConverter = javascriptConverter;
-	}
-
-	public void setGetDefaultProviders(KrollDefaultValueProvider[] getDefaultProviders) {
-		this.getDefaultProviders = getDefaultProviders;
-	}
-	
-	public void setSetDefaultProviders(KrollDefaultValueProvider[] setDefaultProviders) {
-		this.setDefaultProviders = setDefaultProviders;
 	}
 }
