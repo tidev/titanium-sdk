0262139b4576d239c03fe9e486227969688278dc
Stephen Tramer <sptramer@gmail.com>
2010-10-27 12:48:28 -0700

    [#2201 state:fixed-in-qa] Addition of 'success' and 'error' callbacks to photo/video saving.

--- demos/KitchenSink/Resources/examples/photo_gallery_file.js
+++ demos/KitchenSink/Resources/examples/photo_gallery_file.js
@@ -1,5 +1,17 @@
 var f = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory,'images/appcelerator_small.png');
 
-Titanium.Media.saveToPhotoGallery(f);
+Titanium.Media.saveToPhotoGallery(f,{
+	success: function(e) {
+		Titanium.UI.createAlertDialog({
+			title:'Photo Gallery',
+			message:'Check your photo gallery for an appcelerator logo'
+		}).show();		
+	},
+	error: function(e) {
+		Titanium.UI.createAlertDialog({
+			title:'Error saving',
+			message:e.error
+		}).show();
+	}
+});
 
-Titanium.UI.createAlertDialog({title:'Photo Gallery',message:'Check your photo gallery for an appcelerator logo'}).show();
