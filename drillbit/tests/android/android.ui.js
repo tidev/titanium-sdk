98da99d5801e426ff5cc63555b6a398ec662a5eb
billdawson <bill@billdawson.com>
2010-11-10 22:57:47 +0100

    [#2000 state:fixed-in-qa] add unit test

--- drillbit/tests/android/android.ui.js
+++ drillbit/tests/android/android.ui.js
@@ -1,5 +1,11 @@
 describe("Ti.UI.Android tests", {
 	androidUIAPIs: function() {
 		valueOf(Ti.UI.Android).shouldNotBeNull();
+	}, 
+	testModuleNameCollision: function() {
+		// Make sure both Ti.UI.Android and Ti.Android are properly accessible.
+		// cf https://appcelerator.lighthouseapp.com/projects/32238/tickets/2000
+		valueOf(Ti.UI.Android.SOFT_INPUT_ADJUST_PAN).shouldBe(32);
+		valueOf(Ti.Android.ACTION_ALL_APPS).shouldBe('android.intent.action.ALL_APPS');
 	}
 })
