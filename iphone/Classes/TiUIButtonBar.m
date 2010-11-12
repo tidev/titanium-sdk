0a946e709d8077a35aab89557fcad611056fa2c5
Stephen Tramer <sptramer@gmail.com>
2010-11-12 15:53:45 -0800

    [#1880 state:fixed-in-qa] Perform a little magic - but not too much - when we have a conflict between 'undefined' proxy size and actual specified control size for tabbed and button bars.

--- iphone/Classes/TiUIButtonBar.m
+++ iphone/Classes/TiUIButtonBar.m
@@ -22,6 +22,12 @@
 	return self;
 }
 
+-(void)dealloc
+{
+	RELEASE_TO_NIL(segmentedControl);
+	[super dealloc];
+}
+
 -(BOOL)hasTouchableListener
 {
 	// since this guy only works with touch events, we always want them
@@ -42,6 +48,19 @@
 	return segmentedControl;
 }
 
+// For regression #1880.  Because there are essentially TWO kinds of 'width' going on with tabbed/button bars
+// (width of all elements, width of the proxy) we assume that if the user has set the width of the bar completely,
+// AND the width of the proxy is undefined, they want magic!
+-(void)frameSizeChanged:(CGRect)frame_ bounds:(CGRect)bounds_
+{
+	// Treat 'undefined' like 'auto' when we have an available width for ALL control segments
+	if (controlSpecifiedWidth && TiDimensionIsUndefined([(TiViewProxy*)[self proxy] layoutProperties]->width)) {
+		UISegmentedControl* ourControl = [self segmentedControl];
+		CGRect controlBounds = bounds_;
+		controlBounds.size = [ourControl sizeThatFits:CGSizeZero];
+		[ourControl setBounds:controlBounds];
+	}
+}
 
 - (void)setTabbedBar: (BOOL)newIsTabbed;
 {
@@ -131,6 +150,7 @@
 	ENSURE_ARRAY(value);
 
 	int thisSegmentIndex = 0;
+	controlSpecifiedWidth = YES;
 	for (id thisSegmentEntry in value)
 	{
 		NSString * thisSegmentTitle = [TiUtils stringValue:thisSegmentEntry];
@@ -162,6 +182,7 @@
 		[segmentedControl setWidth:thisSegmentWidth forSegmentAtIndex:thisSegmentIndex];
 		[segmentedControl setEnabled:thisSegmentEnabled forSegmentAtIndex:thisSegmentIndex];
 		thisSegmentIndex ++;
+		controlSpecifiedWidth &= (thisSegmentWidth != 0.0);
 	}
 
 	if (![segmentedControl isMomentary])
