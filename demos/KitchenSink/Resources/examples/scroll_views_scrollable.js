c644943cb7e6937be96d42ed68222e97bd2b06e1
Blain Hamon <blain@Millenium-Falcon.local>
2010-11-16 14:24:50 -0800

    [#2347 state:fixed-in-qa] Whoops. Forgot to implement scrollable's parent view for child. Also, minor whoopsy in test was including orientation.

--- demos/KitchenSink/Resources/examples/scroll_views_scrollable.js
+++ demos/KitchenSink/Resources/examples/scroll_views_scrollable.js
@@ -1,4 +1,38 @@
-Ti.include('../examples/orientation.js'); // needed for getOrientation()
+function getOrientation(o)
+{  //Came from orientation.js, but we didn't need the buttons and such
+	switch (o)
+	{
+		case Titanium.UI.PORTRAIT:
+		{
+			return 'portrait';
+		}
+		case Titanium.UI.UPSIDE_PORTRAIT:
+		{
+			return 'upside portrait';
+		}
+		case Titanium.UI.LANDSCAPE_LEFT:
+		{
+			return 'landscape left';
+		}
+		case Titanium.UI.LANDSCAPE_RIGHT:
+		{
+			return 'landscape right';
+		}
+		case Titanium.UI.FACE_UP:
+		{
+			return 'face up';
+		}
+		case Titanium.UI.FACE_DOWN:
+		{
+			return 'face down';
+		}
+		case Titanium.UI.UNKNOWN:
+		{
+			return 'unknown';
+		}
+	}
+}
+
 
 var win = Titanium.UI.currentWindow;
 win.backgroundColor = '#ccc';
