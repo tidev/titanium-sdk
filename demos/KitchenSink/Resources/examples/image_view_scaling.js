c0aff27b322129e48f2092c569efff33a9b0a4a3
billdawson <bill@billdawson.com>
2010-11-24 13:50:09 +0100

    [#2160] Check for blob being null before trying to read its dimensions.

--- demos/KitchenSink/Resources/examples/image_view_scaling.js
+++ demos/KitchenSink/Resources/examples/image_view_scaling.js
@@ -1,29 +1,18 @@
 var win = Titanium.UI.currentWindow;
 
-if (Titanium.Platform.name == 'android') 
-{
-	// iphone moved to a single image property - android needs to do the same
-	var imageView = Titanium.UI.createImageView({
-		url:'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png'
-	});
-
-}
-else
-{
-	var imageView = Titanium.UI.createImageView({
-		image:'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png'
-	});
+var imageView = Titanium.UI.createImageView({
+	image:'http://www.appcelerator.com/wp-content/uploads/2009/06/titanium_desk.png'
+});
 	
-}
-
 win.add(imageView);
 
 var l = Titanium.UI.createLabel({
 	text:'',
 	bottom:30,
 	color:'#999',
-	height:20,
-	width:300,
+	height:60,
+	right: 10,
+	left: 10,
 	textAlign:'center'
 });
 win.add(l);
@@ -31,7 +20,11 @@ win.add(l);
 setTimeout(function() {
 	// wait for URL to load
 	var blob = imageView.toBlob();
-	l.text = blob.width + "x" + blob.height;
+	if (blob == null) {
+		l.text = 'Unable to retrieve image dimensions. The image is a remote url -- are you connected to the network?';
+	} else {
+		l.text = blob.width + "x" + blob.height;
+	}
 }, 2000);
 
 
