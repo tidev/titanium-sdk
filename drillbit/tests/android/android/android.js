2bdf67a239b93097ef6dca26209d8235a4c8775a
marshall <mculpepper@appcelerator.com>
2010-12-14 20:40:21 -0600

    add FLAG_ constants for use in PendingIntent w/ unit test. updated PendingIntent apidoc [#2564 state:fixed-in-qa]

--- drillbit/tests/android/android/android.js
+++ drillbit/tests/android/android/android.js
@@ -3,7 +3,7 @@ describe("Ti.Android tests", {
 		valueOf(Ti.Android).shouldNotBeNull();
 		valueOf(Ti.Android.currentActivity).shouldNotBeNull();
 	},
-	
+
 	//https://appcelerator.lighthouseapp.com/projects/32238/tickets/1592-android-move-menu-to-tiandroidactivity
 	androidMenu: function() {
 		var activity = Ti.Android.currentActivity;
@@ -16,7 +16,7 @@ describe("Ti.Android tests", {
 		activity.onPrepareOptionsMenu = function(e) {};
 		valueOf(activity.onPrepareOptionsMenu).shouldBeFunction();
 	},
-	
+
 	//https://appcelerator.lighthouseapp.com/projects/32238-titanium-mobile/tickets/2492-android-custom-js-activities-dont-correctly-pre-parse-the-url-attribute
 	jsActivityUrl: asyncTest(function() {
 		var intent = Ti.Android.createIntent({
@@ -29,7 +29,15 @@ describe("Ti.Android tests", {
 			valueOf(e.resultCode).shouldBe(Ti.Android.RESULT_OK);
 		}));
 	}),
-	
+
+	// https://appcelerator.lighthouseapp.com/projects/32238/tickets/2564-android-expose-pendingintent-flag_-constants-in-android-module
+	pendingIntentFlags: function() {
+		valueOf(Ti.Android.FLAG_CANCEL_CURRENT).shouldBeNumber();
+		valueOf(Ti.Android.FLAG_NO_CREATE).shouldBeNumber();
+		valueOf(Ti.Android.FLAG_ONE_SHOT).shouldBeNumber();
+		valueOf(Ti.Android.FLAG_UPDATE_CURRENT).shouldBeNumber();
+	},
+
 	options: {
 		forceBuild: true
 	}
