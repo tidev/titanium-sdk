a18707e021fcf0d46f8c3809ae20bc5b2da47e2c
billdawson <bill@billdawson.com>
2010-12-15 01:55:51 +0100

    [#2153] Drillbit unit test to be sure Titanium/Ti _are_ available in local html files loaded in webview

--- drillbit/tests/ui/ui.js
+++ drillbit/tests/ui/ui.js
@@ -31,7 +31,21 @@ describe("Ti.UI tests", {
 		},
 		timeout: 10000,
 		timeoutError: 'Timed out waiting for page to load and JS to eval'
+	}),
+	// https://appcelerator.lighthouseapp.com/projects/32238-titanium-mobile/tickets/2153
+	webviewBindingAvailable: asyncTest( {
+		start: function(callback) {
+			var w = Ti.UI.createWindow();
+			w.open();
+			var wv = Ti.UI.createWebView({top: 0, width: 10, height: 10, url: 'test.html'});
+			var listener = this.async(function(){
+				valueOf(wv.evalJS('typeof Titanium')).shouldBe('object');
+				w.close();
+			});
+			wv.addEventListener('load', listener);
+			w.add(wv);
+		},
+		timeout: 10000,
+		timeoutError: 'Timed out waiting for page to load and JS to eval'
 	})
-
-
 });
