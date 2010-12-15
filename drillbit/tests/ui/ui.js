a15892512a6eb0f7605a3a98615513e3f554ebde
billdawson <bill@billdawson.com>
2010-12-15 01:38:13 +0100

    [#1036 state:fixed-in-qa] Drillbit test to be sure Ti/Titanium _not_ available in external web sites

--- drillbit/tests/ui/ui.js
+++ drillbit/tests/ui/ui.js
@@ -1,7 +1,7 @@
 describe("Ti.UI tests", {
 
 	// https://appcelerator.lighthouseapp.com/projects/32238-titanium-mobile/tickets/2583
-	evalJSLockup: asyncTest( {
+	webviewEvalJSLockup: asyncTest( {
 		start: function(callback) {
 			var w = Ti.UI.createWindow();
 			w.open();
@@ -16,5 +16,22 @@ describe("Ti.UI tests", {
 		timeout: 10000,
 		timeoutError: 'Timed out waiting for page to load and JS to eval'
 	}),
+	//https://appcelerator.lighthouseapp.com/projects/32238-titanium-mobile/tickets/1036
+	webviewBindingUnavailable: asyncTest( {
+		start: function(callback) {
+			var w = Ti.UI.createWindow();
+			w.open();
+			var wv = Ti.UI.createWebView({top: 0, width: 10, height: 10, url: 'http://www.google.com'});
+			var listener = this.async(function(){
+				valueOf(wv.evalJS('Titanium')).shouldBe('');
+				w.close();
+			});
+			wv.addEventListener('load', listener);
+			w.add(wv);
+		},
+		timeout: 10000,
+		timeoutError: 'Timed out waiting for page to load and JS to eval'
+	})
+
 
 });
