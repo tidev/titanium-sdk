32042f3000ff7858415d3927c68e8bd67c000593
Steffen Bewersdorff <steffenbewersdorff@gmail.com>
2011-01-01 23:04:22 +0100

    added an example for iPhone4 decimal keypad layout to the kitchensink

--- demos/KitchenSink/Resources/examples/textfield_keyboards.js
+++ demos/KitchenSink/Resources/examples/textfield_keyboards.js
@@ -145,3 +145,20 @@ b7.addEventListener('click', function()
 	tf1.focus();
 });
 win.add(b7);
+
+var b8 = Titanium.UI.createButton({
+	title:'Decimal Pad',
+	height:40,
+	width:145,
+	right:10,
+	top:205
+});
+b8.addEventListener('click', function()
+{
+	tf1.blur();
+	tf1.keyboardType = Titanium.UI.KEYBOARD_DECIMAL_PAD;
+	tf1.enableReturnKey = true;
+	tf1.returnKeyType = Titanium.UI.RETURNKEY_DONE;
+	tf1.focus();
+});
+win.add(b8);
