6039cbcc99ec1ccc01aacb5357b32289da69e016
Andreas Sandberg <asandberg@sugarcrm.com>
2011-01-12 10:32:21 +0800

    Fixing the hasCheck issue with droid table view rows

--- android/modules/ui/src/ti/modules/titanium/ui/TableViewRowProxy.java
+++ android/modules/ui/src/ti/modules/titanium/ui/TableViewRowProxy.java
@@ -105,6 +105,11 @@ public class TableViewRowProxy extends TiViewProxy
 		return (TableViewProxy)parent;
 	}
 
+  @Kroll.setProperty @Kroll.method
+  public void setHasCheck(boolean check) { 
+     Log.w(LCAT, "Trigger hasCheck set");
+  }
+
 	@Override
 	public void set(Scriptable scope, String name, Object value)
 			throws NoSuchFieldException {
