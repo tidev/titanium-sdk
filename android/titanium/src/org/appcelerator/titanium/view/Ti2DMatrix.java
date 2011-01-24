15e1a2e25a563b55ff15f42c0f6b4e5e88fb1671
marshall <mculpepper@appcelerator.com>
2011-01-24 17:07:01 -0600

    Ti2DMatrix now saves it's state in a linked list so all operations can be applied in order and relative to widget height/width. scale and rotate also now accept "from" arguments to allow for more fine-grained control of each matrix operation [#2873 state:fixed-in-qa]

--- android/titanium/src/org/appcelerator/titanium/view/Ti2DMatrix.java
+++ android/titanium/src/org/appcelerator/titanium/view/Ti2DMatrix.java
@@ -19,18 +19,65 @@ import android.graphics.Matrix;
 @Kroll.proxy
 public class Ti2DMatrix extends KrollProxy
 {
-	protected Matrix matrix;
+	public static final float DEFAULT_ANCHOR_VALUE = -1;
+	protected Ti2DMatrix next, prev;
+
+	protected static class Operation
+	{
+		protected static final int TYPE_SCALE = 0;
+		protected static final int TYPE_TRANSLATE = 1;
+		protected static final int TYPE_ROTATE = 2;
+		protected static final int TYPE_MULTIPLY = 3;
+		protected static final int TYPE_INVERT = 4;
+
+		protected float scaleFromX, scaleFromY, scaleToX, scaleToY;
+		protected float translateX, translateY;
+		protected float rotateFrom, rotateTo;
+		protected float anchorX = 0.5f, anchorY = 0.5f;
+		protected Ti2DMatrix multiplyWith;
+		protected int type;
+
+		public Operation(int type)
+		{
+			this.type = type;
+		}
+
+		public void apply(float interpolatedTime, Matrix matrix,
+			int childWidth, int childHeight, float anchorX, float anchorY)
+		{
+			anchorX = anchorX == DEFAULT_ANCHOR_VALUE ? this.anchorX : anchorX;
+			anchorY = anchorY == DEFAULT_ANCHOR_VALUE ? this.anchorY : anchorY;
+			switch (type) {
+				case TYPE_SCALE:
+					matrix.preScale((interpolatedTime * (scaleToX - scaleFromX)) + scaleFromX, (interpolatedTime * (scaleToX - scaleFromY)) + scaleFromY); break;
+				case TYPE_TRANSLATE:
+					matrix.preTranslate(interpolatedTime * translateX, interpolatedTime * translateY); break;
+				case TYPE_ROTATE:
+					matrix.preRotate((interpolatedTime * (rotateTo - rotateFrom)) + rotateFrom, anchorX * childWidth, anchorY * childHeight); break;
+				case TYPE_MULTIPLY:
+					matrix.preConcat(multiplyWith.interpolate(interpolatedTime, childWidth, childHeight, anchorX, anchorY)); break;
+				case TYPE_INVERT:
+					matrix.invert(matrix); break;
+			}
+		}
+	}
+
+	protected Operation op;
 
 	public Ti2DMatrix(TiContext tiContext)
 	{
 		// Default Identity Matrix
-		this(tiContext, new Matrix());
+		super(tiContext);
 	}
 
-	protected Ti2DMatrix(TiContext tiContext, Matrix matrix)
+	protected Ti2DMatrix(TiContext tiContext, Ti2DMatrix prev, int opType)
 	{
 		super(tiContext);
-		this.matrix = matrix;
+		if (prev != null) {
+			this.prev = prev;
+			prev.next = this;
+		}
+		this.op = new Operation(opType);
 	}
 
 	@Override
@@ -38,101 +85,113 @@ public class Ti2DMatrix extends KrollProxy
 	{
 		super.handleCreationDict(dict);
 		if (dict.containsKey(TiC.PROPERTY_ROTATE)) {
-			handleRotate(matrix, dict, TiConvert.toDouble(dict, TiC.PROPERTY_ROTATE));
+			op = new Operation(Operation.TYPE_ROTATE);
+			op.rotateFrom = 0;
+			op.rotateTo = TiConvert.toFloat(dict, TiC.PROPERTY_ROTATE);
+			handleAnchorPoint(dict);
 		}
 		if (dict.containsKey(TiC.PROPERTY_SCALE)) {
-			float scale = TiConvert.toFloat(dict, TiC.PROPERTY_SCALE);
-			handleScale(matrix, dict, scale, scale);
+			op = new Operation(Operation.TYPE_SCALE);
+			op.scaleFromX = op.scaleFromY = 1.0f;
+			op.scaleToX = op.scaleToY = TiConvert.toFloat(dict, TiC.PROPERTY_SCALE);
+			handleAnchorPoint(dict);
 		}
 	}
 
-	protected void handleRotate(Matrix m, KrollDict dict, double angle)
+	protected void handleAnchorPoint(KrollDict dict)
 	{
-		float degrees = (float) angle;
-		boolean rotateWithAnchorPoint = false;
 		if (dict.containsKey(TiC.PROPERTY_ANCHOR_POINT)) {
 			KrollDict anchorPoint = dict.getKrollDict(TiC.PROPERTY_ANCHOR_POINT);
 			if (anchorPoint != null) {
-				float px = TiConvert.toFloat(anchorPoint, TiC.PROPERTY_X);
-				float py = TiConvert.toFloat(anchorPoint, TiC.PROPERTY_Y);
-				m.preRotate(degrees, px, py);
-				rotateWithAnchorPoint = true;
+				op.anchorX = TiConvert.toFloat(anchorPoint, TiC.PROPERTY_X);
+				op.anchorY = TiConvert.toFloat(anchorPoint, TiC.PROPERTY_Y);
 			}
-		}
-		if (!rotateWithAnchorPoint) {
-			m.preRotate(degrees, 0.5f, 0.5f);
-		}
-	}
-
-	protected void handleScale(Matrix m, KrollDict dict, float scaleX, float scaleY)
-	{
-		boolean scaleWithAnchorPoint = false;
-		if (dict.containsKey(TiC.PROPERTY_ANCHOR_POINT)) {
-			KrollDict anchorPoint = dict.getKrollDict(TiC.PROPERTY_ANCHOR_POINT);
-			if (anchorPoint != null) {
-				float px = TiConvert.toFloat(anchorPoint, TiC.PROPERTY_X);
-				float py = TiConvert.toFloat(anchorPoint, TiC.PROPERTY_Y);
-				m.preScale(scaleX, scaleY, px, py);
-				scaleWithAnchorPoint = true;
-			}
-		}
-		if (!scaleWithAnchorPoint) {
-			m.preScale(scaleX, scaleY, 0.5f, 0.5f);
 		}
 	}
 
 	@Kroll.method
 	public Ti2DMatrix translate(KrollInvocation invocation, double x, double y)
 	{
-		Matrix m = new Matrix(matrix);
-		m.preTranslate((float)x, (float)y);
-		return new Ti2DMatrix(invocation.getTiContext(), m);
+		Ti2DMatrix newMatrix = new Ti2DMatrix(invocation.getTiContext(),
+			this, Operation.TYPE_TRANSLATE);
+		newMatrix.op.translateX = (float) x;
+		newMatrix.op.translateY = (float) y;
+		return newMatrix;
 	}
 
 	@Kroll.method
 	public Ti2DMatrix scale(KrollInvocation invocation, Object args[])
 	{
-		float scaleX = 1.0f, scaleY = 1.0f;
+		Ti2DMatrix newMatrix = new Ti2DMatrix(invocation.getTiContext(),
+			this, Operation.TYPE_SCALE);
+		newMatrix.op.scaleFromX = newMatrix.op.scaleFromY = 1.0f;
+		newMatrix.op.scaleToX = newMatrix.op.scaleToY = 1.0f;
 		// varargs for API backwards compatibility
+		if (args.length == 4) {
+			// scale(fromX, fromY, toX, toY)
+			newMatrix.op.scaleFromX = TiConvert.toFloat(args[0]);
+			newMatrix.op.scaleFromY = TiConvert.toFloat(args[1]);
+			newMatrix.op.scaleToX = TiConvert.toFloat(args[2]);
+			newMatrix.op.scaleToY = TiConvert.toFloat(args[3]);
+		}
 		if (args.length == 2) {
 			// scale(toX, toY)
-			scaleX = TiConvert.toFloat(args[0]);
-			scaleY = TiConvert.toFloat(args[1]);
+			newMatrix.op.scaleToX = TiConvert.toFloat(args[0]);
+			newMatrix.op.scaleToY = TiConvert.toFloat(args[1]);
 		} else if (args.length == 1) {
 			// scale(scaleFactor)
-			scaleX = scaleY = TiConvert.toFloat(args[0]);
+			newMatrix.op.scaleToX = newMatrix.op.scaleToY = TiConvert.toFloat(args[0]);
 		}
-		Matrix m = new Matrix(matrix);
-		handleScale(m, getProperties(), scaleX, scaleY);
-		return new Ti2DMatrix(invocation.getTiContext(), m);
+		newMatrix.handleAnchorPoint(newMatrix.getProperties());
+		return newMatrix;
 	}
 
 	@Kroll.method
-	public Ti2DMatrix rotate(KrollInvocation invocation, double angle)
+	public Ti2DMatrix rotate(KrollInvocation invocation, Object[] args)
 	{
-		Matrix m = new Matrix(matrix);
-		handleRotate(m, getProperties(), angle);
-		return new Ti2DMatrix(invocation.getTiContext(), m);
+		Ti2DMatrix newMatrix = new Ti2DMatrix(invocation.getTiContext(),
+			this, Operation.TYPE_ROTATE);
+		if (args.length == 1) {
+			newMatrix.op.rotateFrom = 0;
+			newMatrix.op.rotateTo = TiConvert.toFloat(args[0]);
+		} else if (args.length == 2) {
+			newMatrix.op.rotateFrom = TiConvert.toFloat(args[0]);
+			newMatrix.op.rotateTo = TiConvert.toFloat(args[1]);
+		}
+		newMatrix.handleAnchorPoint(newMatrix.getProperties());
+		return newMatrix;
 	}
 
 	@Kroll.method
 	public Ti2DMatrix invert(KrollInvocation invocation)
 	{
-		Matrix m = new Matrix(matrix);
-		m.invert(m);
-		return new Ti2DMatrix(invocation.getTiContext(), m);
+		return new Ti2DMatrix(invocation.getTiContext(),
+			this, Operation.TYPE_INVERT);
 	}
 
 	@Kroll.method
 	public Ti2DMatrix multiply(KrollInvocation invocation, Ti2DMatrix other)
 	{
-		Matrix m = new Matrix(matrix);
-		m.preConcat(other.matrix);
-		return new Ti2DMatrix(invocation.getTiContext(), m);
+		return new Ti2DMatrix(invocation.getTiContext(),
+			this, Operation.TYPE_MULTIPLY);
 	}
 
-	public Matrix getMatrix()
+	public Matrix interpolate(float interpolatedTime, int childWidth, int childHeight, float anchorX, float anchorY)
 	{
+		Ti2DMatrix first = this;
+		do {
+			first = first.prev;
+		} while (first.prev != null);
+
+		Matrix matrix = new Matrix();
+		for (Ti2DMatrix current = first; current != this; current = current.next) {
+			if (current.op != null) {
+				current.op.apply(interpolatedTime, matrix, childWidth, childHeight, anchorX, anchorY);
+			}
+		}
+		if (op != null) {
+			op.apply(interpolatedTime, matrix, childWidth, childHeight, anchorX, anchorY);
+		}
 		return matrix;
 	}
 }
