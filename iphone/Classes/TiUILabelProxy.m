bec2690421df9ed07d2d3bb03bbea4a53d385943
Blain Hamon <bhamon@appcelerator.com>
2011-02-24 11:14:28 -0800

    Adding label tracking for memory stuff.

--- iphone/Classes/TiUILabelProxy.m
+++ iphone/Classes/TiUILabelProxy.m
@@ -10,7 +10,50 @@
 #import "TiUILabel.h"
 #import "TiUtils.h"
 
+#define LABEL_TRACKING
+
+#ifdef LABEL_TRACKING
+id blessedObject = nil;
+#endif
+
 @implementation TiUILabelProxy
+
+#ifdef LABEL_TRACKING
+-(void)markProgress:(id)comment
+{
+	if (blessedObject == self)
+	{
+		NSLog(@"%X (%@) during %@ is currently at retain count %d",self,self,comment,[self retainCount]);
+	}
+}
+
+- (id) init
+{
+	self = [super init];
+	if (self != nil)
+	{
+		if (blessedObject == nil)
+		{
+			blessedObject = self;
+		}
+		[self markProgress:@"init"];
+	}
+	return self;
+}
+
+-(id)retain
+{
+	[self markProgress:@"retain"];
+	return [super retain];
+}
+
+-(void)release
+{
+	[self markProgress:@"release"];
+	[super release];
+}
+#endif
+
 
 USE_VIEW_FOR_AUTO_WIDTH
 
