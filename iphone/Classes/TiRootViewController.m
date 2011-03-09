1758852d61d4d21fac578daa6bf3d00f7b86e91b
Blain Hamon <bhamon@appcelerator.com>
2011-03-08 21:01:41 -0800

    Fixing up some of the circular retains and leaks

--- iphone/Classes/TiRootViewController.m
+++ iphone/Classes/TiRootViewController.m
@@ -422,6 +422,9 @@ What this does mean is that any
 {
 	if (!isCurrentlyVisible || (focusedViewController != [viewControllerStack lastObject]))
 	{
+		if([viewControllerStack containsObject:focusedViewController]){
+			[viewControllerStack removeObject:focusedViewController];
+		}
 		return;
 	}
 	
