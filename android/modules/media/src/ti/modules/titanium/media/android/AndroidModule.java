aaaa80fc7cb96429076e4054139b4c9077f6ceb4
billdawson <bill@billdawson.com>
2011-03-21 15:26:04 +0100

    Move setSystemWallpaper from Ti.Media to Ti.Media.Android [#1267]

--- android/modules/media/src/ti/modules/titanium/media/android/AndroidModule.java
+++ android/modules/media/src/ti/modules/titanium/media/android/AndroidModule.java
@@ -1,16 +1,23 @@
 package ti.modules.titanium.media.android;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollInvocation;
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.titanium.TiBlob;
 import org.appcelerator.titanium.TiContext;
 import org.appcelerator.titanium.kroll.KrollCallback;
+import org.appcelerator.titanium.util.Log;
 import org.appcelerator.titanium.util.TiConvert;
+import org.appcelerator.titanium.view.TiDrawableReference;
 
 import ti.modules.titanium.media.MediaModule;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.graphics.Bitmap;
 import android.media.MediaScannerConnection;
 import android.media.MediaScannerConnection.MediaScannerConnectionClient;
 import android.net.Uri;
@@ -18,6 +25,7 @@ import android.net.Uri;
 @Kroll.module(parentModule=MediaModule.class)
 public class AndroidModule extends KrollModule
 {
+	private static final String LCAT = "TiMedia.Android";
 	public AndroidModule(TiContext context)
 	{
 		super(context);
@@ -27,6 +35,28 @@ public class AndroidModule extends KrollModule
 	public void scanMediaFiles(KrollInvocation invocation, Object[] paths, Object[] mimeTypes, KrollCallback callback)
 	{
 		(new MediaScannerClient(invocation.getTiContext(), paths, mimeTypes, callback)).scan();
+	}
+	@Kroll.method
+	public void setSystemWallpaper(KrollInvocation invocation, TiBlob image, boolean scale)
+	{
+		Context ctx = invocation.getActivity();
+		WallpaperManager wm = WallpaperManager.getInstance(ctx);
+		TiDrawableReference ref = TiDrawableReference.fromBlob(invocation.getTiContext(), image);
+		Bitmap b = null;
+		if (scale) {
+			b = ref.getBitmap(wm.getDesiredMinimumWidth());
+		} else {
+			b = ref.getBitmap();
+		}
+		if (b != null) {
+			try {
+				wm.setBitmap(b);
+			} catch (IOException e) {
+				Log.e(LCAT, "Unable to set wallpaper bitmap", e);
+			}
+		} else {
+			Log.w(LCAT, "Unable to get bitmap to set wallpaper");
+		}
 	}
 	public class MediaScannerClient implements MediaScannerConnectionClient
 	{
