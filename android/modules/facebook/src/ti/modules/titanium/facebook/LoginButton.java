0dc764d5d260b291e6423b4aecf6afb66696ea69
billdawson <bill@billdawson.com>
2011-03-22 22:43:44 +0100

    Move the facebook login/logout buttons also into the facebook module res folder, and change the LoginButton.java's handling of the button images to use these resources. [#3163]

--- android/modules/facebook/src/ti/modules/titanium/facebook/LoginButton.java
+++ android/modules/facebook/src/ti/modules/titanium/facebook/LoginButton.java
@@ -7,22 +7,17 @@
 
 package ti.modules.titanium.facebook;
 
-import java.io.InputStream;
-
 import org.appcelerator.kroll.KrollDict;
-import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.util.Log;
 import org.appcelerator.titanium.util.TiConvert;
-import org.appcelerator.titanium.util.TiUIHelper;
 import org.appcelerator.titanium.view.TiUIView;
 
 import android.R;
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
+import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.util.StateSet;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -31,12 +26,21 @@ import android.widget.ImageButton;
 public class LoginButton extends TiUIView implements TiFacebookStateListener
 {
 	private static final String LCAT = "TiLoginButton";
+	private static final int FB_BLUE = 0xFF6D84B4;
 	private FacebookModule facebook = null;
 	private boolean wide;
+	// Resource IDs for the various button images.
+	private int mLoginId = -1;
+	private int mLoginWideId = -1;
+	private int mLoginPressedId = -1;
+	private int mLoginWidePressedId = -1;
+	private int mLogoutId = -1;
+	private int mLogoutPressedId = -1;
 
 	public LoginButton(final TiViewProxy proxy) {
 		super(proxy);
 		initFacebook();
+		discoverResourceIds(proxy.getContext());
 		ImageButton btn = new ImageButton(proxy.getContext()) {
 			@Override
 			protected void drawableStateChanged() {
@@ -82,20 +86,47 @@ public class LoginButton extends TiUIView implements TiFacebookStateListener
 	private void handleUpdateButtonImage(boolean pressed)
 	{
 		boolean loggedIn = facebook.loggedIn();
+		String stateDescription;
+		int resid = 0;
+		if (loggedIn) {
+			if (pressed) {
+				resid = mLogoutPressedId;
+				stateDescription = "logged-in and button pressed";
+			} else {
+				resid = mLogoutId;
+				stateDescription = "logged-in, button not pressed";
+			}
+		} else {
+			if (wide) {
+				if (pressed) {
+					resid = mLoginWidePressedId;
+					stateDescription = "not logged-in, using wide button, button is pressed";
+				} else {
+					resid = mLoginWideId;
+					stateDescription = "not logged-in, using wide button, button is not pressed";
+				}
+			} else {
+				if (pressed) {
+					resid = mLoginPressedId;
+					stateDescription = "not logged-in, using narrow button, button is pressed";
+				} else {
+					resid = mLoginId;
+					stateDescription = "not logged-in, using narrow button, button is not pressed";
+				}
+			}
+		}
 		ImageButton btn = (ImageButton) getNativeView();
 		if (btn == null) {
 			return;
 		}
-		String path = "ti/modules/titanium/facebook/resources/log"
-				+ (!loggedIn ? "in" : "out") + (wide && !loggedIn ? "2" : "")
-				+ (pressed ? "_down" : "") + ".png";
-		InputStream is = getClass().getClassLoader().getResourceAsStream(path);
-		if (is == null) {
-			Log.e(LCAT, "Error loading Facebook image from " + path);
-			return;
+		if (resid > 0) {
+			btn.setBackgroundColor(Color.TRANSPARENT);
+			btn.setImageResource(resid);
+		} else {
+			Log.w(LCAT, "Facebook resource image could not be located!  State: " + stateDescription);
+			// At least give it a "facebook blue" background so it can be seen.
+			btn.setBackgroundColor(FB_BLUE);
 		}
-		Bitmap bitmap = TiUIHelper.createBitmap(is);
-		btn.setImageDrawable(new BitmapDrawable(bitmap));
 	}
 	
 	@Override
@@ -164,5 +195,16 @@ public class LoginButton extends TiUIView implements TiFacebookStateListener
 				}
 			});
 		}
+	}
+	private void discoverResourceIds(Context context)
+	{
+		String packageName = context.getPackageName();
+		Resources resources = context.getResources();
+		mLoginId = resources.getIdentifier("facebook_login", "drawable", packageName);
+		mLoginPressedId = resources.getIdentifier("facebook_login_down", "drawable", packageName);
+		mLoginWideId = resources.getIdentifier("facebook_login_wide", "drawable", packageName);
+		mLoginWidePressedId = resources.getIdentifier("facebook_login_wide_down", "drawable", packageName);
+		mLogoutId = resources.getIdentifier("facebook_logout", "drawable", packageName);
+		mLogoutPressedId = resources.getIdentifier("facebook_logout_down", "drawable", packageName);
 	}
 }
