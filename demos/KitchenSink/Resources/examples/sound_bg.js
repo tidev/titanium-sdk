74a51717e395bd32eef9ef7ada6ded5810dfc078
Gerry Cardinal III <gerry3@gmail.com>
2011-04-04 17:33:22 -0700

    Background Audio (sound_bg.js) example - add warning re: iOS device-only

--- demos/KitchenSink/Resources/examples/sound_bg.js
+++ demos/KitchenSink/Resources/examples/sound_bg.js
@@ -2,21 +2,36 @@ var win = Ti.UI.currentWindow;
 
 
 var button = Ti.UI.createButton({
-	title:"Start",
-	width:100,
-	height:40,
-	bottom:20
+    title:"Start",
+    width:100,
+    height:40,
+    bottom:20
 });
 
 win.add(button);
 
+var warningLabel = Ti.UI.createLabel({
+    text: 'Note: iOS background audio and remote controls are device-only. Check out the Remote Streaming example to stream audio in the background.',
+    color: 'red',
+    left: 10,
+    bottom: 80,
+    height: 'auto',
+    width: 300,
+    font: {
+        fontSize: 14
+    },
+    textAlign: 'center'
+});
+
+win.add(warningLabel);
+
 var text = Ti.UI.createLabel({
-	text:"Click the button to start the audio and then exit the app.\n\nThe audio should continue playing and you can use the controls to control the media.",
-	width:"auto",
-	height:"auto",
-	top:10,
-	left:10,
-	right:10
+    text:"Click the button to start the audio and then exit the app.\n\nThe audio should continue playing and you can use the controls to control the media.",
+    width:"auto",
+    height:"auto",
+    top:10,
+    left:10,
+    right:10
 });
 
 win.add(text);
@@ -25,8 +40,8 @@ var sound = Titanium.Media.createSound({url:'../cricket.wav'});
 
 button.addEventListener('click',function()
 {
-	sound.audioSessionMode = Ti.Media.AUDIO_SESSION_MODE_PLAYBACK; 
-	sound.looping = true;
-	sound.play();
+    // support background audio
+    sound.audioSessionMode = Ti.Media.AUDIO_SESSION_MODE_PLAYBACK; 
+    sound.looping = true;
+    sound.play();
 });
-
