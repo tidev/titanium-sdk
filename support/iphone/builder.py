9b7ce286ac1ea37d59c2b0cefb3daff58bcb01a0
Stephen Tramer <sptramer@gmail.com>
2011-05-31 14:23:10 -0700

    Build for device in Debug mode, not Release.  Only packaged apps should be distributed in Release configuration.

--- support/iphone/builder.py
+++ support/iphone/builder.py
@@ -511,6 +511,7 @@ def main(args):
 			output_dir = os.path.expanduser(dequote(args[8].decode("utf-8")))
 			if argc > 9:
 				devicefamily = dequote(args[9].decode("utf-8"))
+			print "[INFO] Switching to production mode for distribution"
 			deploytype = 'production'
 		elif command == 'simulator':
 			link_version = check_iphone_sdk(iphone_version)
@@ -547,6 +548,7 @@ def main(args):
 					debughost=None
 				else:
 					debughost,debugport = debughost.split(":")
+			target = 'Debug'
 			deploytype = 'test'
 		
 		# setup up the useful directories we need in the script
