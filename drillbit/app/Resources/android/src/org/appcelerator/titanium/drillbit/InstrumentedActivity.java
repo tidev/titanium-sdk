adbfab17d6992920a4d836d0badc00a4f80720da
marshall <mculpepper@appcelerator.com>
2011-06-01 16:41:12 -0500

    forgot to add custom Instrumentation and Activity impls TIMOB-4279

---
+++ drillbit/app/Resources/android/src/org/appcelerator/titanium/drillbit/InstrumentedActivity.java
@@ -0,0 +1,8 @@
+package org.appcelerator.titanium.drillbit;
+
+import android.app.Instrumentation;
+
+public interface InstrumentedActivity
+{
+	public void setInstrumentation(Instrumentation instrumentation);
+}
