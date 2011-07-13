a6bb0956c0c96504f764c05c0897370be3fc8c63
Stephen Tramer <sptramer@gmail.com>
2011-07-13 12:56:57 -0700

    [TIMOB-4338] Remove generated code header. Whoops.

--- iphone/Classes/KrollCoverage.m
+++ iphone/Classes/KrollCoverage.m
@@ -3,8 +3,6 @@
  * Copyright (c) 2009-2011 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
- * 
- * WARNING: This is generated code. Modify at your own risk and without support.
  */
 #ifdef KROLL_COVERAGE
 
