a872a5c2776b8b3363e2ba3ebc5451ec0a8c917c
billdawson <bill@billdawson.com>
2011-07-19 00:02:33 +0200

    Add longpress and longclick events for examples in KitchenSink TIMOB-1653

--- demos/KitchenSink/Resources/examples/window_events.js
+++ demos/KitchenSink/Resources/examples/window_events.js
@@ -214,6 +214,17 @@ var l10 = Titanium.UI.createLabel({
 
 win.add(l10);
 
+var l11 = Titanium.UI.createLabel({
+	text:'longpress not fired',
+	top:290,
+	left:10,
+	width:300,
+	height:'auto',
+	font:{fontSize:13,fontFamily:'Helvetica Neue'}
+});
+
+win.add(l11);
+
 function pad (x)
 {
 	if (x < 10)
@@ -350,5 +361,14 @@ win.addEventListener('dblclick', function(e)
 	setTimeout(function()
 	{
 		l10.color = '#333';
+	},200);
+});
+win.addEventListener('longpress', function(e)
+{
+	l11.color = 'red';
+	l11.text = 'longpress fired x ' + e.x + ' y ' + e.y;
+	setTimeout(function()
+	{
+		l11.color = '#333';
 	},200);
 });
