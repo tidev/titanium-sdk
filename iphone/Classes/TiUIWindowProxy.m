654d885ed414197fbe8f8b5e7ba9d3447ff16b6f
Blain Hamon <bhamon@bhammon-mbp.local>
2011-07-19 18:07:38 -0700

    TIMOB-4355: Must send proper blurs, but to avoid regression, make sure we're don't call it a second time.

--- iphone/Classes/TiUIWindowProxy.m
+++ iphone/Classes/TiUIWindowProxy.m
@@ -225,7 +225,7 @@
 		BOOL animate = args!=nil && [args count]>0 ? [TiUtils boolValue:@"animated" properties:[args objectAtIndex:0] def:YES] : YES;
 		[tab windowClosing:self animated:animate];
 	}
-	else
+	else if(focused)
 	{
 		// if we don't have a tab, we need to fire blur
 		// events ourselves
