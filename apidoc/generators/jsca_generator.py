5ecd23ed351d0294dc85e85e28ebdc3e9c4fbd6a
billdawson <bill@billdawson.com>
2011-08-04 23:15:52 +0200

    TIMOB-4270 Return a skeletal jsca so that at least scons+packaging will work

--- apidoc/generators/jsca_generator.py
+++ apidoc/generators/jsca_generator.py
@@ -3,9 +3,19 @@
 # Copyright (c) 2010-2011 Appcelerator, Inc. All Rights Reserved.
 # Licensed under the Apache Public License (version 2)
 
-import sys
+import sys, os
 from common import info, err, warn, msg
 
+try:
+	import json
+except:
+	import simplejson as json
+
 def generate(raw_apis, annotated_apis, options):
-	err("JSCA is not yet supported")
-	sys.exit(1)
+	warn("JSCA is not yet supported. A skeleton api.jsca will be created.")
+	out_path = os.path.join(options.output, "api.jsca")
+	api = {"types": [], "aliases": [{"type": "Titanium", "name": "Ti"}]}
+	f = open(out_path, "w")
+	json.dump(api, f, indent=4)
+	print json.dumps(api, indent=4)
+	f.close()
