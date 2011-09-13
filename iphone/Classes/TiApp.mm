29f4b4ed8f67e696146b9bf9a2b0136874a196ef
Blain Hamon <bhamon@bhamon-mbp2.local>
2011-09-12 19:30:32 -0700

    Orientation refactor: DefaultImageView (Seen during launch) moved into RootViewController, improved image placement.

--- iphone/Classes/TiApp.mm
+++ iphone/Classes/TiApp.mm
@@ -99,7 +99,7 @@ void MyUncaughtExceptionHandler(NSException *exception)
 	return sharedApp;
 }
 
-+(UIViewController<TiRootController>*)controller;
++(TiRootViewController*)controller;
 {
 	return [sharedApp controller];
 }
@@ -146,100 +146,6 @@ void MyUncaughtExceptionHandler(NSException *exception)
 	return launchOptions;
 }
 
-- (UIImage*)defaultImageForOrientation:(UIDeviceOrientation) orientation
-{	
-	UIImage* image;
-
-	if([TiUtils isIPad])
-	{
-		// Specific orientation check
-		switch (orientation) {
-			case UIDeviceOrientationPortrait:
-				image = [UIImage imageNamed:@"Default-Portrait.png"];
-				break;
-			case UIDeviceOrientationPortraitUpsideDown:
-				image = [UIImage imageNamed:@"Default-PortraitUpsideDown.png"];
-				break;
-			case UIDeviceOrientationLandscapeLeft:
-				image = [UIImage imageNamed:@"Default-LandscapeLeft.png"];
-				break;
-			case UIDeviceOrientationLandscapeRight:
-				image = [UIImage imageNamed:@"Default-LandscapeRight.png"];
-				break;
-			default:
-				image = nil;
-		}
-		if (image != nil) {
-			return image;
-		}
-			
-		// Generic orientation check
-		if (UIDeviceOrientationIsPortrait(orientation)) {
-			image = [UIImage imageNamed:@"Default-Portrait.png"];
-		}
-		else if (UIDeviceOrientationIsLandscape(orientation)) {
-			image = [UIImage imageNamed:@"Default-Landscape.png"];
-		}
-			
-		if (image != nil) {
-			return image;
-		}
-	}
-	
-	// Default 
-	return [UIImage imageNamed:@"Default.png"];
-}
-
-- (UIView*)attachSplash
-{
-	UIView * controllerView = [controller view];
-	
-	RELEASE_TO_NIL(loadView);
-
-	CGRect destRect;
-
-	if([TiUtils isIPad]) //iPad, 1024*748 or 748*1004, under the status bar.
-	{
-		destRect = [controllerView bounds];
-	}
-	else //iPhone: 320*480, placing behind the statusBar.
-	{
-		destRect = [controllerView convertRect:[[UIScreen mainScreen] bounds] fromView:nil];
-		destRect.origin.y -= [[UIApplication sharedApplication] statusBarFrame].size.height;
-	}
-
-	loadView = [[UIImageView alloc] initWithFrame:destRect];
-	[loadView setContentMode:UIViewContentModeScaleAspectFill];
-	loadView.image = [self defaultImageForOrientation:[[UIDevice currentDevice] orientation]];
-	[controller.view addSubview:loadView];
-	splashAttached = YES;
-	return loadView;
-}
-
-- (BOOL)isSplashVisible
-{
-	return splashAttached;
-}
-
--(UIView*)splash
-{
-	return loadView;
-}
-
-- (void)hideSplash:(id)event
-{
-	// this is called when the first window is loaded
-	// and should only be done once (obviously) - the
-	// caller is responsible for setting up the animation
-	// context before calling this and committing it afterwards
-	if (loadView!=nil && splashAttached)
-	{
-		splashAttached = NO;
-		loaded = YES;
-		[loadView removeFromSuperview];
-		RELEASE_TO_NIL(loadView);
-	}
-}
 
 -(void)initController
 {
@@ -249,9 +155,6 @@ void MyUncaughtExceptionHandler(NSException *exception)
 	controller = [[TiRootViewController alloc] init];
 	
 	// attach our main view controller... IF we haven't already loaded the main window.
-	if (!loaded) {
-		[self attachSplash];
-	}
 	[window setRootViewController:controller];
     [window makeKeyAndVisible];
 }
