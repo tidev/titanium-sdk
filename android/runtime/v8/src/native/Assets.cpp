f71472a2799065c1d7525d454eecd3ee82c6b484
Max Stepanov <mstepanov@aptana.com>
2011-09-15 16:21:40 -0700

    Implement assets access

---
+++ android/runtime/v8/src/native/Assets.cpp
@@ -0,0 +1,53 @@
+#include <jni.h>
+#include <v8.h>
+#include "org_appcelerator_kroll_runtime_Assets.h"
+#include "JNIUtil.h"
+#include "TypeConverter.h"
+
+static jobject gAssets;
+static jmethodID gReadResourceMethod;
+
+using namespace titanium;
+
+/*
+ * Class:     org_appcelerator_kroll_runtime_Assets
+ * Method:    assign
+ * Signature: (Lorg/appcelerator/kroll/runtime/Assets;)V
+ */
+JNIEXPORT void JNICALL Java_org_appcelerator_kroll_runtime_Assets_assign
+  (JNIEnv *env, jclass clazz, jobject assets)
+{	
+	if (gAssets) {
+		env->DeleteGlobalRef(gAssets);
+		gAssets = 0;
+	}
+	if (assets) {
+		gAssets = env->NewGlobalRef(assets);
+		gReadResourceMethod = env->GetMethodID(clazz, "readResource", "(Ljava/lang/String;)[C");
+	}
+}
+
+namespace assets {
+	
+v8::Handle<v8::Primitive> readResource(v8::Handle<v8::String> path)
+{
+	JNIEnv *env = JNIUtil::getJNIEnv();
+	if (!gAssets || !env) {
+		return v8::Null();
+	}
+	jstring jpath = TypeConverter::jsStringToJavaString(path);
+	jcharArray jarray = (jcharArray)env->CallObjectMethod(gAssets, gReadResourceMethod, jpath);
+	if (!jarray) {
+		return v8::Null();
+	}
+	jint len = env->GetArrayLength(jarray);
+	jchar *pchars = (jchar*)env->GetPrimitiveArrayCritical(jarray, 0);
+	if (!pchars) {
+		return v8::Null();
+	}
+	v8::Handle<v8::String> jsString = v8::String::New (pchars, len);
+	env->ReleasePrimitiveArrayCritical(jarray, pchars, 0);
+	return jsString;
+}
+
+}
