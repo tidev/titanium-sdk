8b379e32f12a86843e4f590b886311be0602127f
Max Stepanov <mstepanov@aptana.com>
2011-09-16 15:27:26 -0700

    load native module support from main js file

--- android/runtime/v8/src/js/kroll.js
+++ android/runtime/v8/src/js/kroll.js
@@ -1,16 +1,76 @@
-var Kroll = {
-	moduleInfo: {},
-	externalChildModules: {},
+(function(self) {
+  global = this;
 
-	addModuleInfo: function(info) {
-		customModuleInfo[info.id] = info;
-	},
+  function startup() {
+  }
 
-	getModuleInfo: function(id) {
-		return customModuleInfo[id];
-	},
+  var runInThisContext = self.binding('evals').Script.runInThisContext;
 
-	getCustomModuleIds: function() {
-		return Object.keys(customModuleInfo);
-	}
-};
+  function NativeModule(id) {
+    this.filename = id + '.js';
+    this.id = id;
+    this.exports = {};
+    this.loaded = false;
+  }
+
+  NativeModule._source = self.binding('natives');
+  NativeModule._cache = {};
+
+  NativeModule.require = function(id) {
+    if (id == 'native_module') {
+      return NativeModule;
+    }
+
+    var cached = NativeModule.getCached(id);
+    if (cached) {
+      return cached.exports;
+    }
+
+    if (!NativeModule.exists(id)) {
+      throw new Error('No such native module ' + id);
+    }
+
+    var nativeModule = new NativeModule(id);
+
+    nativeModule.compile();
+    nativeModule.cache();
+
+    return nativeModule.exports;
+  };
+
+  NativeModule.getCached = function(id) {
+    return NativeModule._cache[id];
+  }
+
+  NativeModule.exists = function(id) {
+    return (id in NativeModule._source);
+  }
+
+  NativeModule.getSource = function(id) {
+    return NativeModule._source[id];
+  }
+
+  NativeModule.wrap = function(script) {
+    return NativeModule.wrapper[0] + script + NativeModule.wrapper[1];
+  };
+
+  NativeModule.wrapper = [
+    '(function (exports, require, module, __filename, __dirname) { ',
+    '\n});'
+  ];
+
+  NativeModule.prototype.compile = function() {
+    var source = NativeModule.getSource(this.id);
+    source = NativeModule.wrap(source);
+
+    var fn = runInThisContext(source, this.filename, true);
+    fn(this.exports, NativeModule.require, this, this.filename);
+    this.loaded = true;
+  };
+
+  NativeModule.prototype.cache = function() {
+    NativeModule._cache[this.id] = this;
+  };
+
+  startup();
+});
