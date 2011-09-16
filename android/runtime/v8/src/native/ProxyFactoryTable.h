c3e54e4080cb26cd884c0aadbbf33372ef9f6284
Opie Cyrus <ocyrus@appcelerator.com>
2011-09-16 10:33:11 -0700

    add boiler plate headers for proxy lookup

---
+++ android/runtime/v8/src/native/ProxyFactoryTable.h
@@ -0,0 +1,32 @@
+/*
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+#ifndef PROXY_FACTORY_TABLE_H
+#define PROXY_FACTORY_TABLE_H
+
+#include <jni.h>
+
+#include "ProxyFactory.h"
+
+
+namespace titanium
+{
+        class ProxyFactoryTable
+        {
+                public:
+			static ProxyFactory * lookup (jclass javaClass);
+
+
+            	private:
+		// stub out shit here
+	};
+}
+
+
+#endif
+
+
