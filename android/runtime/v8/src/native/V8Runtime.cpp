ba09f8f6c17e9fcd6fe5a8ad818127568506da89
Opie Cyrus <ocyrus@appcelerator.com>
2011-09-17 13:22:42 -0700

    crazy debug logging in V8Runtime.cpp - rip this shit out as soon as we can

--- android/runtime/v8/src/native/V8Runtime.cpp
+++ android/runtime/v8/src/native/V8Runtime.cpp
@@ -24,10 +24,13 @@ namespace titanium {
 /* static */
 void V8Runtime::collectWeakRef(Persistent<Value> ref, void *parameter)
 {
+	LOGD("collectWeakRef", "-------1");
 	jobject v8Object = (jobject) parameter;
-
+	LOGD("collectWeakRef", "-------2");
 	ref.Dispose();
+	LOGD("collectWeakRef", "-------3");
 	JNIUtil::getJNIEnv()->DeleteGlobalRef(v8Object);
+	LOGD("collectWeakRef", "-------4");
 }
 
 /* static */
@@ -37,14 +40,20 @@ jobject V8Runtime::newObject(Handle<Object> object)
 	LOGI(TAG, "Creating new object...");
 
 	JNIEnv *env = JNIUtil::getJNIEnv();
+	LOGD("newObject", "-------1");
 	if (!env) return NULL;
 
-	jlong ptr = reinterpret_cast<jlong>(*Persistent<Object>::New(object));jobject
-	v8Object = env->NewGlobalRef(env->NewObject(JNIUtil::v8ObjectClass, JNIUtil::v8ObjectInitMethod, ptr));
+	LOGD("newObject", "-------2");
+	jlong ptr = reinterpret_cast<jlong>(*Persistent<Object>::New(object));
+	LOGD("newObject", "-------3");
+	jobject v8Object = env->NewGlobalRef(env->NewObject(JNIUtil::v8ObjectClass, JNIUtil::v8ObjectInitMethod, ptr));
 
+	LOGD("newObject", "-------4");
 	// make a 2nd persistent weakref so we can be informed of GC
 	Persistent<Object> weakRef = Persistent<Object>::New(object);
+	LOGD("newObject", "-------5");
 	weakRef.MakeWeak(reinterpret_cast<void*>(v8Object), V8Runtime::collectWeakRef);
+	LOGD("newObject", "-------6");
 	return v8Object;
 }
 
@@ -54,60 +63,101 @@ static Handle<Value> binding(const Arguments& args)
 {
 	static Persistent<Object> binding_cache;
 
+        LOGD("binding", "-------1");
 	HandleScope scope;
+	LOGD("binding", "-------2");
 	Local<String> module = args[0]->ToString();
+	LOGD("binding", "-------3");
 	String::Utf8Value module_v(module);
-
+	LOGD("binding", "-------4");
 	if (binding_cache.IsEmpty()) {
+		LOGD("binding", "-------5");
 		binding_cache = Persistent<Object>::New(Object::New());
+		LOGD("binding", "-------6");
 	}
 
 	Local<Object> exports;
+	LOGD("binding", "-------7");
 	if (binding_cache->Has(module)) {
+		LOGD("binding", "-------8");
 		exports = binding_cache->Get(module)->ToObject();
+		LOGD("binding", "-------9");
 	} else if (!strcmp(*module_v, "natives")) {
+		LOGD("binding", "-------10");
 		exports = Object::New();
+		LOGD("binding", "-------11");
 		KrollJavaScript::DefineNatives(exports);
+		LOGD("binding", "-------12");
 		binding_cache->Set(module, exports);
+		LOGD("binding", "-------13");
 	} else if (!strcmp(*module_v, "evals")) {
+		LOGD("binding", "-------14");
 		exports = Object::New();
+		LOGD("binding", "-------15");
 		ScriptsModule::Initialize(exports);
+		LOGD("binding", "-------16");
 		binding_cache->Set(module, exports);
+		LOGD("binding", "-------17");
 	} else {
+		LOGD("binding", "-------18");
 		return ThrowException(Exception::Error(String::New("No such module")));
 	}
+	LOGD("binding", "-------19");
 	return scope.Close(exports);
 }
 
 /* static */
 void V8Runtime::bootstrap()
 {
+	LOGD("bootstrap", "-------1");
 	Local<FunctionTemplate> global_template = FunctionTemplate::New();
+        LOGD("bootstrap", "-------2");
 	EventEmitter::Initialize(global_template);
 
+        LOGD("bootstrap", "-------3");
 	global = Persistent<Object>::New(global_template->GetFunction()->NewInstance());
+        LOGD("bootstrap", "-------4");
 	global->Set(String::NewSymbol("binding"), FunctionTemplate::New(binding)->GetFunction());
+        LOGD("bootstrap", "-------5");
 	global->Set(String::NewSymbol("EventEmitter"), EventEmitter::constructorTemplate->GetFunction());
+        LOGD("bootstrap", "-------6");
 	global->Set(String::NewSymbol("API"), APIModule::init());
 
+        LOGD("bootstrap", "-------7");
 	TryCatch try_catch;
+        LOGD("bootstrap", "-------8");
 	Handle<Value> result = ExecuteString(KrollJavaScript::MainSource(), IMMUTABLE_STRING_LITERAL("kroll.js"));
+        LOGD("bootstrap", "-------9");
 	if (try_catch.HasCaught()) {
+       		LOGD("bootstrap", "-------10");
 		ReportException(try_catch, true);
+        	LOGD("bootstrap", "-------11");
 		JNIUtil::terminateVM();
+        	LOGD("bootstrap", "-------12");
 	}
+        LOGD("bootstrap", "-------13");
 	if (!result->IsFunction()) {
 		LOGF(TAG, "kroll.js result is not a function");
 		ReportException(try_catch, true);
+        	LOGD("bootstrap", "-------14");
 		JNIUtil::terminateVM();
+        	LOGD("bootstrap", "-------15");
 	}
+        LOGD("bootstrap", "-------16");
 	Handle<Function> mainFunction = Handle<Function>::Cast(result);
+        LOGD("bootstrap", "-------17");
 	Local<Object> global = v8::Context::GetCurrent()->Global();
+        LOGD("bootstrap", "-------18");
 	Local<Value> args[] = { Local<Value>::New(global) };
+        LOGD("bootstrap", "-------19");
 	mainFunction->Call(global, 1, args);
+        LOGD("bootstrap", "-------20");
 	if (try_catch.HasCaught()) {
+		LOGD("bootstrap", "-------21");
 		ReportException(try_catch, true);
+	        LOGD("bootstrap", "-------22");
 		JNIUtil::terminateVM();
+        	LOGD("bootstrap", "-------23");
 	}
 }
 
@@ -153,26 +203,37 @@ JNIEXPORT void JNICALL Java_org_appcelerator_kroll_runtime_v8_V8Runtime_evalData
 {
 	HandleScope scope;
 
+	LOGD("evalData", "-------1");
 	if (!buffer) {
 		// TODO throw exception
 		return;
 	}
 
+	LOGD("evalData", "-------2");
 	jint len = env->GetArrayLength(buffer);
+	LOGD("evalData", "-------3");
 	jchar *pchars = (jchar*) env->GetPrimitiveArrayCritical(buffer, 0);
+	LOGD("evalData", "-------4");
 	if (!pchars) {
 		// TODO throw exception
 		return;
 	}
 
+	LOGD("evalData", "-------5");
 	Handle<String> jsFilename = titanium::TypeConverter::javaStringToJsString(filename);
+	LOGD("evalData", "-------6");
 	ScriptOrigin origin(jsFilename);
 
+	LOGD("evalData", "-------7");
 	v8::Handle<v8::String> jsString = v8::String::New(pchars, len);
+	LOGD("evalData", "-------8");
 	v8::Handle<v8::Script> script = Script::New(jsString, &origin);
+	LOGD("evalData", "-------9");
 	env->ReleasePrimitiveArrayCritical(buffer, pchars, 0);
 
+	LOGD("evalData", "-------10");
 	script->Run();
+	LOGD("evalData", "-------11");
 }
 
 /*
@@ -182,15 +243,21 @@ JNIEXPORT void JNICALL Java_org_appcelerator_kroll_runtime_v8_V8Runtime_evalData
  */
 JNIEXPORT void JNICALL Java_org_appcelerator_kroll_runtime_v8_V8Runtime_dispose(JNIEnv *env, jclass clazz)
 {
+	LOGD("dispose", "-------1");
 	context.Dispose();
+	LOGD("dispose", "-------2");
 	V8::Dispose();
+	LOGD("dispose", "-------3");
 	env->DeleteGlobalRef(jruntime);
+	LOGD("dispose", "-------4");
 	jruntime = NULL;
 }
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved)
 {
+	LOGD("onload", "-------1");
 	titanium::JNIUtil::javaVm = vm;
+	LOGD("onload", "-------2");
 	return JNI_VERSION_1_4;
 }
 
