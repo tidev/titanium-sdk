00e4bfd52b381c86510e4aeacb51fed6d555ceac
Joshua Roesslein <jroesslein@appcelerator.com>
2011-09-16 17:10:42 -0700

    Generate proxy source list in makefile.

--- android/runtime/v8/src/native/Android.mk
+++ android/runtime/v8/src/native/Android.mk
@@ -15,19 +15,16 @@ THIS_DIR := $(LOCAL_PATH)
 GENERATED_DIR := $(LOCAL_PATH)/../../generated
 SRC_JS_DIR := $(LOCAL_PATH)/../js
 
-TITANIUM_APT_GEN_DIR := ../../../../titanium/.apt_generated
-ABS_TITANIUM_APT_GEN_DIR := $(LOCAL_PATH)/$(TITANIUM_APT_GEN_DIR)
-
 TOOLS_DIR := $(THIS_DIR)/../../tools
 JS2C := $(TOOLS_DIR)/js2c.py
 
 include $(CLEAR_VARS)
-include $(ABS_TITANIUM_APT_GEN_DIR)/sources.mk
+include $(LOCAL_PATH)/genSources.mk
 
 TITANIUM_GEN_SOURCES := $(addprefix $(TITANIUM_APT_GEN_DIR)/,$(GEN_SOURCES))
 
 LOCAL_MODULE := kroll-v8
-LOCAL_CFLAGS := -I$(LIBV8_DIR)/include -I$(ABS_TITANIUM_APT_GEN_DIR) -I$(GENERATED_DIR) -g
+LOCAL_CFLAGS := -I$(LIBV8_DIR)/include $(PROXY_CFLAGS) -I$(GENERATED_DIR) -g
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -L$(LIBV8_DIR)/lib -ldl -llog -L$(TARGET_OUT)
 LOCAL_SRC_FILES += \
 	../../generated/KrollNatives.h \
@@ -36,21 +33,17 @@ LOCAL_SRC_FILES += \
 	JNIUtil.cpp \
 	KrollJavaScript.cpp \
 	KrollProxy.cpp \
-	V8Util.cpp \
 	TypeConverter.cpp \
 	V8Object.cpp \
 	V8Runtime.cpp \
-	$(TITANIUM_GEN_SOURCES)
+	V8Util.cpp \
+	$(PROXY_SOURCES)
 
 LOCAL_JS_FILES := \
 	$(SRC_JS_DIR)/kroll.js \
 	$(SRC_JS_DIR)/events.js
 
 $(LOCAL_PATH)/KrollJavaScript.cpp: $(GENERATED_DIR)/KrollNatives.h
-
-$(GENERATED_DIR)/KrollNatives.h: $(LOCAL_JS_FILES)
-	mkdir $(GENERATED_DIR) || echo
-	python $(JS2C) $(GENERATED_DIR)/KrollNatives.h $(LOCAL_JS_FILES)
 
 LOCAL_STATIC_LIBRARIES := libv8
 
---
+++ android/runtime/v8/src/native/genSources.mk
@@ -0,0 +1,20 @@
+GENERATED_DIR := $(LOCAL_PATH)/../../generated
+
+THIS_DIR := $(LOCAL_PATH)
+ANDROID_DIR := ../../../..
+ABS_ANDROID_DIR := $(LOCAL_PATH)/$(ANDROID_DIR)
+
+TI_APT_GEN := $(ABS_ANDROID_DIR)/titanium/.apt_generated
+ABS_PROXY_SOURCES := $(wildcard $(ABS_ANDROID_DIR)/modules/*/.apt_generated/*.cpp) \
+	$(wildcard $(TI_APT_GEN)/*.cpp)
+
+PROXY_SOURCES := $(patsubst $(ABS_ANDROID_DIR)/%,$(ANDROID_DIR)/%,$(ABS_PROXY_SOURCES))
+
+PROXY_GEN_DIRS := $(wildcard $(ABS_ANDROID_DIR)/modules/*/.apt_generated) \
+	$(ABS_ANDROID_DIR)/titanium/.apt_generated
+PROXY_CFLAGS := $(addprefix -I,$(PROXY_GEN_DIRS))
+
+$(GENERATED_DIR)/KrollNatives.h: $(LOCAL_JS_FILES)
+	mkdir $(GENERATED_DIR) || echo
+	python $(JS2C) $(GENERATED_DIR)/KrollNatives.h $(LOCAL_JS_FILES)
+
