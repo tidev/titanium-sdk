3ede966dddc9229b485034586e2d65602d862f15
Opie Cyrus <dizzymonkey@gmail.com>
2011-09-26 23:45:59 -0500

    draft of include modification

--- android/runtime/v8/src/js/titanium.js
+++ android/runtime/v8/src/js/titanium.js
@@ -9,6 +9,14 @@ var Titanium = binding.Titanium;
 
 // assign any Titanium props/methods/aliases here
 
+Titanium.include = function(filename) {
+	var source = kroll.binding('assets').readResource(filename);
+	var wrappedSource = '(function (exports, require, module, __filename, __dirname) { ' + source + '\n});'
+
+	var wrappedFile = kroll.binding('evals').Script.runInThisContext(source, filename + '.js', true);
+	wrappedFile(exports, require, this, filename);
+}
+
 Object.prototype.extend = function(other) {
 	if (!object) return;
 
