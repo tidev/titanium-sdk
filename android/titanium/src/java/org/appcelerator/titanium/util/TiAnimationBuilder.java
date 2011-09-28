9176490facf06d1b4ceaba5ef3e77fb11a65a6fa
Allen Yeung <ayeung@aptana.com>
2011-09-28 13:46:26 -0700

    TIMOB-2851: Code review change
    Conflicts:
    
    	android/titanium/src/java/org/appcelerator/titanium/util/TiAnimationBuilder.java

--- android/titanium/src/java/org/appcelerator/titanium/util/TiAnimationBuilder.java
+++ android/titanium/src/java/org/appcelerator/titanium/util/TiAnimationBuilder.java
@@ -202,12 +202,12 @@ public class TiAnimationBuilder
 			addAnimation(animationSet, animation);
 			animation.setAnimationListener(animationListener);
 			
+			TiUIView uiView = viewProxy.peekView();
 			if (viewProxy.hasProperty(TiC.PROPERTY_OPACITY) && fromOpacity != null && toOpacity != null
-				&& viewProxy.peekView() != null) {
+				&& uiView != null) {
 				// Initialize the opacity to 1 when we are going to change it in
 				// the animation. If the opacity of the view was initialized to
 				// 0, the animation doesn't work
-				TiUIView uiView = viewProxy.getOrCreateView();
 				uiView.setOpacity(1);
 			}
 		}
