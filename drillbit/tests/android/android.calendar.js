43b4e4e5c57dc447cf9c1ec355f7067da6517518
nataliehuynh <nataliehuynh@yahoo.com>
2011-10-11 15:56:49 -0700

    Removed check of exception for every constant

--- drillbit/tests/android/android.calendar.js
+++ drillbit/tests/android/android.calendar.js
@@ -5,37 +5,21 @@ describe("Ti.Android.Calendar tests", {
 		// Just tests if the module is even reachable, by referencing one of its constants
 		valueOf( function() { Ti.Android.Calendar.METHOD_ALERT; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.METHOD_ALERT).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.METHOD_DEFAULT; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.METHOD_DEFAULT).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.METHOD_EMAIL; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.METHOD_EMAIL).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.METHOD_SMS; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.METHOD_SMS).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.STATE_DISMISSED; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.STATE_DISMISSED).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.STATE_FIRED; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.STATE_FIRED).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.STATE_SCHEDULED; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.STATE_SCHEDULED).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.STATUS_CANCELED; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.STATUS_CANCELED).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.STATUS_CONFIRMED; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.STATUS_CONFIRMED).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.STATUS_TENTATIVE; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.STATUS_TENTATIVE).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.VISIBILITY_CONFIDENTIAL; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.VISIBILITY_CONFIDENTIAL).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.VISIBILITY_DEFAULT; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.VISIBILITY_DEFAULT).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.VISIBILITY_PRIVATE; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.VISIBILITY_PRIVATE).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.VISIBILITY_PUBLIC; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.VISIBILITY_PUBLIC).shouldBeNumber();
-		valueOf( function() { Ti.Android.Calendar.allAlerts; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.allAlerts).shouldBeObject();
-		valueOf( function() { Ti.Android.Calendar.allCalendars; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.allCalendars).shouldBeObject();
-		valueOf( function() { Ti.Android.Calendar. selectableCalendars; }).shouldNotThrowException();
 		valueOf(Ti.Android.Calendar.selectableCalendars).shouldBeObject();		
 	},
 	
