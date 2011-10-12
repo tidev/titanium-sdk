907380ce5119ccda5308dab457f1dc0965eaf4ac
Allen Yeung <ayeung@aptana.com>
2011-10-12 10:29:37 -0700

    removed unneccesary null check

--- android/modules/ui/src/ti/modules/titanium/ui/widget/TiUILabel.java
+++ android/modules/ui/src/ti/modules/titanium/ui/widget/TiUILabel.java
@@ -147,7 +147,7 @@ public class TiUILabel extends TiUIView
 	@Override
 	protected void setOpacity(View view, float opacity)
 	{
-		if (view != null && view instanceof TextView) {
+		if (view instanceof TextView) {
 			TiUIHelper.setPaintOpacity(((TextView) view).getPaint(), opacity);
 		}
 		super.setOpacity(view, opacity);
@@ -157,7 +157,7 @@ public class TiUILabel extends TiUIView
 	public void clearOpacity(View view)
 	{
 		super.clearOpacity(view);
-		if (view != null && view instanceof TextView) {
+		if (view instanceof TextView) {
 			((TextView) view).getPaint().setColorFilter(null);
 		}
 	}
