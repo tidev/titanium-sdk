532f77c339f91f914e58010f2c7901b2b47f8ab0
Allen Yeung <ayeung@aptana.com>
2011-10-18 17:44:29 -0700

    TIMOB-5752: Update platformId to currentMachineId

--- android/titanium/src/java/org/appcelerator/titanium/util/TiPlatformHelper.java
+++ android/titanium/src/java/org/appcelerator/titanium/util/TiPlatformHelper.java
@@ -95,9 +95,9 @@ public class TiPlatformHelper
 			db.updatePlatformParam("unique_machine_id", currentMachineId);
 			db.updatePlatformParam("hardware_machine_id", platformId);
 			db.updatePlatformParam("previous_machine_id", storedMachineId);
-			platformId = currentMachineId;
 		}
-
+		
+		platformId = currentMachineId;
 		sessionId = createUUID();
 	}
 	
