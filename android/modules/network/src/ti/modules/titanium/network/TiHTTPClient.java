843d24c060c1bec254ae17385f321d696738bbec
donthorp <don@donthorp.net>
2011-10-24 18:26:16 -0500

    [TIMOB-2849] Added imports missing from cherry-picks

--- android/modules/network/src/ti/modules/titanium/network/TiHTTPClient.java
+++ android/modules/network/src/ti/modules/titanium/network/TiHTTPClient.java
@@ -21,6 +21,7 @@ import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -49,6 +50,7 @@ import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.conn.scheme.SocketFactory;
 import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.entity.AbstractHttpEntity;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntity;
