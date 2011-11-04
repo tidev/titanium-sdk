f042015ae945c68c228408a089298b4f40fb014a
Ping Wang <pwang@appcelerator.com>
2011-11-04 16:27:57 -0700

    Modified label_basic.js

--- demos/KitchenSink/Resources/examples/label_basic.js
+++ demos/KitchenSink/Resources/examples/label_basic.js
@@ -4,6 +4,7 @@ var l1 = Titanium.UI.createLabel({
 	id:'font_label_test',
 	text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat',
 	top:0,
+	height:170,
 	textAlign:'center'
 });
 
