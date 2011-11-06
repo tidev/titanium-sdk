da3f444b8635ab8faa3f2501d1ae5a1f95af2c3f
Joshua Roesslein <jroesslein@appcelerator.com>
2011-11-06 11:53:45 -0800

    Fix JsActivity setResult().

--- android/runtime/common/src/js/activity.js
+++
@@ -1,8 +0,0 @@
-var Ti = require("titanium");
-
-var doStartActivityForResult = Ti.Activity.prototype.startActivityForResult;
-Ti.Activity.prototype.startActivityForResult = function(intent, callback) {
-	this.on("result", callback);
-	this.on("error", callback);
-	doStartActivityForResult.call(this, intent);
-}
