dc1091d8d23dea3c441923c893a35104c56d6129
Chris Barber <chris@cb1inc.com>
2011-11-08 11:56:27 -0800

    [TIMOB-6010] Added runtime to the Ti.Platform. Also changed the Ti.Platform name and osname.

--- mobileweb/src/Ti.Platform/platform.js
+++ mobileweb/src/Ti.Platform/platform.js
@@ -1,149 +1,4 @@
 (function(api){
-	/** Browser detect is from http://www.quirksmode.org/js/detect.html **/
-	
-	var BrowserDetect = {
-		init: function () {
-			this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
-			this.version = this.searchVersion(navigator.userAgent)
-				|| this.searchVersion(navigator.appVersion)
-				|| "an unknown version";
-			this.OS = this.searchString(this.dataOS) || "an unknown OS";
-		},
-		searchString: function (data) {
-			for (var i=0;i<data.length;i++)	{
-				var dataString = data[i].string;
-				var dataProp = data[i].prop;
-				this.versionSearchString = data[i].versionSearch || data[i].identity;
-				if (dataString) {
-					if (dataString.indexOf(data[i].subString) != -1)
-						return data[i].identity;
-				}
-				else if (dataProp)
-					return data[i].identity;
-			}
-		},
-		searchVersion: function (dataString) {
-			var index = dataString.indexOf(this.versionSearchString);
-			if (index == -1) return;
-			return parseFloat(dataString.substring(index+this.versionSearchString.length+1));
-		},
-		dataBrowser: [
-			{
-				string: navigator.userAgent,
-				subString: "Chrome",
-				identity: "Chrome"
-			},
-			{ 	string: navigator.userAgent,
-				subString: "OmniWeb",
-				versionSearch: "OmniWeb/",
-				identity: "OmniWeb"
-			},
-			{
-				string: navigator.vendor,
-				subString: "Apple",
-				identity: "Safari",
-				versionSearch: "Version"
-			},
-			{
-				prop: window.opera,
-				identity: "Opera"
-			},
-			{
-				string: navigator.vendor,
-				subString: "iCab",
-				identity: "iCab"
-			},
-			{
-				string: navigator.vendor,
-				subString: "KDE",
-				identity: "Konqueror"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "Firefox",
-				identity: "Firefox"
-			},
-			{
-				string: navigator.vendor,
-				subString: "Camino",
-				identity: "Camino"
-			},
-			{	// for newer Netscapes (6+)
-				string: navigator.userAgent,
-				subString: "Netscape",
-				identity: "Netscape"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "MSIE",
-				identity: "Explorer",
-				versionSearch: "MSIE"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "Gecko",
-				identity: "Mozilla",
-				versionSearch: "rv"
-			},
-			{ 	// for older Netscapes (4-)
-				string: navigator.userAgent,
-				subString: "Mozilla",
-				identity: "Netscape",
-				versionSearch: "Mozilla"
-			}
-		],
-		dataOS : [
-			{
-				string: navigator.platform,
-				subString: "Win",
-				identity: "Windows"
-			},
-			{
-				string: navigator.platform,
-				subString: "Mac",
-				identity: "Mac"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "iPhone",
-				identity: "iphone"
-		    	},
-			{
-				string: navigator.platform,
-				subString: "Linux",
-				identity: "Linux"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "Android",
-				identity: "android"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "iPod",
-				identity: "ipod"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "Symbian",
-				identity: "symbian"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "Blackberry",
-				identity: "blackberry"
-			},
-			{
-				string: navigator.userAgent,
-				subString: "iPad",
-				identity: "ipad"
-			}
-		]
-		
-	};
-	
-	BrowserDetect.init();
-
 	// Interfaces
 	Ti._5.EventDriven(api);
 
@@ -214,7 +69,7 @@
 	});
 
 	Object.defineProperty(api, 'name', {
-		get: function(){return BrowserDetect.browser;},
+		get: function(){return navigator.userAgent;},
 		set: function(val){return false;}
 	});
 
@@ -225,12 +80,19 @@
 	});
 
 	Object.defineProperty(api, 'osname', {
-		get: function(){return BrowserDetect.OS;},
+		get: function(){return "mobileweb";},
 		set: function(val){return false;}
 	});
 
 	Object.defineProperty(api, 'ostype', {
 		get: function(){return navigator.platform;},
+		set: function(val){return false;}
+	});
+
+	var match = navigator.userAgent.toLowerCase().match(/(webkit|gecko|trident|presto)/),
+		runtime = match ? match[0] : "unknown";
+	Object.defineProperty(api, 'runtime', {
+		get: function(){return runtime;},
 		set: function(val){return false;}
 	});
 
