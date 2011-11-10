6bfbcc8d027f3a91695cbeb92b86402155ef851b
Reggie Seagraves <rseagraves@appcelerator.com>
2011-11-10 09:49:22 -0800

    [timob-6009]  Update for review comments

--- demos/KitchenSink/Resources/examples/platform.js
+++ demos/KitchenSink/Resources/examples/platform.js
@@ -282,9 +282,9 @@ Titanium.Platform.addEventListener('battery', function(e)
 
 Titanium.API.info("Current Phone Locale is "+Titanium.Platform.locale);
 Titanium.API.info("OS name is " + Titanium.Platform.osname);
+Titanium.API.info("Runtime: " + Titanium.Platform.runtime);
 
 if (Titanium.Platform.osname == 'iphone' || Titanium.Platform.osname == 'ipad') {
 	Titanium.API.info("Data network: " + Titanium.Platform.dataAddress);
 	Titanium.API.info("Netmask: " + Titanium.Platform.netmask);
-	Titanium.API.info("runtime: " + Titanium.Platform.runtime);
 }
