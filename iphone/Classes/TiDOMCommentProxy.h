62efb4bad5fd6135addc1dd31e5bfec457cff93e
Vishal Duggal <vishalduggal@gmail.com>
2011-11-11 12:57:03 -0800

    Copyright Block Fix

--- iphone/Classes/TiDOMCommentProxy.h
+++ iphone/Classes/TiDOMCommentProxy.h
@@ -3,7 +3,7 @@
 //  Titanium
 //
 //  Created by default on 10/31/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2011 Appcelerator, Inc. All rights reserved.
 //
 
 #import "TIDOMCharacterDataProxy.h"
