7e297d30b9b0fd9cfbaeb65e4dcba22293535ea1
Vishal Duggal <vishalduggal@gmail.com>
2011-11-11 13:18:24 -0800

    ifdef guard

--- iphone/Classes/TiDOMCommentProxy.h
+++ iphone/Classes/TiDOMCommentProxy.h
@@ -5,9 +5,12 @@
 //  Created by default on 10/31/11.
 //  Copyright (c) 2011 Appcelerator, Inc. All rights reserved.
 //
+#if defined(USE_TI_XML) || defined(USE_TI_NETWORK)
 
 #import "TIDOMCharacterDataProxy.h"
 
 @interface TiDOMCommentProxy : TiDOMCharacterDataProxy
 
 @end
+
+#endif
--- iphone/Classes/TiDOMCommentProxy.m
+++ iphone/Classes/TiDOMCommentProxy.m
@@ -5,9 +5,11 @@
 //  Created by default on 10/31/11.
 //  Copyright (c) 2011 Appcelerator, Inc. All rights reserved.
 //
+#if defined(USE_TI_XML) || defined(USE_TI_NETWORK)
 
 #import "TiDOMCommentProxy.h"
 
 @implementation TiDOMCommentProxy
 
 @end
+#endif
