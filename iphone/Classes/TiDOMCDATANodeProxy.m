73130e0b8ae1f01a6f74db0fe5baa22d71517ead
Vishal Duggal <vishalduggal@gmail.com>
2011-11-11 13:11:04 -0800

    ifdef guard

--- iphone/Classes/TiDOMCDATANodeProxy.m
+++ iphone/Classes/TiDOMCDATANodeProxy.m
@@ -5,9 +5,11 @@
 //  Created by default on 10/31/11.
 //  Copyright (c) 2011 Appcelerator, Inc. All rights reserved.
 //
+#if defined(USE_TI_XML) || defined(USE_TI_NETWORK)
 
 #import "TiDOMCDATANodeProxy.h"
 
 @implementation TiDOMCDATANodeProxy
 
 @end
+#endif
