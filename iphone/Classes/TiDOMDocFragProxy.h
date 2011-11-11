d8aa6b6ef48e2b2e88366522f6b1546a72287e29
Vishal Duggal <vishalduggal@gmail.com>
2011-11-11 13:19:25 -0800

    comments regarding class names

--- iphone/Classes/TiDOMDocFragProxy.h
+++ iphone/Classes/TiDOMDocFragProxy.h
@@ -4,6 +4,7 @@
 //
 //  Created by default on 10/31/11.
 //  Copyright (c) 2011 Appcelerator, Inc. All rights reserved.
+//  Corresponds to Interface DocumentFragment of DOM2 Spec
 //
 #if defined(USE_TI_XML) || defined(USE_TI_NETWORK)
 
