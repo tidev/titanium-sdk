7a9748c3892585120ac02742274cafd341d72c41
Bryan Hughes <bhughes@appcelerator.com>
2011-11-14 15:21:12 -0800

    [TIMOB-5994] Integrated Facebook and hooked up authentication.

--- mobileweb/src/Ti.Facebook/facebook.js
+++ mobileweb/src/Ti.Facebook/facebook.js
@@ -21,13 +21,12 @@
 		set: function(val){return _expirationDate = val;}
 	});
 
-	var _forceDialogAuth = null;
 	Object.defineProperty(api, 'forceDialogAuth', {
-		get: function(){return _forceDialogAuth;},
-		set: function(val){return _forceDialogAuth = val;}
+		get: function(){return true;},
+		set: function(val){return true;}
 	});
 
-	var _loggedIn = null;
+	var _loggedIn = false;
 	Object.defineProperty(api, 'loggedIn', {
 		get: function(){return _loggedIn;},
 		set: function(val){return _loggedIn = val;}
@@ -45,31 +44,108 @@
 		set: function(val){return _uid = val;}
 	});
 
+	// Setup the Facebook initialization callback
+	var _facebookInitialized = false;
+	var _authAfterInitialized = false;
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId  : _appid, // App ID
+			status : true, // check login status
+			cookie : true, // enable cookies to allow the server to access the session
+			oauth  : true, // enable OAuth 2.0
+			xfbml  : true  // parse XFBML
+		});
+		_facebookInitialized = true;
+		if (_authAfterInitialized) {
+			api.authorize();
+		}
+	};
+	
+	// Create the div required by Facebook
+	fbDiv = document.createElement('div');
+	fbDiv.id = 'fb-root';
+	document.getElementsByTagName('body')[0].appendChild(fbDiv);
+	
+	// Load the Facebook SDK Asynchronously.
+	var fbScriptTag, id = 'facebook-jssdk'; 
+	if (!document.getElementById(id)) {
+		fbScriptTag = document.createElement('script');
+		fbScriptTag.id = id; 
+		fbScriptTag.async = true;
+		fbScriptTag.src = "//connect.facebook.net/en_US/all.js";
+		document.getElementsByTagName('head')[0].appendChild(fbScriptTag);
+	}
+
 	// Methods
 	api.authorize = function(){
-		console.debug('Method "Titanium.Facebook.authorize" is not implemented yet.');
+		
+		// Sanity check
+		if (_appid == null) {
+			console.debug('App ID not set. Facebook authorization cancelled.');
+			return;
+		}
+		
+		// Check if facebook is still initializing, and if so queue the auth request
+		if (!_facebookInitialized) {
+			_authAfterInitialized = true;
+			return;
+		}
+		
+		// Authorize
+		FB.login(function(response) {
+			if (response.authResponse) {
+				_expirationDate = new Date((new Date()).getTime() + response.authResponse.expiresIn * 1000);
+				FB.api('/me', function(response) {
+					_loggedIn = true;
+					_uid = response.id;
+					var undef;
+					var oEvent = {
+						cancelled	: false,
+						data		: response,
+						error		: undef,
+						source		: undef,
+						success		: true,
+						type		: undef,
+						uid			: _uid
+					};
+					api.fireEvent('login', oEvent);
+				});
+			} else {
+				var undef;
+				var oEvent = {
+					cancelled	: true,
+					data		: response,
+					error		: "The user cancelled or there was an internal error",
+					source		: undef,
+					success		: false,
+					type		: undef,
+					uid			: response.id
+				};
+				api.fireEvent('login', oEvent);
+			}
+		}, {'scope':_permissions.join()});
 	};
-	api.createLoginButton = function(){
+	api.createLoginButton = function(parameters){
 		console.debug('Method "Titanium.Facebook.createLoginButton" is not implemented yet.');
 	};
-	api.dialog = function(){
+	api.dialog = function(action,params,callback){
 		console.debug('Method "Titanium.Facebook.dialog" is not implemented yet.');
 	};
 	api.logout = function(){
-		console.debug('Method "Titanium.Facebook.logout" is not implemented yet.');
+		FB.logout(function(response) {
+			_loggedIn = false;
+			var undef;
+			var oEvent = {
+				source		: undef,
+				type		: undef
+			};
+			api.fireEvent('logout', oEvent);
+		});
 	};
-	api.request = function(){
+	api.request = function(method,params,callback){
 		console.debug('Method "Titanium.Facebook.request" is not implemented yet.');
 	};
-	api.requestWithGraphPath = function(){
+	api.requestWithGraphPath = function(path,params,httpMethod,callback){
 		console.debug('Method "Titanium.Facebook.requestWithGraphPath" is not implemented yet.');
 	};
-
-	// Events
-	api.addEventListener('login', function(){
-		console.debug('Event "login" is not implemented yet.');
-	});
-	api.addEventListener('logout', function(){
-		console.debug('Event "logout" is not implemented yet.');
-	});
 })(Ti._5.createClass('Titanium.Facebook'));
