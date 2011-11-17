0da584e370b1e2e5aea5ae20103f2b23e0360a12
Allen Yeung <ayeung@aptana.com>
2011-11-16 18:04:28 -0800

    TIMOB-6103: Android: Views: Rhino only: Delete Row crashes when all the rows are deleted

--- android/modules/ui/src/java/ti/modules/titanium/ui/TableViewProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/TableViewProxy.java
@@ -27,7 +27,7 @@ import ti.modules.titanium.ui.widget.TiUITableView;
 import android.app.Activity;
 import android.os.Message;
 
-@Kroll.proxy(creatableInModule=UIModule.class, propertyAccessors = {
+@Kroll.proxy(creatableInModule = UIModule.class, propertyAccessors = { 
 	TiC.PROPERTY_FILTER_ATTRIBUTE,
 	TiC.PROPERTY_FILTER_CASE_INSENSITIVE,
 	TiC.PROPERTY_HEADER_TITLE,
@@ -44,7 +44,7 @@ public class TableViewProxy extends TiViewProxy
 
 	private static final int INSERT_ROW_BEFORE = 0;
 	private static final int INSERT_ROW_AFTER = 1;
-	
+
 	private static final int MSG_UPDATE_VIEW = TiViewProxy.MSG_LAST_ID + 5001;
 	private static final int MSG_SCROLL_TO_INDEX = TiViewProxy.MSG_LAST_ID + 5002;
 	private static final int MSG_SET_DATA = TiViewProxy.MSG_LAST_ID + 5003;
@@ -57,7 +57,8 @@ public class TableViewProxy extends TiViewProxy
 	public static final String CLASSNAME_HEADER = "__header__";
 	public static final String CLASSNAME_NORMAL = "__normal__";
 
-	class RowResult {
+	class RowResult
+	{
 		int sectionIndex;
 		TableViewSectionProxy section;
 		TableViewRowProxy row;
@@ -70,7 +71,7 @@ public class TableViewProxy extends TiViewProxy
 	{
 		super();
 
-		//eventManager.addOnEventChangeListener(this);
+		// eventManager.addOnEventChangeListener(this);
 	}
 
 	public TableViewProxy(TiContext tiContext)
@@ -79,7 +80,8 @@ public class TableViewProxy extends TiViewProxy
 	}
 
 	@Override
-	public void handleCreationDict(KrollDict dict) {
+	public void handleCreationDict(KrollDict dict)
+	{
 		Object data[] = null;
 		if (dict.containsKey(TiC.PROPERTY_DATA)) {
 			Object o = dict.get(TiC.PROPERTY_DATA);
@@ -117,21 +119,24 @@ public class TableViewProxy extends TiViewProxy
 	}
 
 	@Override
-	public TiUIView createView(Activity activity) {
+	public TiUIView createView(Activity activity)
+	{
 		return new TiUITableView(this);
 	}
 
-	public TiUITableView getTableView() {
+	public TiUITableView getTableView()
+	{
 		return (TiUITableView) getOrCreateView();
 	}
 
 	@Kroll.method
-	public void updateRow(Object row, Object data, @Kroll.argument(optional=true) KrollDict options) {
+	public void updateRow(Object row, Object data, @Kroll.argument(optional = true) KrollDict options)
+	{
 		TableViewSectionProxy sectionProxy = null;
 		int rowIndex = -1;
 		if (row instanceof Number) {
 			RowResult rr = new RowResult();
-			rowIndex = ((Number)row).intValue();
+			rowIndex = ((Number) row).intValue();
 			locateIndex(rowIndex, rr);
 			sectionProxy = rr.section;
 		} else if (row instanceof TableViewRowProxy) {
@@ -156,7 +161,7 @@ public class TableViewProxy extends TiViewProxy
 
 	// options argument exists in order to maintain parity with iOS, do not remove
 	@Kroll.method
-	public void appendRow(Object rows, @Kroll.argument(optional=true) KrollDict options)
+	public void appendRow(Object rows, @Kroll.argument(optional = true) KrollDict options)
 	{
 		if (TiApplication.isUIThread()) {
 			handleAppendRow(rows);
@@ -171,30 +176,22 @@ public class TableViewProxy extends TiViewProxy
 	{
 		Object[] rowList = null;
 
-		if (rows instanceof Object[])
-		{
-			rowList = (Object[])rows;
-		}
-		else
-		{
+		if (rows instanceof Object[]) {
+			rowList = (Object[]) rows;
+		} else {
 			rowList = new Object[] { rows };
 		}
 
 		ArrayList<TableViewSectionProxy> sections = getSectionsArray();
-		if (sections.size() == 0)
-		{
+		if (sections.size() == 0) {
 			processData(rowList);
-		}
-		else
-		{
-			for (int i = 0; i < rowList.length; i++)
-			{
+		} else {
+			for (int i = 0; i < rowList.length; i++) {
 				TableViewRowProxy rowProxy = rowProxyFor(rowList[i]);
 
 				TableViewSectionProxy lastSection = sections.get(sections.size() - 1);
 				TableViewSectionProxy addedToSection = addRowToSection(rowProxy, lastSection);
-				if (lastSection == null || !lastSection.equals(addedToSection))
-				{
+				if (lastSection == null || !lastSection.equals(addedToSection)) {
 					sections.add(addedToSection);
 				}
 				rowProxy.setProperty(TiC.PROPERTY_SECTION, addedToSection);
@@ -207,32 +204,35 @@ public class TableViewProxy extends TiViewProxy
 	}
 
 	@Kroll.method
-	public void deleteRow(int index, @Kroll.argument(optional=true) KrollDict options) {
+	public void deleteRow(int index, @Kroll.argument(optional = true) KrollDict options)
+	{
 		if (TiApplication.isUIThread()) {
 			handleDeleteRow(index);
 			return;
 		}
-		Message message = getMainHandler().obtainMessage(MSG_DELETE_ROW);
-		//Message msg = getUIHandler().obtainMessage(MSG_DELETE_ROW);
-		message.arg1 = index;
-		message.sendToTarget();
 
+		Object asyncResult = TiMessenger.sendBlockingMainMessage(getMainHandler().obtainMessage(MSG_DELETE_ROW), index);
+
+		if (asyncResult instanceof IllegalStateException) {
+			throw (IllegalStateException) asyncResult;
+		}
 	}
 
-	private void handleDeleteRow(int index) {
+	private void handleDeleteRow(int index) throws IllegalStateException
+	{
 		RowResult rr = new RowResult();
 		if (locateIndex(index, rr)) {
 			rr.section.removeRowAt(rr.rowIndexInSection);
 			getTableView().setModelDirty();
 			updateView();
 		} else {
-			throw new IllegalStateException(
-				"Index out of range. Non-existant row at " + index);
+			throw new IllegalStateException("Index out of range. Non-existent row at " + index);
 		}
 	}
 
 	@Kroll.method
-	public int getIndexByName(String name) {
+	public int getIndexByName(String name)
+	{
 		int index = -1;
 		int idx = 0;
 		if (name != null) {
@@ -254,16 +254,23 @@ public class TableViewProxy extends TiViewProxy
 	}
 
 	@Kroll.method
-	public void insertRowBefore(int index, Object data, @Kroll.argument(optional=true) KrollDict options) {
+	public void insertRowBefore(int index, Object data, @Kroll.argument(optional = true) KrollDict options)
+	{
 		if (TiApplication.isUIThread()) {
 			handleInsertRowBefore(index, data);
 			return;
 		}
 
-		TiMessenger.sendBlockingMainMessage(getMainHandler().obtainMessage(MSG_INSERT_ROW, INSERT_ROW_BEFORE, index), data);
+		Object asyncResult = TiMessenger.sendBlockingMainMessage(
+			getMainHandler().obtainMessage(MSG_INSERT_ROW, INSERT_ROW_BEFORE, index), data);
+
+		if (asyncResult instanceof IllegalStateException) {
+			throw (IllegalStateException) asyncResult;
+		}
 	}
-	
-	private void handleInsertRowBefore(int index, Object data) {
+
+	private void handleInsertRowBefore(int index, Object data) throws IllegalStateException
+	{
 		if (getSectionsArray().size() > 0) {
 			if (index < 0) {
 				index = 0;
@@ -274,8 +281,7 @@ public class TableViewProxy extends TiViewProxy
 				TableViewRowProxy rowProxy = rowProxyFor(data);
 				rr.section.insertRowAt(rr.rowIndexInSection, rowProxy);
 			} else {
-				throw new IllegalStateException(
-					"Index out of range. Non-existant row at " + index);
+				throw new IllegalStateException("Index out of range. Non-existent row at " + index);
 			}
 		} else {
 			// Add first row.
@@ -287,16 +293,23 @@ public class TableViewProxy extends TiViewProxy
 	}
 
 	@Kroll.method
-	public void insertRowAfter(int index, Object data, @Kroll.argument(optional=true) KrollDict options) {
+	public void insertRowAfter(int index, Object data, @Kroll.argument(optional = true) KrollDict options)
+	{
 		if (TiApplication.isUIThread()) {
 			handleInsertRowAfter(index, data);
 			return;
 		}
 
-		TiMessenger.sendBlockingMainMessage(getMainHandler().obtainMessage(MSG_INSERT_ROW, INSERT_ROW_AFTER, index), data);
+		Object asyncResult = TiMessenger.sendBlockingMainMessage(
+			getMainHandler().obtainMessage(MSG_INSERT_ROW, INSERT_ROW_AFTER, index), data);
+
+		if (asyncResult instanceof IllegalStateException) {
+			throw (IllegalStateException) asyncResult;
+		}
 	}
 
-	private void handleInsertRowAfter(int index, Object data) {
+	private void handleInsertRowAfter(int index, Object data) throws IllegalStateException
+	{
 		RowResult rr = new RowResult();
 		if (locateIndex(index, rr)) {
 			// TODO check for section
@@ -305,8 +318,7 @@ public class TableViewProxy extends TiViewProxy
 			getTableView().setModelDirty();
 			updateView();
 		} else {
-			throw new IllegalStateException(
-				"Index out of range. Non-existant row at " + index);
+			throw new IllegalStateException("Index out of range. Non-existent row at " + index);
 		}
 	}
 
@@ -329,8 +341,8 @@ public class TableViewProxy extends TiViewProxy
 
 	/**
 	 * If the row does not carry section information, it will be added
-	 * to the currentSection.  If it does carry section information (i.e., a header), 
-	 * that section will be created and the row added to it.  Either way,
+	 * to the currentSection. If it does carry section information (i.e., a header),
+	 * that section will be created and the row added to it. Either way,
 	 * whichever section the row gets added to will be returned.
 	 */
 	private TableViewSectionProxy addRowToSection(TableViewRowProxy row, TableViewSectionProxy currentSection)
@@ -404,25 +416,28 @@ public class TableViewProxy extends TiViewProxy
 		}
 	}
 
-	private void handleSetData(Object[] data) {
+	private void handleSetData(Object[] data)
+	{
 		if (data != null) {
 			processData(data);
 			getTableView().setModelDirty();
 			updateView();
-		} 
+		}
 	}
-	
+
 	@Kroll.getProperty @Kroll.method
-	public Object[] getData() {
+	public Object[] getData()
+	{
 		ArrayList<TableViewSectionProxy> sections = getSectionsArray();
 		if (sections != null) {
 			return sections.toArray();
 		}
-		
+
 		return new Object[0];
 	}
 
-	private TableViewRowProxy rowProxyFor(Object row) {
+	private TableViewRowProxy rowProxyFor(Object row)
+	{
 		TableViewRowProxy rowProxy = null;
 		if (row instanceof TableViewRowProxy) {
 			rowProxy = (TableViewRowProxy) row;
@@ -446,7 +461,8 @@ public class TableViewProxy extends TiViewProxy
 		}
 
 		if (rowProxy == null) {
-			Log.e(LCAT, "unable to create table view row proxy for object, likely an error in the type of the object passed in...");
+			Log.e(LCAT,
+				"unable to create table view row proxy for object, likely an error in the type of the object passed in...");
 			return null;
 		}
 
@@ -454,7 +470,8 @@ public class TableViewProxy extends TiViewProxy
 		return rowProxy;
 	}
 
-	private boolean locateIndex(int index, RowResult rowResult) {
+	private boolean locateIndex(int index, RowResult rowResult)
+	{
 		boolean found = false;
 		int rowCount = 0;
 		int sectionIndex = 0;
@@ -478,7 +495,8 @@ public class TableViewProxy extends TiViewProxy
 		return found;
 	}
 
-	public void updateView() {
+	public void updateView()
+	{
 		if (TiApplication.isUIThread()) {
 			getTableView().updateView();
 			return;
@@ -488,23 +506,26 @@ public class TableViewProxy extends TiViewProxy
 	}
 
 	@Kroll.method
-	public void scrollToIndex(int index) {
+	public void scrollToIndex(int index)
+	{
 		Message message = getMainHandler().obtainMessage(MSG_SCROLL_TO_INDEX);
-		//Message msg = getUIHandler().obtainMessage(MSG_SCROLL_TO_INDEX);
+		// Message msg = getUIHandler().obtainMessage(MSG_SCROLL_TO_INDEX);
 		message.arg1 = index;
 		message.sendToTarget();
 	}
 
 	@Kroll.method
-	public void scrollToTop(int index) {
+	public void scrollToTop(int index)
+	{
 		Message message = getMainHandler().obtainMessage(MSG_SCROLL_TO_TOP);
-		//Message msg = getUIHandler().obtainMessage(MSG_SCROLL_TO_TOP);
+		// Message msg = getUIHandler().obtainMessage(MSG_SCROLL_TO_TOP);
 		message.arg1 = index;
 		message.sendToTarget();
 	}
 
 	@Override
-	public boolean handleMessage(Message msg) {
+	public boolean handleMessage(Message msg)
+	{
 		if (msg.what == MSG_UPDATE_VIEW) {
 			getTableView().updateView();
 			((AsyncResult) msg.obj).setResult(0);
@@ -520,12 +541,18 @@ public class TableViewProxy extends TiViewProxy
 			return true;
 		} else if (msg.what == MSG_INSERT_ROW) {
 			AsyncResult result = (AsyncResult) msg.obj;
-			if (msg.arg1 == INSERT_ROW_AFTER) {
-				handleInsertRowAfter(msg.arg2, result.getArg());
-			} else {
-				handleInsertRowBefore(msg.arg2, result.getArg());
+			try {
+				if (msg.arg1 == INSERT_ROW_AFTER) {
+					handleInsertRowAfter(msg.arg2, result.getArg());
+
+				} else {
+					handleInsertRowBefore(msg.arg2, result.getArg());
+				}
+				result.setResult(0);
+
+			} catch (IllegalStateException e) {
+				result.setResult(e);
 			}
-			result.setResult(0);
 			return true;
 		} else if (msg.what == MSG_APPEND_ROW) {
 			AsyncResult result = (AsyncResult) msg.obj;
@@ -533,7 +560,13 @@ public class TableViewProxy extends TiViewProxy
 			result.setResult(0);
 			return true;
 		} else if (msg.what == MSG_DELETE_ROW) {
-			handleDeleteRow(msg.arg1);
+			AsyncResult result = (AsyncResult) msg.obj;
+			try {
+				handleDeleteRow(msg.arg1);
+				result.setResult(0);
+			} catch (IllegalStateException e) {
+				result.setResult(e);
+			}
 			return true;
 		} else if (msg.what == MSG_SCROLL_TO_TOP) {
 			getTableView().scrollToTop(msg.arg1);
@@ -546,7 +579,8 @@ public class TableViewProxy extends TiViewProxy
 	// labels only send out click events when they are explicitly told to do so.
 	// we need to tell each label child to enable clicks when a click listener is added
 	@Override
-	public void eventListenerAdded(String eventName, int count, KrollProxy proxy) {
+	public void eventListenerAdded(String eventName, int count, KrollProxy proxy)
+	{
 		super.eventListenerAdded(eventName, count, proxy);
 		if (eventName.equals(TiC.EVENT_CLICK) && proxy == this) {
 			for (TableViewSectionProxy section : getSections()) {
@@ -556,9 +590,10 @@ public class TableViewProxy extends TiViewProxy
 			}
 		}
 	}
-	
+
 	@Override
-	public void eventListenerRemoved(String eventName, int count, KrollProxy proxy) {
+	public void eventListenerRemoved(String eventName, int count, KrollProxy proxy)
+	{
 		super.eventListenerRemoved(eventName, count, proxy);
 		if (eventName.equals(TiC.EVENT_CLICK) && count == 0 && proxy == this) {
 			for (TableViewSectionProxy section : getSections()) {
