9d3161e6c1c1ff80a55a0b0d454c510ec6d3b711
Opie Cyrus <dizzymonkey@gmail.com>
2011-11-18 03:04:11 -0600

    draft of 3rd party module implementation for rhino and partial v8

--- support/android/templates/App.java
+++ support/android/templates/App.java
@@ -6,6 +6,7 @@
 package ${config['appid']};
 
 import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.kroll.runtime.rhino.KrollBindings;
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.KrollModuleInfo;
 import org.appcelerator.kroll.KrollRuntime;
@@ -63,30 +64,19 @@ public final class ${config['classname']}Application extends TiApplication
 		${onAppCreate(module)} \
 		% endfor
 
-		% for module in custom_modules:
-		${onAppCreate(module)} \
-		% endfor
-	}
-
-	/*
-	@Override
-	protected void bootModules(TiContext context)
-	{
-		% for module in app_modules:
-		// ${module['api_name']} module
-		modules.add(new ${module['class_name']}(context));
-			% for child_module in module['external_child_modules']:
-		// ${module['api_name']}.${child_module['name']}
-		KrollModule.addExternalChildModule(${module['class_name']}.class, ${child_module['proxyClassName']}.class);
-			% endfor
-		% endfor
-
+/*
 		% if len(custom_modules) > 0:
 		// Custom modules
 		KrollModuleInfo moduleInfo;
 		% endif
+*/
+		% for  module in custom_modules:
+		${onAppCreate(module)} \
 
-		% for module in custom_modules:
+		KrollBindings.addExternalBinding("${module['class_name']}", ${module['class_name']}Prototype.class);
+		<% module_id = module['manifest'].moduleid %>
+		${module_id}.${module["manifest"].name}GeneratedBindings.init();
+/*
 		<% manifest = module['manifest'] %>
 		moduleInfo = new KrollModuleInfo(
 			"${manifest.name}", "${manifest.moduleid}", "${manifest.guid}", "${manifest.version}",
@@ -98,37 +88,7 @@ public final class ${config['classname']}Application extends TiApplication
 		% endif
 
 		KrollModule.addModuleInfo(moduleInfo);
+*/
 		% endfor
-
-		% if config['deploy_type'] != 'production':
-		org.appcelerator.titanium.TiVerify verify = new org.appcelerator.titanium.TiVerify(context.getActivity(), this);
-		verify.verify();
-		modules.add(new ti.modules.titanium.debug.DebugModule(context));
-		% endif
 	}
-
-	% if len(custom_modules) > 0:
-	@Override
-	public KrollModule requireModule(TiContext context, KrollModuleInfo info)
-	{
-		KrollModule module = super.requireModule(context, info);
-		if (module != null) {
-			return module;
-		}
-
-		String id = info.getId();
-		% for module in custom_modules:
-		if ("${module['manifest'].moduleid}".equals(id)) {
-			module = new ${module['class_name']}(context);
-		}
-		% endfor
-
-		if (module != null) {
-			modules.add(module);
-			return module;
-		}
-		return null;
-	}
-	% endif
-	*/
 }
