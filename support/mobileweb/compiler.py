21cf70865d073aaf190cbb52e13c352a6d2ec106
Chris Barber <chris@cb1inc.com>
2011-11-23 06:56:12 -0600

    [TIMOB-5859] [TIMOB-6013] [TIMOB-6014] [TIMOB-6015] Finished AMD support including plugin support. Implemented Ti.include as a AMD plugin.

--- support/mobileweb/compiler.py
+++ support/mobileweb/compiler.py
@@ -178,7 +178,8 @@ class Compiler(object):
 	appDescription: '${app_description | jsQuoteEscapeFilter}',\n\
 	appCopyright: '${app_copyright | jsQuoteEscapeFilter}',\n\
 	appGuid: '${app_guid | jsQuoteEscapeFilter}',\n\
-	tiVersion: '${ti_version | jsQuoteEscapeFilter}'\n\
+	tiVersion: '${ti_version | jsQuoteEscapeFilter}',\n\
+	baseUrl: './'\n\
 };\n".encode('utf-8') + self.load_api(os.path.join(src_dir,"loader.js"))
 		
 		titanium_js = HEADER + loader_js + titanium_js + FOOTER
