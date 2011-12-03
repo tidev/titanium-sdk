a29adb95ff9ba2e5c3a45156a46d16deaba0a9b2
Anthony Decena <anthony@mindelusions.com>
2011-12-02 22:40:25 -0800

    [TIMOB-6426] Added uric parser, Fixed canOpenURL() function

--- mobileweb/src/Ti.Platform/platform.js
+++ mobileweb/src/Ti.Platform/platform.js
@@ -113,6 +113,38 @@
 		set: function(val){return false;}
 	});
 
+
+	function _canOpenURL(str) {
+	    var o   = _canOpenURL.options,
+	        m   = o.parser[o.strictMode ? "strict" : "loose"].exec(str),
+	        uri = {},
+	        i   = 14;
+	
+	    while (i--) uri[o.key[i]] = m[i] || "";
+	
+	    uri[o.q.name] = {};
+	    uri[o.key[12]].replace(o.q.parser, function ($0, $1, $2) {
+	        if ($1) uri[o.q.name][$1] = $2;
+	    });
+	
+	    return uri;
+	};
+	
+	_canOpenURL.options = {
+	    strictMode: false,
+	    key: ["source","protocol","authority","userInfo","user","password","host","port","relative","path","directory","file","query","anchor"],
+	    q:   {
+	        name:   "queryKey",
+	        parser: /(?:^|&)([^&=]*)=?([^&]*)/g
+	    },
+	    parser: {
+	        strict: /^(?:([^:\/?#]+):)?(?:\/\/((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?))?((((?:[^?#\/]*\/)*)([^?#]*))(?:\?([^#]*))?(?:#(.*))?)/,
+	        loose:  /^(?:(?![^:@]+:[^:@\/]*@)([^:\/?#.]+):)?(?:\/\/)?((?:(([^:@]*)(?::([^:@]*))?)?@)?([^:\/?#]*)(?::(\d*))?)    (((\/(?:[^?#](?![^?#\/]*\.[^?#\/.]+(?:[?#]|$)))*\/?)?([^?#\/]*))(?:\?([^#]*))?(?:#(.*))?)/
+	    }
+	};
+	
+	_canOpenURL.options.strictMode = true;
+
 	// Methods
 	api.canOpenURL = function(url){
 		// Lots of stuff going on here. Basically we want to match any protocol urls ie: http, ftp, etc.
