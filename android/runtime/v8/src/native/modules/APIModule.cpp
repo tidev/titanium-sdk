050a1e3f7fca05ff49d34d96d259c59d4eb6e3d3
Joshua Roesslein <jroesslein@appcelerator.com>
2011-12-12 17:03:50 -0800

    [TIMOB-6567] Expose a way for the debugger to terminate an application.

--- android/runtime/v8/src/native/modules/APIModule.cpp
+++ android/runtime/v8/src/native/modules/APIModule.cpp
@@ -8,6 +8,8 @@
 #include <android/log.h>
 #include <v8.h>
 #include <string.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include "AndroidUtil.h"
 
@@ -48,6 +50,14 @@ void APIModule::Initialize(Handle<Object> target)
 	DEFINE_PROTOTYPE_METHOD(constructorTemplate, "critical", logCritical);
 	DEFINE_PROTOTYPE_METHOD(constructorTemplate, "fatal", logFatal);
 	DEFINE_PROTOTYPE_METHOD(constructorTemplate, "log", log);
+
+	// Expose a method for terminating the application for the debugger.
+	// Debugger will send an evaluation request calling this method
+	// when it wants the application to terminate immediately.
+	if (V8Runtime::debuggerEnabled) {
+		DEFINE_PROTOTYPE_METHOD(constructorTemplate, "terminate", terminate);
+	}
+
 	constructorTemplate->Inherit(KrollModule::proxyTemplate);
 
 	target->Set(String::NewSymbol("API"), constructorTemplate->GetFunction()->NewInstance());
@@ -180,6 +190,11 @@ Handle<Value> APIModule::log(const Arguments& args)
 	}
 
 	return Undefined();
+}
+
+Handle<Value> APIModule::terminate(const Arguments& args)
+{
+	kill(getpid(), 9);
 }
 
 void APIModule::Dispose()
