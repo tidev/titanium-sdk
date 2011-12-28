85a045d52f2a9c758a5ff047269d2de9ebfad4fd
Anthony Decena <anthony@mindelusions.com>
2011-12-28 11:06:05 -0800

    bare bones TextField

--- mobileweb/src/Ti/UI/TextField.js
+++ mobileweb/src/Ti/UI/TextField.js
@@ -1,8 +1,29 @@
-define("Ti/UI/TextField", ["Ti/_/declare", "Ti/_/UI/Widget"], function(declare, Widget) {
+define("Ti/UI/TextField", ["Ti/_/declare", "Ti/_/UI/Widget", "Ti/_/dom", "Ti/_/css", "Ti/_/style"], function(declare, Widget, dom, css, style) {
+
+	var set = style.set,
+        undef;
 
 	return declare("Ti.UI.TextField", Widget, {
 
-		//
+		constructor: function(args) {
+			this.textField = dom.create("input", {
+				className: css.clean("TiUITextFieldField"),
+			});
+			this.textField.type = "text";
+			this.domNode.appendChild(this.textField);			
+		},
+
+		properties: {
+            _defaultWidth: "auto",
+            _defaultHeight: "auto",
+			hintText: {
+				set: function(value) {
+					set(this.input, "placeholder", value);
+					return value;
+				}
+			}
+
+		}
 
 	});
 
