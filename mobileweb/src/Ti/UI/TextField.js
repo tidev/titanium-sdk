c5ca0009e7440cd571abe33cbd775938971c4bb8
Bryan Hughes <bhughes@appcelerator.com>
2011-12-28 13:10:52 -0800

    [TIMOB-6772] Stubbed out text field.

--- mobileweb/src/Ti/UI/TextField.js
+++ mobileweb/src/Ti/UI/TextField.js
@@ -9,20 +9,96 @@ define("Ti/UI/TextField", ["Ti/_/declare", "Ti/_/UI/Widget", "Ti/_/dom", "Ti/_/c
 			this.textField = dom.create("input", {
 				className: css.clean("TiUITextFieldField"),
 			});
-			this.textField.type = "text";
 			this.domNode.appendChild(this.textField);			
+		},
+		
+		blur: function() {
+			console.debug('Method "Titanium.UI.TextArea#.blur" is not implemented yet.');
+		},
+		
+		focus: function() {
+			console.debug('Method "Titanium.UI.TextArea#.focus" is not implemented yet.');
+		},
+		
+		hasText: function() {
+			console.debug('Method "Titanium.UI.TextArea#.hasText" is not implemented yet.');
 		},
 
 		properties: {
             _defaultWidth: "auto",
             _defaultHeight: "auto",
-			hintText: {
+			hintText: undef,
+			editable: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.TextField#.editable" is not implemented yet.');
+					return value;
+				},
 				set: function(value) {
-					this.textField.placeholder = value;
+					console.debug('Property "Titanium.UI.TextField#.editable" is not implemented yet.');
+					return value;
+				}
+			},
+			
+			paddingLeft: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.TextField#.paddingLeft" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.TextField#.paddingLeft" is not implemented yet.');
+					return value;
+				}
+			},
+			
+			paddingRight: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.TextField#.paddingRight" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.TextField#.paddingRight" is not implemented yet.');
+					return value;
+				}
+			},
+			
+			passwordMask: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.TextField#.passwordMask" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.TextField#.passwordMask" is not implemented yet.');
+					return value;
+				}
+			},
+			
+			textAlign: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.TextField#.textAlign" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.TextField#.textAlign" is not implemented yet.');
+					return value;
+				}
+			},
+			
+			verticalAlign: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.TextField#.verticalAlign" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.TextField#.verticalAlign" is not implemented yet.');
 					return value;
 				}
 			}
-
 		}
 
 	});
