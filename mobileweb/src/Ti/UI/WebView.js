261da2284b9207f50d9949acd7bed892f5973d98
Bryan Hughes <bhughes@appcelerator.com>
2011-12-28 11:45:11 -0800

    [TIMOB-6772] Stubbed out webview.

--- mobileweb/src/Ti/UI/WebView.js
+++ mobileweb/src/Ti/UI/WebView.js
@@ -1,8 +1,88 @@
 define("Ti/UI/WebView", ["Ti/_/declare", "Ti/_/UI/Widget"], function(declare, Widget) {
 
 	return declare("Ti.UI.WebView", Widget, {
-
-		//
+		
+		canGoBack: function(x,y) {
+			console.debug('Method "Titanium.UI.WebView#.canGoBack" is not implemented yet.');
+		},
+		
+		canGoForward: function(x,y) {
+			console.debug('Method "Titanium.UI.WebView#.canGoForward" is not implemented yet.');
+		},
+		
+		evalJS: function(x,y) {
+			console.debug('Method "Titanium.UI.WebView#.evalJS" is not implemented yet.');
+		},
+		
+		goBack: function(x,y) {
+			console.debug('Method "Titanium.UI.WebView#.goBack" is not implemented yet.');
+		},
+		
+		goForward: function(x,y) {
+			console.debug('Method "Titanium.UI.WebView#.goForward" is not implemented yet.');
+		},
+		
+		reload: function(x,y) {
+			console.debug('Method "Titanium.UI.WebView#.reload" is not implemented yet.');
+		},
+		
+		repaint: function(x,y) {
+			console.debug('Method "Titanium.UI.WebView#.repaint" is not implemented yet.');
+		},
+		
+		stopLoading: function(x,y) {
+			console.debug('Method "Titanium.UI.WebView#.stopLoading" is not implemented yet.');
+		},
+		
+		properties: {
+			data: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.WebView#.data" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.WebView#.data" is not implemented yet.');
+					return value;
+				}
+			},
+			
+			html: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.WebView#.html" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.WebView#.html" is not implemented yet.');
+					return value;
+				}
+			},
+			
+			loading: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.WebView#.loading" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.WebView#.loading" is not implemented yet.');
+					return value;
+				}
+			},
+			
+			url: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.WebView#.url" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.WebView#.url" is not implemented yet.');
+					return value;
+				}
+			}
+		}
 
 	});
 
