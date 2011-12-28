317d72016e59599a37f94accad36a992d013474b
Bryan Hughes <bhughes@appcelerator.com>
2011-12-28 13:02:51 -0800

    [TIMOB-6772] Cleaned up text area.

--- mobileweb/src/Ti/UI/TextArea.js
+++ mobileweb/src/Ti/UI/TextArea.js
@@ -9,27 +9,51 @@ define("Ti/UI/TextArea", ["Ti/_/declare", "Ti/_/UI/Widget", "Ti/_/dom", "Ti/_/cs
 			this.textArea = dom.create("textarea", {
 				className: css.clean("TiUITextAreaTextArea")
 			});
-			set(this.textArea, "width", "100%");
-			set(this.textArea, "height", "100%");
 			this.domNode.appendChild(this.textArea);
+		},
+		
+		blur: function() {
+			console.debug('Method "Titanium.UI.TextArea#.blur" is not implemented yet.');
+		},
+		
+		focus: function() {
+			console.debug('Method "Titanium.UI.TextArea#.focus" is not implemented yet.');
+		},
+		
+		hasText: function() {
+			console.debug('Method "Titanium.UI.TextArea#.hasText" is not implemented yet.');
 		},
 
 		properties: {
             _defaultWidth: "auto",
             _defaultHeight: "auto",
-			value: {
-				get: function() {return this.textArea.value;},
+			value: undef,
+			hintText: undef,
+			
+			editable: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.TextArea#.editable" is not implemented yet.');
+					return value;
+				},
 				set: function(value) {
-					this.textArea.value = value;
+					console.debug('Property "Titanium.UI.TextArea#.editable" is not implemented yet.');
 					return value;
 				}
 			},
-			hintText: {
+			
+			textAlign: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.TextArea#.textAlign" is not implemented yet.');
+					return value;
+				},
 				set: function(value) {
-					this.textArea.placeholder = value;
+					console.debug('Property "Titanium.UI.TextArea#.textAlign" is not implemented yet.');
 					return value;
 				}
-			}
+			},
+			
 		}
 	});
 
