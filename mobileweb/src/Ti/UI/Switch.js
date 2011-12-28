c8ec06657f3b52216cc1e8eb416da669e0204aae
Bryan Hughes <bhughes@appcelerator.com>
2011-12-28 15:40:04 -0800

    [TIMOB_6772] Stubbed out switch

--- mobileweb/src/Ti/UI/Switch.js
+++ mobileweb/src/Ti/UI/Switch.js
@@ -2,7 +2,58 @@ define("Ti/UI/Switch", ["Ti/_/declare", "Ti/_/UI/Widget"], function(declare, Wid
 
 	return declare("Ti.UI.Switch", Widget, {
 
-		//
+		properties: {
+			_defaultWidth: "auto",
+            _defaultHeight: "auto",
+            
+            title: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.Switch#.title" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.Switch#.title" is not implemented yet.');
+					return value;
+				}
+			},
+			
+            titleOff: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.Switch#.titleOff" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.Switch#.titleOff" is not implemented yet.');
+					return value;
+				}
+			},
+			
+            titleOn: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.Switch#.titleOn" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.Switch#.titleOn" is not implemented yet.');
+					return value;
+				}
+			},
+			
+            value: {
+				get: function(value) {
+					// TODO
+					console.debug('Property "Titanium.UI.Switch#.value" is not implemented yet.');
+					return value;
+				},
+				set: function(value) {
+					console.debug('Property "Titanium.UI.Switch#.value" is not implemented yet.');
+					return value;
+				}
+			}
+		}
 
 	});
 
