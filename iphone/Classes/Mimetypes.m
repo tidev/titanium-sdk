b1fd4023b3e78bfa1a25be9d44d8008db82e2da1
Vishal Duggal <vishalduggal@gmail.com>
2012-01-03 13:17:42 -0800

    [TIMOB-6915] Mimetype info now retreived from system

--- iphone/Classes/Mimetypes.m
+++ iphone/Classes/Mimetypes.m
@@ -6,62 +6,44 @@
  */
 
 #import "Mimetypes.h"
-
-const NSString * htmlMimeType = @"text/html";
-const NSString * textMimeType = @"text/plain";
-const NSString * jpegMimeType = @"image/jpeg";
+#import <MobileCoreServices/UTType.h>
 const NSString * svgMimeType = @"image/svg+xml";
 
-static NSDictionary * mimeTypeFromExtensionDict = nil;
 
 @implementation Mimetypes
 
-+ (void)initialize
-{
-	if (mimeTypeFromExtensionDict == nil)
-	{
-		mimeTypeFromExtensionDict = [[NSDictionary alloc] initWithObjectsAndKeys:
-									 @"image/png",@"png",@"image/gif",@"gif",
-									 jpegMimeType,@"jpg",jpegMimeType,@"jpeg",
-									 @"image/x-icon",@"ico",
-									 htmlMimeType,@"html",htmlMimeType,@"htm",
-									 textMimeType,@"text",textMimeType,@"txt",
-									 svgMimeType,@"svgz",svgMimeType,@"svg",
-									 @"text/json",@"json",
-									 @"text/javascript",@"js",
-									 @"text/x-javascript",@"js",
-									 @"application/x-javascript",@"js",
-									 @"text/css",@"css",
-									 @"text/xml",@"xml",
-									 @"audio/x-wav",@"wav",
-									 @"video/mpeg",@"mov",
-									 @"video/mpeg",@"m4v",
-									 nil];
-	}
-}
-
 + (NSString *)extensionForMimeType:(NSString *)mimetype
 {
-	[Mimetypes initialize];
-	for (NSString *key in mimeTypeFromExtensionDict)
-	{
-		NSString *value = [mimeTypeFromExtensionDict objectForKey:key];
-		if ([value isEqualToString:mimetype])
-		{
-			return key;
-		}
+	CFStringRef uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassMIMEType, (CFStringRef)mimetype, NULL);
+	CFStringRef extension = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassFilenameExtension);
+	
+	//Release the UTI
+	CFRelease(uti);
+	
+	if (extension == NULL) {
+		return @"bin";
 	}
-	return @"bin";
+	else {
+		return [(NSString*)extension autorelease];
+	}
+	
+	
 }
 
 + (NSString *)mimeTypeForExtension:(NSString *)ext
 {
-	[Mimetypes initialize];
-	NSString *result=[mimeTypeFromExtensionDict objectForKey:[ext pathExtension]];
-	if (result == nil){
-		result = @"application/octet-stream";
-	}
-	return result;
-}
+	CFStringRef uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (CFStringRef)[ext pathExtension], NULL);
+	CFStringRef mimetype = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType);
 
+	//Release the UTI
+	CFRelease(uti);
+	
+	if (mimetype == NULL) {
+		return @"application/octet-stream";
+	}
+	else {
+		return [(NSString*)mimetype autorelease];
+	}
+	
+}
 @end
