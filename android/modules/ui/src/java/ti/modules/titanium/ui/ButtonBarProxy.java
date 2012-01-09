def8ed1fe9aed670ce89d6578e344e4550f2725b
Hieu Pham <hpham@appcelerator.com>
2012-01-09 13:30:38 -0800

    timob-6870: undo excess changes

--- android/modules/ui/src/java/ti/modules/titanium/ui/ButtonBarProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/ButtonBarProxy.java
@@ -7,7 +7,6 @@
 package ti.modules.titanium.ui;
 
 import org.appcelerator.kroll.annotations.Kroll;
-import org.appcelerator.kroll.common.Log;
 import org.appcelerator.titanium.TiContext;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.view.TiUIView;
@@ -17,7 +16,6 @@ import android.app.Activity;
 /**
  * This class is @deprecated. Any usage will crash application.
  */
-@Deprecated
 @Kroll.proxy(creatableInModule=UIModule.class)
 public class ButtonBarProxy extends TiViewProxy
 {
@@ -34,7 +32,6 @@ public class ButtonBarProxy extends TiViewProxy
 	@Override
 	public TiUIView createView(Activity activity)
 	{
-		Log.w("ButtonBarProxy", "Ti.UI.createButtonBar() is deprecated starting 1.8.1 and will be removed in 1.9.0. Any usage of this will result in an application crash");
 		return null;
 	}
 
