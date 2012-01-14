63461cb3b3009925a51b3344861c29a3c9bbfd85
Hieu Pham <hpham@appcelerator.com>
2012-01-13 16:26:45 -0800

    timob-6870: fix imports

--- android/modules/ui/src/java/ti/modules/titanium/ui/ViewProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/ViewProxy.java
@@ -7,8 +7,6 @@
 package ti.modules.titanium.ui;
 
 import org.appcelerator.kroll.annotations.Kroll;
-import org.appcelerator.kroll.common.Log;
-import org.appcelerator.titanium.TiBaseActivity;
 import org.appcelerator.titanium.TiContext;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.view.TiUIView;
