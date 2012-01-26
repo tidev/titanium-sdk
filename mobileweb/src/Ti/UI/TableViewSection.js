3c7c63a9dcb1ee1efa55225a729a9df25e86a1e2
Bryan Hughes <bhughes@appcelerator.com>
2012-01-25 17:17:12 -0800

    [TIMOB-7221] Did some re-architecting to table view in order to properly support table view sections.

--- mobileweb/src/Ti/UI/TableViewSection.js
+++ mobileweb/src/Ti/UI/TableViewSection.js
@@ -1,76 +1,162 @@
-define("Ti/UI/TableViewSection", ["Ti/_/declare", "Ti/_/UI/Widget"], function(declare, Widget) {
+define("Ti/UI/TableViewSection", ["Ti/_/declare", "Ti/_/UI/Widget", "Ti/_/style","Ti/UI/MobileWeb/TableViewSeparatorStyle"], 
+	function(declare, Widget, style, TableViewSeparatorStyle) {
+	
+	var is = require.is,
+		isDef = require.isDef,
+		set = style.set;
 
 	return declare("Ti.UI.TableViewSection", Widget, {
+		
+		constructor: function(args) {
+			
+			this._indexedContent = [];
+			
+			// Use horizontal layouts so that the default location is always (0,0)
+			this.add(this._header = Ti.UI.createView({height: 'auto', layout: 'horizontal'}));
+			this.add(this._rows = Ti.UI.createView({height: 'auto', layout: 'vertical'}));
+			this.add(this._footer = Ti.UI.createView({height: 'auto', layout: 'horizontal'}));
+			
+			// Create the parts out of Ti controls so we can make use of the layout system
+			this.layout = 'vertical';
+		},
 
 		_defaultHeight: "auto",
-		_defaultWidth: "auto",
+		_defaultWidth: "100%",
 		
 		_handleTouchEvent: function(type, e) {
 			if (type === "click" || type === "singletap") {
 				this._parent && this._parent._parent && (this._parent._parent._tableViewSectionClicked = this);
 			}
-			View.prototype._handleTouchEvent.apply(this,arguments);
+			Widget.prototype._handleTouchEvent.apply(this,arguments);
 		},
+		
+		_tableView: null,
+		
+		_createSeparator: function() {
+			var showSeparator = this._tableView && this._tableView.separatorStyle === TableViewSeparatorStyle.SINGLE_LINE;
+			return Ti.UI.createView({
+				height: showSeparator ? 1 : 0,
+				width: "100%",
+				backgroundColor: showSeparator ? this._tableView.separatorColor : "transparent"
+			});
+		},
+		
+		_refreshRows: function() {
+			if (this._tableView) {
+				// Update the row information
+				var rows = this._rows.children,
+					tableView = this._tableView; 
+				for (var i = 1; i < rows.length; i += 2) {
+					var row = rows[i];
+					row._defaultHeight = tableView.rowHeight;
+					set(row.domNode,'minHeight',tableView.minRowHeight);
+					set(row.domNode,'maxHeight',tableView.maxRowHeight);
+				}
+				
+				for (var i = 0; i < rows.length; i += 2) {
+					var row = rows[i];
+					if (tableView.separatorStyle === TableViewSeparatorStyle.SINGLE_LINE) {
+						row.height = 1;
+						row.backgroundColor = tableView.separatorColor;
+					} else {
+						row.height = 0;
+						row.backgroundColor = "transparent";
+					}
+				}
+			}
+		},
+		
+		_insertHelper: function(value, index) {
+			if (!isDef(value.declaredClass) || value.declaredClass != "Ti.UI.TableViewRow") {
+				value = Ti.UI.createTableViewRow(value);
+			}
 			
+			this._rows._insertAt(value, 2 * index + 1);
+			this._rows._insertAt(this._createSeparator(), 2 * index + 2);
+			this.rowCount++;
+		},
+		
+		_insert: function(value, index) {
+			
+			var rows = this._rows.children,
+				rowCount = this.rowCount;
+			if (!isDef(index)) {
+				index = rowCount;
+			}
+			if (index < 0 || index > rowCount) {
+				return;
+			}
+			
+			if (rows.length === 0) {
+				this._rows.add(this._createSeparator());
+			}
+			
+			if (is(value,"Array")) {
+				for (var i in value) {
+					this._insertHelper(value[i],index++);
+				}
+			} else {
+				this._insertHelper(value,index);
+			}
+		},
+		
+		_remove: function(index) {
+			if (index < 0 || index > this.rowCount) {
+				return;
+			}	
+			this._rows.remove(this._rows.children[2 * index + 1]);
+			this._rows.remove(this._rows.children[2 * index + 1]);
+			
+			// Remove the last separator, if there are no rows left
+			if (this._rows.children.length === 1) {
+				this._rows.remove(this._rows.children[0]);
+			}
+		},
+					
 		properties: {
 			footerTitle: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.TableViewSection#.footerTitle" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.TableViewSection#.footerTitle" is not implemented yet.');
+				set: function(value, oldValue) {
+					if (oldValue != value) {
+						this._footerTitleControl && this._footer.remove(this._footerTitleControl);
+						this._footer.add(this._footerTitleControl = Ti.UI.createLabel({text: value}));
+						Ti.UI._doFullLayout();
+					}
 					return value;
 				}
 			},
-			
 			footerView: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.TableViewSection#.footerView" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.TableViewSection#.footerView" is not implemented yet.');
+				set: function(value, oldValue) {
+					if (oldValue != value) {
+						this._footerTitleControl && this._footer.remove(this._footerTitleControl);
+						this._footer.add(this._footerTitleControl = value);
+						Ti.UI._doFullLayout();
+					}
 					return value;
 				}
 			},
-			
 			headerTitle: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.TableViewSection#.headerTitle" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.TableViewSection#.headerTitle" is not implemented yet.');
+				set: function(value, oldValue) {
+					if (oldValue != value) {
+						this._headerTitleControl && this._header.remove(this._headerTitleControl);
+						this._header.add(this._headerTitleControl = Ti.UI.createLabel({text: value}));
+						Ti.UI._doFullLayout();
+					}
 					return value;
 				}
 			},
-			
 			headerView: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.TableViewSection#.headerView" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.TableViewSection#.headerView" is not implemented yet.');
+				set: function(value, oldValue) {
+					if (oldValue != value) {
+						this._headerTitleControl && this._header.remove(this._headerTitleControl);
+						this._header.add(this._headerTitleControl = value);
+						Ti.UI._doFullLayout();
+					}
 					return value;
 				}
 			},
 			
-			rowCount: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.TableViewSection#.rowCount" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.TableViewSection#.rowCount" is not implemented yet.');
-					return value;
-				}
+			rowCount: function(value) {
+				return Math.floor(this._rows.children.length / 2);
 			}
 		}
 
