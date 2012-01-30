4e2249131eb50b6bb5ced73ad83f2905a0b5f10c
Bryan Hughes <bhughes@appcelerator.com>
2012-01-30 15:11:52 -0800

    [TIMOB-7426] Base scrollbar implementation.

--- mobileweb/src/Ti/UI/View.js
+++ mobileweb/src/Ti/UI/View.js
@@ -1,6 +1,9 @@
 define("Ti/UI/View",
-	["Ti/_/declare", "Ti/_/dom", "Ti/_/UI/Element", "Ti/_/lang", "Ti/_/string", "Ti/_/Layouts"],
-	function(declare, dom, Element, lang, string, Layouts) {
+	["Ti/_/declare", "Ti/_/dom", "Ti/_/UI/Element", "Ti/_/lang", "Ti/_/string", "Ti/_/Layouts", "Ti/_/style"],
+	function(declare, dom, Element, lang, string, Layouts, style) {
+		
+	var unitize = dom.unitize,
+		set = style.set;
 
 	return declare("Ti.UI.View", Element, {
 
@@ -56,6 +59,121 @@ define("Ti/UI/View",
 				this.remove(children[0]);
 			}
 			this._triggerLayout();
+		},
+		
+		_getScrollableContentWidth: function() {
+			return 600;
+		},
+		
+		_getScrollablePosition: function() {
+			return {x: 0, y: 0};
+		},
+		
+		_createHorizontalScrollBar: function() {
+			var scrollBar = this._horizontalScrollBar = dom.create("div", {
+				className: "TiUIScrollBar",
+				style: {
+					position: 'absolute',
+					zIndex: 0x7FFFFFFF, // Max (32-bit) z-index
+					border: "3px solid lightGrey",
+					borderRadius: "3px",
+					height: "0px",
+					bottom: "0px",
+					opacity: 0
+				}
+			}, this.domNode);
+		},
+		
+		_createVerticalScrollBar: function() {
+			var scrollBar = this._verticalScrollBar = dom.create("div", {
+				className: "TiUIScrollBar",
+				style: {
+					position: 'absolute',
+					zIndex: 0x7FFFFFFF, // Max (32-bit) z-index
+					border: "3px solid lightGrey",
+					borderRadius: "3px",
+					width: "0px",
+					right: "0px",
+					opacity: 0
+				}
+			}, this.domNode);
+		},
+		
+		_startScrollBars: function(normalizedScrollPosition, visibleAreaRatio) {
+			
+			if (this._horizontalScrollBar) {
+				var startingX = normalizedScrollPosition.x,
+					measuredWidth = this._measuredWidth;
+				startingX < 0 && (startingX = 0);
+				startingX > 1 && (startingX = 1);
+				this._horizontalScrollBarWidth = (measuredWidth - 6) * visibleAreaRatio.x;
+				this._horizontalScrollBarWidth < 10 && (this._horizontalScrollBarWidth = 10);
+				set(this._horizontalScrollBar, {
+					opacity: 1,
+					left: unitize(startingX * (measuredWidth - this._horizontalScrollBarWidth - 6)),
+					width: unitize(this._horizontalScrollBarWidth)
+				});
+			}
+			
+			if (this._verticalScrollBar) {
+				var startingY = normalizedScrollPosition.y,
+					measuredHeight = this._measuredHeight;
+				startingY < 0 && (startingY = 0);
+				startingY > 1 && (startingY = 1);
+				this._verticalScrollBarHeight = (measuredHeight - 6) * visibleAreaRatio.y;
+				this._verticalScrollBarHeight < 10 && (this._verticalScrollBarHeight = 10);
+				set(this._verticalScrollBar, {
+					opacity: 1,
+					top: unitize(startingY * (measuredHeight - this._verticalScrollBarHeight - 6)),
+					height: unitize(this._verticalScrollBarHeight)
+				});
+			}
+		},
+		
+		_updateScrollBars: function(normalizedScrollPosition) {
+			if (this._horizontalScrollBar) {
+				var newX = normalizedScrollPosition.x,
+					measuredWidth = this._measuredWidth;
+				newX < 0 && (newX = 0);
+				newX > 1 && (newX = 1);
+				set(this._horizontalScrollBar,"left",unitize(newX * (measuredWidth - this._horizontalScrollBarWidth - 6)));
+			}
+			
+			if (this._verticalScrollBar) {
+				var newY = normalizedScrollPosition.y,
+					measuredHeight = this._measuredHeight;
+				newY < 0 && (newY = 0);
+				newY > 1 && (newY = 1);
+				set(this._verticalScrollBar,"top",unitize(newY * (measuredHeight - this._verticalScrollBarHeight - 6)));
+			}
+		},
+		
+		_endScrollBars: function() {
+			if (this._horizontalScrollBar) {
+				var horizontalScrollBar = this._horizontalScrollBar;
+				if (horizontalScrollBar) {
+					set(horizontalScrollBar,"transition","all 1s ease-in-out");
+					setTimeout(function(){
+						set(horizontalScrollBar,"opacity",0);
+						setTimeout(function(){
+							set(horizontalScrollBar,"transition","");
+						},1000);
+					},0);
+				}
+			}
+			
+			if (this._verticalScrollBar) {
+				var verticalScrollBar = this._verticalScrollBar;
+				if (verticalScrollBar) {
+					set(verticalScrollBar,"transition","all 1s ease-in-out");
+					setTimeout(function(){
+						set(verticalScrollBar,"opacity",0);
+						setTimeout(function(){
+							set(verticalScrollBar,"transition","");
+						},1000);
+					},0);
+				}
+			}
 		},
 
 		destroy: function() {
