3aac3548ead38192f93c51d139bd15396f3478d1
unknown <qa@.(none)>
2012-02-02 13:25:37 -0800

    TIMOB-6315: remove trailing \ in THIS_DIR variable

--- android/runtime/v8/ndk-build.cmd
+++ android/runtime/v8/ndk-build.cmd
@@ -9,8 +9,10 @@ if "%ANDROID_NDK%" == "" (echo "Error: The path to the Android NDK must be set i
 
 set THIS_DIR=%~dp0
 
+if %THIS_DIR:~-1%==\ set THIS_DIR=%THIS_DIR:~0,-1%
+
 if not "%NUM_CPUS%" == "" (set ARGS=-j %NUM_CPUS%)
 
 
-%ANDROID_NDK%\ndk-build.cmd NDK_APPLICATION_MK=%THIS_DIR%Application.mk NDK_PROJECT_PATH=%THIS_DIR% NDK_MODULE_PATH=%THIS_DIR%src\ndk-modules TI_DIST_DIR=C:\Users\qa\titanium\titanium_mobile\dist %ARGS% %*
+%ANDROID_NDK%\ndk-build.cmd NDK_APPLICATION_MK=%THIS_DIR%\Application.mk NDK_PROJECT_PATH=%THIS_DIR% NDK_MODULE_PATH=%THIS_DIR%\src\ndk-modules TI_DIST_DIR=C:\Users\qa\titanium\titanium_mobile\dist %ARGS% %*
 
