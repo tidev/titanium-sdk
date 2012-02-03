d2ffdb67e83e68e797a4918b1eaf812554c9d4b7
unknown <qa@.(none)>
2012-02-02 19:48:15 -0800

    TIMOB-6315: Removed unused generated dir rule.  Use space instead of blank on binding class name

--- android/runtime/v8/ndk-build.cmd
+++ android/runtime/v8/ndk-build.cmd
@@ -7,6 +7,7 @@
 @echo off
 if "%ANDROID_NDK%" == "" (echo "Error: The path to the Android NDK must be set in the ANDROID_NDK environment variable" & exit /b %ERRORLEVEL)
 
+set DIST_DIR=%~dp0..\..\..\dist
 set THIS_DIR=%~dp0
 
 if %THIS_DIR:~-1%==\ set THIS_DIR=%THIS_DIR:~0,-1%
@@ -14,5 +15,5 @@ if %THIS_DIR:~-1%==\ set THIS_DIR=%THIS_DIR:~0,-1%
 if not "%NUM_CPUS%" == "" (set ARGS=-j %NUM_CPUS%)
 
 
-%ANDROID_NDK%\ndk-build.cmd NDK_APPLICATION_MK=%THIS_DIR%\Application.mk NDK_PROJECT_PATH=%THIS_DIR% NDK_MODULE_PATH=%THIS_DIR%\src\ndk-modules TI_DIST_DIR=C:\Users\qa\titanium\titanium_mobile\dist %ARGS% %*
+%ANDROID_NDK%\ndk-build.cmd NDK_APPLICATION_MK=%THIS_DIR%\Application.mk NDK_PROJECT_PATH=%THIS_DIR% NDK_MODULE_PATH=%THIS_DIR%\src\ndk-modules TI_DIST_DIR=%DIST_DIR% %ARGS% %*
 
