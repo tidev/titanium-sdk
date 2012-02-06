0251869d91346c4bdae0de57f8493cb6979fac0f
Bryan Hughes <bhughes@appcelerator.com>
2012-02-06 12:05:30 -0800

    [TIMOB-7170] Alert dialog implementation.

--- mobileweb/src/Ti/UI/AlertDialog.js
+++ mobileweb/src/Ti/UI/AlertDialog.js
@@ -1,39 +1,100 @@
 define("Ti/UI/AlertDialog", ["Ti/_/declare", "Ti/_/Evented"], function(declare, Evented) {
 
+	var undef;
+
 	return declare("Ti.UI.AlertDialog", Evented, {
 		show: function() {
-			alert(this.message);
+			
+			// Create the window and a background to dim the current view
+			var alertWindow = this._alertWindow = Ti.UI.createWindow();
+			alertWindow.add(Ti.UI.createView({
+				backgroundColor: "black",
+				opacity: 0.5,
+				left: 0,
+				top: 0,
+				right: 0,
+				bottom: 0
+			}));
+			
+			// Create the alert dialog itself
+			var alertDialog = Ti.UI.createView({
+				width: "50%",
+				height: "auto",
+				backgroundColor: "white",
+				layout: "vertical",
+				borderRadius: 3
+			});
+			alertWindow.add(alertDialog);
+			
+			// Add the title
+			alertDialog.add(Ti.UI.createLabel({
+				text: this.title,
+				font: {fontWeight: "bold"},
+				left: 5,
+				right: 5,
+				top: 5,
+				height: "auto",
+				textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
+			}));
+			
+			// Add the message
+			alertDialog.add(Ti.UI.createLabel({
+				text: this.message,
+				left: 5,
+				right: 5,
+				top: 5,
+				height: "auto",
+				textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
+			}));
+			
+			var self = this;
+			function addButton(title, index, bottom) {
+				var button = Ti.UI.createButton({
+					left: 5,
+					right: 5,
+					top: 5,
+					bottom: bottom,
+					height: "auto",
+					title: title,
+					index: index
+				});
+				alertDialog.add(button);
+				button.addEventListener("singletap",function(){
+					alertWindow.close();
+					self._alertWindow = undef;
+					self.fireEvent("click",{
+						index: index,
+						cancel: self.cancel === index
+					});
+				});
+			}
+			
+			// Add the buttons
+			if (require.is(this.buttonNames,"Array")) {
+				var buttonNames = this.buttonNames,
+					i = 0;
+				for (; i < buttonNames.length; i++) {
+					addButton(buttonNames[i], i, i === buttonNames.length - 1 ? 5 : 0);
+				}
+			} else {
+				addButton(this.ok, 0, 5);
+			}
+			
+			// Show the alert dialog
+			alertWindow.open();
 		},
 
 		hide: function() {
-			// Do nothing since alert() is blocking.
+			if (this._alertWindow) {
+				this._alertWindow.close();
+			}
 		},
 		
 		properties: {
 			
-			buttonNames: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.AlertDialog#.buttonNames" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.AlertDialog#.buttonNames" is not implemented yet.');
-					return value;
-				}
-			},
+			buttonNames: undef,
 			
-			cancel: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.AlertDialog#.cancel" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.AlertDialog#.cancel" is not implemented yet.');
-					return value;
-				}
-			},
+			cancel: -1,
 			
 			message: "",
 			
@@ -49,17 +110,7 @@ define("Ti/UI/AlertDialog", ["Ti/_/declare", "Ti/_/Evented"], function(declare, 
 				}
 			},
 			
-			ok: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.AlertDialog#.ok" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.AlertDialog#.ok" is not implemented yet.');
-					return value;
-				}
-			},
+			ok: "OK",
 			
 			okid: {
 				get: function(value) {
@@ -73,17 +124,7 @@ define("Ti/UI/AlertDialog", ["Ti/_/declare", "Ti/_/Evented"], function(declare, 
 				}
 			},
 			
-			title: {
-				get: function(value) {
-					// TODO
-					console.debug('Property "Titanium.UI.AlertDialog#.title" is not implemented yet.');
-					return value;
-				},
-				set: function(value) {
-					console.debug('Property "Titanium.UI.AlertDialog#.title" is not implemented yet.');
-					return value;
-				}
-			},
+			title: "",
 			
 			titleid: {
 				get: function(value) {
