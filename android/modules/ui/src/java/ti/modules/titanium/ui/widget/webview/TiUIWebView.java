59ae0173f289f93d02112434b59413e02c1ca23d
billdawson <bill@billdawson.com>
2012-02-08 22:23:16 +0100

    TIMOB-7556 Enable dom storage in webview, required by some modern sites like Twitter mobile.

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/webview/TiUIWebView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/webview/TiUIWebView.java
@@ -92,6 +92,7 @@ public class TiUIWebView extends TiUIView
 		settings.setJavaScriptCanOpenWindowsAutomatically(true);
 		settings.setLoadsImagesAutomatically(true);
 		settings.setLightTouchEnabled(true);
+		settings.setDomStorageEnabled(true); // Required by some sites such as Twitter. Is on in our iOS WebView too.
 
 		// enable zoom controls by default
 		boolean enableZoom = true;
