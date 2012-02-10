1e47c28e8519df8d19cbb618f51cc4d81a5615b3
Allen Yeung <ayeung@aptana.com>
2012-02-10 15:37:26 -0800

    TIMOB-6873: Added comment

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/tableview/TiTableView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/tableview/TiTableView.java
@@ -507,6 +507,7 @@ public class TiTableView extends FrameLayout
 		// Layout is finished, re-enable focus events.
 		if (focusListener != null) {
 			focusedView.setOnFocusChangeListener(focusListener);
+			// If the configuration changed, we manually fire the blur event
 			if (changed) {
 				focusListener.onFocusChange(focusedView, false);
 			}
