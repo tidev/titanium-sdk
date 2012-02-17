aeea932a69849588002ef42144fafb83e8d13e21
Hidayet Dogan <hdogan@gmail.com>
2012-02-17 19:42:06 +0200

    [TIMOB-3223] iOS - showBookmark missing from searchBar

--- iphone/Classes/TiUISearchBar.m
+++ iphone/Classes/TiUISearchBar.m
@@ -112,6 +112,13 @@
 	[search setTranslucent:[TiUtils barTranslucencyForColor:newBarColor]];
 }
 
+-(void)setShowBookmark_:(id)value
+{
+	UISearchBar *search = [self searchBar];
+	[search setShowsBookmarkButton:[TiUtils boolValue:value]];
+	[search sizeToFit];
+}
+
 -(CALayer *)backgroundImageLayer
 {
 	if(backgroundLayer==nil)
@@ -201,7 +208,13 @@
 // called when bookmark button pressed
 - (void)searchBarBookmarkButtonClicked:(UISearchBar *)searchBar                   
 {	
-	//TODO: update to the new event model
+	NSString * text = [searchBar text];
+	[self.proxy replaceValue:text forKey:@"value" notification:NO];
+	
+	if ([self.proxy _hasListeners:@"bookmark"])
+	{
+		[self.proxy fireEvent:@"bookmark" withObject:[NSDictionary dictionaryWithObject:text forKey:@"value"]];
+	}
 	
 	if (delegate!=nil && [delegate respondsToSelector:@selector(searchBarBookmarkButtonClicked:)])
 	{
