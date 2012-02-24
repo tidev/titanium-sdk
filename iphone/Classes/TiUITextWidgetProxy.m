8079fa43dfcf23e926128b1e941a2c4ff4e76093
Stephen Tramer <stramer@appcelerator.com>
2012-02-24 13:14:43 -0800

    [TIMOB-6463] - Protect/unprotect keyboardToolbarItems - Numerous TODOs added regarding memory management (outside scope of this PR)

--- iphone/Classes/TiUITextWidgetProxy.m
+++ iphone/Classes/TiUITextWidgetProxy.m
@@ -142,15 +142,39 @@ DEFINE_DEF_BOOL_PROP(suppressReturn,YES);
 
 -(void)setKeyboardToolbar:(id)value
 {
+    // TODO: The entire codebase needs to be evaluated for the following:
+    //
+    // - Any property setter which potentially takes an array of proxies MUST ALWAYS have its
+    // content evaluated to protect them. This is INCREDIBLY CRITICAL and almost certainly a major
+    // source of memory bugs in Titanium iOS!!!
+    //
+    // - Any property setter which is active on the main thread only MAY NOT protect their object
+    // correctly or in time (see the comment in -[KrollObject noteKeylessKrollObject:]).
+    //
+    // This may have to be done as part of TIMOB-6292 (convert KrollContext to serialized GCD)
+    
+    if ([value isKindOfClass:[NSArray class]]) {
+        for (id item in value) {
+            ENSURE_TYPE(item, TiProxy);
+            [self rememberProxy:item];
+        }
+    }
+    
 	//Because views aren't lock-protected, ANY and all references, even checking if non-nil, should be done in the main thread.
+    
+    // TODO: ENSURE_UI_THREAD needs to be deprecated in favor of more effective and concicse mechanisms
+    // which use the main thread only when necessary to reduce latency.
 	ENSURE_UI_THREAD_1_ARG(value);
 	[self replaceValue:value forKey:@"keyboardToolbar" notification:YES];
-
+    
 	if (value == nil)
 	{
 //TODO: Should we remove these gracefully?
 		[keyboardTiView removeFromSuperview];
 		[keyboardUIToolbar removeFromSuperview];
+        for (TiProxy* proxy in keyboardToolbarItems) {
+            [self forgetProxy:proxy];
+        }
 		RELEASE_TO_NIL(keyboardTiView);
 		RELEASE_TO_NIL(keyboardToolbarItems);
 		[keyboardUIToolbar setItems:nil];
@@ -163,12 +187,16 @@ DEFINE_DEF_BOOL_PROP(suppressReturn,YES);
 		[keyboardTiView removeFromSuperview];
 		RELEASE_TO_NIL(keyboardTiView);
 
-//TODO: Make sure these are actually proxies.
+        // TODO: Check for proxies
 		[keyboardToolbarItems autorelease];
+        for (TiProxy* proxy in keyboardToolbarItems) {
+            [self forgetProxy:proxy];
+        }
+        
 		keyboardToolbarItems = [value copy];
-		if(keyboardUIToolbar != nil){
+		if(keyboardUIToolbar != nil) {
 			[self updateUIToolbar];
-		}
+		}        
 //TODO: If we have focus while this happens, we need to signal an update.
 		return;
 	}
@@ -183,6 +211,9 @@ DEFINE_DEF_BOOL_PROP(suppressReturn,YES);
 		[keyboardTiView removeFromSuperview];
 		[keyboardUIToolbar removeFromSuperview];
 		RELEASE_TO_NIL(keyboardTiView);
+        for (TiProxy* proxy in keyboardToolbarItems) {
+            [self forgetProxy:proxy];
+        }
 		RELEASE_TO_NIL(keyboardToolbarItems);
 		[keyboardUIToolbar setItems:nil];
 	
