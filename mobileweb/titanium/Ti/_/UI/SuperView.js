e0f59cf53e0f0d8c9065a687bdbbb6371e118d63
Bryan Hughes <bhughes@appcelerator.com>
2012-03-08 00:26:12 -0800

    [TIMOB-7566] Fleshed out the details of navigation.

--- mobileweb/titanium/Ti/_/UI/SuperView.js
+++ mobileweb/titanium/Ti/_/UI/SuperView.js
@@ -9,46 +9,63 @@ define(["Ti/_/declare", "Ti/_/dom", "Ti/_/lang", "Ti/UI", "Ti/UI/View"], functio
 		POP_STATE_REWINDING_HISTORY = -4,
 		POP_STATE_RESETTING = -5,
 		historyPopState = POP_STATE_WAITING_FOR_OPERATION,
-		prefix = location.href;
+		prefix = location.href,
+		widgetToClose;
 	
 	function pushToHistory(widget) {
 		historyPopState = POP_STATE_PUSHING;
 		historyStack.push(widget);
-		location.href = prefix + "#" + (widget.title || widget.widgetId);
+		location.href = prefix + "#" + widget.widgetId;
 	}
 	
-	function removeFromHistory(widget) {
-		if (historyStack.length > 0 && historyPopState === POP_STATE_WAITING_FOR_OPERATION) {
+	function removeFromHistory(widget, recursive) {
+		if (historyPopState === POP_STATE_WAITING_FOR_OPERATION) {
 			var historyStackIndex = historyStack.indexOf(widget);
 			if (historyStackIndex !== -1) {
-				historyStack.splice(historyStackIndex,historyStack.length - historyStackIndex);
+				historyStack.splice(historyStackIndex,recursive ? historyStack.length - historyStackIndex : 1);
 				historyPopState = POP_STATE_REWINDING_HISTORY;
 				hist.go(-historyStack.length - 1);
 			}
 		}
 	}
 	
-	window.addEventListener("popstate", function(e) {
+	window.addEventListener("hashchange", function(e) {
 		switch(historyPopState) {
 			case POP_STATE_WAITING_FOR_OPERATION: 
 				// We need to undo the previous operation and redo it
 				var listItem = window.location.href.split("#")[1];
+				if (prefix === window.location.href) {
+					listItem = historyStack[0].widgetId;
+				}
 				if (listItem) {
+					var historyStackIndex = -1,
+						historyStackLength = historyStack.length,
+						widget;
 					historyPopState = POP_STATE_UNDOING_OPERATION;
-					hist.forward();
+					for (var i = 0; i < historyStackLength; i++) {
+						widget = historyStack[i];
+						if (listItem === widget.widgetId) {
+							historyStackIndex = i + 1;
+							break;
+						}
+					}
+					for (i = historyStackIndex; i < historyStackLength; i++) {
+						historyStack[i].close();
+					}
+					widgetToClose = historyStack[historyStackIndex];
+					hist.go(historyStackLength - historyStackIndex);
 				}
 				break;
 			case POP_STATE_UNDOING_OPERATION:
-				var widget = historyStack[historyStack.length - 1];
 				historyPopState = POP_STATE_WAITING_FOR_OPERATION;
-				widget.close();
+				removeFromHistory(widgetToClose, true);
 				break;
 			case POP_STATE_PUSHING: 
 				historyPopState = POP_STATE_WAITING_FOR_OPERATION;
 				break;
 			case POP_STATE_REWINDING_HISTORY:
 				historyPopState = POP_STATE_RESETTING;
-				location.href = prefix + "#_history_reset";
+				location.href = prefix + "#history_reset";
 				break;
 			case POP_STATE_RESETTING: 
 				historyPopState = 0;
@@ -57,7 +74,7 @@ define(["Ti/_/declare", "Ti/_/dom", "Ti/_/lang", "Ti/UI", "Ti/UI/View"], functio
 			default: 
 				if (historyPopState < historyStack.length) {
 					var widget = historyStack[historyPopState++];
-					location.href = prefix + "#" + (widget.title || widget.widgetId);
+					location.href = prefix + "#" + widget.widgetId;
 				} else {
 					historyPopState = POP_STATE_WAITING_FOR_OPERATION;
 				}
@@ -95,7 +112,9 @@ define(["Ti/_/declare", "Ti/_/dom", "Ti/_/lang", "Ti/UI", "Ti/UI/View"], functio
 				this._opened = 0;
 				UI._removeWindow(this);
 				
-				removeFromHistory(this);
+				if (historyPopState === POP_STATE_WAITING_FOR_OPERATION) {
+					removeFromHistory(this);
+				}
 				
 				this.fireEvent("close");
 			}
