16099b68f47e23bf38a3ad331b126fc3c0ab8c1b
Allen Yeung <ayeung@aptana.com>
2012-03-07 16:58:50 -0800

    TIMOB-1891: Android: Transparent view with border color & width properties results in completely opaque view

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -429,6 +429,7 @@ public abstract class TiUIView
 			boolean hasImage = hasImage(d);
 			boolean hasColorState = hasColorState(d);
 			boolean hasBorder = hasBorder(d);
+			boolean nativeViewNull = (nativeView == null);
 
 			boolean requiresCustomBackground = hasImage || hasColorState || hasBorder;
 
@@ -441,7 +442,7 @@ public abstract class TiUIView
 
 				if (d.containsKeyAndNotNull(TiC.PROPERTY_BACKGROUND_COLOR)) {
 					Integer bgColor = TiConvert.toColor(d, TiC.PROPERTY_BACKGROUND_COLOR);
-					if (nativeView != null) {
+					if (!nativeViewNull) {
 						nativeView.setBackgroundColor(bgColor);
 						nativeView.postInvalidate();
 					}
@@ -449,7 +450,7 @@ public abstract class TiUIView
 					if (key.equals(TiC.PROPERTY_OPACITY)) {
 						setOpacity(TiConvert.toFloat(newValue));
 					}
-					if (nativeView != null) {
+					if (!nativeViewNull) {
 						nativeView.setBackgroundDrawable(null);
 						nativeView.postInvalidate();
 					}
@@ -465,10 +466,7 @@ public abstract class TiUIView
 				if (!hasColorState) {
 					if (d.get(TiC.PROPERTY_BACKGROUND_COLOR) != null) {
 						bgColor = TiConvert.toColor(d, TiC.PROPERTY_BACKGROUND_COLOR);
-						if (newBackground
-							|| (key.equals(TiC.PROPERTY_OPACITY)
-							|| key.equals(TiC.PROPERTY_BACKGROUND_COLOR)))
-						{
+						if (newBackground || (key.equals(TiC.PROPERTY_OPACITY) || key.equals(TiC.PROPERTY_BACKGROUND_COLOR))) {
 							background.setBackgroundColor(bgColor);
 						}
 					}
@@ -487,9 +485,15 @@ public abstract class TiUIView
 						handleBorderProperty(key, newValue);
 					}
 				}
+
 				applyCustomBackground();
+
+				if (key.equals(TiC.PROPERTY_OPACITY)) {
+					setOpacity(TiConvert.toFloat(newValue));
+				}
+
 			}
-			if (nativeView != null) {
+			if (!nativeViewNull) {
 				nativeView.postInvalidate();
 			}
 		} else if (key.equals(TiC.PROPERTY_SOFT_KEYBOARD_ON_FOCUS)) {
@@ -542,10 +546,6 @@ public abstract class TiUIView
 			bgColor = TiConvert.toColor(d, TiC.PROPERTY_BACKGROUND_COLOR);
 			nativeView.setBackgroundColor(bgColor);
 		}
-		if (d.containsKey(TiC.PROPERTY_OPACITY) && !nativeViewNull) {
-			setOpacity(TiConvert.toFloat(d, TiC.PROPERTY_OPACITY));
-			
-		}
 		
 		if (d.containsKey(TiC.PROPERTY_VISIBLE) && !nativeViewNull) {
 			nativeView.setVisibility(TiConvert.toBoolean(d, TiC.PROPERTY_VISIBLE) ? View.VISIBLE : View.INVISIBLE);
@@ -567,6 +567,11 @@ public abstract class TiUIView
 		}
 
 		initializeBorder(d, bgColor);
+
+		if (d.containsKey(TiC.PROPERTY_OPACITY) && !nativeViewNull) {
+			setOpacity(TiConvert.toFloat(d, TiC.PROPERTY_OPACITY));
+
+		}
 
 		if (d.containsKey(TiC.PROPERTY_TRANSFORM)) {
 			Ti2DMatrix matrix = (Ti2DMatrix) d.get(TiC.PROPERTY_TRANSFORM);
