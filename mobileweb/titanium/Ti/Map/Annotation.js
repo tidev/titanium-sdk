38b51e6ffb2c939e989d9279830a65470554d593
Chris Barber <chris@cb1inc.com>
2012-03-19 05:00:41 -0700

    [TIMOB-5698] Finished zooming, bounds, and routes in Ti.Map.View. About halfway done with annnotations.

--- mobileweb/titanium/Ti/Map/Annotation.js
+++ mobileweb/titanium/Ti/Map/Annotation.js
@@ -1,43 +1,39 @@
 define(["Ti/_/declare", "Ti/_/Evented", "Ti/Locale"], function(declare, Evented, Locale) {
 
-	var undef;
-
 	return declare("Ti.Map.Annotation", Evented, {
 
-		onclick: function(mapview, idx, src) {
+		_onclick: function(mapview, idx, src) {
 			this.fireEvent("click", {
-				index: idx,
-				title: this.title,
-				map: mapview,
+				annotation: this,
 				clicksource: src,
-				annotation: this
+				index: idx,
+				map: mapview,
+				title: this.title
 			});
+		},
+
+		_getTitle: function() {
+			return Locale._getString(this.titleid, this.title);
+		},
+
+		_getSubtitle: function() {
+			return Locale._getString(this.subtitleid, this.subtitle);
 		},
 
 		properties: {
 			animate: false,
-			image: undef,		// string or Titanium.Blob
-			latitude: undef,	// number
-			longitude: undef,	// number
-			leftButton: undef,	// number or string
-			leftView: undef,	// Ti.UI.View
-			pincolor: undef,	// ANNOTATION_RED or ANNOTATION_PURPLE or ANNOTATION_GREEN
-			rightButton: undef,	// number or string
-			rightView: undef,	// Ti.UI.View
-			subtitle: undef,	// string
-			subtitleid: {
-				set: function(value) {
-					this.subtitle = Locale.getString(value);
-					return value;
-				}
-			},
-			title: undef,
-			titleid: {
-				set: function(value) {
-					this.title = Locale.getString(value);
-					return value;
-				}
-			}
+			image: void 0,			// string or Titanium.Blob
+			latitude: void 0,		// number
+			longitude: void 0,		// number
+			leftButton: void 0,		// number or string
+			leftView: void 0,		// Ti.UI.View
+			pincolor: void 0,		// ANNOTATION_RED or ANNOTATION_PURPLE or ANNOTATION_GREEN
+			rightButton: void 0,	// number or string
+			rightView: void 0,		// Ti.UI.View
+			subtitle: void 0,		// string
+			subtitleid: void 0,
+			title: void 0,
+			titleid: void 0
 		}
 
 	});
--- mobileweb/titanium/Ti/_/Map/Google.js
+++ mobileweb/titanium/Ti/_/Map/Google.js
@@ -1,7 +1,7 @@
 define(["Ti/_/declare", "Ti/_/lang", "Ti/App/Properties", "Ti/Map", "Ti/UI/View"], function(declare, lang, Properties, Map, View) {
 
 	function mapType(type) {
-		var t = google.maps.MapTypeId;
+		var t = gmaps.MapTypeId;
 		switch (type) {
 			case Map.HYBRID_TYPE: return t.HYBRID;
 			case Map.SATELLITE_TYPE: return t.SATELLITE;
@@ -11,29 +11,92 @@ define(["Ti/_/declare", "Ti/_/lang", "Ti/App/Properties", "Ti/Map", "Ti/UI/View"
 	};
 
 	var isDef = lang.isDef,
+		mix = require.mix,
+		defaultRegion = {
+			latitude: 39.828175,
+			longitude: -98.5795,
+			latitudeDelta: 30.137412,
+			longitudeDelta: 63.235658
+		},
+		gmaps,
+		activeInfoWindow,
+		// the order of the markers MUST match the ANNOTATION_* constants defined in Ti.Map
+		markers = ["red", "green", "purple"],
+		onload = Ti.deferStart(),
 		MapView = declare("Ti.Map.View", View, {
 
 			constructor: function() {
 				this.properties.annotations = [];
 				this._routes = [];
-				this.properties.__values__.region = {
-					longitude: -98.5795,
-					latitude: 39.828175,
-					longitudeDelta: 0,
-					latitudeDelta: 0
-				};
+				this.fireEvent("loading");
 			},
 
 			postscript: function() {
-				var m = google.maps,
-					opts = {
-						zoom: 2,
+				var region = this.region,
+					gmap = this._gmap = new gmaps.Map(this.domNode, {
 						disableDefaultUI: true,
+						zoom: 2,
 						zoomControl: true,
-						center: new m.LatLng(this.region.latitude, this.region.longitude),
+						center: new gmaps.LatLng(region.latitude, region.longitude),
 						mapTypeId: mapType(this.mapType)
-					};
-				this._map = new m.Map(this.domNode, opts);
+					});
+
+				gmaps.event.addListener(gmap, "bounds_changed", lang.hitch(this, "_fitRegion"));
+				this._updateMap(region, 1);
+
+				this.annotations.forEach(function(a, i) {
+					var marker = markers[a.pincolor | 0],
+						title = a._getTitle(),
+						subtitle = a._getSubtitle(),
+						onInfoWindowClick = lang.hitch(this, function() {
+							a._onclick(this, i, "pin");
+						}),
+						opts = {
+							map: gmap,
+							icon: marker[0],
+							shadow: marker[1],
+							position: new gmaps.LatLng(a.latitude, a.longitude),
+							title: title
+						};
+
+					a.animate && (opts.animation = gmaps.Animation.DROP);
+					marker = a.marker = new gmaps.Marker(opts);
+
+					gmaps.event.addListener(marker, "click", lang.hitch(this, function() {
+						if (activeInfoWindow) {
+							activeInfoWindow.close();
+							onInfoWindowClick();
+						}
+
+						(activeInfoWindow = new gmaps.InfoWindow({
+							// image: void 0,			// string or Titanium.Blob
+							// leftButton: void 0,		// number or string
+							// leftView: void 0,		// Ti.UI.View
+							// rightButton: void 0,	// number or string
+							// rightView: void 0,		// Ti.UI.View
+							content: "<p><strong>" + title + "</strong></p><p>" + subtitle + "</p>"
+						})).open(gmap, marker);
+
+						gmaps.event.addListener(activeInfoWindow, "closeclick", onInfoWindowClick);
+
+						onInfoWindowClick();
+
+						this.fireEvent("click", {
+							annotation: a,
+							clicksource: "pin",
+							index: i,
+							latitude: a.latitude,
+							longitude: a.longitude,
+							map: this,
+							subtitle: subtitle,
+							title: title
+						});
+					}));
+
+					this.properties.__values__.annotations[i] = a;
+				}, this);
+
+				// TODO: userLocation???
 			},
 
 			addAnnotation: function(/*Object|Ti.Map.Annotation*/annotation) {
@@ -49,14 +112,17 @@ define(["Ti/_/declare", "Ti/_/lang", "Ti/App/Properties", "Ti/Map", "Ti/UI/View"
 			},
 
 			addRoute: function(/*Object*/route) {
-				this._routes.push(route);
-				/*
-				TODO: add the route
-				color 	String 					Color to use when drawing the route.
-				name 	String 					Route name.
-				points 	Array<MapPointType> 	Array of map points making up the route. {longitude,latitude}
-				width 	Number 					Line width to use when drawing the route. 
-				*/
+				if (route && (route.points || []).length) {
+					route.pline = new gmaps.Polyline({
+						map: this._gmap,
+						path: route.points.map(function(p) {
+							return new gmaps.LatLng(p.latitude, p.longitude);
+						}),
+						strokeColor: route.color || "#000",
+						strokeWeight: route.width || 1
+					});
+					this._routes.push(route);
+				}
 			},
 
 			deselectAnnotation: function(/*String|Ti.Map.Annotation*/annotation) {
@@ -95,7 +161,7 @@ define(["Ti/_/declare", "Ti/_/lang", "Ti/App/Properties", "Ti/Map", "Ti/UI/View"
 						i = 0;
 					for (; i < r.length; i++) {
 						if (r[i].name === route.name) {
-							// TODO: remove the route
+							route.pline.setMap(null);
 							r.splice(i--, 1);
 						}
 					}
@@ -113,10 +179,12 @@ define(["Ti/_/declare", "Ti/_/lang", "Ti/App/Properties", "Ti/Map", "Ti/UI/View"
 				isDef(location.animate) && (this.animated = location.animate);
 				isDef(location.animated) && (this.animated = location.animated);
 				isDef(location.regionFit) && (this.regionFit = location.regionFit);
+				this._updateMap(location);
 			},
 
 			zoom: function(level) {
-				// TODO: Relative zoom level (positive to zoom in, negative to zoom out).
+				var gmap = this._gmap;
+				gmap.setZoom(gmap.getZoom() + level);
 			},
 
 			_getAnnotationTitle: function(/*String|Ti.Map.Annotation*/a) {
@@ -136,6 +204,46 @@ define(["Ti/_/declare", "Ti/_/lang", "Ti/App/Properties", "Ti/Map", "Ti/UI/View"
 				return null;
 			},
 
+			_fitRegion: function() {
+				var c = this.constants,
+					gmap = this._gmap,
+					center = gmap.getCenter(),
+					bounds = gmap.getBounds(),
+					ne = bounds.getNorthEast(),
+					sw = bounds.getSouthWest(),
+					latD = c.latitudeDelta = ne.lat() - sw.lat(),
+					lngD = c.longitudeDelta = ne.lng() - sw.lng(),
+					region = {
+						latitude: center.lat(),
+						longitude: center.lng(),
+						latitudeDelta: latD,
+						longitudeDelta: lngD
+					};
+
+				this.regionFit && (this.properties.__values__.region = region);
+
+				if (!this._initialized) {
+					this._initialized = 1;
+					this.fireEvent("complete");
+				}
+
+				this.fireEvent("regionChanged", region);
+			},
+
+			_updateMap: function(region, dontAnimate) {
+				var gmap = this._gmap;
+				if (gmap) {
+					var animated = !dontAnimate && this.animated,
+						latD = region.latitudeDelta / 2,
+						lngD = region.longitudeDelta / 2;
+					gmap[animated ? "panTo" : "setCenter"](new gmaps.LatLng(region.latitude, region.longitude));
+					gmap[animated ? "panToBounds" : "fitBounds"](new gmaps.LatLngBounds(
+						new gmaps.LatLng(region.latitude - latD, region.longitude - lngD),
+						new gmaps.LatLng(region.latitude + latD, region.longitude + lngD)
+					));
+				}
+			},
+
 			constants: {
 				latitudeDelta: 0,
 				longitudeDelta: 0
@@ -145,19 +253,25 @@ define(["Ti/_/declare", "Ti/_/lang", "Ti/App/Properties", "Ti/Map", "Ti/UI/View"
 				animated: false,
 				annotations: {
 					set: function(value) {
-						return value.filter(function(a) { return a && a.declaredClass === "Ti.Map.Annotation"; });
+						value = value.filter(function(a) { return a && a.declaredClass === "Ti.Map.Annotation"; });
+						// TODO: update annotations
+						return value;
 					}
 				},
 				mapType: {
 					set: function(value) {
-						this._map && this._map.setMapTypeId(mapType(value));
+						this._gmap && this._gmap.setMapTypeId(mapType(value));
 						return value;
 					}
 				},
 				region: {
-					set: function(value, oldValue) {
-						return require.mix(oldValue, value);
-					}
+					set: function(newValue, oldValue) {
+						return mix({}, defaultRegion, oldValue, newValue);
+					},
+					post: function(value) {
+						this._updateMap(value);
+					},
+					value: null
 				},
 				regionFit: true,
 				userLocation: false
@@ -165,7 +279,22 @@ define(["Ti/_/declare", "Ti/_/lang", "Ti/App/Properties", "Ti/Map", "Ti/UI/View"
 
 		});
 
-	window.TiMapViewInit = Ti.deferStart();
+	window.TiMapViewInit = function() {
+		gmaps = google.maps;
+
+		var prefix = "/themes/" + require.config.ti.theme + "/Map/marker_",
+			point = gmaps.Point;
+
+		function makeMarker(color, x1, x2) {
+			return new gmaps.MarkerImage(prefix + color + ".png", new gmaps.Size(x1, 34), new point(x2, 0), new point(10, 34));
+		}
+
+		markers = markers.map(function(color) {
+			return [makeMarker(color, 20, 0), makeMarker(color, 37, 20)];
+		});
+
+		onload();
+	};
 
 	require(["http://maps.googleapis.com/maps/api/js?key=" + Properties.getString("ti.map.apikey", "") + "&sensor=false&callback=TiMapViewInit"]);
 
