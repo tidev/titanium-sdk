9fc42585ad519df681bb6d1f24f962a425ff637e
Vishal Duggal <vishalduggal@gmail.com>
2012-04-05 14:52:57 -0700

    Bug Fix

--- iphone/Classes/TiUITableView.m
+++ iphone/Classes/TiUITableView.m
@@ -1334,7 +1334,7 @@
         }
         else if (sender == footerViewProxy) {
             UIView *footerView = [[self tableView] tableFooterView];
-            [footerView setFrame:footerView bounds]];
+            [footerView setFrame:[footerView bounds]];
             [[self tableView] setTableFooterView:footerView];
         }
     },NO);
