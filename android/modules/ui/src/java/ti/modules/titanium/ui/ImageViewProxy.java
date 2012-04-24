7cdce2d7cbdbdcc4af8b3caa634ff16865356c39
Hieu Pham <hpham@appcelerator.com>
2012-04-24 15:43:21 -0700

    timob-8656: changes based on feedback

--- android/modules/ui/src/java/ti/modules/titanium/ui/ImageViewProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/ImageViewProxy.java
@@ -119,7 +119,8 @@ public class ImageViewProxy extends ViewProxy
 	}
 	
 	@Kroll.getProperty @Kroll.method
-	public boolean getPaused() {
+	public boolean getPaused() 
+	{
 		return getImageView().isPaused();
 	}
 	
