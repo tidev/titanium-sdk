1fb02d7a13b31cc7d6317a20c07aace6e275721b
Bryan Hughes <bhughes@appcelerator.com>
2012-04-24 17:07:49 -0700

    [TIMOB-8275] Bug fix in tableview.

--- mobileweb/titanium/Ti/UI/TableView.js
+++ mobileweb/titanium/Ti/UI/TableView.js
@@ -166,28 +166,31 @@ define(["Ti/_/declare", "Ti/UI/View", "Ti/_/style", "Ti/_/lang","Ti/UI/MobileWeb
 				sections = this._sections.children,
 				row = this._tableViewRowClicked,
 				section = this._tableViewSectionClicked;
-
-			if (row && section) {
-				if (regexpClickTap.test(type)) {
-					for (; i < sections.length; i += 2) {
-						localIndex = sections[i]._rows.children.indexOf(row);
-						if (localIndex !== -1) {
-							index += Math.floor(localIndex / 2);
-							break;
-						} else {
-							index += sections[i].rowCount;
+			if (type === "click" || type === "singletap") {
+				if (row && section) {
+					if (regexpClickTap.test(type)) {
+						for (; i < sections.length; i += 2) {
+							localIndex = sections[i]._rows.children.indexOf(row);
+							if (localIndex !== -1) {
+								index += Math.floor(localIndex / 2);
+								break;
+							} else {
+								index += sections[i].rowCount;
+							}
 						}
+						e.row = e.rowData = row;
+						e.index = index;
+						e.section = section;
+						e.searchMode = false; 
 					}
-					e.row = e.rowData = row;
-					e.index = index;
-					e.section = section;
-					e.searchMode = false; 
+	
+					View.prototype._handleTouchEvent.apply(this, arguments);
+	
+					this._tableViewRowClicked = null;
+					this._tableViewSectionClicked = null;
 				}
-
+			} else {
 				View.prototype._handleTouchEvent.apply(this, arguments);
-
-				this._tableViewRowClicked = null;
-				this._tableViewSectionClicked = null;
 			}
 		},
 
