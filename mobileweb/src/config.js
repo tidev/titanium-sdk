24cfd74a72c69b22a33cc472580ab3ce79c579b2
Bryan Hughes <bhughes@appcelerator.com>
2012-05-03 10:19:08 -0700

    [TIMOB-8885] Code beefed up.

--- mobileweb/src/config.js
+++ mobileweb/src/config.js
@@ -34,7 +34,7 @@ var require = {
 		"opera": typeof opera === "undefined" || opera.toString() != "[object Opera]",
 		"ti-analytics-use-xhr": ${has_analytics_use_xhr | jsQuoteEscapeFilter},
 		"ti-show-errors": ${has_show_errors | jsQuoteEscapeFilter},
-		"ti-instrumentation": ${has_instrumentation | jsQuoteEscapeFilter}
+		"ti-instrumentation": ${has_instrumentation | jsQuoteEscapeFilter} && g.instrumentation
 	},
 	locales: ${locales},
 	packages: ${packages},
