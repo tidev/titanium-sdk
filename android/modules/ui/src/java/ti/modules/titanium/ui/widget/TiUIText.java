125dbb613560a672687e545a196e06a8ebd5f080
Hieu Pham <hpham@appcelerator.com>
2012-05-11 15:50:32 -0700

    timob-9098: fix comment

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
@@ -431,7 +431,7 @@ public class TiUIText extends TiUIView
 		if (passwordMask) {
 			tv.setTransformationMethod(PasswordTransformationMethod.getInstance());
 			textTypeAndClass |= InputType.TYPE_TEXT_VARIATION_PASSWORD;
-			//turn off text UI in landscape mode in 4.0 b/c Android numeric passwords show in landscape.
+			//turn off text UI in landscape mode in 4.0 b/c Android numeric passwords are not masked correctly in landscape mode.
 			if (Build.VERSION.SDK_INT == TiC.API_LEVEL_ICE_CREAM_SANDWICH && (type == KEYBOARD_NUMBERS_PUNCTUATION || 
 					type == KEYBOARD_DECIMAL_PAD || type == KEYBOARD_NUMBER_PAD)) {
 				tv.setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI);
