ab3640ea2b5d1ff0485ec2e6e6c6bee8566b3449
srahim <srahim@appcelerator.com>
2012-05-14 13:24:02 -0700

    [Timob-9122]Placing the missing android check.

--- drillbit/tests/filesystem/filesystem.js
+++ drillbit/tests/filesystem/filesystem.js
@@ -462,17 +462,19 @@ describe("Ti.Filesystem tests", {
 		}
 	},
 	filesInApplicationCacheDirectoryExists: function() {
-		var newDirectory = Ti.Filesystem.getFile(Ti.Filesystem.applicationCacheDirectory, 'newDir');
-		newDirectory.createDirectory();
-
-		var newFile = Ti.Filesystem.getFile(newDirectory.getNativePath(), 'this-file-exists.js');
-		newFile.write("testing a file");
-
-		var appDataFileDoesNotExist = Ti.Filesystem.getFile(newDirectory.getNativePath(), 'this-file-does-not-exist.js');
-
-		valueOf(newDirectory.isDirectory()).shouldBeTrue();
-		valueOf(newDirectory.exists()).shouldBeTrue();
-		valueOf(newFile.exists()).shouldBeTrue();
-		valueOf(appDataFileDoesNotExist.exists()).shouldBeFalse();
+		if(Ti.Platform.osname === 'android') {
+			var newDirectory = Ti.Filesystem.getFile(Ti.Filesystem.applicationCacheDirectory, 'newDir');
+			newDirectory.createDirectory();
+		
+			var newFile = Ti.Filesystem.getFile(newDirectory.getNativePath(), 'this-file-exists.js');
+			newFile.write("testing a file");
+		
+			var appDataFileDoesNotExist = Ti.Filesystem.getFile(newDirectory.getNativePath(), 'this-file-does-not-exist.js');
+		
+			valueOf(newDirectory.isDirectory()).shouldBeTrue();
+			valueOf(newDirectory.exists()).shouldBeTrue();
+			valueOf(newFile.exists()).shouldBeTrue();
+			valueOf(appDataFileDoesNotExist.exists()).shouldBeFalse();
+		}
 	}
 });
