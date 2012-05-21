a809a8f8b345c0368886af51e18acbce8dff8a3e
billdawson <bill@billdawson.com>
2012-05-21 23:56:42 +0200

    TIMOB-4581 Calculate Titanium module dependencies used by packaged CommonJS modules and include the dependencies when app is built.

--- support/android/builder.py
+++ support/android/builder.py
@@ -1894,7 +1894,7 @@ class Builder(object):
 								self.project_gen_dir,
 								self.project_dir, 
 								include_all_modules=include_all_ti_modules)
-			compiler.compile(compile_bytecode=self.compile_js)
+			compiler.compile(compile_bytecode=self.compile_js, external_modules=self.modules)
 			self.compiled_files = compiler.compiled_files
 			self.android_jars = compiler.jar_libraries
 			self.merge_internal_module_resources()
