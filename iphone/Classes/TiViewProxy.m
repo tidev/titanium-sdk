53aba02fac7390b2d546bfd3f0f7de1a5d89ed8b
Vishal Duggal <vishalduggal@gmail.com>
2012-05-31 15:00:50 -0700

    [TIMOB-7773] Ensure viewAttached before propagating state

--- iphone/Classes/TiViewProxy.m
+++ iphone/Classes/TiViewProxy.m
@@ -1164,27 +1164,35 @@ LAYOUTPROPERTIES_SETTER(setMinHeight,minimumHeight,TiFixedValueRuleFromObject,[s
 
 -(void)parentWillAppear:(id)args
 {
-    pthread_rwlock_rdlock(&childrenLock);
-    [children makeObjectsPerformSelector:@selector(parentWillAppear:) withObject:args];
-    pthread_rwlock_unlock(&childrenLock);
+    if ([self viewAttached]) {
+        pthread_rwlock_rdlock(&childrenLock);
+        [children makeObjectsPerformSelector:@selector(parentWillAppear:) withObject:args];
+        pthread_rwlock_unlock(&childrenLock);
+    }
 }
 -(void)parentDidAppear:(id)args
 {
-    pthread_rwlock_rdlock(&childrenLock);
-    [children makeObjectsPerformSelector:@selector(parentDidAppear:) withObject:args];
-    pthread_rwlock_unlock(&childrenLock);
+    if ([self viewAttached]) {
+        pthread_rwlock_rdlock(&childrenLock);
+        [children makeObjectsPerformSelector:@selector(parentDidAppear:) withObject:args];
+        pthread_rwlock_unlock(&childrenLock);
+    }
 }
 -(void)parentWillDisappear:(id)args
 {
-    pthread_rwlock_rdlock(&childrenLock);
-    [children makeObjectsPerformSelector:@selector(parentWillDisappear:) withObject:args];
-    pthread_rwlock_unlock(&childrenLock);
+    if ([self viewAttached]) {
+        pthread_rwlock_rdlock(&childrenLock);
+        [children makeObjectsPerformSelector:@selector(parentWillDisappear:) withObject:args];
+        pthread_rwlock_unlock(&childrenLock);
+    }
 }
 -(void)parentDidDisappear:(id)args
 {
-    pthread_rwlock_rdlock(&childrenLock);
-    [children makeObjectsPerformSelector:@selector(parentDidDisappear:) withObject:args];
-    pthread_rwlock_unlock(&childrenLock);
+    if ([self viewAttached]) {
+        pthread_rwlock_rdlock(&childrenLock);
+        [children makeObjectsPerformSelector:@selector(parentDidDisappear:) withObject:args];
+        pthread_rwlock_unlock(&childrenLock);
+    }
 }
 
 
