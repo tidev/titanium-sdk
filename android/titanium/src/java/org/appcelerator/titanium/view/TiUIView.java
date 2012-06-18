9550c460c89164b957d5c610f74fd8c7c5bbe6cb
Joshua Roesslein <jroesslein@appcelerator.com>
2012-06-18 11:33:44 -0700

    Revert View focus/blur behavior to the way it was before.

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -673,9 +673,9 @@ public abstract class TiUIView
 	{
 		if (hasFocus) {
 			TiUIHelper.requestSoftInputChange(proxy, v);
-			proxy.fireEvent(TiC.EVENT_FOCUS, getFocusEventObject(hasFocus), false);
+			proxy.fireEvent(TiC.EVENT_FOCUS, getFocusEventObject(hasFocus));
 		} else {
-			proxy.fireEvent(TiC.EVENT_BLUR, getFocusEventObject(hasFocus), false);
+			proxy.fireEvent(TiC.EVENT_BLUR, getFocusEventObject(hasFocus));
 		}
 	}
 
