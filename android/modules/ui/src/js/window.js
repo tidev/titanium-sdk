b56dcb4b723da680d3e6a19ab9e3f896cbf1fb4a
Allen Yeung <ayeung@aptana.com>
2012-06-19 15:39:30 -0700

    TIMOB-9100: Android: When trying to remove the contents of a window, the window doesn't have any children

--- android/modules/ui/src/js/window.js
+++ android/modules/ui/src/js/window.js
@@ -183,6 +183,11 @@ exports.bootstrapWindow = function(Titanium) {
 		}
 	}
 	
+	var childrenGetter = function() {
+		return this._children;
+	}
+
+	Window.prototype.getChildren = childrenGetter;
 
 	Window.prototype.open = function(options) {
 		var self = this;
@@ -263,6 +268,7 @@ exports.bootstrapWindow = function(Titanium) {
 			this.fireEvent("open");
 
 		}
+
 	}
 
 	Window.prototype.setWindow = function(existingWindow) {
@@ -526,6 +532,8 @@ exports.bootstrapWindow = function(Titanium) {
 		window.on('addedToTab', function () {
 			rememberWindowAndAddCloseListener(self);
 		});
+
+		Object.defineProperty(window, "children", { get: childrenGetter});
 
 		return window;
 		
