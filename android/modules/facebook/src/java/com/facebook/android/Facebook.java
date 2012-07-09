089b985e2ec99008fbfd5dfdb62959ee90fb6d55
Ping Wang <pwang@appcelerator.com>
2012-07-09 15:20:45 -0700

    timob-8339: address comment

--- android/modules/facebook/src/java/com/facebook/android/Facebook.java
+++ android/modules/facebook/src/java/com/facebook/android/Facebook.java
@@ -18,7 +18,7 @@
  * MODIFICATIONS
  * 
  * Appcelerator Titanium Mobile
- * Copyright (c) 2012 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2011-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
