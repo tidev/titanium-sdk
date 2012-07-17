5bf485bf75a0e54fb09007148f299bd697a67d62
Allen Yeung <ayeung@appelerator.com>
2012-07-16 20:47:53 -0700

    Added comment

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/webview/polling.js
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/webview/polling.js
@@ -1,6 +1,7 @@
 function checkForJSCode() {
 	var code = TiApp.getJSCode();
 	if ( code != undefined) {
+		// Force this to be a string, since this does not evaluate in the 2.2 emulator correctly.
 		eval(code + "");
 	} else {
 		clearInterval(refreshIntervalId);
