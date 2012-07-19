ef8d7d4c6b6422d11331bf5b689d4da3209fc368
billdawson <bill@billdawson.com>
2012-07-20 01:38:36 +0200

    TIMOB-10042 fix re-layout of animated views so they can get touch events at their new location.
    Make sure alpha animations are reversible.
    
    Remove duplicative code and set animation duration in the right place.
    
    Make sure re-layout of animated view only occurs if the animation was not, itself, auto-reversed.
    
    Fix small bug in TiMenuSupport (NPE).
    
    From TiUIView, remove the caching of animated-to locations since we don't need them anymore because we're changing the view's layout params post-animation.

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -97,9 +97,6 @@ public abstract class TiUIView
 	// Same for rotation animation.
 	private float animatedRotationDegrees = 0f; // i.e., no rotation.
 
-	// Same for translate animation.
-	private Pair<Integer, Integer> animatedXYTranslationValues = Pair.create(Integer.valueOf(0), Integer.valueOf(0));
-
 	private KrollDict lastUpEvent = new KrollDict(2);
 	// In the case of heavy-weight windows, the "nativeView" is null,
 	// so this holds a reference to the view which is used for touching,
@@ -1388,31 +1385,12 @@ public abstract class TiUIView
 	}
 
 	/**
-	 * Retrieve the saved translate animation x & y deltas, which we store here since Android provides no property
-	 * for looking them up.
-	 */
-	public Pair<Integer, Integer> getAnimatedXYTranslationValues()
-	{
-		return animatedXYTranslationValues;
-	}
-
-	/**
-	 * Store the translate animation x and y delta values
-	 * since Android provides no property for looking them up.
-	 */
-	public void setAnimatedXYTranslationValues(Pair<Integer, Integer> newValues)
-	{
-		animatedXYTranslationValues = newValues;
-	}
-
-	/**
 	 * "Forget" the values we save after scale and rotation animations.
 	 */
 	private void resetPostAnimationValues()
 	{
 		animatedRotationDegrees = 0f; // i.e., no rotation.
 		animatedScaleValues = Pair.create(Float.valueOf(1f), Float.valueOf(1f)); // 1 means no scaling
-		animatedXYTranslationValues = Pair.create(Integer.valueOf(0), Integer.valueOf(0));
 	}
 
 }
