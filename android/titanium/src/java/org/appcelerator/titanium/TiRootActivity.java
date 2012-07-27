56822cbcfc54fdd96456bfd49c22b6922c191675
billdawson <bill@billdawson.com>
2012-07-27 18:36:59 +0200

    TIMOB-9285 Refactor common code between TiLaunchActivity and TiRootActivity
    Conflicts:
    	android/titanium/src/java/org/appcelerator/titanium/TiLaunchActivity.java
    	android/titanium/src/java/org/appcelerator/titanium/TiRootActivity.java

--- android/titanium/src/java/org/appcelerator/titanium/TiRootActivity.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiRootActivity.java
@@ -21,6 +21,7 @@ public class TiRootActivity extends TiLaunchActivity
 {
 	private static final String LCAT = "TiRootActivity";
 	private static final boolean DBG = TiConfig.LOGD;
+	private boolean finishing = false;
 
 	@Override
 	public String getUrl()
@@ -31,31 +32,16 @@ public class TiRootActivity extends TiLaunchActivity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-		TiApplication tiApp = getTiApp();
-
-		// If not the task root, then this "launch" activity instance is
-		// likely a manifestation of Android bug 2373, and we give
-		// developers an option to simply finish it off right away so that
-		// the running activity stack remains unchanged.
-		if (!isTaskRoot()) {
-			TiProperties sysProperties = tiApp.getSystemProperties();
-			if (sysProperties != null && sysProperties.getBool("ti.android.bug2373.finishfalseroot", false)) {
-				finishing2373 = true;
-				activityOnCreate(savedInstanceState);
-				finish();
-				return;
-			}
+		if (willFinishFalseRootActivity(savedInstanceState)) {
+			return;
 		}
-
 
 		if (checkInvalidLaunch(savedInstanceState)) {
 			// Android bug 2373 detected and we're going to restart.
 			return;
 		}
 
-		if (checkInvalidKindleFireRelaunch(savedInstanceState)) {
-			return;
-		}
+		TiApplication tiApp = getTiApp();
 
 		if (tiApp.isRestartPending() || TiBaseActivity.isUnsupportedReLaunch(this, savedInstanceState)) {
 			super.onCreate(savedInstanceState); // Will take care of scheduling restart and finishing.
@@ -113,6 +99,7 @@ public class TiRootActivity extends TiLaunchActivity
 	protected void onDestroy()
 	{
 		super.onDestroy();
+
 		if (finishing2373) {
 			return;
 		}
@@ -123,4 +110,21 @@ public class TiRootActivity extends TiLaunchActivity
 		TiFastDev.onDestroy();
 	}
 
+	@Override
+	public void finish()
+	{
+		if (finishing2373) {
+			super.finish();
+			return;
+		}
+
+		// Ensure we only run the finish logic once. We want to avoid an infinite loop since this method can be called
+		// from the finish method inside TiBaseActivity ( which can be triggered by terminateActivityStack() )
+		if (!finishing) {
+			finishing = true;
+			TiApplication.removeFromActivityStack(this);
+	//		TiApplication.terminateActivityStack(); Not in 2_0_X
+			super.finish();
+		}
+	}
 }
