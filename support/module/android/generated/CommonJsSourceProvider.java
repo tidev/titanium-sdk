bf18dd0ec774e2a87c53ce1f6cf6b367f8c0f08b
billdawson <bill@billdawson.com>
2012-07-31 03:20:33 +0200

    TIMOB-9730 Extend KrollSourceCodeProvider class to allow asking for specific module file by name rather than just one monolithic module file.

--- support/module/android/generated/CommonJsSourceProvider.java
+++ support/module/android/generated/CommonJsSourceProvider.java
@@ -7,14 +7,25 @@
  * Warning: This file is GENERATED, and should not be modified
  */
 package ${moduleid};
+
+import java.util.ArrayList;
+
 import org.appcelerator.kroll.common.KrollSourceCodeProvider;
 
 public class CommonJsSourceProvider implements KrollSourceCodeProvider
 {
 	public String getSourceCode()
 	{
-		AssetCryptImpl source = new AssetCryptImpl();
-		return source.readAsset("${moduleid}.js");
+		return getSourceCode("${moduleid}");
 	}
 
+	public String getSourceCode(String module)
+	{
+		if (module.equals("${moduleid}/${moduleid}")) {
+			module = "${moduleid}";
+		}
+
+		AssetCryptImpl source = new AssetCryptImpl();
+		return source.readAsset(module + ".js");
+	}
 }
