73ad00208c066310e086c29e2e88643c0007b46d
Allen Yeung <ayeung@appelerator.com>
2012-07-30 18:34:11 -0700

    TIMOB-9994: Added postlayout events for various controls/wigets

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
@@ -100,6 +100,14 @@ public class TiUIText extends TiUIView
 			}
 			return true;
 		}
+
+		@Override
+		protected void onLayout(boolean changed, int left, int top, int right, int bottom)
+		{
+			super.onLayout(changed, left, top, right, bottom);
+			TiUIHelper.firePostLayoutEvent(proxy);
+		}
+
 	}
 
 	public TiUIText(TiViewProxy proxy, boolean field)
