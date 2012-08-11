8ec9d12149940ee4687aefc40e97ecad492799bb
Max Stepanov <mstepanov@aptana.com>
2012-08-10 22:06:44 -0700

    ASI namespace

--- iphone/iphone/ThirdpartyNS.h
+++ iphone/iphone/ThirdpartyNS.h
@@ -82,4 +82,71 @@
 #define Reachability __TI_NS_SYMBOL(Reachability)
 #endif
 
+// ASINetworkQueue
+#ifndef ASINetworkQueue
+#define ASINetworkQueue __TI_NS_SYMBOL(ASINetworkQueue)
+#endif
+
+// ASIInputStream
+#ifndef ASIInputStream
+#define ASIInputStream __TI_NS_SYMBOL(ASIInputStream)
+#endif
+
+// ASIHTTPRequest
+#ifndef ASIHTTPRequest
+#define ASIHTTPRequest __TI_NS_SYMBOL(ASIHTTPRequest)
+#endif
+#ifndef ASIHTTPRequestVersion
+#define ASIHTTPRequestVersion __TI_NS_SYMBOL(ASIHTTPRequestVersion)
+#endif
+#ifndef NetworkRequestErrorDomain
+#define NetworkRequestErrorDomain __TI_NS_SYMBOL(NetworkRequestErrorDomain)
+#endif
+#ifndef ASIWWANBandwidthThrottleAmount
+#define ASIWWANBandwidthThrottleAmount __TI_NS_SYMBOL(ASIWWANBandwidthThrottleAmount)
+#endif
+
+// ASIFormDataRequest
+#ifndef ASIFormDataRequest
+#define ASIFormDataRequest __TI_NS_SYMBOL(ASIFormDataRequest)
+#endif
+
+// ASIAuthenticationDialog
+#ifndef ASIAutorotatingViewController
+#define ASIAutorotatingViewController __TI_NS_SYMBOL(ASIAutorotatingViewController)
+#endif
+#ifndef ASIAuthenticationDialog
+#define ASIAuthenticationDialog __TI_NS_SYMBOL(ASIAuthenticationDialog)
+#endif
+
+// ASIProgressDelegate
+#ifndef ASIProgressDelegate
+#define ASIProgressDelegate __TI_NS_SYMBOL(ASIProgressDelegate)
+#endif
+
+// ASIHTTPRequestDelegate
+#ifndef ASIHTTPRequestDelegate
+#define ASIHTTPRequestDelegate __TI_NS_SYMBOL(ASIHTTPRequestDelegate)
+#endif
+
+// ASIDownloadCache
+#ifndef ASIDownloadCache
+#define ASIDownloadCache __TI_NS_SYMBOL(ASIDownloadCache)
+#endif
+
+// ASICacheDelegate
+#ifndef ASICacheDelegate
+#define ASICacheDelegate __TI_NS_SYMBOL(ASICacheDelegate)
+#endif
+
+// ASIDataDecompressor
+#ifndef ASIDataDecompressor
+#define ASIDataDecompressor __TI_NS_SYMBOL(ASIDataDecompressor)
+#endif
+
+// ASIDataCompressor
+#ifndef ASIDataCompressor
+#define ASIDataCompressor __TI_NS_SYMBOL(ASIDataCompressor)
+#endif
+
 #endif
