749e2a0e34869e8e19aeea8f73f51a516f2c07d7
Martin Guillon <martin.guillon@gmail.com>
2012-08-14 10:43:41 +0200

    more work on integrating PersistentHTTPClient

--- iphone/Classes/TiNetworkPersistentHTTPClientProxy.m
+++ iphone/Classes/TiNetworkPersistentHTTPClientProxy.m
@@ -1,13 +1,25 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-2012 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
+ * 
+ * WARNING: This is generated code. Modify at your own risk and without support.
  */
 
 #ifdef USE_TI_NETWORK
 
+#import "TiNetworkHTTPClientResultProxy.h"
 #import "TiNetworkPersistentHTTPClientProxy.h"
+ @implementation TiNetworkPersistentHTTPClientProxy
+
+-(id)init
+{
+	if (self = [super init])
+	{
+	}
+	return self;
+}
 
 -(void)setResponseHandlersFroRequest:(ASIFormDataRequest*)request
 {
