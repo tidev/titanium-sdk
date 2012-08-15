c0455cb9a66c88908d370e25e7f4579efd70e57b
Joshua Roesslein <jroesslein@appcelerator.com>
2012-08-15 11:23:47 -0700

    Trim out more code not needed in this class.

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUITabHostGroup.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUITabHostGroup.java
@@ -6,25 +6,13 @@
  */
 package ti.modules.titanium.ui.widget.tabgroup;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.appcelerator.kroll.common.Log;
-import org.appcelerator.kroll.common.TiConfig;
 import org.appcelerator.titanium.TiApplication;
-import org.appcelerator.titanium.TiC;
-import org.appcelerator.titanium.proxy.TiBaseWindowProxy;
-import org.appcelerator.titanium.proxy.TiViewProxy;
-import org.appcelerator.titanium.util.TiConvert;
 
-import ti.modules.titanium.ui.R;
 import ti.modules.titanium.ui.TabGroupProxy;
 import ti.modules.titanium.ui.TabProxy;
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
@@ -49,62 +37,9 @@ public class TiUITabHostGroup extends TiUIAbstractTabGroup implements OnTabChang
 	{
 		super(proxy, activity);
 
-		activity.setContentView(R.layout.titanium_tabgroup);
-
 		tabHost = (TabHost) activity.findViewById(android.R.id.tabhost);
-
-		/* TODO(josh): remove?
-		// Set to GONE to overcome a NullPointerException
-		// deep in Android code in pre api 8.  See Android issue
-		// 2772.
-		tabHost.setVisibility(View.GONE);
-		tabHost.clearAllTabs();
-		*/
-
 		tabHost.setOnTabChangedListener(this);
 		tabHost.setup();
-
-		setNativeView(tabHost);
-	}
-
-	// TODO(josh): yuck! Figure out this mess. Looks like a hack to get tab click events.
-	protected void registerTouchForTabGroup(final View touchable, final TabProxy tabProxy)
-	{
-		if (touchable == null) {
-			return;
-		}
-		
-		registerTouchEvents(touchable);
-
-		final int tabCount = tabHost.getTabWidget().getTabCount();
-		
-		boolean clickable = touchable.isClickable();
-		if (!clickable) {
-			touchable.setOnClickListener(null); // This will set clickable to true in the view, so make sure it stays here so the next line turns it off.
-			touchable.setClickable(false);
-			touchable.setOnLongClickListener(null);
-			touchable.setLongClickable(false);
-		} else if ( ! (touchable instanceof AdapterView) ) {
-			// n.b.: AdapterView throws if click listener set.
-			// n.b.: setting onclicklistener automatically sets clickable to true.
-			touchable.setOnClickListener(new OnClickListener()
-			{
-				public void onClick(View v)
-				{
-					// We have to set the current tab here to restore the widget's default behavior since
-					// setOnClickListener seems to overwrite it
-					tabHost.setCurrentTab(tabCount - 1);
-					tabProxy.fireEvent(TiC.EVENT_CLICK, null);
-				}
-			});
-			setOnLongClickListener(touchable);
-		}
-	}
-
-	@Override
-	public void removeTab(TabProxy tab) {
-		// TODO(josh): see if we can implement this, otherwise just leave as a no-op
-		// and document this isn't support for this type of tab group.
 	}
 
 	@Override
@@ -137,6 +72,12 @@ public class TiUITabHostGroup extends TiUIAbstractTabGroup implements OnTabChang
 	}
 
 	@Override
+	public void removeTab(TabProxy tab) {
+		// TODO(josh): see if we can implement this, otherwise just leave as a no-op
+		// and document this isn't support for this type of tab group.
+	}
+
+	@Override
 	public void selectTab(TabProxy tab) {
 		TiUITabHostTab tabView = (TiUITabHostTab) tab.peekView();
 		tabHost.setCurrentTabByTag(tabView.id);
@@ -147,27 +88,6 @@ public class TiUITabHostGroup extends TiUIAbstractTabGroup implements OnTabChang
 		String id = tabHost.getCurrentTabTag();
 		TiUITabHostTab tabView = tabViews.get(id);
 		return (TabProxy) tabView.getProxy();
-	}
-
-	@Override
-	public void onFocusChange(View v, boolean hasFocus)
-	{
-		// ignore focus change for tab group.
-		// we can simply fire focus/blur from onTabChanged (to avoid chicken/egg event problems)
-	}
-
-	@SuppressWarnings("unused")
-	private TiViewProxy getTabWindow(TabProxy tab)
-	{
-		TiViewProxy viewProxy = tab.getWindow();
-		if (viewProxy instanceof TiBaseWindowProxy) {
-			TiViewProxy wrappedViewProxy = ((TiBaseWindowProxy) viewProxy).getWrappedView();
-			if (wrappedViewProxy != null) {
-				viewProxy = wrappedViewProxy;
-			}
-		}
-
-		return viewProxy;
 	}
 
 	@Override
@@ -254,40 +174,5 @@ public class TiUITabHostGroup extends TiUIAbstractTabGroup implements OnTabChang
 		}
 	}
 	*/
-	
-	public void setTabIndicatorSelected(Object t)
-	{
-		ArrayList<TabProxy> tabList = ((TabGroupProxy)proxy).getTabList();
-		
-		if (t != null && tabList != null) {	
-			int index = -1;
-			int len = tabList.size();
-			
-			if (t instanceof Number) {
-				index = TiConvert.toInt(t);
-				if (index >= len) {
-					return;
-				}
-			} else if (t instanceof TabProxy) {
-				TabProxy tab = (TabProxy) t;
-				for (int i=0; i<len; i++) {
-					if (tabList.get(i) == tab) {
-						index = i;
-						break;
-					}
-				}
-			} else {
-				Log.w(TAG, "Attempt to set tab indicator using a non-supported argument. Ignoring");
-				return;
-			}
-			
-			if (index >= 0) {
-				View tabIndicator = tabHost.getTabWidget().getChildTabViewAt(index);
-				if (!tabIndicator.isSelected()) {
-					tabIndicator.setSelected(true);
-				}
-			}
-		}
-	}
 
 }
