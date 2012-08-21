b108874307f77fd82473a4da511cbe00c91f2219
Hidayet Doğan <hdogan@gmail.com>
2012-08-21 16:24:13 +0300

    TIMOB-10507: Alert dialog styles UI API coding.

--- iphone/Classes/TiUIiPhoneProxy.h
+++ iphone/Classes/TiUIiPhoneProxy.h
@@ -49,6 +49,9 @@
 #ifdef USE_TI_UIIPHONETABLEVIEWCELLSELECTIONSTYLE
 	TiProxy *tableViewCellSelectionStyle;
 #endif
+#ifdef USE_TI_UIIPHONEALERTDIALOGSTYLE
+	TiProxy *alertDialogStyle;
+#endif
 }
 
 #ifdef USE_TI_UIIPHONEANIMATIONSTYLE
@@ -89,6 +92,9 @@
 #endif
 #ifdef USE_TI_UIIPHONETABLEVIEWCELLSELECTIONSTYLE
 @property(nonatomic,readonly) TiProxy* TableViewCellSelectionStyle;
+#endif
+#ifdef USE_TI_UIIPHONEALERTDIALOGSTYLE
+@property(nonatomic, readonly) TiProxy *AlertDialogStyle;
 #endif
 
 
