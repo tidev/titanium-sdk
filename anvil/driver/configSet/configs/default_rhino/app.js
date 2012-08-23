bf036c65f92fbd1c62d82935dbf6564761a48d47
Opie Cyrus <dizzymonkey@gmail.com>
2012-08-23 03:13:40 -0500

    TIMOB-9862 correction to previous changes to tests incorrectly removed

--- anvil/driver/configSet/configs/default_rhino/app.js
+++ anvil/driver/configSet/configs/default_rhino/app.js
@@ -5,39 +5,38 @@
  * Please see the LICENSE included with this distribution for details.
  */
 
-var suites = [
-	{name: "analytics"},
-	{name: "blob"},
-	{name: "buffer"},
-	{name: "codec"},
-	{name: "commonjs/commonjs"},
-	{name: "console"},
-	{name: "database"},
-	{name: "facebook"},
-	{name: "filesystem/filesystem"},
-	{name: "json"},
-	{name: "jss"},
-	{name: "kroll"},
-	{name: "locale"},
-	{name: "media/media"},
-	{name: "network_httpclient"},
-	{name: "network"},
-	{name: "network_socket"},
-	{name: "network_socket_tcp"},
-	{name: "platform"},
-	{name: "properties"},
-	{name: "stream"},
-	{name: "titanium"},
-	{name: "ui/ui"},
-	{name: "ui_2dMatrix"},
-	{name: "ui_clipboard"},
-	{name: "ui_controls"},
-	{name: "utils/utils"},
-	{name: "yahoo"}
-];
+var suites = [];
 
 if (Ti.Platform.osname === 'android') {
-	suites = suites.concat([
+	suites = [
+		{name: "analytics"},
+		{name: "blob"},
+		{name: "buffer"},
+		{name: "codec"},
+		{name: "commonjs/commonjs"},
+		{name: "console"},
+		{name: "database"},
+		{name: "facebook"},
+		{name: "filesystem/filesystem"},
+		{name: "json"},
+		{name: "jss"},
+		{name: "kroll"},
+		{name: "locale"},
+		{name: "media/media"},
+		{name: "network_httpclient"},
+		{name: "network"},
+		{name: "network_socket"},
+		{name: "network_socket_tcp"},
+		{name: "platform"},
+		{name: "properties"},
+		{name: "stream"},
+		{name: "titanium"},
+		{name: "ui/ui"},
+		{name: "ui_2dMatrix"},
+		{name: "ui_clipboard"},
+		{name: "ui_controls"},
+		{name: "utils/utils"},
+		{name: "yahoo"},
 		{name: "android/android_database/android_database"},
 		{name: "android/android_filesystem"},
 		{name: "android/android_geolocation"},
@@ -48,14 +47,7 @@ if (Ti.Platform.osname === 'android') {
 		{name: "android/android_resources/android_resources"},
 		{name: "android/android_string"},
 		{name: "android/android_ui/android_ui"}
-	]);
-
-} else if((Ti.Platform.osname === 'iPhone') || (Ti.Platform.osname === 'iPad')) {
-	suites = suites.concat([
-		{name: "iphone/iphone_2Dmatrix"},
-		{name: "iphone/iphone_ui"},
-		{name: "iphone/iphone_UI_3DMatrix"}
-	]);
+	];
 }
 
 
