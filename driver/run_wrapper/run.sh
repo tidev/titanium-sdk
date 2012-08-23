57feb0fdc8db3e409d974a776ce019ce0b5232f1
Opie Cyrus <dizzymonkey@gmail.com>
2012-08-23 14:09:21 -0500

    TIMOB-9862 updated the driver automation script to handle zombie driver instances

--- driver/run_wrapper/run.sh
+++ driver/run_wrapper/run.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-process="driver.js --mode=remote --platform=ios"
+process="driver.js --mode=remote --platform=<REPLACE WITH PLATFORM>"
+driver_timeout=$((20 * 60))
 
 # move back to the actual driver directory - this is important since the driver depends on relative pathing
 cd ..
 
 while [ true ]
 do
-        if ps aux | grep -v grep | grep "$process" > /dev/null
+        driver_pid=`ps aux | grep -v grep | grep "$process" | awk '{ print $2 }'`
+
+        if [ $driver_pid ]
         then
-                # no-op
-                echo "" > /dev/null
+                last_mtime=`stat -f %m run_wrapper/run_out.txt`
+                driver_timeout_threshold=$(( $(date '+%s') - $driver_timeout ))
+
+                if [ $last_mtime -lt $driver_timeout_threshold ]
+                then
+                        kill -9 $driver_pid
+                fi
 
         else
-                /usr/local/bin/node $process > run_wrapper/run_out.txt 2>&1
+                /usr/local/bin/node $process > run_wrapper/run_out.txt 2>&1 &
         fi
 
         sleep 10
 done
-
