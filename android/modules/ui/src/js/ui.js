75a2b1d7f7642f47f4f6935077a6dad25423c2f4
Joshua Roesslein <jroesslein@appcelerator.com>
2012-09-05 11:56:05 -0700

    Add PersistentHandle API for retaining objects.

--- android/modules/ui/src/js/ui.js
+++ android/modules/ui/src/js/ui.js
@@ -1,4 +1,36 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
 var bootstrap = require("bootstrap");
+
+// Objects retained by persistent handles.
+// Each element in this array acts as a storage "cell"
+// keeping the object reachable and alive until it is removed.
+var persistentObjects = [];
+
+// Keeps an object alive until dispose() is called.
+// This is currently used to keep "top level" objects
+// (ex: windows, tab groups) alive until their lifecycle ends.
+function PersistentHandle(object) {
+	this.cell = persistentObjects.length;
+	persistentObjects.push(object);
+}
+
+PersistentHandle.prototype.dispose = function() {
+	if (this.cell == -1) {
+		// This handle has already been disposed.
+		return;
+	}
+
+	persistentObjects.splice(this.cell, 1);
+	this.cell = -1;
+}
+
+exports.PersistentHandle = PersistentHandle;
 
 exports.bootstrap = function(Titanium) {
 	var Window = require("window").bootstrapWindow(Titanium);
@@ -76,3 +108,4 @@ exports.bootstrap = function(Titanium) {
 	Titanium.UI.ActivityIndicator.INDETERMINANT = 0;
 	Titanium.UI.ActivityIndicator.DETERMINANT = 1;
 }
+
