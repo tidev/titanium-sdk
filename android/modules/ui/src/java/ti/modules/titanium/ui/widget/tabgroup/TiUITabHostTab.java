65f72fee6df021a5c62e7c687a891d4c31fcc309
Joshua Roesslein <jroesslein@appcelerator.com>
2012-09-06 01:23:01 -0700

    Add missing copyright headers.

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUITabHostTab.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUITabHostTab.java
@@ -1,3 +1,9 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
 package ti.modules.titanium.ui.widget.tabgroup;
 
 import org.appcelerator.kroll.KrollDict;
