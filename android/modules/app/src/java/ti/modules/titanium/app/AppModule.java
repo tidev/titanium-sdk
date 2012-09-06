11d2fbcaa632bc0e09dd91e7a2580311324b50c3
billdawson <bill@billdawson.com>
2012-09-06 22:40:55 +0200

    TIMOB-8657 New accessibilityEnabled property in Ti.App.

--- android/modules/app/src/java/ti/modules/titanium/app/AppModule.java
+++ android/modules/app/src/java/ti/modules/titanium/app/AppModule.java
@@ -1,11 +1,23 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2012 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
 package ti.modules.titanium.app;
 
 import org.appcelerator.kroll.KrollModule;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.titanium.ITiAppInfo;
 import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.TiContext;
 import org.appcelerator.titanium.util.TiPlatformHelper;
+
+import android.content.Context;
+import android.os.Build;
+import android.provider.Settings;
+import android.view.accessibility.AccessibilityManager;
 
 @Kroll.module
 public class AppModule extends KrollModule
@@ -102,5 +114,26 @@ public class AppModule extends KrollModule
 	@Kroll.method
 	public String appURLToPath(String url) {
 		return resolveUrl(null, url);
+	}
+
+	@Kroll.method @Kroll.getProperty
+	public boolean getAccessibilityEnabled()
+	{
+		AccessibilityManager manager = (AccessibilityManager) TiApplication.getInstance().getSystemService(
+			Context.ACCESSIBILITY_SERVICE);
+		boolean enabled = manager.isEnabled();
+
+		if (!enabled && Build.VERSION.SDK_INT < TiC.API_LEVEL_HONEYCOMB) {
+			// Prior to Honeycomb, AccessibilityManager.isEnabled() would sometimes
+			// return false erroneously the because manager service would asynchronously set the
+			// enabled property in the manager client. So when checking the value, it
+			// might not have been set yet. In studying the changes they made for
+			// Honeycomb, we can see that they do the following in order to determine
+			// if accessibility really is enabled or not:
+			enabled = Settings.Secure.getInt(TiApplication.getInstance().getContentResolver(),
+				Settings.Secure.ACCESSIBILITY_ENABLED, 0) == 1;
+		}
+
+		return enabled;
 	}
 }
