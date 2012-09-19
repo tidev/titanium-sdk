bb7edb912350ebe0cbad183fb1d40909f40aa478
Martin Guillon <martin.guillon@gmail.com>
2012-09-19 16:18:15 +0200

    Fixed gradientLayer/backgroundImageLayer resize correctly animating or not

--- iphone/Classes/TiUIView.m
+++ iphone/Classes/TiUIView.m
@@ -336,13 +336,18 @@ DEFINE_EXCEPTIONS
 	if(!CGSizeEqualToSize(oldSize, newBounds.size))
 	{
 		oldSize = newBounds.size;
-        
-        [CATransaction begin];
-        [CATransaction setValue:(id)kCFBooleanTrue
+        if (!animating)
+        {
+            [CATransaction begin];
+            [CATransaction setValue:(id)kCFBooleanTrue
                          forKey:kCATransactionDisableActions];
+        }
 		[gradientLayer setFrame:newBounds];
-		[[self backgroundImageLayer] setFrame:newBounds];
-        [CATransaction commit];
+		[backgroundImageLayer setFrame:newBounds];
+        if (!animating)
+        {
+            [CATransaction commit];
+        }
 
 		[self frameSizeChanged:[TiUtils viewPositionRect:self] bounds:newBounds];
 	}
