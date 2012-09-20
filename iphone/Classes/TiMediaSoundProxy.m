7a51f0978590f0c0965c755a54ee3b04dc42f325
Hieu Pham <hpham@appcelerator.com>
2012-09-19 18:30:52 -0700

    timob-7502: Change 'time' property in sound to milliseconds

--- iphone/Classes/TiMediaSoundProxy.m
+++ iphone/Classes/TiMediaSoundProxy.m
@@ -174,7 +174,7 @@
 -(NSNumber*)time
 {
 	if (player != nil) {
-		return NUMDOUBLE([player currentTime]);
+		return NUMDOUBLE([player currentTime] * 1000.0);
 	}
 	return NUMDOUBLE(0);
 }
@@ -182,9 +182,9 @@
 -(void)setTime:(NSNumber*)value
 {
 	if (player != nil) {
-		[player setCurrentTime:[TiUtils doubleValue:value]];
+		[player setCurrentTime:([TiUtils doubleValue:(value)] / 1000.0)];
 	} else {
-		resumeTime = [TiUtils doubleValue:value];
+		resumeTime = [TiUtils doubleValue:value] / 1000.0;
 	}
 }
 
