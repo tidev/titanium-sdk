49675090912b98dda40c6d2f521e68d969747b77
Hieu Pham <hpham@appcelerator.com>
2012-09-28 11:28:30 -0700

    timob-5855: comment addressed.

--- iphone/Classes/TiUIEmailDialogProxy.m
+++ iphone/Classes/TiUIEmailDialogProxy.m
@@ -56,8 +56,8 @@
 - (void)open:(id)args
 {
 	[self rememberSelf];
-	NSObject* properties = [args objectAtIndex:0];
-	ENSURE_TYPE_OR_NIL(properties, NSDictionary);
+	NSDictionary* properties = nil;
+	ENSURE_ARG_OR_NIL_AT_INDEX(properties, args, 0, NSDictionary);
 	Class arrayClass = [NSArray class];
 	NSArray * toArray = [self valueForUndefinedKey:@"toRecipients"];
 	ENSURE_CLASS_OR_NIL(toArray,arrayClass);
@@ -130,7 +130,7 @@
 		}
 	}
 	
-	BOOL animated = [TiUtils boolValue:@"animated" properties:(NSDictionary*)properties def:YES];
+	BOOL animated = [TiUtils boolValue:@"animated" properties:properties def:YES];
 	[self retain];
 	[[TiApp app] showModalController:composer animated:animated];
 }
