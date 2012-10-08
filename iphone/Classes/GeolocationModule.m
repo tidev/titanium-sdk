2386df3df5198c7930430ea55e1b1547a8b677af
srahim <srahim@appcelerator.com>
2012-10-08 09:49:46 -0700

    [TIMOB-10773] placing the old property back in for back-ward compatibility reasons.

--- iphone/Classes/GeolocationModule.m
+++ iphone/Classes/GeolocationModule.m
@@ -810,7 +810,7 @@ MAKE_SYSTEM_PROP(ERROR_REGION_MONITORING_DELAYED, kCLErrorRegionMonitoringSetupD
 }
 
 #pragma mark Delegates
-
+#if __IPHONE_OS_VERSION_MAX_ALLOWED < __IPHONE_6_0
 //Using new delegate instead of the old deprecated method - (void)locationManager:didUpdateToLocation:fromLocation:
 
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations{
@@ -828,6 +828,14 @@ MAKE_SYSTEM_PROP(ERROR_REGION_MONITORING_DELAYED, kCLErrorRegionMonitoringSetupD
     [self fireApplicationAnalyticsIfNeeded:locations];
 	[self fireSingleShotLocationIfNeeded:event stopIfNeeded:YES];
 }
+#endif
+
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_6_0
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation
+{
+    [self locationManager:manager didUpdateLocations:[[[NSArray alloc] initWithObjects:newLocation,nil] autorelease]];
+}
+#endif
 
 - (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error
 {
