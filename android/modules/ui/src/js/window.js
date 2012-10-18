60a105b84e84ccdda63e87199e833ab0607d2f0a
Hieu Pham <hpham@appcelerator.com>
2012-10-17 17:41:28 -0700

    timob-11264: fix 'android:back' for tabgroup
    Conflicts:
    
    	android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUIAbstractTab.java

--- android/modules/ui/src/js/window.js
+++ android/modules/ui/src/js/window.js
@@ -264,12 +264,12 @@ exports.bootstrapWindow = function(Titanium) {
 		if (this.propertyCache) {
 			kroll.extend(this._properties, this.propertyCache);
 		}
-		this.window = existingWindow;
+		var win = this.window = existingWindow;
 		this.view = this.window;
 		this.setWindowView(this.view);
-        this.window._internalActivity.addEventListener('android:back', function(){
-            this.window.fireEvent('android:back', null);
-        });
+		this.window._internalActivity.on('android:back', function(){
+			win.fireEvent('android:back');
+		});
 		this.addChildren();
 
 		var self = this;
