20bd3f4074cebab77d3d95771da01485c8701c29
Hieu Pham <hpham@appcelerator.com>
2012-10-17 17:44:34 -0700

    timob-11264: clean up code
    Conflicts:
    
    	android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUIAbstractTab.java

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUIAbstractTab.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/tabgroup/TiUIAbstractTab.java
@@ -56,7 +56,7 @@ public abstract class TiUIAbstractTab extends TiUIView {
 			Activity tabGroupActivity = ((TabProxy) proxy).getTabGroup().getActivity();
 			contentView.setActivity(tabGroupActivity);
 
-
+			//set proxy support so events can be added.
 			contentView.getKrollObject().setProxySupport(contentView);
 			// Assign parent so events bubble up correctly.
 			contentView.setParent(proxy);
