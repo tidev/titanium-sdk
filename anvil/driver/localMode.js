a26a225811a2dd911614120901640f1466426280
Jon Alter <jon@local>
2012-10-24 12:30:00 -0700

    [TIMOB-11568] requiring wrench in localMode.js and fixing failSilent being undefined

--- anvil/driver/localMode.js
+++ anvil/driver/localMode.js
@@ -13,6 +13,7 @@
 var fs = require("fs");
 var path = require("path");
 var readline = require("readline");
+var wrench = require('wrench');
 
 var driverUtils = require(path.join(driverGlobal.driverDir, "driverUtils"));
 
@@ -51,7 +52,7 @@ module.exports = new function() {
 			};
 
 			if (path.existsSync(path.join(driverGlobal.logsDir, "json_results"))) {
-				wrench.rmdirSyncRecursive(path.join(driverGlobal.logsDir, "json_results"), failSilent);
+				wrench.rmdirSyncRecursive(path.join(driverGlobal.logsDir, "json_results"), true);
 			}
 
 			startCallback();
