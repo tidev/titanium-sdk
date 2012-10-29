9039ce551bcd4a438a6510d97693e991afd6bbe9
Vishal Duggal <vishalduggal@gmail.com>
2012-10-29 16:28:07 -0700

    Formatting

--- iphone/Classes/TiUIWebView.m
+++ iphone/Classes/TiUIWebView.m
@@ -296,7 +296,7 @@ static NSString * const kTitaniumJavascript = @"Ti.App={};Ti.API={};Ti.App._list
 	{
 		[self performSelector:reloadMethod withObject:reloadData withObject:reloadDataProperties];
 		return;
-    }
+	}
 	[webview reload];
 }
 
