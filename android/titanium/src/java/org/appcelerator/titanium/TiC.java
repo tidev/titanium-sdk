b9cb80f6de70f3b505d634bde59f4f027f3e2df7
Ping Wang <pwang@appcelerator.com>
2012-10-29 10:44:33 -0700

    timob-11580: event property should be "playbackState"

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -1523,6 +1523,11 @@ public class TiC
 	/**
 	 * @module.api
 	 */
+	public static final String PROPERTY_PLAYBACK_STATE = "playbackState";
+
+	/**
+	 * @module.api
+	 */
 	public static final String PROPERTY_PLUGIN_STATE = "pluginState";
 
 	/**
