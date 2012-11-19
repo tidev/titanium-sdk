2fa0dcf6a116d0e6679997a4f3ef8d3421e6c46f
Vishal Duggal <vishalduggal@gmail.com>
2012-11-19 10:44:05 -0800

    [TIMOB-5435] Address Comments

--- android/modules/network/src/java/ti/modules/titanium/network/HTTPClientProxy.java
+++ android/modules/network/src/java/ti/modules/titanium/network/HTTPClientProxy.java
@@ -190,7 +190,7 @@ public class HTTPClientProxy extends KrollProxy
 	@Kroll.setProperty @Kroll.method
 	public void setUsername(String value)
 	{
-		this.setProperty("username", value);
+		this.setProperty(TiC.PROPERTY_USERNAME, value);
 	}
 
 	@Kroll.getProperty @Kroll.method
@@ -202,7 +202,7 @@ public class HTTPClientProxy extends KrollProxy
 	@Kroll.setProperty @Kroll.method
 	public void setPassword(String value)
 	{
-		this.setProperty("password", value);
+		this.setProperty(TiC.PROPERTY_PASSWORD, value);
 	}
 
 	@Kroll.getProperty @Kroll.method
@@ -214,7 +214,7 @@ public class HTTPClientProxy extends KrollProxy
 	@Kroll.setProperty @Kroll.method
 	public void setDomain(String value)
 	{
-		this.setProperty("domain", value);
+		this.setProperty(TiC.PROPERTY_DOMAIN, value);
 	}
 
 	@Kroll.getProperty @Kroll.method
