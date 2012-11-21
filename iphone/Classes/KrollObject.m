a9b2a242e00f569a7c884d507df968397cf6bd8b
Max Stepanov <mstepanov@aptana.com>
2012-11-21 15:11:18 -0800

    comment

--- iphone/Classes/KrollObject.m
+++ iphone/Classes/KrollObject.m
@@ -707,6 +707,7 @@ bool KrollHasInstance(TiContextRef ctx, TiObjectRef constructor, TiValueRef poss
 					argcount:1 type:KrollMethodInvoke name:nil context:[self context]] autorelease];
 #endif
 			}
+			// Special handling for className due to conflict with NSObject private API
 			if ([key isEqualToString:@"className"]) {
 				return [target valueForUndefinedKey:key];
 			}
