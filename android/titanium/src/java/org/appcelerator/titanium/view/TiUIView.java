a526726b1a330187f0bdc48fdfeb1d02283c083e
Hieu Pham <hpham@appcelerator.com>
2012-12-03 15:26:50 -0800

    timob-6367: close softkeyboard on blur

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -766,6 +766,11 @@ public abstract class TiUIView
 			});
 			proxy.fireEvent(TiC.EVENT_FOCUS, getFocusEventObject(hasFocus));
 		} else {
+			TiMessenger.postOnMain(new Runnable() {
+				public void run() {
+					TiUIHelper.showSoftKeyboard(v, false);
+				}
+			});
 			proxy.fireEvent(TiC.EVENT_BLUR, getFocusEventObject(hasFocus));
 		}
 	}
@@ -798,7 +803,6 @@ public abstract class TiUIView
 	public void blur()
 	{
 		if (nativeView != null) {
-			TiUIHelper.showSoftKeyboard(nativeView, false);
 			nativeView.clearFocus();
 		}
 	}
