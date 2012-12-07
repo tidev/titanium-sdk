be0815e14eaea0246fceef84d1bb34e82a3ce093
Allen Yeung <ayeung@appelerator.com>
2012-12-06 17:31:16 -0800

    Revert "TIMOB-9955: Android: ScrollView with a wrap view becomes unscrollable on android Ti 2.1"
    This reverts commit 533f6d187c8582114cfc601dc36cbd641229a5dc.

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIScrollView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIScrollView.java
@@ -21,8 +21,6 @@ import org.appcelerator.titanium.view.TiUIView;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -46,20 +44,10 @@ public class TiUIScrollView extends TiUIView
 		private int parentWidth = 0;
 		private int parentHeight = 0;
 		private boolean canCancelEvents = true;
-		private GestureDetector gestureDetector;
 
 		public TiScrollViewLayout(Context context, LayoutArrangement arrangement)
 		{
 			super(context, arrangement, proxy);
-			gestureDetector = new GestureDetector(new SimpleOnGestureListener()
-			{
-				@Override
-				public void onLongPress(MotionEvent e)
-				{
-					// Only do this for long presses to match iOS behavior
-					requestDisallowInterceptTouchEvent(true);
-				}
-			});
 		}
 
 		public void setParentWidth(int width)
@@ -81,10 +69,11 @@ public class TiUIScrollView extends TiUIView
 		public boolean dispatchTouchEvent(MotionEvent ev)
 		{
 			// If canCancelEvents is false, then we want to prevent the scroll view from canceling the touch
-			// events of the child view by calling requestDisallowInterceptTouchEvent(true)
+			// events of the child view
 			if (!canCancelEvents) {
-				gestureDetector.onTouchEvent(ev);
+				requestDisallowInterceptTouchEvent(true);
 			}
+
 			return super.dispatchTouchEvent(ev);
 		}
 
