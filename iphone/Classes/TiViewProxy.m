b7f8f39c83ba08e6321ddc5fa42af6932e536a86
Vishal Duggal <vishalduggal@gmail.com>
2012-12-07 12:09:01 -0800

    [TIMOB-11046] Layout at end of animation if required

--- iphone/Classes/TiViewProxy.m
+++ iphone/Classes/TiViewProxy.m
@@ -1625,6 +1625,10 @@ LAYOUTFLAGS_SETTER(setHorizontalWrap,horizontalWrap,horizontalWrap,[self willCha
 {
 	[self forgetProxy:animation];
 	[[self view] animationCompleted];
+	//If there are dirty flags let us add ourselves to the queue to cleanup layout
+	if (dirtyflags) {
+		[self willEnqueue];
+	}
 }
 
 -(void)makeViewPerformSelector:(SEL)selector withObject:(id)object createIfNeeded:(BOOL)create waitUntilDone:(BOOL)wait
@@ -2757,6 +2761,8 @@ if(OSAtomicTestAndSetBarrier(flagBit, &dirtyflags))	\
 	{
         // changing the layout while animating is bad, ignore for now
         DebugLog(@"[WARN] New layout set while view %@ animating: Will relayout after animation.", child);
+        //Since this did not layout here explicitly add it to the layout queue
+        [child willChangePosition];
 	}
 	else
 	{
