29f193ae8f80c814f9737f1fab90e8e7e7c56948
Vishal Duggal <vishalduggal@gmail.com>
2013-03-29 14:39:46 -0700

    [TIMOB-13262] Remove unrequired import

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
@@ -32,7 +32,6 @@ import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnFocusChangeListener;
 import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.ListView;
