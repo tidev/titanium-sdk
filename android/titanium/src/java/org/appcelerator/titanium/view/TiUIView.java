2b548666e4bd61dcf51b23befe03ea0864727162
salachi <salachi@hotmail.com>
2013-04-11 23:00:34 +0530

    TIMOB-11050-fill x,y in rowClicked

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -1459,6 +1459,11 @@ public abstract class TiUIView
 		});
 	}
 	
+	public KrollDict dictFromLastUpdateEvent()
+	{
+		return dictFromEvent(lastUpEvent);
+	}
+	
 	public boolean fireEvent(String eventName, KrollDict data) {
 		return fireEvent(eventName, data, true);
 	}
