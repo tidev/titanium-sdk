a4cc12484396a1dbe2616539dcb2a2b79e1e2bea
Bryan Hughes <bhughes@appcelerator.com>
2013-04-15 16:39:16 -0700

    [TIMOB-13569] Code cleanup

--- mobileweb/cli/commands/_build.js
+++ mobileweb/cli/commands/_build.js
@@ -12,13 +12,11 @@ var ti = require('titanium-sdk'),
 	__n = i18n.__n,
 	cleanCSS = require('clean-css'),
 	afs = appc.fs,
-	xml = appc.xml,
 	parallel = appc.async.parallel,
 	UglifyJS = require('uglify-js'),
 	fs = require('fs'),
 	path = require('path'),
 	wrench = require('wrench'),
-	DOMParser = require('xmldom').DOMParser,
 	jsExtRegExp = /\.js$/,
 	HTML_HEADER = [
 		'<!--',
@@ -363,7 +361,7 @@ build.prototype = {
 		this.logger.info(__n('Searching for %s Titanium Module', 'Searching for %s Titanium Modules', this.tiapp.modules.length));
 		appc.timodule.find(this.tiapp.modules, 'mobileweb', this.deployType, this.titaniumSdkVersion, this.moduleSearchPaths, this.logger, function (modules) {
 			if (modules.missing.length) {
-				this.logger.error(__('Could not find all required Titanium Modules:'))
+				this.logger.error(__('Could not find all required Titanium Modules:'));
 				modules.missing.forEach(function (m) {
 					this.logger.error('   id: ' + m.id + '\t version: ' + (m.version || 'latest') + '\t platform: ' + m.platform + '\t deploy-type: ' + m.deployType);
 				}, this);
@@ -407,7 +405,7 @@ build.prototype = {
 
 				var libDir = ((pkgJson.directories && pkgJson.directories.lib) || '').replace(/^\//, '');
 
-				var mainFilePath = path.join(moduleDir, libDir, (pkgJson.main || '').replace(jsExtRegExp, '') + '.js')
+				var mainFilePath = path.join(moduleDir, libDir, (pkgJson.main || '').replace(jsExtRegExp, '') + '.js');
 				if (!afs.exists(mainFilePath)) {
 					this.logger.error(__('Invalid Titanium Mobile Module "%s": unable to find main file "%s"', module.id, pkgJson.main) + '\n');
 					process.exit(1);
