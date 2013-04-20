4d64eff969c4399489fcf3d470f35bbb25c67a1a
Jesper Jonsson <jesper.m.jonsson@gmail.com>
2013-04-20 12:01:29 +0200

    Android Ti.App resume/pause event support

--- android/titanium/src/java/org/appcelerator/titanium/TiActivity.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiActivity.java
@@ -6,12 +6,28 @@
  */
 package org.appcelerator.titanium;
 
+import org.appcelerator.kroll.common.Log;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 
 
 public class TiActivity extends TiBaseActivity
 {
+	private static final String TAG = "TiActivity";
+
+	private boolean appDidPause = false;
+	private boolean appIsCreatingNewActivity = false;
+	private BroadcastReceiver intentMessageReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			appIsCreatingNewActivity = true;
+		}
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +52,15 @@ public class TiActivity extends TiBaseActivity
 		if (getTiApp().isRestartPending()) {
 			return;
 		}
+
+		// Listen for new activities so that we can determine if onUserLeaveHint is called becouse the app will go into the background or a new activity will just be displayed.
+		LocalBroadcastManager.getInstance(this.getApplicationContext()).registerReceiver(intentMessageReceiver, new IntentFilter("org.appcelerator.action.NEW_TI_ACTIVITY"));
+
+		// If activity did pause becouse the app did go into the background, fire Ti.App resume event!
+		if (isResumed && appDidPause) {
+			appDidPause = false;
+			TiApplication.getInstance().fireAppEvent("resume", null);
+		}
 	}
 
 	@Override
@@ -46,6 +71,23 @@ public class TiActivity extends TiBaseActivity
 		if (getTiApp().isRestartPending()) {
 			return;
 		}
+
+		// Activity paused, no need to listen until resumed.
+		LocalBroadcastManager.getInstance(this.getApplicationContext()).unregisterReceiver(intentMessageReceiver);
+		appIsCreatingNewActivity = false;
 	}
 
+	@Override
+	protected void onUserLeaveHint()
+	{
+		super.onUserLeaveHint();
+
+		Log.d(TAG, "Activity " + this + " onUserLeaveHint", Log.DEBUG_MODE);
+
+		// If the app isn't creating a new activity, then the app will go into the background, fire Ti.App pause event!
+		if (!appIsCreatingNewActivity && !appDidPause) {
+			TiApplication.getInstance().fireAppEvent("pause", null);
+			appDidPause = true;
+		}
+	}
 }
