be394fc0a9be032b14d5ba8fa2e6039526064ee8
unknown <exalture@.(none)>
2013-06-13 10:18:09 +0530

    TIMOB-9572 set default visible property as true

--- android/titanium/src/java/org/appcelerator/titanium/proxy/TiViewProxy.java
+++ android/titanium/src/java/org/appcelerator/titanium/proxy/TiViewProxy.java
@@ -104,6 +104,7 @@ public abstract class TiViewProxy extends KrollProxy implements Handler.Callback
 		pendingAnimationLock = new Object();
 
 		defaultValues.put(TiC.PROPERTY_BACKGROUND_REPEAT, false);
+		defaultValues.put(TiC.PROPERTY_VISIBLE, true);
 	}
 
 	@Override
