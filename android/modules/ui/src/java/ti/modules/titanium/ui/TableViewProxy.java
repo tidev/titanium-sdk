0550052205d60b6289343aaf127162fdcd38d265
unknown <exalture@.(none)>
2013-06-24 16:35:26 +0530

    TIMOB-5756 add log instead of crash

--- android/modules/ui/src/java/ti/modules/titanium/ui/TableViewProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/TableViewProxy.java
@@ -338,7 +338,7 @@ public class TableViewProxy extends TiViewProxy
 				getTableView().setModelDirty();
 				updateView();
 			} else {
-				throw new IllegalStateException("Unable to delete row. Index out of range. Non-existent row at " + index);
+				Log.e(TAG, "Unable to delete row. Index out of range. Non-existent row at " + index);
 			}
 		} else if (row instanceof TableViewRowProxy) {
 			TableViewRowProxy rowProxy = (TableViewRowProxy) row;
