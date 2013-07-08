1e60210e1c422ceb351760f772e7b1d95cd97f4d
Mark Mokryn <mokesil@gmail.com>
2013-07-08 14:32:04 +0300

    TIMOB-13065: fixes file origins for Jelly Bean

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/webview/TiUIWebView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/webview/TiUIWebView.java
@@ -139,6 +139,7 @@ public class TiUIWebView extends TiUIView
 		settings.setJavaScriptCanOpenWindowsAutomatically(true);
 		settings.setLoadsImagesAutomatically(true);
 		settings.setLightTouchEnabled(true);
+		settings.setAllowUniversalAccessFromFileURLs(true); // default is "false" for JellyBean, TIMOB-13065
 		settings.setDomStorageEnabled(true); // Required by some sites such as Twitter. This is in our iOS WebView too.
 		File path = TiApplication.getInstance().getFilesDir();
 		if (path != null) {
