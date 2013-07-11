38f159ac9346fea528ff1b15b9526fa9b0b46f24
Vishal Duggal <vishalduggal@gmail.com>
2013-07-11 14:10:44 -0700

    Add Check for ios 7

--- iphone/Classes/TiUtils.h
+++ iphone/Classes/TiUtils.h
@@ -489,6 +489,12 @@ typedef enum {
 +(BOOL)isIOS6OrGreater;
 
 /**
+ Whether or not the current OS version is equal to or greater than 7.0.
+ @return _YES_ if the current OS version is equal to or greater thann 7.0, _NO_ otherwise.
+ */
++(BOOL)isIOS7OrGreater;
+
+/**
  Whether or not the current device is an iPhone 4.
  @return _YES_ if the current device is an iPhone 4, _NO_ otherwise.
  */
