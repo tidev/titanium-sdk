50c114e2b0b68b8ffdd2733b62dd5ff34cd8931f
bijupmb <bijup@exalture.com>
2013-07-26 23:10:38 +0530

    FIX TO TIMOB-14371

--- support/anvil/configSet/Resources/suites/commonjs/relative/program.js
+++ support/anvil/configSet/Resources/suites/commonjs/relative/program.js
@@ -1,7 +1,8 @@
 
 exports.run = function(testRun, valueOf) {
-	var a = require('commonjs/relative/submodule/a');
-	var b = require('commonjs/relative/submodule/b');
+	// Actual path from root directory("Resources") to relative/submodule -> Resources/suites/commonjs/relative/submodule
+	var a = require('/suites/commonjs/relative/submodule/a');//Path modified to correct absolute path
+	var b = require('/suites/commonjs/relative/submodule/b');//Path modified to correct absolute path
 
 	valueOf(testRun, a.foo).shouldBe(b.foo);
 	return testRun;
--- support/anvil/configSet/Resources/suites/includes/l2/l3/slash_to_root.js
+++ support/anvil/configSet/Resources/suites/includes/l2/l3/slash_to_root.js
@@ -1,1 +1,2 @@
-Ti.include('suites/includes/root.js');
+//Absolute path from root directory("Resources")to "root.js" -> Resources/suites/includes/root.js
+Ti.include('/suites/includes/root.js');//Prepended a '/' to the path to make it correct absolute path
