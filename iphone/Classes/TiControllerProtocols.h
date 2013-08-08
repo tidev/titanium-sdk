c48defb0a391025f8abb871b16f0cccbf1bf4a25
Vishal Duggal <vishalduggal@gmail.com>
2013-08-08 12:35:38 -0700

    Accessibility stuff

--- iphone/Classes/TiControllerProtocols.h
+++ iphone/Classes/TiControllerProtocols.h
@@ -91,6 +91,7 @@
 //Titanium Support
 -(CGRect)resizeView;
 -(void)repositionSubviews;
+@property(nonatomic,readonly) TiViewProxy<TiKeyboardFocusableView> * keyboardFocusedProxy;
 #if defined(DEBUG) || defined(DEVELOPER)
 -(void)shutdownUi;
 #endif
