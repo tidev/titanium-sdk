9760c44218f492b8c0898eb07a0360e5a5672adb
bijupmb <bijup@exalture.com>
2013-09-26 19:06:10 +0530

    Anvil test case for Android_apply_properties Test suite

--- support/anvil/configSet/Resources/suites/android/android_apply_properties.js
+++ support/anvil/configSet/Resources/suites/android/android_apply_properties.js
@@ -1,5 +1,3 @@
-
-           
 /*
  * Appcelerator Titanium Mobile
  * Copyright (c) 2011-2012 by Appcelerator, Inc. All Rights Reserved.
@@ -17,22 +15,22 @@ module.exports = new function() {
     
 	this.name = "android_apply_properties";
 	this.tests = [
-                  {name: "test_TIMOB_10415"},
-                  ]
+        {name: "test_TIMOB_10415"}
+    ]
     
 	this.test_TIMOB_10415 = function(testRun) {
-            var eventBegins = new Date(2010, 11, 26, 12, 0, 0);
-            var eventEnds = new Date(2010, 11, 26, 14, 0, 0);
-            var details = {
-            title: 'Do some stuff',
-            description: "I'm going to do some stuff at this time.",
+        var eventBegins = new Date(2010, 11, 26, 12, 0, 0);
+        var eventEnds = new Date(2010, 11, 26, 14, 0, 0);
+        var details = {
+			title: 'Do some stuff',
+			description: "I'm going to do some stuff at this time.",
             begin: eventBegins,
             end: eventEnds,
             location:'my_location'
-            };
-            var event = Ti.Calendar.selectableCalendars[0].createEvent(details);
-            valueOf(testRun, event.location).shouldBe('my_location');
+        };
+        var event = Ti.Calendar.selectableCalendars[0].createEvent(details);
+        valueOf(testRun, event.location).shouldBe('my_location');
             
-            finish(testRun);
+        finish(testRun);
     }
 }
