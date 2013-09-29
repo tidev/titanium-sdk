fe6c8e7fdd6dea1368351f528525971558bf114b
Martin Guillon <martin.guillon@gmail.com>
2013-09-29 12:19:29 +0200

    a lot of cleanup. This should be sufficient and it works

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIScrollableView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIScrollableView.java
@@ -46,8 +46,6 @@ public class TiUIScrollableView extends TiUIView
 
 	private static final int PAGE_LEFT = 200;
 	private static final int PAGE_RIGHT = 201;
-	private View mTouchTarget;
-	private int mPreviousState = ViewPager.SCROLL_STATE_IDLE;
 
 	private final ViewPager mPager;
 	private final ArrayList<TiViewProxy> mViews;
@@ -107,20 +105,8 @@ public class TiUIScrollableView extends TiUIView
 			@Override
 			public void onPageScrollStateChanged(int scrollState)
 			{
-// All of this is to inhibit any scrollable container from consuming our touch events as the user is changing pages
-        if (mPreviousState == ViewPager.SCROLL_STATE_IDLE) {
-            if (scrollState == ViewPager.SCROLL_STATE_DRAGGING) {
-                mTouchTarget = mPager;
-                mContainer.requestDisallowInterceptTouchEvent(true);
-           }
-        } else {
-            if (scrollState == ViewPager.SCROLL_STATE_IDLE || scrollState == ViewPager.SCROLL_STATE_SETTLING) {
-                mTouchTarget = null;
-                mContainer.requestDisallowInterceptTouchEvent(false);
-           }
-        }
+				mPager.requestDisallowInterceptTouchEvent(state != ViewPager.SCROLL_STATE_IDLE);
 
-        mPreviousState = scrollState;
 				if ((scrollState == ViewPager.SCROLL_STATE_IDLE) && isValidScroll) {
 					int oldIndex = mCurIndex;
 
@@ -569,38 +555,6 @@ public class TiUIScrollableView extends TiUIView
 				showPager();
 			}
 			return super.onTrackballEvent(event);
-		}
-
-		@Override
-	    public boolean onTouchEvent(MotionEvent ev) {
-	        //We capture any touches not already handled by the ViewPager
-	        // to implement scrolling from a touch outside the pager bounds.
-	        switch (ev.getAction()) {
-	            case MotionEvent.ACTION_UP:
-				case MotionEvent.ACTION_CANCEL:
-					requestDisallowInterceptTouchEvent(false); //to make sure we go back to the initial state!
-					break;
-	            default:
-	                break;
-	        }
-	 
-	        return ((View) mPager).onTouchEvent(ev);
-	    }
-
-		@Override
-		public boolean dispatchTouchEvent(MotionEvent ev)
-		{
-			if (mTouchTarget != null) {
-	            boolean wasProcessed = mTouchTarget.dispatchTouchEvent(ev);
-
-	            if (!wasProcessed) {
-	                mTouchTarget = null;
-					requestDisallowInterceptTouchEvent(false);
-	            }
-
-	            return wasProcessed;
-	        }
-			return super.dispatchTouchEvent(ev);
 		}
 
 		@Override
