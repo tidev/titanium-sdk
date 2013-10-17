ce6228b4a387374075c0d28daac30f21142eb0ad
Chris Barber <chris@cb1inc.com>
2013-10-17 01:26:17 -0700

    Added support for the global ignore files and dirs to the Android create and iOS build.

--- iphone/cli/commands/_build.js
+++ iphone/cli/commands/_build.js
@@ -873,6 +873,7 @@ function sendAnalytics(cli) {
 
 function build(logger, config, cli, finished) {
 	this.logger = logger;
+	this.config = config;
 	this.cli = cli;
 
 	this.titaniumIosSdkPath = afs.resolvePath(__dirname, '..', '..');
@@ -1003,8 +1004,8 @@ build.prototype = {
 		afs.copyDirSyncRecursive(src, dest, opts || {
 			preserve: true,
 			logger: this.logger.debug,
-			ignoreDirs: ['.git', '.svn', 'CVS'],
-			ignoreFiles: ['.gitignore', '.cvsignore']
+			ignoreDirs: new RegExp(this.config.get('cli.ignoreDirs')),
+			ignoreFiles: new RegExp(this.config.get('cli.ignoreFiles'))
 		});
 	},
 
@@ -1012,8 +1013,8 @@ build.prototype = {
 		afs.copyDirRecursive(src, dest, callback, opts || {
 			preserve: true,
 			logger: this.logger.debug,
-			ignoreDirs: ['.git', '.svn', 'CVS'],
-			ignoreFiles: ['.gitignore', '.cvsignore']
+			ignoreDirs: new RegExp(this.config.get('cli.ignoreDirs')),
+			ignoreFiles: new RegExp(this.config.get('cli.ignoreFiles'))
 		});
 	},
 
@@ -1414,8 +1415,8 @@ build.prototype = {
 			copyOpts = {
 				preserve: true,
 				logger: this.logger.debug,
-				ignoreDirs: ['.git','.svn', 'CVS'],
-				ignoreFiles: ['.gitignore', '.cvsignore', 'bridge.txt', 'libTitanium.a'],
+				ignoreDirs: new RegExp(this.config.get('cli.ignoreDirs')),
+				ignoreFiles: /^(bridge\.txt|libTitanium\.a|\.gitignore|\.npmignore|\.cvsignore|\.DS_Store|\._.*|[Tt]humbs.db|\.vspscc|\.vssscc|\.sublime-project|\.sublime-workspace|\.project|\.tmproj)$'/,
 				callback: function (src, dest, contents, logger) {
 					if (extRegExp.test(src) && src.indexOf('TiCore') == -1) {
 						logger && logger(__('Processing %s', src.cyan));
@@ -2263,8 +2264,9 @@ build.prototype = {
 
 	createSymlinks: function (callback) {
 		if (this.target == 'simulator' && this.deployType == 'development') {
-			var ignoreRegExp = /^\.gitignore|\.cvsignore|\.DS_Store|\.git|\.svn|_svn|CVS$/,
-				icon = (this.tiapp.icon || 'appicon.png').match(/^(.*)\.(.+)$/),
+			var icon = (this.tiapp.icon || 'appicon.png').match(/^(.*)\.(.+)$/),
+				ignoreDirs = new RegExp(this.config.get('cli.ignoreDirs')),
+				ignoreFiles = new RegExp(this.config.get('cli.ignoreFiles')),
 				unsymlinkableFileRegExp = new RegExp("^Default.*\.png|.+\.(otf|ttf)|iTunesArtwork" + (icon ? '|' + icon[1].replace(/\./g, '\\.') + '.*\\.' + icon[2] : '') + "$"),
 				symlinkHook = this.cli.createHook('build.ios.copyResource', this, function (srcFile, destFile, cb) {
 					this.logger.debug(__('Symlinking %s => %s', srcFile.cyan, destFile.cyan));
@@ -2281,10 +2283,12 @@ build.prototype = {
 						wrench.mkdirSyncRecursive(dest);
 
 						series(this, fs.readdirSync(src).map(function (file) {
+							var srcFile = path.join(src, file),
+								destFile = path.join(dest, file),
+								isDir = fs.existsSync(srcFile) && fs.statSync(srcFile).isDirectory();
+
 							return function (next) {
-								if ((this.deviceFamily != 'iphone' || ipadSplashImages.indexOf(file) == -1) && !ignoreRegExp.test(file) && (!doIgnoreDirs || ti.availablePlatformsNames.indexOf(file) == -1)) {
-									var srcFile = path.join(src, file),
-										destFile = path.join(dest, file);
+								if ((this.deviceFamily != 'iphone' || ipadSplashImages.indexOf(file) == -1) && !(isDir ? ignoreDirs : ignoreFiles).test(file) && (!doIgnoreDirs || ti.availablePlatformsNames.indexOf(file) == -1)) {
 									if (fs.statSync(srcFile).isDirectory()) {
 										setTimeout(function () {
 											symlinkResources(srcFile, destFile, false, next);
@@ -2529,7 +2533,8 @@ build.prototype = {
 	compileResources: function (src, dest, callback) {
 		if ((this.target != 'simulator' || this.deployType != 'development') && fs.existsSync(src)) {
 			var compiledTargets = {},
-				ignoreRegExp = /^\.gitignore|\.cvsignore|\.DS_Store|\.git|\.svn|_svn|CVS$/,
+				ignoreDirs = new RegExp(this.config.get('cli.ignoreDirs')),
+				ignoreFiles = new RegExp(this.config.get('cli.ignoreFiles')),
 				recursivelyCopy = function (from, to, rel, ignore, done) {
 					wrench.mkdirSyncRecursive(to);
 					series(this, fs.readdirSync(from).map(function (file) {
@@ -2538,7 +2543,8 @@ build.prototype = {
 								t = f.replace(from, to),
 								fstat = fs.statSync(f),
 								p = rel ? rel + '/' + file : file;
-							if (ignoreRegExp.test(file) || (ignore && ignore.indexOf(file) != -1)) {
+
+							if ((fstat.isDirectory() ? ignoreDirs : ignoreFiles).test(file) || (ignore && ignore.indexOf(file) != -1)) {
 								this.logger.debug(__('Ignoring %s', f.cyan));
 							} else if (fstat.isDirectory()) {
 								recursivelyCopy(f, t, p, null, next);
