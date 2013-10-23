a677a9c9d217e88e68902011606b7236139249eb
Bryan Hughes <bhughes@appcelerator.com>
2013-10-23 13:22:03 -0700

    [TIMOB-11492] Added support for pointer events

--- mobileweb/titanium/Ti/_/UI/Element.js
+++ mobileweb/titanium/Ti/_/UI/Element.js
@@ -24,6 +24,7 @@ define(
 		},
 		pixelUnits = 'px',
 		useTouch = has('touch'),
+		usePointer = global.navigator.msPointerEnabled,
 		gestureEvents = [
 			'touchstart',
 			'touchend',
@@ -57,12 +58,14 @@ define(
 			on(self, 'touchstart', self, '_doBackground');
 			on(self, 'touchend', self, '_doBackground');
 
-			on(self.domNode, useTouch ? 'touchstart' : 'mousedown', function(evt){
+			// NOTE: this code signifies that a given element was involved in the event, but doesn't actually handle it.
+			// The code to actually handle the event is in UI.js
+			on(self.domNode, usePointer ? 'MSPointerDown' : useTouch ? 'touchstart' : 'mousedown', function(evt){
 				var handles = [
-					on(global, useTouch ? 'touchmove' : 'mousemove', function(evt){
+					on(global, usePointer ? 'MSPointerMove' : useTouch ? 'touchmove' : 'mousemove', function(evt){
 						(useTouch || touching) && (evt._elements || (evt._elements = [])).push(self);
 					}),
-					on(global, useTouch ? 'touchend' : 'mouseup', function(evt){
+					on(global, usePointer ? 'MSPointerUp' : useTouch ? 'touchend' : 'mouseup', function(evt){
 						touching = 0;
 						(evt._elements || (evt._elements = [])).push(self);
 						event.off(handles);
