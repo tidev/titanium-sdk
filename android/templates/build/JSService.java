a17e4df9ce774b068cf467a7e0b026b5eb9645d7
Chris Barber <chris@cb1inc.com>
2013-10-25 11:27:34 -0700

    [TIMOB-9028] Rewrote the Android build system.

---
+++ android/templates/build/JSService.java
@@ -0,0 +1,9 @@
+package <%- appid %>;
+
+import ti.modules.titanium.android.TiJSService;
+
+public final class <%- service.classname %> extends TiJSService {
+	public <%- service.classname %>() {
+		super("<%- service.url %>");
+	}
+}
---
+++ cli/commands/clean.js
@@ -0,0 +1,139 @@
+/*
+ * clean.js: Titanium Mobile CLI clean command
+ *
+ * Copyright (c) 2012-2013, Appcelerator, Inc.  All Rights Reserved.
+ * See the LICENSE file for more information.
+ */
+
+var appc = require('node-appc'),
+	i18n = appc.i18n(__dirname),
+	__ = i18n.__,
+	__n = i18n.__n,
+	ti = require('titanium-sdk'),
+	fs = require('fs'),
+	path = require('path'),
+	wrench = require('wrench'),
+	async = require('async');
+
+exports.cliVersion = '>=3.2';
+exports.desc = __('removes previous build directories');
+
+exports.config = function (logger, config, cli) {
+	return function (finished) {
+		cli.createHook('clean.config', function (callback) {
+			callback({
+				options: appc.util.mix({
+					platform: {
+						// this is for backwards compatibility and eventually should be dropped
+						hidden: true
+					},
+					platforms: {
+						// note: --platforms is not required for the clean command
+						abbr: 'p',
+						desc: __('one or more platforms to clean'),
+						values: ti.targetPlatforms,
+						skipValueCheck: true // we do our own validation
+					},
+					'project-dir': {
+						abbr: 'd',
+						desc: __('the directory containing the project, otherwise the current working directory')
+					}
+				}, ti.commonOptions(logger, config))
+			});
+		})(function (err, results, result) {
+			finished(result);
+		});
+	};
+};
+
+exports.validate = function (logger, config, cli) {
+	var platforms = cli.argv.platforms || cli.argv.platform;
+	if (platforms) {
+		platforms = ti.scrubPlatforms(platforms);
+
+		if (platforms.bad.length) {
+			logger.error(__n('Invalid platform: %%s', 'Invalid platforms: %%s', platforms.bad.length, platforms.bad.join(', ')) + '\n');
+			logger.log(__('Available platforms for SDK version %s:', ti.manifest.sdkVersion) + '\n');
+			ti.targetPlatforms.forEach(function (p) {
+				logger.log('    ' + p.cyan);
+			});
+			logger.log();
+			process.exit(1);
+		}
+
+		cli.argv.platforms = platforms.scrubbed;
+	} else {
+		cli.argv.platforms = null;
+	}
+
+	ti.validateProjectDir(logger, cli, cli.argv, 'project-dir');
+
+	return function (finished) {
+		ti.loadPlugins(logger, config, cli, cli.argv['project-dir'], function () {
+			finished();
+		});
+	};
+};
+
+exports.run = function (logger, config, cli) {
+	var buildDir = path.join(cli.argv['project-dir'], 'build');
+
+	function done(err) {
+		if (err) {
+			logger.error(__('Failed to clean project in %s', appc.time.prettyDiff(cli.startTime, Date.now())) + '\n');
+		} else {
+			logger.info(__('Project cleaned successfully in %s', appc.time.prettyDiff(cli.startTime, Date.now())) + '\n');
+		}
+	}
+
+	if (cli.argv.platforms) {
+		async.series(cli.argv.platforms.map(function (platform) {
+			return function (next) {
+				cli.fireHook('clean.pre', function () {
+					cli.fireHook('clean.' + platform + '.pre', function () {
+						var dir = path.join(buildDir, platform);
+						if (appc.fs.exists(dir)) {
+							logger.debug(__('Deleting %s', dir.cyan));
+							wrench.rmdirSyncRecursive(dir);
+						} else {
+							logger.debug(__('Directory does not exist %s', dir.cyan));
+						}
+						cli.fireHook('clean.' + platform + '.post', function () {
+							cli.fireHook('clean.post', function () {
+								next();
+							});
+						});
+					});
+				});
+			};
+		}), done);
+	} else if (appc.fs.exists(buildDir)) {
+		logger.debug(__('Deleting all platform build directories'));
+
+		cli.fireHook('clean.pre', function () {
+			async.series(fs.readdirSync(buildDir).map(function (dir) {
+				return function (next) {
+					var fulldir = path.join(buildDir, dir);
+					if (fs.lstatSync(fulldir).isDirectory()) {
+						cli.fireHook('clean.' + dir + '.pre', function () {
+							logger.debug(__('Deleting %s', fulldir.cyan));
+							wrench.rmdirSyncRecursive(fulldir);
+							cli.fireHook('clean.' + dir + '.post', function () {
+								next();
+							});
+						});
+					} else {
+						next();
+					}
+				};
+			}), function () {
+				cli.fireHook('clean.post', function () {
+					done();
+				});
+			});
+		});
+	} else {
+		logger.debug(__('Directory does not exist %s', buildDir.cyan));
+		done();
+	}
+};
