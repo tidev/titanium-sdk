114c7e0f7acc2e59bab6a7c14696c27b46bdced0
Bryan Hughes <bhughes@appcelerator.com>
2013-10-31 11:24:20 -0700

    [TIMOB-15486] Bug fix

--- mobileweb/titanium/Ti/_/Filesystem/Local.js
+++ mobileweb/titanium/Ti/_/Filesystem/Local.js
@@ -61,7 +61,7 @@ define(["Ti/_", "Ti/_/Evented", "Ti/_/declare", "Ti/_/encoding", "Ti/_/lang", "T
 			};
 		} else {
 			var xhr = new XMLHttpRequest;
-			xhr.overrideMimeType('text/plain; charset=x-user-defined');
+			xhr.overrideMimeType && xhr.overrideMimeType('text/plain; charset=x-user-defined');
 			xhr.open("GET", '.' + path, false);
 			xhr.send(null);
 			return xhr.status === 200 ? { data: xhr.responseText, mimeType: xhr.getResponseHeader("Content-Type") } : null;
