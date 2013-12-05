10366e7fa248261eb319dadb5f69e9cf84f0f1e1
Ping Wang <pwang@appcelerator.com>
2013-12-05 14:40:13 -0800

    timob-15890: set the default shadowRadius to 0.5 so the shadow is visible by default.

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIButton.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIButton.java
@@ -28,9 +28,10 @@ import android.widget.Button;
 public class TiUIButton extends TiUIView
 {
 	private static final String TAG = "TiUIButton";
+	private static final float DEFAULT_SHADOW_RADIUS = 0.5f;
 	
 	private int defaultColor;
-	private float shadowRadius = 0f;
+	private float shadowRadius = DEFAULT_SHADOW_RADIUS;
 	private float shadowX = 0f;
 	private float shadowY = 0f;
 	private int shadowColor = Color.TRANSPARENT;
@@ -112,7 +113,7 @@ public class TiUIButton extends TiUIView
 		}
 		if (d.containsKey(TiC.PROPERTY_SHADOW_RADIUS)) {
 			needShadow = true;
-			shadowRadius = TiConvert.toFloat(d.get(TiC.PROPERTY_SHADOW_RADIUS), 0);
+			shadowRadius = TiConvert.toFloat(d.get(TiC.PROPERTY_SHADOW_RADIUS), DEFAULT_SHADOW_RADIUS);
 		}
 		if (d.containsKey(TiC.PROPERTY_SHADOW_COLOR)) {
 			needShadow = true;
@@ -162,7 +163,7 @@ public class TiUIButton extends TiUIView
 				btn.setShadowLayer(shadowRadius, shadowX, shadowY, shadowColor);
 			}
 		} else if (key.equals(TiC.PROPERTY_SHADOW_RADIUS)) {
-			shadowRadius = TiConvert.toFloat(newValue, 0);
+			shadowRadius = TiConvert.toFloat(newValue, DEFAULT_SHADOW_RADIUS);
 			btn.setShadowLayer(shadowRadius, shadowX, shadowY, shadowColor);
 		} else if (key.equals(TiC.PROPERTY_SHADOW_COLOR)) {
 			shadowColor = TiConvert.toColor(TiConvert.toString(newValue));
