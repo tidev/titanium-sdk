49acef027fa30b046aeae879f1528a2131693fb7
Chris Barber <chris@cb1inc.com>
2013-12-08 12:14:48 -0800

    [TIMOB-15922] Fixed bug with passing the wrong path into the exists check for app icons.

--- iphone/cli/commands/_build.js
+++ iphone/cli/commands/_build.js
@@ -1795,9 +1795,9 @@ iOSBuilder.prototype.createInfoPlist = function createInfoPlist(next) {
 	Array.isArray(plist.CFBundleIconFiles) || (plist.CFBundleIconFiles = []);
 	['.png', '@2x.png', '-72.png', '-60.png', '-60@2x.png', '-76.png', '-76@2x.png', '-Small-50.png', '-72@2x.png', '-Small-50@2x.png', '-Small.png', '-Small@2x.png', '-Small-40.png', '-Small-40@2x.png'].forEach(function (name) {
 		name = iconName + name;
-		if (fs.existsSync(this.projectDir, 'Resources', name) ||
-			fs.existsSync(this.projectDir, 'Resources', 'iphone', name) ||
-			fs.existsSync(this.projectDir, 'Resources', this.platformName, name)) {
+		if (fs.existsSync(path.join(this.projectDir, 'Resources', name)) ||
+			fs.existsSync(path.join(this.projectDir, 'Resources', 'iphone', name)) ||
+			fs.existsSync(path.join(this.projectDir, 'Resources', 'ios', name))) {
 			if (plist.CFBundleIconFiles.indexOf(name) == -1) {
 				plist.CFBundleIconFiles.push(name);
 			}
