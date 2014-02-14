3142040eab9751b290c7fec6ec6c02bfbcafc7ee
bijupmb <bijup@exalture.com>
2014-02-14 12:20:19 +0530

    modified

--- android/modules/ui/src/java/ti/modules/titanium/ui/RadioButtonProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/RadioButtonProxy.java
@@ -1,20 +1,17 @@
 package ti.modules.titanium.ui;
 
-import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.proxy.TiViewProxy;
-import org.appcelerator.titanium.util.TiConvert;
 import org.appcelerator.titanium.view.TiUIView;
+
 import ti.modules.titanium.ui.widget.TiUIRadioButton;
 import android.app.Activity;
 
 
-@Kroll.proxy(creatableInModule = UIModule.class, propertyAccessors = { TiC.PROPERTY_FONT, TiC.PROPERTY_VERTICAL_ALIGN, })
+@Kroll.proxy(creatableInModule = UIModule.class, propertyAccessors = { TiC.PROPERTY_FONT, TiC.PROPERTY_COLOR })
 public class RadioButtonProxy extends TiViewProxy
 {
-
-	private String text;
 
 	@Override
 	public TiUIView createView(Activity activity)
@@ -23,20 +20,11 @@ public class RadioButtonProxy extends TiViewProxy
 		return new TiUIRadioButton(this);
 	}
 
-	@Override
-	public void handleCreationDict(KrollDict dict)
-	{
-		super.handleCreationDict(dict);
-		if (dict.containsKey(TiC.PROPERTY_TEXT)) {
-			text = TiConvert.toString(dict, TiC.PROPERTY_TEXT);
-		}
-
-	}
-
 	@Kroll.getProperty @Kroll.method
 	public String getText()
 	{
-		return text;
+		TiUIView radView = peekView();
+		return ((TiUIRadioButton) radView).getText();
 	}
 
 	@Kroll.getProperty @Kroll.method
@@ -45,7 +33,7 @@ public class RadioButtonProxy extends TiViewProxy
 		TiUIView radView = peekView();
 		return ((TiUIRadioButton) radView).isChecked();
 	}
-
+	
 	@Override
 	public String getApiName()
 	{
