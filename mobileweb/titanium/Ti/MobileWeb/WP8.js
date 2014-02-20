58065559c3cf68f72daeaef3c5117bf60bf5375c
Ben Hatfield <bhatfield@appcelerator.com>
2014-02-19 17:06:34 -0800

    [TIDOC-1487] Implement APIs to create Native Windows Phone components

--- mobileweb/titanium/Ti/MobileWeb/WP8.js
+++ mobileweb/titanium/Ti/MobileWeb/WP8.js
@@ -1,143 +1,193 @@
 /*global window, define*/
 define(['Ti/_/lang'], function(lang) {
 
-	var global = window,
+    var global = window,
 		proxyList = {},
 		sendNativeMessage = require.sendNativeMessage;
 
-	function Handle(hndId) {
-		this._hnd = hndId;
-		this._listeners = {};
-	}
+    function Handle(hndId) {
+        this._hnd = hndId;
+        this._listeners = {};
+    }
 
-	Handle.prototype._send = function (action, data) {
-		var res;
-		data.hnd = this._hnd;
-		global.handleProxyResponse = function (r) {
-			res = r;
-		};
-		sendNativeMessage('r', action + JSON.stringify(data));
-		global.handleProxyResponse = void 0;
-		res = JSON.parse(res);
-		if (res.primitiveValue) {
-			return res.primitiveValue;
-		} else if (res.hnd) {
-			return proxyList[res.hnd] || (proxyList[res.hnd] = new Handle(res.hnd));
-		}
-	};
+    handleEvent = function (x) {
+        var payload = JSON.parse(x);
+        payload.source = proxyList[payload._hnd];
+        var cbs = proxyList[payload._hnd]._listeners[payload.type];
+        cbs.forEach(function(cb){
+            cb(payload);
+        });
+    }
 
-	Handle.prototype.invoke = function (name, argTypes, argValues) {
-		return this._send('in', {
-			name: name,
-			argTypes: argTypes,
-			argValues: argValues.map(function (value) {
-				var isHandle = value instanceof Handle;
-				return {
-					valueHnd: isHandle ? value._hnd : void 0,
-					valuePrimitive: isHandle ? void 0 : value,
-				};
-			})
-		});
-	};
+    handleError = function (x) {
+        var payload = JSON.parse(x);
+        Ti.API.error(x);
+        throw x;
+    }
 
-	Handle.prototype.getProp = function (name, isAttached) {
-		return this._send('gp', {
-			name: name,
-			isAttached: isAttached
-		});
-	};
+    Handle.prototype._send = function (action, data) {
+        var res;
+        data.hnd = this._hnd;
+        global.handleProxyResponse = function (r) {
+            res = r;
+        };
+        sendNativeMessage('r', action + JSON.stringify(data));
+        global.handleProxyResponse = void 0;
+        res = JSON.parse(res);
+        if (res.primitiveValue) {
+            return res.primitiveValue;
+        } else if (res.hnd) {
+            return proxyList[res.hnd] || (proxyList[res.hnd] = new Handle(res.hnd));
+        }
+    };
 
-	Handle.prototype.setProp = function (name, value, isAttached) {
-		var isHandle = value instanceof Handle;
-		return this._send('sp', {
-			name: name,
-			valueHnd: isHandle ? value._hnd : void 0,
-			valuePrimitive: isHandle ? void 0 : value,
-			isAttached: isAttached
-		});
-	};
+    Handle.prototype.invoke = function (name, argTypes, argValues) {
+        return this._send('in', {
+            name: name,
+            argTypes: argTypes,
+            argValues: argValues.map(function (value) {
+                var isHandle = value instanceof Handle;
+                return {
+                    valueHnd: isHandle ? value._hnd : void 0,
+                    valuePrimitive: isHandle ? void 0 : value,
+                };
+            })
+        });
+    };
 
-	Handle.prototype.getAtIndex = function (name, index) {
-		return this._send('gi', {
-			name: name,
-			index: index
-		});
-	};
+    Handle.prototype.getProp = function (name, isAttached) {
+        return this._send('gp', {
+            name: name,
+            isAttached: isAttached
+        });
+    };
 
-	Handle.prototype.setAtIndex = function (name, value, index) {
-		return this._send('si', {
-			name: name,
-			value: value,
-			index: index
-		});
-	};
+    Handle.prototype.setProp = function (name, value, isAttached) {
+        var isHandle = value instanceof Handle;
+        return this._send('sp', {
+            name: name,
+            valueHnd: isHandle ? value._hnd : void 0,
+            valuePrimitive: isHandle ? void 0 : value,
+            isAttached: isAttached
+        });
+    };
 
-	Handle.prototype.getEnum = function (name, value) {
-		return this._send('ge', {
-			name: name,
-			value: value
-		});
-	};
+    Handle.prototype.getAtIndex = function (name, index) {
+        return this._send('gi', {
+            name: name,
+            index: index
+        });
+    };
 
-	Handle.prototype.destory = function () {
-		this._send('de', {});
-	};
+    Handle.prototype.setAtIndex = function (name, value, index) {
+        return this._send('si', {
+            name: name,
+            value: value,
+            index: index
+        });
+    };
 
-	Handle.prototype.addEventListener = function(name, cb) {
-		if (!this._listeners[name]) {
-			this._listeners[name] = [];
-			sendNativeMessage('r', 'ae' + JSON.stringify({
-				hnd: this._hnd,
-				name: name
-			}));
-		}
-		this._listeners[name].push(cb);
-	};
+    Handle.prototype.getEnum = function (name, value) {
+        return this._send('ge', {
+            name: name,
+            value: value
+        });
+    };
 
-	Handle.prototype.removeEventListener = function (name, cb) {
-		var idx = this._listeners[name].indexOf(cb);
-		if (!this._listeners[name] || !~idx) {
-			return;
-		}
-		this._listeners[name] = this._listeners[name].splice(idx, 1);
-		this._listeners[name].length || sendNativeMessage('r', 're' + JSON.stringify({
-			hnd: this._hnd,
-			name: name
-		}));
-	};
+    Handle.prototype.destroy = function () {
+        for (var evt in this._listeners) {
+            for (var cb in this._listeners[evt]) {
+                this.removeEventListener(evt, this._listeners[evt][cb]);
+            }
+        }
+        this._send('de', {});
+        delete proxyList[this._hnd];
+    };
 
-	return lang.setObject('Ti.MobileWeb.WP8', {
-		createInstance: function (className, argTypes, argValues) {
-			var hnd;
-			global.handleProxyResponse = function (r) {
-				hnd = r;
-			};
-			sendNativeMessage('r', 'ci' + JSON.stringify({
-				className: className,
-				argTypes: argTypes,
-				argValues: argValues.map(function (value) {
-					var isHandle = value instanceof Handle;
-					return {
-						valueHnd: isHandle ? value._hnd : void 0,
-						valuePrimitive: isHandle ? void 0 : value,
-					};
-				})
-			}));
-			global.handleProxyResponse = void 0;
-			return proxyList[hnd] = new Handle(hnd);
-		},
+    Handle.prototype.addEventListener = function(name, cb) {
+        if (!this._listeners[name]) {
+            this._listeners[name] = [];
+            sendNativeMessage('r', 'ae' + JSON.stringify({
+                hnd: this._hnd,
+                name: name
+            }));
+        }
+        this._listeners[name].push(cb);
+    };
 
-		getRootGrid: function () {
-			if (proxyList.root) {
-				return proxyList.root;
-			}
-			var hnd;
-			global.handleProxyResponse = function (r) {
-				hnd = r;
-			};
-			sendNativeMessage('r', 'gr');
-			global.handleProxyResponse = void 0;
-			return proxyList[hnd] = new Handle(hnd);
-		}
-	});
+    Handle.prototype.removeEventListener = function (name, cb) {
+        var idx = this._listeners[name].indexOf(cb) + 1;
+        if (!this._listeners[name] || !~idx) {
+            return;
+        }
+        this._listeners[name] = this._listeners[name].splice(idx, 1);
+        this._listeners[name].length || sendNativeMessage('r', 're' + JSON.stringify({
+            hnd: this._hnd,
+            name: name
+        }));
+    };
+
+    return lang.setObject('Ti.MobileWeb.WP8', {
+        createInstance: function (className, argTypes, argValues) {
+            var hnd;
+            global.handleProxyResponse = function (r) {
+                hnd = r;
+            };
+            sendNativeMessage('r', 'ci' + JSON.stringify({
+                className: className,
+                argTypes: argTypes,
+                argValues: argValues.map(function (value) {
+                    var isHandle = value instanceof Handle;
+                    return {
+                        valueHnd: isHandle ? value._hnd : void 0,
+                        valuePrimitive: isHandle ? void 0 : value,
+                    };
+                })
+            }));
+            global.handleProxyResponse = void 0;
+            return proxyList[hnd] = new Handle(hnd);
+        },
+
+        getRootGrid: function () {
+            if (proxyList.root) {
+                return proxyList.root;
+            }
+            var hnd;
+            global.handleProxyResponse = function (r) {
+                hnd = r;
+            };
+            sendNativeMessage('r', 'gr');
+            global.handleProxyResponse = void 0;
+            return proxyList[hnd] = new Handle(hnd);
+        },
+
+
+        getPhoneApplicationPage: function () {
+            if (proxyList.app) {
+                return proxyList.app;
+            }
+            var hnd;
+            global.handleProxyResponse = function (r) {
+                hnd = r;
+            };
+            sendNativeMessage('r', 'ga');
+            global.handleProxyResponse = void 0;
+            return proxyList[hnd] = new Handle(hnd);
+        },
+
+        getWebBrowser: function () {
+            if (proxyList.browser) {
+                return proxyList.browser;
+            }
+            var hnd;
+            global.handleProxyResponse = function (r) {
+                hnd = r;
+            };
+            sendNativeMessage('r', 'gw');
+            global.handleProxyResponse = void 0;
+            return proxyList[hnd] = new Handle(hnd);
+        },
+
+        getProxyList: function () { return proxyList;}
+    });
 });
