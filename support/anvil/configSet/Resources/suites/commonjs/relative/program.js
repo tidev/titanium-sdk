86944e3485649b5ab165b11354eef4702d4ddca7
shameer49 <shameerj@exalture.com>
2014-02-27 21:52:11 +0530

    [QE-649] Fix for commonjs test suite

--- support/anvil/configSet/Resources/suites/commonjs/relative/program.js
+++ support/anvil/configSet/Resources/suites/commonjs/relative/program.js
@@ -1,8 +1,8 @@
 
 exports.run = function(testRun, valueOf) {
 	// Actual path from root directory("Resources") to relative/submodule -> Resources/suites/commonjs/relative/submodule
-    var a = require('/suites/commonjs/relative/submodule/a');//Path modified to correct absolute path
-    var b = require('/suites/commonjs/relative/submodule/b');//Path modified to correct absolute path
+    var a = require('./submodule/a');//Path modified to correct absolute path
+    var b = require('./submodule/b');//Path modified to correct absolute path
 
 	valueOf(testRun, a.foo).shouldBe(b.foo);
 	return testRun;
