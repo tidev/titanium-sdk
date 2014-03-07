5f1e2c1292e052f207af519935221ea775346a1b
Pedro Enrique <pedro.tma@gmail.com>
2014-03-07 13:30:23 -0800

    [TIMOB-16554] Removed unnecessary retain/release on request's response

--- iphone/Classes/TiHTTPClient/HTTPClientProxy.m
+++ iphone/Classes/TiHTTPClient/HTTPClientProxy.m
@@ -19,7 +19,6 @@ extern NSString * const TI_APPLICATION_GUID;
 
 - (void)dealloc
 {
-    RELEASE_TO_NIL(response);
     RELEASE_TO_NIL(httpRequest);
     [super dealloc];
 }
@@ -31,8 +30,13 @@ extern NSString * const TI_APPLICATION_GUID;
     	[httpRequest addRequestHeader:@"User-Agent" value:[[TiApp app] userAgent]];
         [httpRequest addRequestHeader:[NSString stringWithFormat:@"%s-%s%s-%s", "X","Tita","nium","Id"] value:TI_APPLICATION_GUID];
         
-}
+    }
     return httpRequest;
+}
+
+-(TiHTTPResponse*)response
+{
+    return [[self request] response];
 }
 
 #pragma mark - Public methods
@@ -171,8 +175,8 @@ extern NSString * const TI_APPLICATION_GUID;
     } else {
         [[self request] setSynchronous:YES];
         [[self request] send];
-        response = [[[self request] response] retain];
         [[TiApp app] stopNetwork];
+        [self forgetSelf];
     }
 }
 
@@ -200,7 +204,7 @@ extern NSString * const TI_APPLICATION_GUID;
 -(NSString*)getResponseHeader:(id)args
 {
     ENSURE_SINGLE_ARG(args, NSString)
-    return [[response headers] valueForKey:args];
+    return [[[self response] headers] valueForKey:args];
 }
 
 # pragma mark - Callback functions
@@ -208,8 +212,6 @@ extern NSString * const TI_APPLICATION_GUID;
 -(void)tiRequest:(TiHTTPRequest *)request onDataStream:(TiHTTPResponse *)tiResponse
 {
     if(hasOndatastream) {
-        RELEASE_TO_NIL(response);
-        response = [tiResponse retain];
         NSTimeInterval currentTime = [[NSDate date] timeIntervalSince1970];
         NSTimeInterval diff = currentTime - _downloadTime;
         if(_downloadTime == 0 || diff > TI_HTTP_REQUEST_PROGRESS_INTERVAL || [tiResponse readyState] == TiHTTPResponseStateDone) {
@@ -227,8 +229,6 @@ extern NSString * const TI_APPLICATION_GUID;
 -(void)tiRequest:(TiHTTPRequest *)request onSendStream:(TiHTTPResponse *)tiResponse
 {
     if(hasOnsendstream) {
-        RELEASE_TO_NIL(response);
-        response = [tiResponse retain];
         NSTimeInterval currentTime = [[NSDate date] timeIntervalSince1970];
         NSTimeInterval diff = currentTime - _uploadTime;
         if(_uploadTime == 0 || diff > TI_HTTP_REQUEST_PROGRESS_INTERVAL || [tiResponse readyState] == TiHTTPResponseStateDone) {
@@ -249,9 +249,7 @@ extern NSString * const TI_APPLICATION_GUID;
     if([request cancelled]) {
         return;
     }
-    RELEASE_TO_NIL(response);
-    response = [tiResponse retain];
-    int responseCode = [response status];
+    int responseCode = [tiResponse status];
     /**
      *    Per customer request, successful communications that resulted in an
      *    4xx or 5xx response is treated as an error instead of an onload.
@@ -273,8 +271,6 @@ extern NSString * const TI_APPLICATION_GUID;
 
 -(void)tiRequest:(TiHTTPRequest *)request onError:(TiHTTPResponse *)tiResponse
 {
-    RELEASE_TO_NIL(response);
-    response = [tiResponse retain];
     [[TiApp app] stopNetwork];
     if([request cancelled]) {
         return;
@@ -293,8 +289,6 @@ extern NSString * const TI_APPLICATION_GUID;
 -(void)tiRequest:(TiHTTPRequest *)request onReadyStateChage:(TiHTTPResponse *)tiResponse
 {
     if(hasOnreadystatechange) {
-        RELEASE_TO_NIL(response);
-        response = [tiResponse retain];
         [self fireCallback:@"onreadystatechange" withArg:nil withSource:self];
     }
 }
@@ -302,8 +296,6 @@ extern NSString * const TI_APPLICATION_GUID;
 -(void)tiRequest:(TiHTTPRequest *)request onRedirect:(TiHTTPResponse *)tiResponse
 {
     if(hasOnredirect) {
-        RELEASE_TO_NIL(response);
-        response = [tiResponse retain];
         [self fireCallback:@"onredirect" withArg:nil withSource:self];
     }
 }
@@ -360,7 +352,7 @@ extern NSString * const TI_APPLICATION_GUID;
 
 -(NSDictionary*)allResponseHeaders
 {
-    return [response headers];
+    return [[self response] headers];
 }
 
 -(NSString*)apiName
@@ -370,7 +362,7 @@ extern NSString * const TI_APPLICATION_GUID;
 
 -(NSNumber*)connected
 {
-    if([[self request] response] == nil) {
+    if([self response] == nil) {
         return NUMBOOL(NO);
     }
     TiHTTPResponseState state = [[[self request] response] readyState];
@@ -383,29 +375,30 @@ extern NSString * const TI_APPLICATION_GUID;
 
 -(NSNumber*)status
 {
-    return NUMINT([response status]);
+    return NUMINT([[self response] status]);
 }
 -(NSString*)location
 {
-    if(response == nil) {
+    if([self response] == nil) {
         return [self valueForUndefinedKey:@"url"];
     }
-    return [response location];
+    return [[self response] location];
 }
 -(NSString*)connectionType
 {
-    if(response == nil) {
+    if([self response] == nil) {
         return [self valueForUndefinedKey:@"method"];
     }
-    return [response connectionType];
+    return [[self response] connectionType];
 }
 -(NSString*)responseText
 {
-    return [response responseString];
+    return [[self response] responseString];
 }
 -(TiBlob*)responseData
 {
-    return [[[TiBlob alloc] initWithData:[response responseData] mimetype:@""] autorelease];
+    NSString *contentType = [TiUtils stringValue: [[self responseHeaders] valueForKey:@"Content-Type"]];
+    return [[[TiBlob alloc] initWithData:[[self response] responseData] mimetype:contentType] autorelease];
 }
 -(TiDOMDocumentProxy*)responseXML
 {
@@ -418,19 +411,19 @@ extern NSString * const TI_APPLICATION_GUID;
 }
 -(NSDictionary*)responseDictionary
 {
-    return [response responseDictionary];
+    return [[self response] responseDictionary];
 }
 -(NSArray*)responseArray
 {
-    return [response responseArray];
+    return [[self response] responseArray];
 }
 -(NSNumber*)readyState
 {
-    return NUMINT([response readyState]);
+    return NUMINT([[self response] readyState]);
 }
 -(NSDictionary*)responseHeaders
 {
-    return [response headers];
+    return [[self response] headers];
 }
 
 MAKE_SYSTEM_NUMBER(UNSENT, NUMINT(TiHTTPResponseStateUnsent))
