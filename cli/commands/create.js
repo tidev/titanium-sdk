15b2130cde2bd84c0e848699aaaa51bd23987bf0
Chris Barber <chris@cb1inc.com>
2014-04-17 16:19:16 -0700

    [TIMOB-16852] When creating a new app and being prompted for the workspace dir and when app.workspace is not set, default to the current directory.

--- cli/commands/create.js
+++ cli/commands/create.js
@@ -276,7 +276,7 @@ exports.config = function config(logger, config, cli) {
 						prompt: function (callback) {
 							callback(fields.file({
 								complete: true,
-								default: workspaceDir || undefined,
+								default: workspaceDir || '.',
 								ignoreDirs: new RegExp(config.get('cli.ignoreDirs')),
 								ignoreFiles: new RegExp(config.get('cli.ignoreFiles')),
 								promptLabel: __('Directory to place project'),
