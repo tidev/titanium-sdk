84521081527d0f99f72add0a73e46528b072e634
srahim <srahim@appcelerator.com>
2014-05-21 15:12:26 -0700

    [TIMOB-16989]Updating AnalyticsModule to work with APSAnaltyics module

---
+++ iphone/Classes/AnalyticsModule.h
@@ -0,0 +1,12 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2014 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+#import "TiModule.h"
+
+@interface AnalyticsModule : TiModule
+
+@end
