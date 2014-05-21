c100e49966614cee4f595599c824e1dea92b2a4f
Chris Barber <chris@cb1inc.com>
2014-05-20 18:37:27 -0700

    [TIMOB-13526] Tweaked the Android run hook to perform better retrying of install and launching the app. [TIMOB-17003] Fixed typo in error message.

--- android/cli/hooks/run.js
+++ android/cli/hooks/run.js
@@ -152,12 +152,34 @@ exports.init = function (logger, config, cli) {
 					// install the app
 					logger.info(__('Installing apk: %s', builder.apkFile.cyan));
 
-					async.series(deviceInfo.map(function (device) {
-						return function (cb) {
-							builder.target == 'device' && logger.info(__('Installing app on device: %s', (device.model || device.manufacturer || device.id).cyan));
+					var failCounter = 0,
+						installTimeout = config.get('android.appInstallTimeout', 2 * 60 * 1000); // 2 minute default
+						retryInterval = config.get('android.appInstallRetryInterval', 15 * 1000); // 15 second default
 
+					async.eachSeries(deviceInfo, function (device, cb) {
+						builder.target == 'device' && logger.info(__('Installing app on device: %s', (device.model || device.manufacturer || device.id).cyan));
+
+						var intervalTimer = null,
+
+							abortTimer = setTimeout(function () {
+								clearTimeout(intervalTimer);
+								logger.error(__('Application failed to install') + '\n');
+								logger.log(__('The current timeout is set to %s ms', String(installTimeout).cyan));
+								logger.log(__('You can increase this timeout by running: %s', (cli.argv.$ + ' config android.appInstallTimeout <timeout ms>').cyan) + '\n');
+								if (++failCounter >= deviceInfo.length) {
+									process.exit(1);
+								}
+							}, installTimeout);
+
+						(function installApp() {
 							adb.installApp(device.id, builder.apkFile, function (err) {
 								if (err) {
+									if (err instanceof Error && err.message.indexOf('Could not access the Package Manager') != -1) {
+										logger.debug(__('Package manager not started yet, trying again in %sms...', retryInterval));
+										intervalTimer = setTimeout(installApp, retryInterval);
+										return;
+									}
+
 									logger.error(__('Failed to install apk on "%s"', device.id));
 									err = err.toString();
 									err.split('\n').forEach(logger.error);
@@ -168,11 +190,14 @@ exports.init = function (logger, config, cli) {
 									process.exit(1);
 								}
 
+								clearTimeout(intervalTimer);
+								clearTimeout(abortTimer);
+
 								logger.info(__('App successfully installed'));
 								cb();
 							});
-						};
-					}), next);
+						})();
+					}, next);
 				},
 
 				function (next) {
@@ -238,7 +263,7 @@ exports.init = function (logger, config, cli) {
 								logBuffer = logBuffer.concat(data.trim().split('\n'));
 							}
 						}, function () {
-							if (--instances == 0) {
+							if (--instances == 0 && !displayStartLog) {
 								// the adb server shutdown, the emulator quit, or the device was unplugged
 								var endLogTxt = __('End application log');
 								logger.log(('-- ' + endLogTxt + ' ' + (new Array(75 - endLogTxt.length)).join('-')).grey + '\n');
@@ -249,7 +274,7 @@ exports.init = function (logger, config, cli) {
 
 					// listen for ctrl-c
 					process.on('SIGINT', function () {
-						if (!endLog) {
+						if (!endLog && !displayStartLog) {
 							var endLogTxt = __('End application log');
 							logger.log('\r' + ('-- ' + endLogTxt + ' ' + (new Array(75 - endLogTxt.length)).join('-')).grey + '\n');
 						}
@@ -261,39 +286,61 @@ exports.init = function (logger, config, cli) {
 
 				function (next) {
 					logger.info(__('Starting app: %s', (builder.appid + '/.' + builder.classname + 'Activity').cyan));
-					async.series(deviceInfo.map(function (device) {
-						return function (cb) {
+
+					var failCounter = 0,
+						retryInterval = config.get('android.appStartRetryInterval', 30 * 1000), // 30 second default
+						startTimeout = config.get('android.appStartTimeout', 2 * 60 * 1000); // 2 minute default
+
+					async.eachSeries(deviceInfo, function (device, cb) {
+						var watchingPid = false,
+
+							intervalTimer = null,
+
+							abortTimer = setTimeout(function () {
+								clearTimeout(intervalTimer);
+								logger.error(__('Application failed to launch') + '\n');
+								logger.log(__('The current timeout is set to %s ms', String(startTimeout).cyan));
+								logger.log(__('You can increase this timeout by running: %s', (cli.argv.$ + ' config android.appStartTimeout <timeout ms>').cyan) + '\n');
+								if (++failCounter >= deviceInfo.length) {
+									process.exit(1);
+								}
+							}, startTimeout);
+
+						(function startApp() {
+							logger.debug(__('Trying to start the app...'));
 							adb.startApp(device.id, builder.appid, builder.classname + 'Activity', function (err) {
-								var timeout = config.get('android.appStartTimeout', 2 * 60 * 1000),  // 2 minute default
-									waitUntil = Date.now() + timeout,
-									done = false;
+								if (watchingPid) return;
+								watchingPid = true;
+
+								var done = false;
 
 								async.whilst(
 									function () { return !done; },
 									function (cb2) {
-										if (Date.now() > waitUntil) {
-											logger.error(__('Application failed to launch') + '\n');
-											logger.log(__('The current timeout is set to %s ms', String(timeout).cyan));
-											logger.log(__('You can increase this timeout by running: %s', (cli.argv.$ + ' config android.emulatorStartTimeout <timeout ms>').cyan) + '\n');
-											process.exit(1);
-										}
-
 										adb.getPid(device.id, builder.appid, function (err, pid) {
 											if (err || !pid) {
-												setTimeout(cb2, 100);
+												setTimeout(cb2, 250);
 											} else {
+												clearTimeout(intervalTimer);
+												clearTimeout(abortTimer);
+
 												logger.info(__('Application pid: %s', String(pid).cyan));
 												device.appPidRegExp = new RegExp('\\(\\s*' + pid + '\\)\:');
 												done = true;
-												cb2();
+												setTimeout(cb2, 0);
 											}
 										});
 									},
 									cb
 								);
 							});
-						};
-					}), next);
+
+							intervalTimer = setTimeout(function () {
+								logger.debug(__('App still not started, trying again'));
+								startApp();
+							}, retryInterval);
+						})();
+					}, next);
 				},
 
 				function (next) {
