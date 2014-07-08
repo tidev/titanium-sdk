2f4c951bbac1a0ddbb2c21aa99f58272d4bc92df
Vishal Duggal <vishalduggal@gmail.com>
2014-07-08 10:15:00 -0700

    [TIMOB-16957] Proper fix for focus/blur of base windows

--- iphone/Classes/TiUIiPadPopoverProxy.m
+++ iphone/Classes/TiUIiPadPopoverProxy.m
@@ -10,6 +10,7 @@
 #import "TiUIiPadPopover.h"
 #import "TiUtils.h"
 #import "TiWindowProxy.h"
+#import "TiApp.h"
 #import <libkern/OSAtomic.h>
 
 #ifdef USE_TI_UITABLEVIEW
@@ -348,6 +349,14 @@ static NSArray* popoverSequence;
 		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(updatePopover:) name:UIApplicationWillChangeStatusBarOrientationNotification object:nil];
         if (contentViewProxy != nil) {
             if ([contentViewProxy isKindOfClass:[TiWindowProxy class]]) {
+                UIView* topWindowView = [[[TiApp app] controller] topWindowProxyView];
+                if ([topWindowView isKindOfClass:[TiUIView class]]) {
+                    TiViewProxy* theProxy = (TiViewProxy*)[(TiUIView*)topWindowView proxy];
+                    if ([theProxy conformsToProtocol:@protocol(TiWindowProtocol)]) {
+                        [(id<TiWindowProtocol>)theProxy resignFocus];
+                    }
+                }
+                [(TiWindowProxy*)contentViewProxy setIsManaged:YES];
                 [(TiWindowProxy*)contentViewProxy open:nil];
                 [(TiWindowProxy*) contentViewProxy gainFocus];
                 [self updatePopoverNow];
@@ -497,9 +506,6 @@ static NSArray* popoverSequence;
 		currentlyDisplaying = nil;
 	}
     if (contentViewProxy != nil) {
-        if ([contentViewProxy isKindOfClass:[TiWindowProxy class]]) {
-            [(TiWindowProxy*) contentViewProxy setIsManaged:NO];
-        }
         [contentViewProxy windowWillClose];
     } else {
         [self windowWillClose];
@@ -509,6 +515,15 @@ static NSArray* popoverSequence;
 	[[NSNotificationCenter defaultCenter] removeObserver:self name:UIApplicationWillChangeStatusBarOrientationNotification object:nil];
     if (contentViewProxy != nil) {
         [contentViewProxy windowDidClose];
+        if ([contentViewProxy isKindOfClass:[TiWindowProxy class]]) {
+            UIView* topWindowView = [[[TiApp app] controller] topWindowProxyView];
+            if ([topWindowView isKindOfClass:[TiUIView class]]) {
+                TiViewProxy* theProxy = (TiViewProxy*)[(TiUIView*)topWindowView proxy];
+                if ([theProxy conformsToProtocol:@protocol(TiWindowProtocol)]) {
+                    [(id<TiWindowProtocol>)theProxy gainFocus];
+                }
+            }
+        }
     } else {
         [self windowDidClose];
     }
