a9e144737ac03a2fddded9042c9b7b6a0be70d22
Vishal Duggal <vishalduggal@gmail.com>
2014-07-09 11:49:27 -0700

    [TIMOB-17096] Update image of UIImageView when possible

--- iphone/Classes/TiUIWindowProxy.m
+++ iphone/Classes/TiUIWindowProxy.m
@@ -818,9 +818,13 @@
 		NSURL * path = [TiUtils toURL:[self valueForKey:@"titleImage"] proxy:self];
 		//Todo: This should be [TiUtils navBarTitleViewSize] with the thumbnail scaling. For now, however, we'll go with auto.
 		UIImage *image = [[ImageLoader sharedLoader] loadImmediateImage:path withSize:CGSizeZero];
-		if (image!=nil)
-		{
-			newTitleView = [[[UIImageView alloc] initWithImage:image] autorelease];
+		if (image!=nil) {
+			if ([oldView isKindOfClass:[UIImageView class]]) {
+				[(UIImageView*)oldView setImage:image];
+				newTitleView = oldView;
+			} else {
+				newTitleView = [[[UIImageView alloc] initWithImage:image] autorelease];
+			}
 		}
 	}
 
