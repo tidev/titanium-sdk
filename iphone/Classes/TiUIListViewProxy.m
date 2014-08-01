9ab883d2fd02f0ddd7f54c00c9b898dd788665d6
Vishal Duggal <vishalduggal@gmail.com>
2014-08-01 13:03:19 -0700

    [TIMOB-14543] Signal content change to proxy

--- iphone/Classes/TiUIListViewProxy.m
+++ iphone/Classes/TiUIListViewProxy.m
@@ -138,6 +138,7 @@
 			Block_release(block);
 		} else {
 			[self.listView updateIndicesForVisibleRows];
+			[self contentsWillChange];
 			return;
 		}
 	}
@@ -241,6 +242,7 @@
 			section.sectionIndex = idx;
 		}];
 		[tableView reloadData];
+		[self contentsWillChange];
 	}];
 	[insertedSections release];
 }
