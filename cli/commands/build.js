22a82373e07ab9eb0469365fe40dd21a842be4f5
Feon Sua <fmiao@appcelerator.com>
2014-08-27 16:39:53 +0800

    [TIMOB-17227] Remove typeof === 'boolean' check

--- cli/commands/build.js
+++ cli/commands/build.js
@@ -96,14 +96,13 @@ exports.config = function (logger, config, cli) {
 						'project-dir': {
 							abbr: 'd',
 							callback: function (projectDir) {
-								if (typeof projectDir === 'boolean' || projectDir === '') {
-									// no option value was specified, the value was set to TRUE
+								if (projectDir === '') {
+									// no option value was specified
 									// check if current directory is a valid dir
 									// if not output meaningful error message
 									projectDir = conf.options['project-dir'].default;
-									if(!fs.existsSync(path.join(appc.fs.resolvePath(''), 'tiapp.xml'))) {
+									if (!fs.existsSync(path.join(projectDir, 'tiapp.xml'))) {
 										logger.error(__('Invalid project directory "%s" because tiapp.xml not found', projectDir) + '\n');
-										logger.log();
 										process.exit(1);
 									}
 								}
