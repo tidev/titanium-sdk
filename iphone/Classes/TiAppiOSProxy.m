94192eb7b589bea095ab11b164e5f393b431f251
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2014-09-04 09:12:19 -0700

    TC-4654 : iOS 8 : Add isRegisteredForRemoteNotifications to determine if Registered for Notifications
    Add support for isRegisteredForRemoteNotifications so that the developer can check if the user has authorized notifications.
    
    var hasAuthorized = Ti.App.iOS.isRegisteredForRemoteNotifications();
    if(hasAuthorized){
    	Ti.API.info("User has already authorized");
    }else{
    	Ti.API.info("User needs to authorize, maybe ask them before the iOS rights dialog pops up");
    }

--- iphone/Classes/TiAppiOSProxy.m
+++ iphone/Classes/TiAppiOSProxy.m
@@ -500,6 +500,14 @@
 	return NUMINT(0);
 }
 
+-(NSNumber*)isRegisteredForRemoteNotifications:(id)unused
+{
+    if ([TiUtils isIOS8OrGreater]) {
+        return NUMBOOL([[UIApplication sharedApplication] isRegisteredForRemoteNotifications]);
+    }
+    return NUMBOOL(YES);
+}
+
 MAKE_SYSTEM_STR(EVENT_ACCESSIBILITY_LAYOUT_CHANGED,@"accessibilitylayoutchanged");
 MAKE_SYSTEM_STR(EVENT_ACCESSIBILITY_SCREEN_CHANGED,@"accessibilityscreenchanged");
 
