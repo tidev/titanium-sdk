3b87152bbbec0cf950dc1ee01cf005cec866e13d
Chris Barber <chris@cb1inc.com>
2014-09-16 14:49:01 -0700

    [TIMOB-17710] Updated ioslib and fields npm modules. Fixed a small typo in the the build commands when displaying a module's minsdk.

--- node_modules/fields/lib/types/file.js
+++ node_modules/fields/lib/types/file.js
@@ -17,51 +17,55 @@ function File(opts) {
 util.inherits(File, Prompter);
 
 File.prototype._complete = function _complete(value, callback) {
-	var caseSensitive = process.platform != 'win32',
-		p = value.lastIndexOf(path.sep) + 1,
-		filename = p == 0 ? '' : caseSensitive ? value.substring(p) : value.substring(p).toLowerCase(),
-		dir = p == 0 ? value : value.substring(0, p),
-		absDir = common.resolvePath(dir),
+	var isWin = process.platform === 'win32',
+		caseSensitive = !isWin,
+		p = (isWin ? Math.max(value.lastIndexOf('/'), value.lastIndexOf('\\')) : value.lastIndexOf(path.sep)) + 1,
+		filename = p === 0 ? '' : caseSensitive ? value.substring(p) : value.substring(p).toLowerCase(),
+		dir = p === 0 ? value : value.substring(0, p),
+		absDir = common.resolvePath(isWin ? dir.replace(/\//g, '\\') : dir.replace(/\\ /g, ' ')),
 		matches = [],
 		shortestMatch = null;
 
 	if (fs.existsSync(absDir) && fs.statSync(absDir).isDirectory()) {
-		fs.readdirSync(absDir).forEach(function (name) {
-			var file = path.join(absDir, name),
-				isDirectory = fs.existsSync(file) && fs.statSync(file).isDirectory();
-			if ((!isDirectory || !this.ignoreDirs || !this.ignoreDirs.test(name))
-				&& (isDirectory || !this.ignoreFiles || !this.ignoreFiles.test(name))
-				&& (this.showHidden || name.charAt(0) != '.')
-				&& (filename == ''
-					|| (caseSensitive && name.indexOf(filename) == 0)
-					|| (!caseSensitive && name.toLowerCase().indexOf(filename) == 0))
-			) {
-				try {
-					if (isDirectory) {
-						matches.push([name, true]);
-					} else {
+		// we have to try/catch just in case we don't have access
+		try {
+			fs.readdirSync(absDir).forEach(function (name) {
+				var file = path.join(absDir, name),
+					isDirectory = fs.existsSync(file) && fs.statSync(file).isDirectory();
+				if ((!isDirectory || !this.ignoreDirs || !this.ignoreDirs.test(name))
+					&& (isDirectory || !this.ignoreFiles || !this.ignoreFiles.test(name))
+					&& (this.showHidden || name.charAt(0) !== '.')
+					&& (filename === ''
+						|| (caseSensitive && name.indexOf(filename) === 0)
+						|| (!caseSensitive && name.toLowerCase().indexOf(filename) === 0))
+				) {
+					try {
+						if (isDirectory) {
+							matches.push([name, true]);
+						} else {
+							matches.push([name, false]);
+						}
+					} catch (ex) {
 						matches.push([name, false]);
 					}
-				} catch (ex) {
-					matches.push([name, false]);
-				}
-				if (filename) {
-					if (shortestMatch === null) {
-						shortestMatch = name;
-					} else {
-						for (var i = 0; i < name.length && i < shortestMatch.length; i++) {
-							if (name[i] != shortestMatch[i]) {
-								shortestMatch = shortestMatch.substring(0, i);
-								break;
+					if (filename) {
+						if (shortestMatch === null) {
+							shortestMatch = name;
+						} else {
+							for (var i = 0; i < name.length && i < shortestMatch.length; i++) {
+								if (name[i] !== shortestMatch[i]) {
+									shortestMatch = shortestMatch.substring(0, i);
+									break;
+								}
 							}
 						}
 					}
 				}
-			}
-		}.bind(this));
+			}.bind(this));
+		} catch (ex) {}
 	}
 
-	if (matches.length == 1) {
+	if (matches.length === 1) {
 		// only 1 match, so just return it now
 		callback(path.join(dir, matches[0][0]) + (matches[0][1] ? path.sep : ''));
 	} else if (matches.length > 1) {
