1423554330d6ae262c529d63869dfdedd6954828
jonalter <jonalter+github@gmail.com>
2014-09-16 14:25:01 -0700

    [TIMOB-17707] changing Ti.App.iOS event name from 'backgroundNotification' to 'localnotificationaction'

--- iphone/Classes/TiBase.m
+++ iphone/Classes/TiBase.m
@@ -153,7 +153,7 @@ NSString * const kTiURLDowloadProgress = @"TiDownloadProgress";
 NSString * const kTiURLUploadProgress = @"TiUploadProgress";
 NSString * const kTiFrameAdjustNotification = @"TiFrameAdjust";
 NSString * const kTiLocalNotification = @"TiLocalNotification";
-NSString * const kTiBackgroundLocalNotification = @"TiBackgroundLocalNotification";
+NSString * const kTiLocalNotificationAction = @"TiLocalNotificationAction";
 NSString * const kTiUserNotificationSettingsNotification = @"TiUserNotificationSettingsNotification";
 
 NSString* const kTiBehaviorSize = @"SIZE";
