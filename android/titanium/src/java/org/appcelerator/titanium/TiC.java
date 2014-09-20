aaf7cbfdfd7e7607195c15530aac24cbd437f0c4
salachi <salachi@hotmail.com>
2014-09-20 18:38:58 +0530

    TIMOB-16376-Exposed new webview property to control multiple windows

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -2517,6 +2517,11 @@ public class TiC
 	 */
 	public static final String PROPERTY_MAX_DATE = "maxDate";
 	
+	/**
+	 * @module.api
+	 */
+	public static final String PROPERTY_SUPPORT_MULTIPLE_WINDOWS = "supportMultipleWindows";
+	
 	public static final String SIZE_AUTO = "auto";
 	public static final String URL_APP_PREFIX = "app://";
 	public static final String URL_APP_SCHEME = "app";
