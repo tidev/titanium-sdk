ac7fe6ab9c92b873151132c80fb20359cf88af32
Jon Durbin <jond@greenviewdata.com>
2014-11-06 15:06:19 -0500

    Add support for onsendstream callback to android TiHTTPClient when sending StringEntity

--- android/modules/network/src/java/ti/modules/titanium/network/TiHTTPClient.java
+++ android/modules/network/src/java/ti/modules/titanium/network/TiHTTPClient.java
@@ -1397,6 +1397,7 @@ public class TiHTTPClient
 
 	private void handleURLEncodedData(UrlEncodedFormEntity form)
 	{
+		final long contentLength;
 		AbstractHttpEntity entity = null;
 		if (data instanceof String) {
 			try {
@@ -1422,6 +1423,17 @@ public class TiHTTPClient
 			}
 			HttpEntityEnclosingRequest e = (HttpEntityEnclosingRequest)request;
 			e.setEntity(entity);
+
+			contentLength = entity.getContentLength();
+			ProgressEntity progressEntity = new ProgressEntity(entity, new ProgressListener() {
+				public void progress(int progress) {
+					KrollDict data = new KrollDict();
+					data.put("progress", ((double)progress)/contentLength);
+					dispatchCallback("onsendstream", data);
+				}
+			});
+			e.setEntity(progressEntity);
+			e.addHeader("Length", contentLength + "");
 		}
 	}
 
