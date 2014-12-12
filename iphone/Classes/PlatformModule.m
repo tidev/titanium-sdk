4304065140a9a036e6a031c2e5876dbbf5b5151e
jonalter <jonalter+github@gmail.com>
2014-12-12 10:28:10 -0800

    [TIMOB-18193] iOS: Ti.Platform.ostype/architecture support 64-bit

--- iphone/Classes/PlatformModule.m
+++ iphone/Classes/PlatformModule.m
@@ -36,7 +36,11 @@ NSString* const DATA_IFACE = @"pdp_ip0";
 		version = [[theDevice systemVersion] retain];
 		processorCount = [[NSNumber numberWithInt:1] retain];
 		username = [[theDevice name] retain];
+#ifdef __LP64__
+		ostype = [@"64bit" retain];
+#else
 		ostype = [@"32bit" retain];
+#endif
 		
 		if ([TiUtils isIPad])
 		{
@@ -54,8 +58,6 @@ NSString* const DATA_IFACE = @"pdp_ip0";
 		// attempt to determine extended phone info
 		struct utsname u;
 		uname(&u);
-		
-		NSString *arch = @"arm";
 		
 		// detect iPhone 3G model
 		if (!strcmp(u.machine, "iPhone1,2")) 
@@ -86,19 +88,17 @@ NSString* const DATA_IFACE = @"pdp_ip0";
 		else if (!strcmp(u.machine, "i386")) 
 		{
 			model = [@"Simulator" retain];
-			arch = @"i386";
 		}
 		// detect simulator for x86_64
 		else if (!strcmp(u.machine, "x86_64")) 
 		{
 			model = [@"Simulator" retain];
-			arch = @"x86_64";
 		}
 		else 
 		{
 			model = [[NSString alloc] initWithUTF8String:u.machine];
 		}
-		architecture = [arch retain];
+		architecture = [[TiUtils currentArchitecture] retain];
 
 		// needed for platform displayCaps orientation to be correct
 		[[UIDevice currentDevice] beginGeneratingDeviceOrientationNotifications];
