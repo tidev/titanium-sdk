0420116e621d4fd1700e4c4c43062a65bec0541a
vishalduggal <vduggal@appcelerator.com>
2015-01-26 09:45:04 -0800

    [TIMOB-17675] Hook up TiWindowProtocol

--- iphone/Classes/TiUIiOSSplitWindowProxy.m
+++ iphone/Classes/TiUIiOSSplitWindowProxy.m
@@ -46,5 +46,157 @@
     }
 }
 
+#pragma mark - TiViewProxy Overrides
+-(void) windowWillOpen
+{
+    if ([self viewInitialized]) {
+        TiThreadPerformOnMainThread(^{
+            [(TiUIiOSSplitWindow*)self.view initWrappers];
+        }, YES);
+    }
+    [super windowWillOpen];
+}
+
+-(void) windowWillClose
+{
+    if ([self viewInitialized]) {
+        TiThreadPerformOnMainThread(^{
+            [(TiUIiOSSplitWindow*)self.view cleanup];
+        }, YES);
+    }
+    [super windowWillOpen];
+}
+
+#pragma mark - TiWindowProtocol handler
+
+-(void)gainFocus
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView gainFocus];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView gainFocus];
+    }
+    [super gainFocus];
+}
+
+-(void)resignFocus
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView resignFocus];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView resignFocus];
+    }
+    [super resignFocus];
+}
+
+-(BOOL)_handleOpen:(id)args
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if (![masterView isKindOfClass:[TiViewProxy class]]) {
+        DebugLog(@"masterView property must be set to an object of type TiViewProxy");
+        return NO;
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if (![detailView isKindOfClass:[TiViewProxy class]]) {
+        DebugLog(@"detailView property must be set to an object of type TiViewProxy");
+        return NO;
+    }
+    
+    return [super _handleOpen:args];
+}
+
+-(void)viewWillAppear:(BOOL)animated
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView viewWillAppear:animated];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView viewWillAppear:animated];
+    }
+    [super viewWillAppear:animated];
+    
+}
+-(void)viewWillDisappear:(BOOL)animated
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView viewWillDisappear:animated];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView viewWillDisappear:animated];
+    }
+    [super viewWillDisappear:animated];
+}
+-(void)viewDidAppear:(BOOL)animated
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView viewDidAppear:animated];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView viewDidAppear:animated];
+    }
+    [super viewDidAppear:animated];
+}
+-(void)viewDidDisappear:(BOOL)animated
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView viewDidDisappear:animated];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView viewDidDisappear:animated];
+    }
+    [super viewDidDisappear:animated];
+}
+-(void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    }
+    [super willAnimateRotationToInterfaceOrientation:toInterfaceOrientation duration:duration];
+}
+-(void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    }
+    [super willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+}
+-(void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    id masterView = [self valueForUndefinedKey:@"masterView"];
+    if ([masterView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)masterView didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    }
+    id detailView = [self valueForUndefinedKey:@"detailView"];
+    if ([detailView conformsToProtocol:@protocol(TiWindowProtocol)]) {
+        [(id<TiWindowProtocol>)detailView didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    }
+    [super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+}
+
+
 @end
 #endif
