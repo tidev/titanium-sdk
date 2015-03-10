1e761039951e7c7c4aa1387b0d9640948af260a8
Ingo Muschenetz <ingo@Grace.local>
2015-03-10 11:39:39 -0700

    Use grunt to bump versions

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,17 @@ module.exports = function (grunt) {
         clangFormat: {
             src: [] // unused ATM
         },
+		version: {
+			patchTitaniumVersion: {
+				options: {
+					prefix: '^version\\s+=\\s+[\'"]'
+				},
+				src: ['build/titanium_version.py']
+			},
+			patchPackage: {
+				src: ['package.json']
+			}
+		}
 	});
 
 	// Load grunt plugins for modules
@@ -15,12 +26,19 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-appc-js');
 	grunt.loadNpmTasks('grunt-clang-format');
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-version');
 
 	// register tasks
 	grunt.registerTask('lint', ['appcJs']);
 
 	// register tasks
 	grunt.registerTask('format', ['clangFormat']);
+
+	// register tasks
+	grunt.registerTask('bump', ['version::patch']);
+	grunt.registerTask('bump:major', ['version::major']);
+	grunt.registerTask('bump:minor', ['version::minor']);
+	grunt.registerTask('bump:patch', ['version::patch']);
 
 	// register tasks
 	grunt.registerTask('default', ['lint']);
