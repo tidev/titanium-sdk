c8cc2b1e14682eec5e60bf4df9c2d9d127f1bb25
Seth Benjamin <animecyc@gmail.com>
2015-07-10 23:22:25 -0400

    iOS: Add recursive cell dequeue notifier
    Allows for module maintainers to know when a cell is dequeued and respond accordingly

--- iphone/Classes/TiUIListView.m
+++ iphone/Classes/TiUIListView.m
@@ -23,6 +23,7 @@
 @end
 
 static TiViewProxy * FindViewProxyWithBindIdContainingPoint(UIView *view, CGPoint point);
+static void NotifyRootDidDequeueRecursive(NSArray *children);
 
 @implementation TiUIListView {
     UITableView *_tableView;
@@ -1381,6 +1382,9 @@ static TiViewProxy * FindViewProxyWithBindIdContainingPoint(UIView *view, CGPoin
 
     cell.dataItem = item;
     cell.proxy.indexPath = realIndexPath;
+
+    NotifyRootDidDequeueRecursive(cell.proxy.children);
+
     return cell;
 }
 
@@ -2046,6 +2050,17 @@ static TiViewProxy * FindViewProxyWithBindIdContainingPoint(UIView *view, CGPoin
         }
     }
     return nil;
+}
+
+static void NotifyRootDidDequeueRecursive(NSArray *children)
+{
+    [children enumerateObjectsUsingBlock:^(TiViewProxy *child, NSUInteger idx, BOOL *stop) {
+        if([child respondsToSelector:@selector(rootDidDequeue)]) {
+            [child performSelector:@selector(rootDidDequeue)];
+        }
+
+        NotifyRootDidDequeueRecursive(child.children);
+    }];
 }
 
 #endif
