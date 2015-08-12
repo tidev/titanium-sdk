610bdf01e554f87824de8287f79a534ad780d813
Chris Barber <chris@cb1inc.com>
2015-08-12 12:42:32 -0500

    Updated npm deps.

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,22 +36,34 @@ module.exports = function (grunt) {
 			return done(new Error('You must run this using io.js 3.0'));
 		}
 
+		grunt.log.writeln('Removing old Node modules');
 		(function rm(dir, ignore) {
 			fs.existsSync(dir) && fs.readdirSync(dir).forEach(function (name) {
 				var file = path.join(dir, name);
+
+				if (ignore && ignore.indexOf(name) !== -1) {
+					grunt.log.writeln('Skipping ' + name);
+					return;
+				} else if (ignore) {
+					grunt.log.writeln('Removing ' + name);
+				}
+
 				if (fs.existsSync(file) && fs.statSync(file).isDirectory() && (!ignore || ignore.indexOf(name) === -1)) {
-					rm(dir);
+					rm(file);
+					fs.rmdirSync(file);
 				} else {
 					fs.unlinkSync(file);
 				}
 			});
 		}(path.join(__dirname, 'node_modules'), ['sqlite3', 'titanium-sdk']));
 
+		grunt.log.writeln('Running npm install');
 		exec('npm install', function (err, stdout, stderr) {
 			if (err) {
 				return done(err);
 			}
 
+			grunt.log.writeln('Building node-ios-device binaries');
 			exec('make', { cwd: path.join(__dirname, 'node_modules', 'ioslib', 'node_modules', 'node-ios-device') }, function (err, stdout, stderr) {
 				done(err);
 			});
