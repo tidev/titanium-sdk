9126d3aab2a5c355f8140241d2821d47df7a2f0f
cheekiatng <cng@appcelerator.com>
2015-08-26 11:11:46 +0800

    code and doc fix with correct notation

--- iphone/Classes/TiAppiOSUserActivityProxy.m
+++ iphone/Classes/TiAppiOSUserActivityProxy.m
@@ -113,8 +113,8 @@
                                               [TiUtils stringValue:@"expirationDate" properties:props]]];
         }
         
-        if([props objectForKey:@"keyWords"]){
-            [_userActivity setKeywords:[NSSet setWithArray:[props objectForKey:@"keyWords"]]];
+        if([props objectForKey:@"keywords"]){
+            [_userActivity setKeywords:[NSSet setWithArray:[props objectForKey:@"keywords"]]];
         }
         
         if([props objectForKey:@"requiredUserInfoKeys"]){
@@ -398,7 +398,7 @@
     [_userActivity setRequiredUserInfoKeys:[NSSet setWithArray:keys]];
 }
 
--(NSArray*)keyWords
+-(NSArray*)keywords
 {
     if(![TiUtils isIOS9OrGreater]){
         return;
