128d59716db088eeb7de8c320f3b13dfd6588130
Ashraf <msamah@appcelerator.com>
2015-09-21 11:40:34 +0800

    [TIMOB-19435] Allow for setting of the Title

--- android/modules/ui/src/java/ti/modules/titanium/ui/android/TiPreferencesFragment.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/android/TiPreferencesFragment.java
@@ -10,19 +10,23 @@ import android.preference.PreferenceFragment;
 public class TiPreferencesFragment extends PreferenceFragment {
     
     private static final String TAG = "TiPreferencesFragment";
+    private CharSequence title = null;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         String prefsName = getArguments().getString(TiPreferencesActivity.PREFS_KEY);
-        
+
         // Find the layout file, do nothing if not found
         try {
             getPreferenceManager().setSharedPreferencesName(TiApplication.APPLICATION_PREFERENCES_NAME);
             int resid = TiRHelper.getResource("xml." + prefsName);
             if (resid != 0) {
                 addPreferencesFromResource(resid);
+                if (getPreferenceScreen() != null) {
+                	title = getPreferenceScreen().getTitle();
+                }
             } else {
                 Log.e(TAG, "xml." + prefsName + " preferences not found.");
                 return;
@@ -32,4 +36,13 @@ public class TiPreferencesFragment extends PreferenceFragment {
             return;
         }
     }
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		if (title != null && getActivity() != null) {
+			getActivity().setTitle(title);
+		}
+	}
+
 }
