20b96f0bc08a210764696e8034d24dc61d6abd2c
Feon Sua <feonsua@Feons-MacBook-Pro.local>
2015-10-21 19:34:40 -0700

    simplify if/else conditions

--- iphone/cli/commands/_build.js
+++ iphone/cli/commands/_build.js
@@ -4054,7 +4054,11 @@ iOSBuilder.prototype.copyResources = function copyResources(next) {
 
 					case 'png':
 					case 'jpg':
-						useAppThinning ? imageAssets[relPath] = info : resourcesToCopy[relPath] = info;
+						if (useAppThinning) {
+							imageAssets[relPath] = info
+						} else {
+							resourcesToCopy[relPath] = info;
+						}
 						break;
 
 					case 'html':
@@ -4541,13 +4545,12 @@ iOSBuilder.prototype.copyResources = function copyResources(next) {
 		},
 
 		function createAssetImageSets() {
-			if (this.useAppThinning) {
-				this.logger.info(__('Creating assets image set'));
-			} else {
-				this.logger.info(__('Skip creating assets image set'));
+			if (!this.useAppThinning) {
+				this.logger.info(__('App thinning disabled, skipping asset image sets'));
 				return;
 			}
 
+			this.logger.info(__('Creating assets image set'));
 			var assetCatalog = path.join(this.buildDir, 'Assets.xcassets'),
 				imageSets = {},
 				imageNameRegExp = /^(.*?)(@[23]x)?(~iphone|~ipad)?\.(png|jpg)$/;
