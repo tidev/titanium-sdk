bda06291306269abaa2ddb9948509a453adfb0f7
Feon Sua <feonsua@Feons-MacBook-Pro.local>
2015-10-21 18:27:48 -0700

    [TIMOB-19757] Cannot access files in Resources directory with 5.2.x

--- iphone/cli/commands/_build.js
+++ iphone/cli/commands/_build.js
@@ -2093,6 +2093,11 @@ iOSBuilder.prototype.initialize = function initialize() {
 		// force appcelerator.com to not be whitelisted in the Info.plist ATS section
 		this.whitelistAppceleratorDotCom = false;
 	}
+
+	this.useAppThinning = false;
+	if (this.tiapp.properties && this.tiapp.properties.hasOwnProperty('use-app-thinning') && this.tiapp.properties['use-app-thinning'].value === true) {
+		this.useAppThinning = true;
+	}
 };
 
 iOSBuilder.prototype.loginfo = function loginfo() {
@@ -3978,6 +3983,8 @@ iOSBuilder.prototype.writeDebugProfilePlists = function writeDebugProfilePlists(
 iOSBuilder.prototype.copyResources = function copyResources(next) {
 	var filenameRegExp = /^(.*)\.(\w+)$/,
 
+		useAppThinning = this.useAppThinning,
+
 		appIcon = this.tiapp.icon.match(filenameRegExp),
 
 		ignoreDirs = this.ignoreDirs,
@@ -4047,7 +4054,7 @@ iOSBuilder.prototype.copyResources = function copyResources(next) {
 
 					case 'png':
 					case 'jpg':
-						imageAssets[relPath] = info;
+						useAppThinning ? imageAssets[relPath] = info : resourcesToCopy[relPath] = info;
 						break;
 
 					case 'html':
@@ -4534,7 +4541,13 @@ iOSBuilder.prototype.copyResources = function copyResources(next) {
 		},
 
 		function createAssetImageSets() {
-			this.logger.info(__('Creating assets image set'));
+			if (this.useAppThinning) {
+				this.logger.info(__('Creating assets image set'));
+			} else {
+				this.logger.info(__('Skip creating assets image set'));
+				return;
+			}
+
 			var assetCatalog = path.join(this.buildDir, 'Assets.xcassets'),
 				imageSets = {},
 				imageNameRegExp = /^(.*?)(@[23]x)?(~iphone|~ipad)?\.(png|jpg)$/;
