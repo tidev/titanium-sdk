5b1e2f2f3b4cb6568b7a7ce7f7bfae61563e2f6f
Ashraf <msamah@appcelerator.com>
2015-10-27 15:47:24 +0800

    [TIMOB-19784] Android: Cloud Files Create Error

--- android/modules/network/src/java/ti/modules/titanium/network/TiHTTPClient.java
+++ android/modules/network/src/java/ti/modules/titanium/network/TiHTTPClient.java
@@ -1269,8 +1269,6 @@ public class TiHTTPClient
 	    	    printWriter.append("Content-Type: " + contentBody.getMimeType());
 	    	    if (contentBody.getCharset() != null) {
 	    	        printWriter.append("; charset=" + contentBody.getCharset());
-	    	    } else {
-	    	        printWriter.append(";");
 	    	    }
 	    	    printWriter.append(LINE_FEED);
 	    	}
