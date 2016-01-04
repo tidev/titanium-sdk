e923d2dc88fab8ed89b19f5bf0017fabaadd7147
cheekiatng <cng@appcelerator.com>
2016-01-04 10:59:20 +0800

    [TIMOB-19773] iOS: include catching exception when enabling analytics

---
+++ ti_mocha_tests/ti.analytics.test.js
@@ -0,0 +1,34 @@
+/*
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-2015 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+var should = require('./should');
+
+describe("analytics", function() {
+	it("featureEvent", function(finish) {
+		should(function() {
+			Ti.Analytics.featureEvent();
+		}).throw();
+		should(Ti.Analytics.featureEvent("featureEvent.testButton")).be.undefined;
+		should(Ti.Analytics.featureEvent("featureEvent.testButton", {
+			events: "feature"
+		})).be.undefined;
+		finish();
+	});
+	it("navEvent", function(finish) {
+		should(function() {
+			Ti.Analytics.navEvent();
+		}).throw();
+		should(function() {
+			Ti.Analytics.navEvent("here");
+		}).throw();
+		should(Ti.Analytics.navEvent("here", "there")).be.undefined;
+		should(Ti.Analytics.navEvent("here", "there", "navEvent.testButton")).be.undefined;
+		should(Ti.Analytics.navEvent("here", "there", "navEvent.testButton", {
+			events: "nav"
+		})).be.undefined;
+		finish();
+	});
+});
