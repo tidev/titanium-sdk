b43f44288b89acce5cca886c33d1fb6707b71f3e
AngelkPetkov <apetkov@appcelerator.com>
2016-02-02 12:02:38 -0800

    [TIMOB-20174] iOS: Spacing Changes

--- demos/KitchenSink/Resources/app.js
+++ demos/KitchenSink/Resources/app.js
@@ -11,21 +11,21 @@ var tabGroup = Titanium.UI.createTabGroup({id:'tabGroup1'});
 var win1 = Titanium.UI.createWindow({className:'win1'});
 
 var tab1 = Titanium.UI.createTab({
-	id:'tab1',
-	window:win1
+  id:'tab1',
+  window:win1
 });
 
 //
 // create controls tab and root window
 //
 var win2 = Titanium.UI.createWindow({
-	url:'main_windows/controls.js',
-	titleid:'controls_win_title'
+  url:'main_windows/controls.js',
+  titleid:'controls_win_title'
 });
 var tab2 = Titanium.UI.createTab({
-	icon:'images/tabs/KS_nav_ui.png',
-	titleid:'controls_win_title',
-	window:win2
+  icon:'images/tabs/KS_nav_ui.png',
+  titleid:'controls_win_title',
+  window:win2
 });
 
 
@@ -33,13 +33,13 @@ var tab2 = Titanium.UI.createTab({
 // create phone tab and root window
 //
 var win3 = Titanium.UI.createWindow({
-	url:'main_windows/phone.js',
-	titleid:'phone_win_title'
+  url:'main_windows/phone.js',
+  titleid:'phone_win_title'
 });
 var tab3 = Titanium.UI.createTab({
-	icon:'images/tabs/KS_nav_phone.png',
-	titleid:'phone_win_title',
-	window:win3
+  icon:'images/tabs/KS_nav_phone.png',
+  titleid:'phone_win_title',
+  window:win3
 });
 
 
@@ -47,28 +47,28 @@ var tab3 = Titanium.UI.createTab({
 // create platform tab and root window
 //
 var win4 = Titanium.UI.createWindow({
-	url:'main_windows/platform.js',
-	titleid:'platform_win_title'
+  url:'main_windows/platform.js',
+  titleid:'platform_win_title'
 });
 var tab4 = Titanium.UI.createTab({
-	icon:'images/tabs/KS_nav_platform.png',
-	titleid:'platform_win_title',
-//	Commented out as per 1773
-//	active:true,
-	window:win4
+  icon:'images/tabs/KS_nav_platform.png',
+  titleid:'platform_win_title',
+//  Commented out as per 1773
+//  active:true,
+  window:win4
 });
 
 //
 // create mashup tab and root window
 //
 var win5 = Titanium.UI.createWindow({
-	url:'main_windows/mashups.js',
-	titleid:'mashups_win_title'
+  url:'main_windows/mashups.js',
+  titleid:'mashups_win_title'
 });
 var tab5 = Titanium.UI.createTab({
-	icon:'images/tabs/KS_nav_mashup.png',
-	titleid:'mashups_win_title',
-	window:win5
+  icon:'images/tabs/KS_nav_mashup.png',
+  titleid:'mashups_win_title',
+  window:win5
 });
 
 //
@@ -82,21 +82,21 @@ tabGroup.addTab(tab5);
 
 tabGroup.addEventListener('open',function()
 {
-	// set background color back to white after tab group transition
-	Titanium.UI.setBackgroundColor('#fff');
+  // set background color back to white after tab group transition
+  Titanium.UI.setBackgroundColor('#fff');
 });
 
 tabGroup.setActiveTab(1);
 // open tab group with a transition animation
 tabGroup.open({
-	transition: Titanium.UI.iPhone && Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
+  transition: Titanium.UI.iPhone && Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
 });
 
 // setTimeout(function()
 // {
-	// tabGroup.close({
-		// transition:Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
-	// });
+  // tabGroup.close({
+    // transition:Titanium.UI.iPhone.AnimationStyle.FLIP_FROM_LEFT
+  // });
 // },2000);
 
 
@@ -104,40 +104,40 @@ tabGroup.open({
 //  TAB GROUP EVENTS
 //
 var messageWin = Titanium.UI.createWindow({
-	height:30,
-	width:250,
-	bottom:70,
-	borderRadius:10,
-	touchEnabled:false,
+  height:30,
+  width:250,
+  bottom:70,
+  borderRadius:10,
+  touchEnabled:false,
 
-	orientationModes : [
-	Titanium.UI.PORTRAIT,
-	Titanium.UI.UPSIDE_PORTRAIT,
-	Titanium.UI.LANDSCAPE_LEFT,
-	Titanium.UI.LANDSCAPE_RIGHT
-	]
+  orientationModes : [
+  Titanium.UI.PORTRAIT,
+  Titanium.UI.UPSIDE_PORTRAIT,
+  Titanium.UI.LANDSCAPE_LEFT,
+  Titanium.UI.LANDSCAPE_RIGHT
+  ]
 });
 var messageView = Titanium.UI.createView({
-	id:'messageview',
-	height:30,
-	width:250,
-	borderRadius:10,
-	backgroundColor:'#000',
-	opacity:0.7,
-	touchEnabled:false
+  id:'messageview',
+  height:30,
+  width:250,
+  borderRadius:10,
+  backgroundColor:'#000',
+  opacity:0.7,
+  touchEnabled:false
 });
 
 var messageLabel = Titanium.UI.createLabel({
-	id:'messagelabel',
-	text:'',
-	color:'#fff',
-	width:250,
-	height:'auto',
-	font:{
-		fontFamily:'Helvetica Neue',
-		fontSize:13
-	},
-	textAlign:'center'
+  id:'messagelabel',
+  text:'',
+  color:'#fff',
+  width:250,
+  height:'auto',
+  font:{
+    fontFamily:'Helvetica Neue',
+    fontSize:13
+  },
+  textAlign:'center'
 });
 messageWin.add(messageView);
 messageWin.add(messageLabel);
@@ -149,58 +149,58 @@ messageWin.add(messageLabel);
 // tab group close event
 tabGroup.addEventListener('close', function(e)
 {
-	messageLabel.text = 'tab group close event';
-	messageWin.open();
-	if (Ti.Platform.osname == "iphone") {
+  messageLabel.text = 'tab group close event';
+  messageWin.open();
+  if (Ti.Platform.osname == "iphone") {
 //On iOS, when we're closing the tab group, this is a result
 //of the tab group example of 'Close/Animate Tab Group' and
 //we want to reopen the tab group so the user can continue with
 //using Kitchen Sink. HOWEVER, on Android, this is also triggered
 //when the app is being closed via back button, where reopening
 //the tab group is not desired. This is purely a quirk of the tests.
-		tabGroup.open();
-	}
-	setTimeout(function()
-	{
-		messageWin.close({opacity:0,duration:500});
-	},1000);
+    tabGroup.open();
+  }
+  setTimeout(function()
+  {
+    messageWin.close({opacity:0,duration:500});
+  },1000);
 });
 
 
 // tab group open event
 tabGroup.addEventListener('open', function(e)
 {
-	messageLabel.text = 'tab group open event';
-	messageWin.open();
-	setTimeout(function()
-	{
-		messageWin.close({opacity:0,duration:500});
-	},1000);
+  messageLabel.text = 'tab group open event';
+  messageWin.open();
+  setTimeout(function()
+  {
+    messageWin.close({opacity:0,duration:500});
+  },1000);
 
 });
 
 // focus event listener for tracking tab changes
 tabGroup.addEventListener('focus', function(e)
 {
-	messageLabel.text = 'tab changed to ' + e.index + ' old index ' + e.previousIndex;
-	messageWin.open();
-	setTimeout(function()
-	{
-		Ti.API.info('tab ' + e.tab.title  + ' prevTab = ' + (e.previousTab ? e.previousTab.title : null));
-		messageLabel.text = 'active title ' + e.tab.title + ' old title ' + (e.previousTab ? e.previousTab.title : null);
-	},1000);
+  messageLabel.text = 'tab changed to ' + e.index + ' old index ' + e.previousIndex;
+  messageWin.open();
+  setTimeout(function()
+  {
+    Ti.API.info('tab ' + e.tab.title  + ' prevTab = ' + (e.previousTab ? e.previousTab.title : null));
+    messageLabel.text = 'active title ' + e.tab.title + ' old title ' + (e.previousTab ? e.previousTab.title : null);
+  },1000);
 
-	setTimeout(function()
-	{
-		messageWin.close({opacity:0,duration:500});
-	},2000);
+  setTimeout(function()
+  {
+    messageWin.close({opacity:0,duration:500});
+  },2000);
 
 });
 
 // blur event listener for tracking tab changes
 tabGroup.addEventListener('blur', function(e)
 {
-	Titanium.API.info('tab blur - new index ' + e.index + ' old index ' + e.previousIndex);
+  Titanium.API.info('tab blur - new index ' + e.index + ' old index ' + e.previousIndex);
 });
 
 
@@ -209,22 +209,22 @@ tabGroup.addEventListener('blur', function(e)
 //
 Titanium.App.addEventListener('event_one', function(e)
 {
-	messageLabel.text = 'app.js: event one, array length = ' + e.data.length;
-	messageWin.open();
-	setTimeout(function()
-	{
-		messageWin.close({opacity:0,duration:500});
-	},1000);
+  messageLabel.text = 'app.js: event one, array length = ' + e.data.length;
+  messageWin.open();
+  setTimeout(function()
+  {
+    messageWin.close({opacity:0,duration:500});
+  },1000);
 });
 
 Titanium.App.addEventListener('event_two', function(e)
 {
-	messageLabel.text = 'app.js: event two, name = ' + e.name;
-	messageWin.open();
-	setTimeout(function()
-	{
-		messageWin.close({opacity:0,duration:500});
-	},1000);
+  messageLabel.text = 'app.js: event two, name = ' + e.name;
+  messageWin.open();
+  setTimeout(function()
+  {
+    messageWin.close({opacity:0,duration:500});
+  },1000);
 
 });
 
@@ -235,60 +235,60 @@ var indWin = null;
 var actInd = null;
 function showIndicator()
 {
-	if (Ti.Platform.osname != 'android')
-	{
-		// window container
-		indWin = Titanium.UI.createWindow({
-			height:150,
-			width:150
-		});
+  if (Ti.Platform.osname != 'android')
+  {
+    // window container
+    indWin = Titanium.UI.createWindow({
+      height:150,
+      width:150
+    });
 
-		// black view
-		var indView = Titanium.UI.createView({
-			height:150,
-			width:150,
-			backgroundColor:'#000',
-			borderRadius:10,
-			opacity:0.8
-		});
-		indWin.add(indView);
-	}
+    // black view
+    var indView = Titanium.UI.createView({
+      height:150,
+      width:150,
+      backgroundColor:'#000',
+      borderRadius:10,
+      opacity:0.8
+    });
+    indWin.add(indView);
+  }
 
-	// loading indicator
-	actInd = Titanium.UI.createActivityIndicator({
-		style:Titanium.UI.iPhone && Titanium.UI.iPhone.ActivityIndicatorStyle.BIG,
-		height:30,
-		width:30
-	});
+  // loading indicator
+  actInd = Titanium.UI.createActivityIndicator({
+    style:Titanium.UI.iPhone && Titanium.UI.iPhone.ActivityIndicatorStyle.BIG,
+    height:30,
+    width:30
+  });
 
-	if (Ti.Platform.osname != 'android')
-	{
-		indWin.add(actInd);
+  if (Ti.Platform.osname != 'android')
+  {
+    indWin.add(actInd);
 
-		// message
-		var message = Titanium.UI.createLabel({
-			text:'Loading',
-			color:'#fff',
-			width:'auto',
-			height:'auto',
-			font:{fontSize:20,fontWeight:'bold'},
-			bottom:20
-		});
-		indWin.add(message);
-		indWin.open();
-	} else {
-		actInd.message = "Loading";
-	}
-	actInd.show();
+    // message
+    var message = Titanium.UI.createLabel({
+      text:'Loading',
+      color:'#fff',
+      width:'auto',
+      height:'auto',
+      font:{fontSize:20,fontWeight:'bold'},
+      bottom:20
+    });
+    indWin.add(message);
+    indWin.open();
+  } else {
+    actInd.message = "Loading";
+  }
+  actInd.show();
 
 }
 
 function hideIndicator()
 {
-	actInd.hide();
-	if (Ti.Platform.osname != 'android') {
-		indWin.close({opacity:0,duration:500});
-	}
+  actInd.hide();
+  if (Ti.Platform.osname != 'android') {
+    indWin.close({opacity:0,duration:500});
+  }
 }
 
 //
@@ -296,19 +296,19 @@ function hideIndicator()
 //
 Titanium.App.addEventListener('show_indicator', function(e)
 {
-	Ti.API.info("IN SHOW INDICATOR");
-	showIndicator();
+  Ti.API.info("IN SHOW INDICATOR");
+  showIndicator();
 });
 Titanium.App.addEventListener('hide_indicator', function(e)
 {
-	Ti.API.info("IN HIDE INDICATOR");
-	hideIndicator();
+  Ti.API.info("IN HIDE INDICATOR");
+  hideIndicator();
 });
 
 // trap app shutdown event
 Titanium.App.addEventListener('close',function(e)
 {
-	Ti.API.info("The application is being shutdown");
+  Ti.API.info("The application is being shutdown");
 });
 
 // test for loading in a root-level include
@@ -337,23 +337,23 @@ Ti.API.info("should be hello, was = "+String.format('%s','hello'));
 
 // test to check that we can iterate over titanium based objects
 (function(){
-	Ti.API.info("you should see a list of properties (3 or more) below this line");
-	Ti.API.info("---------------------------------------------------------------");
-	for (var p in win1)
-	{
-		Ti.API.info("         win1 property: "+p);
-	}
-	Ti.API.info("Did you see properties? ^^^^^ ");
-	Ti.API.info("---------------------------------------------------------------");
+  Ti.API.info("you should see a list of properties (3 or more) below this line");
+  Ti.API.info("---------------------------------------------------------------");
+  for (var p in win1)
+  {
+    Ti.API.info("         win1 property: "+p);
+  }
+  Ti.API.info("Did you see properties? ^^^^^ ");
+  Ti.API.info("---------------------------------------------------------------");
 
-	Ti.API.info("you should see a list of modules (3 or more) below this line");
-	Ti.API.info("---------------------------------------------------------------");
-	for (var p in Titanium)
-	{
-		Ti.API.info("             module: "+p);
-	}
-	Ti.API.info("Did you see modules? ^^^^^ ");
-	Ti.API.info("---------------------------------------------------------------");
+  Ti.API.info("you should see a list of modules (3 or more) below this line");
+  Ti.API.info("---------------------------------------------------------------");
+  for (var p in Titanium)
+  {
+    Ti.API.info("             module: "+p);
+  }
+  Ti.API.info("Did you see modules? ^^^^^ ");
+  Ti.API.info("---------------------------------------------------------------");
 })();
 
 
@@ -361,52 +361,52 @@ Ti.include("examples/version.js");
 
 if (isiOS4Plus())
 {
-	// register a background service. this JS will run when the app is backgrounded
-	var service = Ti.App.iOS.registerBackgroundService({url:'bg.js'});
+  // register a background service. this JS will run when the app is backgrounded
+  var service = Ti.App.iOS.registerBackgroundService({url:'bg.js'});
 
-	Ti.API.info("registered background service = "+service);
+  Ti.API.info("registered background service = "+service);
 
-	if(Ti.App.getArguments().url)
-	{
-		alert(Ti.App.getArguments().url);
-	}
-	// listen for a local notification event
-	Ti.App.iOS.addEventListener('notification',function(e)
-	{
-		Ti.API.info("local notification received: "+JSON.stringify(e));
-	});
+  if(Ti.App.getArguments().url)
+  {
+    alert(Ti.App.getArguments().url);
+  }
+  // listen for a local notification event
+  Ti.App.iOS.addEventListener('notification',function(e)
+  {
+    Ti.API.info("local notification received: "+JSON.stringify(e));
+  });
 
-	// fired when an app resumes for suspension
-	Ti.App.addEventListener('resume',function(e){
-		Ti.API.info("app is resuming from the background");
-	});
-	Ti.App.addEventListener('resumed',function(e){
-		Ti.API.info("app has resumed from the background");
-		if(Ti.App.getArguments().url)
-		{
-			alert(Ti.App.getArguments().url);
-		}
-	});
+  // fired when an app resumes for suspension
+  Ti.App.addEventListener('resume',function(e){
+    Ti.API.info("app is resuming from the background");
+  });
+  Ti.App.addEventListener('resumed',function(e){
+    Ti.API.info("app has resumed from the background");
+    if(Ti.App.getArguments().url)
+    {
+      alert(Ti.App.getArguments().url);
+    }
+  });
 
-	Ti.App.addEventListener('pause',function(e){
-		Ti.API.info("app was paused from the foreground");
-	});
+  Ti.App.addEventListener('pause',function(e){
+    Ti.API.info("app was paused from the foreground");
+  });
 }
 
 if (Ti.App.Properties.getBool('showNotice', true)){
-	var alertNotice = Ti.UI.createAlertDialog({
-		buttonNames: ['OK', 'Visit docs', 'Don\'t show again'],
-		cancel:0,
-		title: 'Notice',
-		message: 'While this KitchenSink provides an extensive demonstration of the Titanium API, its structure is not recommended for production apps. Please refer to our documentation for more details.'
-	});
-	alertNotice.show();
-	alertNotice.addEventListener('click', function(e){
-		if(e.index === 1){
-			Titanium.Platform.openURL('http://wiki.appcelerator.org/display/guides/Example+Applications');
-		}
-		if(e.index === 2){
-			Ti.App.Properties.setBool('showNotice', false);
-		}
-	});
+  var alertNotice = Ti.UI.createAlertDialog({
+    buttonNames: ['OK', 'Visit docs', 'Don\'t show again'],
+    cancel:0,
+    title: 'Notice',
+    message: 'While this KitchenSink provides an extensive demonstration of the Titanium API, its structure is not recommended for production apps. Please refer to our documentation for more details.'
+  });
+  alertNotice.show();
+  alertNotice.addEventListener('click', function(e){
+    if(e.index === 1){
+      Titanium.Platform.openURL('http://wiki.appcelerator.org/display/guides/Example+Applications');
+    }
+    if(e.index === 2){
+      Ti.App.Properties.setBool('showNotice', false);
+    }
+  });
 }
