a0b3712ba88ed40d5f4dfe7517bc5503838b3e77
Hieu Pham <hpham@appcelerator.com>
2016-02-16 17:39:01 -0800

    [TIMOB-20410]: Allow backwards compat with map module

--- android/titanium/src/java/org/appcelerator/titanium/proxy/TiViewProxy.java
+++ android/titanium/src/java/org/appcelerator/titanium/proxy/TiViewProxy.java
@@ -919,6 +919,13 @@ public abstract class TiViewProxy extends KrollProxy implements Handler.Callback
 		}
 	}
 
+	public TiBlob toImage()
+	{
+		//backward compat with maps
+		return toImage(null);
+	}
+
+
 	@Kroll.method
 	public TiBlob toImage(final @Kroll.argument(optional=true) KrollFunction callback)
 	{
