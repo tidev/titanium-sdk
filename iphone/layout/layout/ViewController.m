1f80536b5d664f995039154aa10182ff758d483c
Pedro Enrique <pedro.tma@gmail.com>
2016-03-01 10:45:32 -0800

    [AutoLayout] Minor stuff

--- iphone/layout/layout/ViewController.m
+++ iphone/layout/layout/ViewController.m
@@ -38,71 +38,9 @@
     [super viewDidLoad];
     
     TiLayoutView* myView = [[TiLayoutView alloc] init];
-
-    TiLayoutView* parentView = [[TiLayoutView alloc] init];
-    [parentView setWidth:@300];
-    [parentView setHeight:@"SIZE"];
-    [parentView setLayout_:@"vertical"];
-    [parentView setBackgroundColor:[UIColor lightGrayColor]];
-    
-    TiLayoutView* view = [[TiLayoutView alloc] init];
-    [view setWidth:@"100"];
-    [view setHeight:@"100"];
-    [view setViewName:@"this_view"];
-    [view setBackgroundColor:[UIColor redColor]];
-
-    TiLabel* label = [[TiLabel alloc] init];
-    [label setText:@"Bacon ipsum dolor amet jowl boudin prosciutto capicola, tail ribeye flank. Pork belly chicken meatloaf picanha chuck frankfurter pig filet mignon jowl ham"];
-
-    [parentView addSubview:view];
-    [parentView addSubview:label];
-
-    [myView addSubview:parentView];
     [[self view] addSubview:myView];
-
     
-    CGFloat height = [parentView heightIfWidthWere:300];
-    NSLog(@"Height: %f", height);
-
-    [parentView setOnLayout:^(TiLayoutView *sender, CGRect rect) {
-        NSLog(@"Height: %f", rect.size.height);
-    }];
-
-
-    NSMutableArray* array = [NSMutableArray array];
-    //
-    for (NSInteger i =0; i < 100; i++) {
-        
-        TiTableViewRow* row = [[TiTableViewRow alloc] init];
-        [row setLayout_:@"vertical"];
-        [row setHeight_:@"SIZE"];
-        
-        
-        if (i % 2) {
-            TiLabel* label = [[TiLabel alloc] init];
-            [label setText:@"Bacon ipsum dolor amet jowl boudin prosciutto capicola, tail ribeye flank. Pork belly chicken meatloaf picanha chuck frankfurter pig filet mignon jowl ham"];
-            TiView* v = [[TiView alloc] init];
-            [v setWidth:@"100"];
-            [v setHeight:@"100"];
-            [v setBackgroundColor: [UIColor redColor]];
-            [row addSubview:label];
-            [row addSubview:v];
-            [[row parentView] setViewName: @"1"];
-        } else {
-            TiLabel* label = [[TiLabel alloc] init];
-            [label setText:@"Bacon ipsum dolor amet jowl boudin prosciutto capicola, tail ribeye flank. Pork belly chicken meatloaf picanha chuck frankfurter pig filet mignon jowl ham. Picanha tenderloin rump ball tip."];
-            [row addSubview:label];
-            [[row parentView] setViewName: @"2"];
-        }
-        [array addObject:row];
-    }
-
-    
-    TiTableView* table = [[TiTableView alloc] init];
-    [table setTableData:array];
-    
-    [myView addSubview:table];
-    [[self view] addSubview:myView];
+    // Write your quicky tests here
     
 }
 @end
