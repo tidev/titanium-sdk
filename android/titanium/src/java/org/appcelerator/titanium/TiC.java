0b40c3e62de83df3b93f43f05fd99e656f33abf9
Lavinia <golu.lavinia@gmail.com>
2016-03-16 15:36:08 +0200

    Update TiC.java

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -1428,7 +1428,7 @@ public class TiC
 	/**
 	 * @module.api
 	 */
-	public static final String PROPERTY_MINIMUMFONTSIZE = "minimumFontSize";
+	public static final String PROPERTY_MINIMUM_FONT_SIZE = "minimumFontSize";
 	
 	/**
 	 * @module.api
