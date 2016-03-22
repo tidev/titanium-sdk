a4fec9cb852c5b76050d22ac5ae0d13c5a37fd27
Angel Petkov <apetkov@appcelerator.com>
2016-03-22 11:56:46 -0700

    [TIMOB-20588] iOS:Log messages are not always displayed on compile time

--- iphone/Classes/KrollContext.m
+++ iphone/Classes/KrollContext.m
@@ -1088,7 +1088,10 @@ static TiValueRef StringFormatDecimalCallback (TiContextRef jsContext, TiObjectR
 	
 	[condition unlock];
 #else
-    [self invoke:obj];
+    dispatch_block_t block = ^{
+        [self invoke:obj];
+    };
+    TiThreadPerformOnMainThread(block, [NSThread isMainThread]);
 #endif
 }
 
