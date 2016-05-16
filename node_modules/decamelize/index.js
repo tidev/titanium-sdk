5707ae5bfbb634e471027375bb4397b6bbdf9c35
Chris Barber <chris@cb1inc.com>
2016-05-16 14:10:30 -0500

    [TIMOB-23285] Updated ioslib to 0.11.0 and node-ios-device to 0.9.5

--- node_modules/decamelize/index.js
+++ node_modules/decamelize/index.js
@@ -1,6 +1,4 @@
 'use strict';
-var escapeStringRegexp = require('escape-string-regexp');
-
 module.exports = function (str, sep) {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expected a string');
@@ -8,9 +6,8 @@ module.exports = function (str, sep) {
 
 	sep = typeof sep === 'undefined' ? '_' : sep;
 
-	var reSep = escapeStringRegexp(sep);
-
-	return str.replace(/([a-z\d])([A-Z])/g, '$1' + sep + '$2')
-					.replace(new RegExp('(' + reSep + '[A-Z])([A-Z])', 'g'), '$1' + reSep + '$2')
-					.toLowerCase();
+	return str
+		.replace(/([a-z\d])([A-Z])/g, '$1' + sep + '$2')
+		.replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1' + sep + '$2')
+		.toLowerCase();
 };
