001b7288066b273a7753b875b335d2c89bba58ac
Ashraf <ashcoding@outlook.com>
2016-06-09 15:42:22 +0800

    [TIMOB-20415] Android: HttpClient Basic Auth Infinite loop fix

--- android/modules/network/src/java/ti/modules/titanium/network/TiAuthenticator.java
+++ android/modules/network/src/java/ti/modules/titanium/network/TiAuthenticator.java
@@ -7,6 +7,8 @@ public class TiAuthenticator extends Authenticator {
     String domain;
     String user;
     String password;
+    private int retryCount = 0;
+    private final static int MAX_RETRY_COUNT = 1;
 
     public TiAuthenticator(String user, String password) {
         super();
@@ -27,9 +29,13 @@ public class TiAuthenticator extends Authenticator {
         if (domain != null && !domain.isEmpty()) {
             user = domain + "\\" + user;
         }
-        if (password != null) {
-            return new PasswordAuthentication(user, password.toCharArray());
+        if (retryCount < MAX_RETRY_COUNT) {
+            retryCount++;
+            if (password != null) {
+                return new PasswordAuthentication(user, password.toCharArray());
+            }
+            return new PasswordAuthentication(user, null);
         }
-        return new PasswordAuthentication(user, null);
+        return null;
     }
 }
