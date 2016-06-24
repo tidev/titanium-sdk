2b48b9941e0d7c8685acff4ac847ff7b2a7b1136
Chris Barber <chris@cb1inc.com>
2016-06-23 22:26:14 -0500

    Update scons-update-node-deps.js

--- build/scons-update-node-deps.js
+++ build/scons-update-node-deps.js
@@ -60,7 +60,7 @@ exec('npm -v', function (err, stdout, stderr) {
 
 	console.log('\nRunning npm install');
 
-	exec('npm install', function (err, stdout, stderr) {
+	exec('npm install --production', function (err, stdout, stderr) {
 		if (err) {
 			console.error(err);
 			process.exit(1);
