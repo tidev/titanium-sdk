6ef86c7fbe3c2310fb6b7e80fb6dc2b98827f5d5
Chris Barber <chris@cb1inc.com>
2016-06-23 22:23:22 -0500

    Moved update-npm-deps to the new scons hotness. (#8083)

--- build/scons.js
+++ build/scons.js
@@ -7,4 +7,5 @@ commander
 	.command('install', 'install the built SDK')
 	.command('package [platforms]', 'package one or more platforms')
 	.command('build [platforms]', 'build one or more platforms')
+	.command('update-node-deps', 'deletes and reinstalls node dependencies')
 	.parse(process.argv);
