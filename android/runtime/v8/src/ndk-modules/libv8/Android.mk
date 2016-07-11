75a62caa8bf321e0019df057b42fb672d3d5b2e8
Christopher Williams <chris.a.williams@gmail.com>
2016-07-11 10:57:09 -0400

    Update Android to V8 5.1.281.59 (#8116)

--- android/runtime/v8/src/ndk-modules/libv8/Android.mk
+++ android/runtime/v8/src/ndk-modules/libv8/Android.mk
@@ -9,7 +9,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-V8_VERSION=5.0.71.33
+V8_VERSION=5.1.281.59
 LIBV8_DIR := ../../../../../../dist/android/libv8/$(V8_VERSION)
 
 # https://jira.appcelerator.org/browse/TIMOB-15263
@@ -41,32 +41,44 @@ LIBV8_MODE := debug
 SUFFIX := _g
 endif
 
-# libicudata
-LOCAL_MODULE := libicudata
-LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
-include $(PREBUILT_STATIC_LIBRARY)
+# Uncomment the libicu* when using V8 with ICU/i18n baked in
 
-# libicui18n
-LOCAL_MODULE := libicui18n
-LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
-include $(PREBUILT_STATIC_LIBRARY)
+# libicudata
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libicudata
+#LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
+#include $(PREBUILT_STATIC_LIBRARY)
 
 # libicuuc
-LOCAL_MODULE := libicuuc
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libicuuc
+#LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
+#LOCAL_STATIC_LIBRARIES := libicudata
+#include $(PREBUILT_STATIC_LIBRARY)
+
+# libicui18n
+#include $(CLEAR_VARS)
+#LOCAL_MODULE := libicui18n
+#LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
+#LOCAL_STATIC_LIBRARIES := libicuuc
+#include $(PREBUILT_STATIC_LIBRARY)
+
+# v8_libbase
+include $(CLEAR_VARS)
+LOCAL_MODULE    := libv8_libbase
 LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
+LOCAL_EXPORT_C_INCLUDES := $(TI_DIST_DIR)/android/libv8/$(V8_VERSION)/$(LIBV8_MODE)/include
 include $(PREBUILT_STATIC_LIBRARY)
 
 # base
+include $(CLEAR_VARS)
 LOCAL_MODULE := libv8_base
 LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
 LOCAL_EXPORT_C_INCLUDES := $(TI_DIST_DIR)/android/libv8/$(V8_VERSION)/$(LIBV8_MODE)/include
-include $(PREBUILT_STATIC_LIBRARY)
-
-# v8_nosnapshot
-include $(CLEAR_VARS)
-LOCAL_MODULE    := libv8_nosnapshot
-LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
-LOCAL_EXPORT_C_INCLUDES := $(TI_DIST_DIR)/android/libv8/$(V8_VERSION)/$(LIBV8_MODE)/include
+# When using V8 i18n support/ICU
+#LOCAL_STATIC_LIBRARIES := libv8_libbase libicuuc libicui18n libicudata
+# When setting v8_enable_i18n_support=0
+LOCAL_STATIC_LIBRARIES := libv8_libbase
 include $(PREBUILT_STATIC_LIBRARY)
 
 # v8_libplatform
@@ -74,11 +86,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := libv8_libplatform
 LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
 LOCAL_EXPORT_C_INCLUDES := $(TI_DIST_DIR)/android/libv8/$(V8_VERSION)/$(LIBV8_MODE)/include $(TI_DIST_DIR)/android/libv8/$(V8_VERSION)/$(LIBV8_MODE)
+LOCAL_STATIC_LIBRARIES := libv8_libbase
 include $(PREBUILT_STATIC_LIBRARY)
 
-# v8_libbase
+# v8_nosnapshot
 include $(CLEAR_VARS)
-LOCAL_MODULE    := libv8_libbase
+LOCAL_MODULE    := libv8_nosnapshot
 LOCAL_SRC_FILES := $(LIBV8_DIR)/$(LIBV8_MODE)/libs/$(SIMPLIFIED_ARCH)/$(LOCAL_MODULE)$(SUFFIX).a
 LOCAL_EXPORT_C_INCLUDES := $(TI_DIST_DIR)/android/libv8/$(V8_VERSION)/$(LIBV8_MODE)/include
+LOCAL_STATIC_LIBRARIES := libv8_base
 include $(PREBUILT_STATIC_LIBRARY)
