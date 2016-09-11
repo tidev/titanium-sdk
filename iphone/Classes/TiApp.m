c531969f86b3512211b2faf8b56a9d58f8d01be6
Hans Knoechel <hknoechel@Hans-Macbook-Pro.fritz.box>
2016-09-11 22:01:51 +0200

    [TIMOB-23890] Fix indentation

--- iphone/Classes/TiApp.m
+++ iphone/Classes/TiApp.m
@@ -437,7 +437,7 @@ TI_INLINE void waitForMemoryPanicCleared();   //WARNING: This must never be run 
 		[launchOptions setObject:sourceApplication forKey:@"source"];
 	}
     
-    [[NSNotificationCenter defaultCenter] postNotificationName:kTiApplicationLaunchedFromURL object:self userInfo:launchOptions];
+	[[NSNotificationCenter defaultCenter] postNotificationName:kTiApplicationLaunchedFromURL object:self userInfo:launchOptions];
     
 	return YES;
 }
