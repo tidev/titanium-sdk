c54131144f160e79f288c90387897bb928f6494e
Hans Knoechel <hknoechel@Hans-Macbook-Pro.fritz.box>
2016-09-11 12:37:50 +0200

    [TIMOB-23891] Update api-docs, add code-docs

--- iphone/Classes/TiUIiOSFeedbackGeneratorProxy.h
+++ iphone/Classes/TiUIiOSFeedbackGeneratorProxy.h
@@ -8,21 +8,48 @@
 
 #import "TiProxy.h"
 
+/**
+ * Enumeration to handle the feedback generator type in this proxy.
+ */
 typedef NS_ENUM(NSInteger, TiUIiOSFeedbackGeneratorType) {
     TiUIiOSFeedbackGeneratorTypeSelection = 0,
     TiUIiOSFeedbackGeneratorTypeImpact = 1,
     TiUIiOSFeedbackGeneratorTypeNotification = 2
 };
 
+/**
+ * Provide support for the iOS 10 haptic engine on supported devices (currently iPhone 7
+ * and iPhone 7 Plus). The developer can specify a feedback generator type to distungish
+ * between a selection-, impact-, and notification-generator to receive different kind of
+ * haptic feedbacks.
+ */
 @interface TiUIiOSFeedbackGeneratorProxy : TiProxy {
     UIFeedbackGenerator *generator;
     TiUIiOSFeedbackGeneratorType type;
 }
 
+/**
+ * Used to prepare the haptic sensor for the upcoming interaction with it.
+ * @param unused Unused parameter to expose this API as a method.
+ */
 - (void)prepare:(id)unused;
 
+/**
+ * Used to trigger a haptic feedback after a selection has been made.
+ * @param unused Unused parameter to expose this API as a method.
+ */
+- (void)selectionChanged:(id)unused;
+
+/**
+ * Used to trigger a haptic feedback after an impact occurred.
+ * @param unused Unused parameter to expose this API as a method.
+ */
 - (void)impactOccurred:(id)unused;
 
+/**
+ * Used to trigger a haptic feedback after a notification has been received.
+ * @param value The type of the notification to manage the peculiarity of the haptic feedback.
+ */
 - (void)notificationOccurred:(id)value;
 
 @end
