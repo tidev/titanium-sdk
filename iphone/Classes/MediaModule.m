c180a425772da86c188f719a25c3a77f43e15197
hansemannn <hansemannn@users.noreply.github.com>
2016-09-28 19:29:02 +0200

    [TIMOB-23935] Fix switchCamera for kroll-thread

--- iphone/Classes/MediaModule.m
+++ iphone/Classes/MediaModule.m
@@ -930,6 +930,7 @@ MAKE_SYSTEM_PROP(VIDEO_TIME_OPTION_EXACT,MPMovieTimeOptionExact);
 -(void)switchCamera:(id)args
 {
     ENSURE_TYPE(args, NSArray);
+    ENSURE_UI_THREAD(switchCamera,args);
     
     // TIMOB-17951
     if ([args objectAtIndex:0] == [NSNull null]) {
