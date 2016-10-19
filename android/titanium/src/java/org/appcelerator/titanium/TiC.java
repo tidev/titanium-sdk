94678a67f2d14c3ff00fea768383afd8afc58620
Gary Mathews <gmathews@appcelerator.com>
2016-10-19 19:06:18 +0100

    [TIMOB-23497] Implement Titanium.UI.ActivityIndicator.indicatorColor

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -1608,6 +1608,11 @@ public class TiC
 	/**
 	 * @module.api
 	 */
+	public static final String PROPERTY_INDICATOR_COLOR = "indicatorColor";
+
+	/**
+	 * @module.api
+	 */
 	public static final String PROPERTY_INCLUDE_FONT_PADDING = "includeFontPadding";
 
 	/**
