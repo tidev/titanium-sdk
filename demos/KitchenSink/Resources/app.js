994721a6062461fc9f40a648cdb6dd17e7831af2
vijay vikram singh <vsingh@axway.com>
2016-10-20 12:00:43 +0800

    Vijay Test

--- demos/KitchenSink/Resources/app.js
+++ demos/KitchenSink/Resources/app.js
@@ -1,4 +1,6 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
+
+// Vijay testing git process
 Titanium.UI.setBackgroundColor('#000');
 
 // create tab group
