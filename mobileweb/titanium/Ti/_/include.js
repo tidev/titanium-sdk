e1e34746d06ea8600f2f30680eaa2c41d95a92df
cheekiatng <cng@appcelerator.com>
2016-11-02 06:54:32 +0800

    revert ti.include for mobile web

---
+++ mobileweb/titanium/Ti/_/include.js
@@ -0,0 +1,54 @@
+define(function() {
+	var cache = {},
+ 		stack = [];
+
+ 	return {
+ 		dynamic: true, // prevent the loader from caching the result
+
+ 		normalize: function(name, normalize) {
+ 			var parts = name.split("!"),
+ 				url = parts[0];
+ 			parts.shift();
+ 			return (/^\./.test(url) ? normalize(url) : url) + (parts.length ? "!" + parts.join("!") : "");
+ 		},
+
+ 		load: function(name, require, onLoad, config) {
+ 			var c,
+ 				x,
+ 				parts = name.split("!"),
+ 				len = parts.length,
+ 				url,
+ 				sandbox;
+
+ 			if (sandbox = len > 1 && parts[0] === "sandbox") {
+ 				parts.shift();
+ 				name = parts.join("!");
+ 			}
+
+ 			url = require.toUrl(/^\//.test(name) ? name : "./" + name, stack.length ? { name: stack[stack.length-1] } : null);
+ 			c = cache[url] || require.cache(url);
+
+ 			if (!c) {
+ 				x = new XMLHttpRequest;
+ 				x.open("GET", url, false);
+ 				x.send(null);
+ 				if (x.status === 200) {
+ 					c = x.responseText;
+ 				} else {
+ 					throw new Error("Failed to load include \"" + url + "\": " + x.status);
+ 				}
+ 			}
+
+ 			stack.push(url);
+ 			try {
+ 				require.evaluate(cache[url] = c, 0, !sandbox);
+ 			} catch (e) {
+ 				throw e;
+ 			} finally {
+ 				stack.pop();
+ 			}
+
+ 			onLoad(c);
+ 		}
+ 	};
+});
