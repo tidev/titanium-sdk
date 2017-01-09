539dd93da3a5111707a9bf94e281b41b2ac3285b
Gary Mathews <gmathews@appcelerator.com>
2017-01-09 19:33:02 +0000

    [TIMOB-20459] Fix shortcut and notification intents

--- android/titanium/src/java/org/appcelerator/titanium/TiActivity.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiActivity.java
@@ -8,7 +8,8 @@ package org.appcelerator.titanium;
 
 import android.content.Intent;
 import android.os.Bundle;
-
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.titanium.proxy.IntentProxy;
 
 public class TiActivity extends TiBaseActivity
 {
@@ -32,6 +33,25 @@ public class TiActivity extends TiBaseActivity
 	@Override
 	protected void onResume()
 	{
+		TiRootActivity rootActivity = getTiApp().getRootActivity();
+		if (rootActivity != null) {
+			Intent rootIntent = rootActivity.getIntent();
+
+			// merge root intent extras
+			if (rootIntent != null) {
+				Intent intent = getIntent();
+				if (intent.getComponent().getClassName().equals(TiActivity.class.getName())) {
+					intent.putExtras(rootIntent);
+					setIntent(intent);
+
+					// fire 'newintent'
+					IntentProxy intentProxy = new IntentProxy(intent);
+					KrollDict data = new KrollDict();
+					data.put(TiC.PROPERTY_INTENT, intentProxy);
+					rootActivity.getActivityProxy().fireSyncEvent(TiC.EVENT_NEW_INTENT, data);
+				}
+			}
+		}
 		super.onResume();
 		if (getTiApp().isRestartPending()) {
 			return;
