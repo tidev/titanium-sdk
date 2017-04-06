b3c420f661e90c71e198f3864d2fc91000af5894
Chris Barber <chris@cb1inc.com>
2017-04-05 19:08:22 -0500

    Upgraded to ioslib@1.3.2 and node-appc@0.2.43 so that everything uses xmldom@0.1.22. (#8932)

--- node_modules/rc/test/nested-env-vars.js
+++ node_modules/rc/test/nested-env-vars.js
@@ -23,24 +23,28 @@ process.env[n+'___z__i__'] = 9999
 // should ignore case for config name section.
 process.env[N+'_test_upperCase'] = 187
 
-var config = require('../')(n, {
-  option: true
-})
+function testPrefix(prefix) {
+	var config = require('../')(prefix, {
+	  option: true
+	})
 
-console.log('\n\n------ nested-env-vars ------\n',config)
+	console.log('\n\n------ nested-env-vars ------\n',{prefix: prefix}, '\n', config);
 
-assert.equal(config.option, true)
-assert.equal(config.someOpt.a, 42)
-assert.equal(config.someOpt.x, 99)
-// Should not override `a` once it's been set
-assert.equal(config.someOpt.a/*.b*/, 42)
-// Should not override `x` once it's been set
-assert.equal(config.someOpt.x/*.y*/, 99)
-assert.equal(config.someOpt.z, 186577)
-// Should not override `z` once it's been set
-assert.equal(config.someOpt.z/*.x*/, 186577)
-assert.equal(config.someOpt.w.w, 18629)
-assert.equal(config.z.i, 9999)
+	assert.equal(config.option, true)
+	assert.equal(config.someOpt.a, 42)
+	assert.equal(config.someOpt.x, 99)
+	// Should not override `a` once it's been set
+	assert.equal(config.someOpt.a/*.b*/, 42)
+	// Should not override `x` once it's been set
+	assert.equal(config.someOpt.x/*.y*/, 99)
+	assert.equal(config.someOpt.z, 186577)
+	// Should not override `z` once it's been set
+	assert.equal(config.someOpt.z/*.x*/, 186577)
+	assert.equal(config.someOpt.w.w, 18629)
+	assert.equal(config.z.i, 9999)
 
-assert.equal(config.test_upperCase, 187)
+	assert.equal(config.test_upperCase, 187)
+}
 
+testPrefix(n);
+testPrefix(N);
