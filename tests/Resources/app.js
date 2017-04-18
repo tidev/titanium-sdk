21636b66a488a945d143bce8e0b9c6bdc453611d
Christopher Williams <chris.a.williams@gmail.com>
2017-04-18 09:51:46 -0400

    Add more ES6 compatability tests

--- tests/Resources/app.js
+++ tests/Resources/app.js
@@ -1,21 +1,85 @@
 /*
  * Appcelerator Titanium Mobile
- * Copyright (c) 2011-2016 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2011-2017 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+
+var win = Ti.UI.createWindow({
+	backgroundColor: 'yellow'
+});
+win.open();
+
 require('./ti-mocha');
 var $results = [],
-	failed = false,
-  mainWindow = Ti.UI.createWindow({
-  	backgroundColor: 'yellow'
-  });
-mainWindow.open();
+	failed = false;
 
 // ============================================================================
 // Add the tests here using "require"
+// ES6 syntax/compatability tests
 require('./es6.arrows.test');
+require('./es6.default.args.test');
+require('./es6.rest.args.test');
+require('./es6.spread.args.test');
+require('./es6.string.interpolation.test');
+// Titanium APIs
+require('./ti.accelerometer.test');
+require('./ti.api.test');
+require('./ti.app.test');
+require('./ti.app.properties.test');
+require('./ti.app.windows.backgroundservice.test');
+require('./ti.blob.test');
+require('./ti.builtin.test');
+require('./ti.buffer.test');
+require('./ti.codec.test');
+require('./ti.contacts.test');
+require('./ti.contacts.group.test');
+require('./ti.contacts.person.test');
+require('./ti.database.test');
+require('./ti.filesystem.test');
+require('./ti.filesystem.file.test');
+require('./ti.filesystem.filestream.test');
+require('./ti.geolocation.test');
+require('./ti.gesture.test');
+require('./ti.internal.test');
+require('./ti.locale.test');
+require('./ti.map.test');
+require('./ti.network.test');
+require('./ti.network.httpclient.test');
+require('./ti.platform.test');
+require('./ti.platform.displaycaps.test');
+require('./ti.require.test');
+require('./ti.stream.test');
+require('./ti.test');
+require('./ti.ui.test');
+require('./ti.ui.2dmatrix.test');
+require('./ti.ui.activityindicator.test');
+require('./ti.ui.alertdialog.test');
+require('./ti.ui.button.test');
+require('./ti.ui.constants.test');
+require('./ti.ui.emaildialog.test');
+require('./ti.ui.imageview.test');
+require('./ti.ui.label.test');
+require('./ti.ui.layout.test');
+require('./ti.ui.listview.test');
+require('./ti.ui.optiondialog.test');
+require('./ti.ui.picker.test');
+require('./ti.ui.progressbar.test');
+require('./ti.ui.scrollableview.test');
+require('./ti.ui.scrollview.test');
+require('./ti.ui.searchbar.test');
+require('./ti.ui.switch.test');
+require('./ti.ui.tab.test');
+require('./ti.ui.tableview.test');
+require('./ti.ui.textfield.test');
+require('./ti.ui.view.test');
+require('./ti.ui.webview.test');
+require('./ti.ui.window.test');
+require('./ti.ui.windows.commandbar.test');
+require('./ti.utils.test');
+require('./ti.xml.test');
 // ============================================================================
+
 
 // add a special mocha reporter that will time each test run using
 // our microsecond timer
@@ -77,6 +141,6 @@ mocha.setup({
 
 // dump the output, which will get interpreted above in the logging code
 mocha.run(function () {
-	mainWindow.backgroundColor = failed ? 'red' : 'green';
+	win.backgroundColor = failed ? 'red' : 'green';
 	Ti.API.info('!TEST_RESULTS_STOP!');
 });
