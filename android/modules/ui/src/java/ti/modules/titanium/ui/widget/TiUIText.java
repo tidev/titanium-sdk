ca78c650a4a156665d00b47a0fdc26f8992c8ca7
maggieaxway <mchen@axway.com>
2017-04-26 20:52:53 +0800

    Fix unwanted change event when change properties

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
@@ -497,6 +497,8 @@ public class TiUIText extends TiUIView
 		int autocorrect = InputType.TYPE_TEXT_FLAG_AUTO_CORRECT;
 		int autoCapValue = 0;
 
+		disableChangeEvent = true;
+
 		if (d.containsKey(TiC.PROPERTY_AUTOCORRECT) && !TiConvert.toBoolean(d, TiC.PROPERTY_AUTOCORRECT, true)) {
 			autocorrect = 0;
 		}
@@ -540,6 +542,8 @@ public class TiUIText extends TiUIView
 
 		} else {
 			if (d.containsKey(TiC.PROPERTY_AUTOCAPITALIZATION)) {
+
+				disableChangeEvent = false;
 
 				switch (TiConvert.toInt(d.get(TiC.PROPERTY_AUTOCAPITALIZATION), TEXT_AUTOCAPITALIZATION_NONE)) {
 					case TEXT_AUTOCAPITALIZATION_NONE:
@@ -674,6 +678,8 @@ public class TiUIText extends TiUIView
 		if (!field) {
 			tv.setSingleLine(false);
 		}
+
+		disableChangeEvent = false;
 
 	}
 
