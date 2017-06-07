27ae41695178f6bd36958a4b1757682463523947
Frankie <fmerzadyan@users.noreply.github.com>
2017-06-07 08:06:20 -0700

    [TIMOB-24711] Android: Update android/dev/TitaniumTest (#9072)

--- android/dev/TitaniumTest/src/com/titanium/test/TitaniumTestActivity.java
+++ android/dev/TitaniumTest/src/com/titanium/test/TitaniumTestActivity.java
@@ -2,6 +2,6 @@ package com.titanium.test;
 
 import org.appcelerator.titanium.TiRootActivity;
 
-public final class TitaniumtestActivity extends TiRootActivity
+public final class TitaniumTestActivity extends TiRootActivity
 {
 }
