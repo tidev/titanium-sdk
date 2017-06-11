60434e704fd982f7de5d8011f0cb47d77057dc94
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-06-11 11:41:59 +0200

    Update APSAnalytics, fix dylib imports

--- iphone/Classes/APSAnalytics/APSAnalytics.h
+++ iphone/Classes/APSAnalytics/APSAnalytics.h
@@ -5,8 +5,8 @@
  * Please see the LICENSE included with this distribution for details.
  */
 
-#import <Foundation/Foundation.h>
-#import <CoreLocation/CoreLocation.h>
+@import Foundation;
+@import CoreLocation;
 
 
 /** Constant indicating development deployment */
@@ -21,7 +21,7 @@ extern NSString * const APSDeployTypeProduction;
  * to send analytic data that can be viewed on the Appcelerator Dashboard.
  *
  * For information on getting started with Appcelerator Platform Services,
- * see [Appclerator Platform Services for iOS](http://bit.ly/1kqteQS).
+ * see [Appcelerator Platform Services for iOS](http://bit.ly/1kqteQS).
  */
 @interface APSAnalytics : NSObject
 
@@ -31,17 +31,6 @@ extern NSString * const APSDeployTypeProduction;
 +(instancetype) sharedInstance;
 
 /**
- * Enable the Analytics Service with the given app key. Calling
- * this method is required to start the process of collecting real-time analytics
- * data and sending it to your Analytics Dashboard. Calling this method more
- * than once results in undefined behavior.
- * @param appKey Application GUID
- * @param deployType Set to either APSDeployTypeDevelopment or APSDeployTypeProduction
- */
--(void)enableWithAppKey:(NSString *)appKey andDeployType:(NSString *)deployType;
-
-
-/**
  * The session timeout in seconds. If the application has been in the background
  * for longer than the timeout, the analytics service logs an end time to the current user session.
  * Default value is 30 s.
@@ -49,25 +38,25 @@ extern NSString * const APSDeployTypeProduction;
 @property (atomic, readwrite) NSTimeInterval sessionTimeout;
 
 /**
- * Retrieves the current deployment type
- * Returns either APSDeployTypeDevelopment or APSDeployTypeProduction
+ * Retrieves the current deployment type.
+ * Returns either APSDeployTypeDevelopment or APSDeployTypeProduction.
  */
 @property (atomic, strong, readonly) NSString *deployType;
 
 //Analytic Event Generators
+
 /**
  * Sends a geolocation event.
  * @param location A CLLocation object containing the location data.
  */
-
 -(void)sendAppGeoEvent:(CLLocation *) location;
 
 /**
  * Sends a navigation event
- * @param firstView String describing the location the user navigated from
- * @param secondView String describing the location the user navigated to
- * @param eventName String describing the event
- * @param payload Extra data to send
+ * @param firstView String describing the location the user navigated from.
+ * @param secondView String describing the location the user navigated to.
+ * @param eventName String describing the event.
+ * @param payload Extra data to send. You can only send strings and numbers.
  */
 -(void)sendAppNavEventFromView:(NSString *)firstView
                         toView:(NSString *)secondView
@@ -75,12 +64,36 @@ extern NSString * const APSDeployTypeProduction;
                        payload:(NSDictionary *)payload;
 
 /**
- * Sends a feature event
- * @param eventName String describing the event
- * @param payload Extra data to send
- * TODO : key/pair vales of string
+ * Sends a feature event.
+ * @param eventName String describing the event.
+ * @param payload Extra data to send. You can only send strings and numbers.
  */
 -(void)sendAppFeatureEvent:(NSString *)eventName
                    payload:(NSDictionary *)payload;
+
+/**
+ * Returns the last Analytics event.
+ * @return The last event.
+ */
+- (NSString *)getLastEvent;
+
+/**
+ * Sets the build type.
+ * @param buildType The build type.
+ */
+- (void)setBuildType:(NSString *)buildType;
+
+/**
+ * Sets the SDK version.
+ * @param aVersion The SDK version.
+ */
+-(void)setSDKVersion:(NSString *)aVersion;
+
+/**
+ * Enables a new APSAnalytics session for a given app-key and deploy-type.
+ * @param appKey The app-key to be used.
+ * @param deployType The deploy-type to collect events from.
+ */
+-(void)enableWithAppKey:(NSString *)appKey andDeployType:(NSString *)deployType;
 
 @end
