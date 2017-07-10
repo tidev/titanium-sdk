ad21cc5a9f4a67cfdd88f65064eef89ba05d9e12
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-07-10 08:59:34 -0700

    [TIMOB-24805] Guard setter again

--- iphone/Classes/TiUIWindowProxy.m
+++ iphone/Classes/TiUIWindowProxy.m
@@ -818,6 +818,7 @@
     }, [NSThread isMainThread]);
 }
 
+#if IS_XCODE_9
 - (void)setLargeTitleEnabled:(id)value
 {
     ENSURE_UI_THREAD(setLargeTitleEnabled, value);
@@ -841,6 +842,7 @@
         [[controller navigationItem] setLargeTitleDisplayMode:[TiUtils intValue:value def:UINavigationItemLargeTitleDisplayModeAutomatic]];
     }
 }
+#endif
 
 -(void)setTitlePrompt:(NSString*)title_
 {
