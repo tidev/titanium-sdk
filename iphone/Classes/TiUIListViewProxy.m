1ccfb307843e64df6471541594e5e6c16c2323b0
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-07-19 19:47:51 -0700

    Update TiUIListViewProxy.m

--- iphone/Classes/TiUIListViewProxy.m
+++ iphone/Classes/TiUIListViewProxy.m
@@ -505,12 +505,10 @@
       [NSThread isMainThread]);
 }
 
--(NSMutableArray *)getSelectedRows:(id)unused
+- (NSMutableArray *)selectedRows
 {
-    ENSURE_ARG_COUNT(unused, 0);
-
-    NSMutableArray *result=[[NSMutableArray alloc]init];
-    NSArray *selectedRows = [self.listView.tableView indexPathsForSelectedRows];
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    NSArray *selectedRows = [[self.listView tableView] indexPathsForSelectedRows];
 
     if (selectedRows != nil) {
         TiThreadPerformOnMainThread(^{
