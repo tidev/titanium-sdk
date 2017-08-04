8e8c30ebb23b5264977e77567cf5c4cc6677ebc7
Sergey Volkov <s.volkov@netris.ru>
2017-08-04 12:07:16 +0300

    [TIMOB-25051] fix persistentObjects leak

--- android/modules/ui/src/js/ui.js
+++ android/modules/ui/src/js/ui.js
@@ -7,18 +7,20 @@
 
 var bootstrap = require("bootstrap");
 
-// Objects retained by persistent handles.
-// Each element in this array acts as a storage "cell"
-// keeping the object reachable and alive until it is removed.
-persistentObjects = [];
-
 // Keeps an object alive until dispose() is called.
 // This is currently used to keep "top level" objects
 // (ex: windows, tab groups) alive until their lifecycle ends.
 function PersistentHandle(object) {
-	this.cell = persistentObjects.length;
-	persistentObjects.push(object);
+	this.cell = PersistentHandle.lastId++;
+	PersistentHandle.objects[this.cell] = object;
 }
+
+// Objects retained by persistent handles.
+// Each element in this array acts as a storage "cell"
+// keeping the object reachable and alive until it is removed.
+PersistentHandle.objects = {};
+
+PersistentHandle.lastId = 0;
 
 PersistentHandle.prototype.dispose = function() {
 	if (this.cell == -1) {
@@ -26,7 +28,7 @@ PersistentHandle.prototype.dispose = function() {
 		return;
 	}
 
-	persistentObjects.splice(this.cell, 1);
+	delete PersistentHandle.objects[this.cell];
 	this.cell = -1;
 }
 
