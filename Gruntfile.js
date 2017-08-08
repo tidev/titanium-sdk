488dd8c75c4626b32f4ad46c64285272154773e1
Christopher Williams <chris.a.williams@gmail.com>
2017-08-08 10:20:02 -0400

    Add doc validation to Gruntfile as part of npm test/lint

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,29 @@ module.exports = function (grunt) {
 		}
 	});
 
+	grunt.registerTask('validate_docs', 'Validates the docs.', function () {
+		const done = this.async();
+		const fork = require('child_process').fork, // eslint-disable-line security/detect-child-process
+			path = require('path');
+
+		const validate = fork(path.join(__dirname, 'apidoc', 'validate'), [], { silent: true });
+		let output = '';
+		validate.stdout.on('data', function (data) {
+			output += data;
+		});
+
+		validate.stderr.on('data', function (data) {
+			output += data;
+		});
+
+		validate.on('close', function (code) {
+			if (code !== 0) {
+				done(new Error('Docs validation failed with exit code: ' + code + '\nOutput: ' + output));
+			}
+			done();
+		});
+	});
+
 	// Load grunt plugins for modules
 	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-appc-js');
@@ -17,13 +40,11 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	// register tasks
-	grunt.registerTask('lint', [ 'appcJs' ]);
+	grunt.registerTask('lint', [ 'appcJs', 'validate_docs' ]);
 
 	// register tasks
 	grunt.registerTask('format', [ 'clangFormat' ]);
 
 	// register tasks
 	grunt.registerTask('default', [ 'lint' ]);
-
-	// TODO Add doc validation!
 };
