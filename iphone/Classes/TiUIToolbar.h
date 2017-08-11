ba77c375ca6d6bc80304cf89465b305607a9e616
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-08-11 11:28:42 +0200

    [TIMOB-25003] Fix new define-statements

--- iphone/Classes/TiUIToolbar.h
+++ iphone/Classes/TiUIToolbar.h
@@ -1,11 +1,11 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2017 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-Present by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
 
-#ifdef USE_TI_UIIOSTOOLBAR
+#if defined(USE_TI_UIIOSTOOLBAR) || defined(USE_TI_UITOOLBAR)
 #import "TiUIView.h"
 
 @protocol TiToolbarLayoutDelegate
