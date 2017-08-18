8522519de5ce1cc5b5997a85a24f5ebc76986d0f
Christopher Williams <chris.a.williams@gmail.com>
2017-08-18 14:51:40 -0400

    Fix for loop error

--- apidoc/validate.js
+++ apidoc/validate.js
@@ -494,7 +494,7 @@ function validateObjectAgainstSyntax(obj, syntax, type, currentKey, className) {
 				const array = parent[type];
 				if (array) {
 					// find matching name in array
-					for (let i = 0; array.length; i++) {
+					for (let i = 0; i < array.length; i++) {
 						if (array[i] && array[i].name === currentKey) { // eslint-disable-line max-depth
 							parent = array[i];
 							break;
