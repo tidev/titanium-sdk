85dd5dfbcc6ab52f93233687b78cc85ed92510bb
Gary Mathews <gmathews@appcelerator.com>
2017-09-07 19:29:31 +0100

    [TIMOB-25241] Response 400 codes should call onerror() instead of onload()

--- android/modules/network/src/java/ti/modules/titanium/network/TiHTTPClient.java
+++ android/modules/network/src/java/ti/modules/titanium/network/TiHTTPClient.java
@@ -455,8 +455,13 @@ public class TiHTTPClient
 
 		if (readyState == READY_STATE_DONE) {
 			KrollDict data1 = new KrollDict();
-			data1.putCodeAndMessage(TiC.ERROR_CODE_NO_ERROR, null);
-			dispatchCallback(TiC.PROPERTY_ONLOAD, data1);
+			if (getStatus() >= 400) {
+				data1.putCodeAndMessage(getStatus(), getStatus() + " : " + getStatusText());
+				dispatchCallback(TiC.PROPERTY_ONERROR, data1);
+			} else {
+				data1.putCodeAndMessage(TiC.ERROR_CODE_NO_ERROR, null);
+				dispatchCallback(TiC.PROPERTY_ONLOAD, data1);
+			}
 		}
 	}
 
