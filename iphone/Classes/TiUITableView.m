32d129963df15ce7bc04f83e11ffc530c2214378
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-09-22 13:31:26 +0200

    [TIMOB-25332] Fix table-view estimated heights

--- iphone/Classes/TiUITableView.m
+++ iphone/Classes/TiUITableView.m
@@ -452,6 +452,11 @@
     tableview.dataSource = self;
     tableview.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 
+    // Fixes incorrect heights in iOS 11 as we calculate them internally already
+    tableview.estimatedRowHeight = 0;
+    tableview.estimatedSectionFooterHeight = 0;
+    tableview.estimatedSectionHeaderHeight = 0;
+
     if (TiDimensionIsDip(rowHeight)) {
       [tableview setRowHeight:rowHeight.value];
     } else {
