cb9ad51e8e6f66713ef4d0f6d8272ff62fe84f90
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-09-26 08:36:17 +0200

    [TIMOB-25334] iOS: Prevent Ti.UI.TableView & Ti.UI.ListView from sliding up in navigation-window (#9470)
    * [TIMOB-25334] Fix table-views from sliding up
    
    * [TIMOB-25334] Fix table-views from sliding up
    
    * [TIMOB-25334] Remove unrelated changes from files
    
    * [TIMOB-25334] Fix clang-format issues

--- iphone/Classes/TiUIListView.m
+++ iphone/Classes/TiUIListView.m
@@ -248,6 +248,12 @@ static TiViewProxy *FindViewProxyWithBindIdContainingPoint(UIView *view, CGPoint
     _tableView.estimatedSectionFooterHeight = 0;
     _tableView.estimatedSectionHeaderHeight = 0;
 
+#if IS_XCODE_9
+    if ([TiUtils isIOS11OrGreater]) {
+      _tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
+#endif
+
 #if IS_XCODE_8
     if ([TiUtils isIOS10OrGreater]) {
       _tableView.prefetchDataSource = self;
