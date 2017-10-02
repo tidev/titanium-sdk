916fe9009f05f9bb46f7a2e79df178f199e19443
Hans Knöchel <hans.knoechel@hs-osnabrueck.de>
2017-10-02 14:10:26 +0200

    [TIMOB-25346] Restore SBJSON for encoded strings

--- iphone/Classes/XHRBridge.m
+++ iphone/Classes/XHRBridge.m
@@ -12,6 +12,7 @@
 #import "TiHost.h"
 #import "TiModule.h"
 #import "TiProxy.h"
+#import "SBJSON.h"
 
 static XHRBridge *xhrBridge = nil;
 
@@ -60,17 +61,9 @@ static XHRBridge *xhrBridge = nil;
   NSString *prearg = [url query];
   NSString *arguments = prearg == nil ? @"" : [prearg stringByRemovingPercentEncoding];
 
-  // Decode Ascii unicode-characters
-  NSString *decodevalue = [[NSString alloc] initWithData:[arguments dataUsingEncoding:NSUTF8StringEncoding]
-                                                encoding:NSNonLossyASCIIStringEncoding];
-
-  // Replace < and > characters with quotes
-  NSString *jsonString = [[decodevalue stringByReplacingOccurrencesOfString:@"<" withString:@"\""]
-      stringByReplacingOccurrencesOfString:@">"
-                                withString:@"\""];
-
-  // Parse the JSON-string to a dictionary
-  NSDictionary *event = [TiUtils jsonParse:jsonString];
+  SBJSON *decoder = [[[SBJSON alloc] init] autorelease];
+  NSError *error = nil;
+  NSDictionary *event = [decoder fragmentWithString:arguments error:&error];
 
   id<TiEvaluator> context = [[xhrBridge host] contextForToken:pageToken];
   TiModule *tiModule = (TiModule *)[[xhrBridge host] moduleNamed:module context:context];
