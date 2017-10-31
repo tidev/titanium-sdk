962723b99dfd55f45504d15c0d710d8d12625a55
Hans Knöchel <hansemannn@users.noreply.github.com>
2017-10-31 20:14:09 +0100

    Update _build.js

--- iphone/cli/commands/_build.js
+++ iphone/cli/commands/_build.js
@@ -5540,7 +5540,7 @@ iOSBuilder.prototype.copyResources = function copyResources(next) {
 					// iPhone Landscape - iOS 8,9 - Retina HD 5.5 (2208x1242)
 					'Default-Landscape-736h@3x.png': { idiom: 'iphone', extent: 'full-screen', minSysVer: '8.0', orientation: 'landscape', width: 2208, height: 1242, scale: 3, subtype: '736h' },
 					// iPhone Landscape - iOS 11 - Retina HD iPhone X (2436x1125)
-					'Default-Landscape-2436h@3x.png': { idiom: 'iphone', extent: 'full-screen', minSysVer: '8.0', orientation: 'landscape', width: 2436, height: 1125, scale: 3, subtype: '2436h' },
+					'Default-Landscape-2436h@3x.png': { idiom: 'iphone', extent: 'full-screen', minSysVer: '11.0', orientation: 'landscape', width: 2436, height: 1125, scale: 3, subtype: '2436h' },
 
 					// iPad Portrait - iOS 7-9 - 1x (????)
 					'Default-Portrait.png':          { idiom: 'ipad',   extent: 'full-screen', minSysVer: '7.0', orientation: 'portrait', width: 768, height: 1024, scale: 1 },
