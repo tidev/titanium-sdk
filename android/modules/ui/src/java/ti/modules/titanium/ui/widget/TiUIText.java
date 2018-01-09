ab8ce438925142343b1bae11da3913663e7ebaac
maggieaxway <mchen@axway.com>
2018-01-09 14:10:09 +0800

    disable change event when programatically changing the attributedString property

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
@@ -828,10 +828,13 @@ public class TiUIText extends TiUIView implements TextWatcher, OnEditorActionLis
 		Bundle bundleText =
 			AttributedStringProxy.toSpannableInBundle(attrString, TiApplication.getAppCurrentActivity());
 		if (bundleText.containsKey(TiC.PROPERTY_ATTRIBUTED_STRING)) {
+			//TIMOB-17210 Android: A textfield change listener is wrongly triggered also if the value is programmatically set before creation
+			disableChangeEvent = true;
 			tv.setText((Spannable) bundleText.getCharSequence(TiC.PROPERTY_ATTRIBUTED_STRING));
 			if (bundleText.getBoolean(TiC.PROPERTY_HAS_LINK, false)) {
 				tv.setMovementMethod(LinkMovementMethod.getInstance());
 			}
+			disableChangeEvent = false;
 		}
 	}
 
