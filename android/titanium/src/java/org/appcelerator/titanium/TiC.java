6934fe0f2b717406867a6ee59cc92789beb314fd
Joshua Quick <jquick@axway.com>
2018-01-12 18:39:30 -0800

    [TIMOB-25667] Android: Add TableView "scrollable" property support

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -2594,6 +2594,11 @@ public class TiC
 	/**
 	 * @module.api
 	 */
+	public static final String PROPERTY_SCROLLABLE = "scrollable";
+
+	/**
+	 * @module.api
+	 */
 	public static final String PROPERTY_SEARCH = "search";
 
 	/**
