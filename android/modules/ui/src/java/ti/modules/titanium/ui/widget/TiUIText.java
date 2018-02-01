f4de346f2e4dbfd8b71d840091f73603d2cee6c4
Gary Mathews <gmathews@appcelerator.com>
2018-01-31 16:06:13 -0800

    [TIMOB-25734] Fix click and touch events for TextField and TextArea

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
@@ -126,6 +126,9 @@ public class TiUIText extends TiUIView
 			tv.setSingleLine();
 			tv.setMaxLines(1);
 		}
+		setOnClickListener(tv);
+		setOnLongClickListener(tv);
+		registerForTouch(tv);
 		tv.addTextChangedListener(this);
 		tv.setOnEditorActionListener(this);
 		tv.setOnFocusChangeListener(this); // TODO refactor to TiUIView?
