acd0d8f5212f0760fdfb57586156eb8511a282fd
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-02-15 14:56:03 +0100

    Update TiUIWebViewProxy.m

--- iphone/Classes/TiUIWebViewProxy.m
+++ iphone/Classes/TiUIWebViewProxy.m
@@ -118,7 +118,7 @@ USE_VIEW_FOR_CONTENT_WIDTH
 
 - (NSNumber *)zoomLevel
 {
-    return [[(TiUIWebView *)[self view] webview] stringByEvaluatingJavaScriptFromString:@"document.body.style.zoom"];
+  return [[(TiUIWebView *)[self view] webview] stringByEvaluatingJavaScriptFromString:@"document.body.style.zoom"];
 }
 
 - (void)goBack:(id)args
