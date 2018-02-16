1536df7e6644e8388358785d6b09788d3d60ef6c
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-02-16 23:02:04 +0100

    Lint unit test

--- tests/Resources/ti.network.httpclient.addontest.js
+++ tests/Resources/ti.network.httpclient.addontest.js
@@ -10,55 +10,55 @@
 'use strict';
 var should = require('./utilities/assertions');
 
-describe('Titanium.Network.HTTPClient', function () {
-    it.ios('basic-auth success', function (finish) {
-    	this.timeout(6e4);
-		var xhr = Ti.Network.createHTTPClient({
-			username: "user",
-    		password: "passwd"
-		}),
-		attempts = 3;
-		xhr.setTimeout(6e4);
+describe('Titanium.Network.HTTPClient', function() {
+  it.ios('basic-auth success', function(finish) {
+    this.timeout(6e4);
+    var xhr = Ti.Network.createHTTPClient({
+        username: "user",
+        password: "passwd"
+      }),
+      attempts = 3;
+    xhr.setTimeout(6e4);
 
-		xhr.onload = function () {
-			try {
-				should(this.responseText).be.a.string;
-				finish();
-			} catch (err) {
-				finish(err);
-			}
-		};
-		xhr.onerror = function (e) {
-			if (attempts-- > 0) {
-				Ti.API.warn('failed, attempting to retry request...');
-				xhr.send();
-			} else {
-				Ti.API.debug(JSON.stringify(e, null , 2));
-				finish(new Error('failed to authenticate: ' + e)); 
-			}
-		};
+    xhr.onload = function() {
+      try {
+        should(this.responseText).be.a.string;
+        finish();
+      } catch (err) {
+        finish(err);
+      }
+    };
+    xhr.onerror = function(e) {
+      if (attempts-- > 0) {
+        Ti.API.warn('failed, attempting to retry request...');
+        xhr.send();
+      } else {
+        Ti.API.debug(JSON.stringify(e, null, 2));
+        finish(new Error('failed to authenticate: ' + e));
+      }
+    };
 
-		xhr.open('GET', 'http://httpbin.org/basic-auth/user/passwd');
-		xhr.send();
-	});
+    xhr.open('GET', 'http://httpbin.org/basic-auth/user/passwd');
+    xhr.send();
+  });
 
-    it.ios('basic-auth failure', function (finish) {
-    	this.timeout(6e4);
-		var xhr = Ti.Network.createHTTPClient({
-			username: "user",
-    		password: "wrong_password",
-		});
-		xhr.setTimeout(6e4);
+  it.ios('basic-auth failure', function(finish) {
+    this.timeout(6e4);
+    var xhr = Ti.Network.createHTTPClient({
+      username: "user",
+      password: "wrong_password",
+    });
+    xhr.setTimeout(6e4);
 
-		xhr.onload = function () {
-			finish(new Error('With wrong password it is authenticating')); 
-		};
-		xhr.onerror = function (e) {
-			//This request should fail as password is wrong.
-			finish(); 
-		};
+    xhr.onload = function() {
+      finish(new Error('With wrong password it is authenticating'));
+    };
+    xhr.onerror = function(e) {
+      //This request should fail as password is wrong.
+      finish();
+    };
 
-		xhr.open('GET', 'http://httpbin.org/basic-auth/user/passwd');
-		xhr.send();
-	});
+    xhr.open('GET', 'http://httpbin.org/basic-auth/user/passwd');
+    xhr.send();
+  });
 });
