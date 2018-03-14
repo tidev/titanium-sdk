d77baa0ccf80eaac8ceccbeb57305838e45b4363
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-03-14 13:33:20 +0100

    Remove workaround

--- iphone/Classes/TiMediaVideoPlayerProxy.m
+++ iphone/Classes/TiMediaVideoPlayerProxy.m
@@ -135,13 +135,6 @@ NSArray *moviePlayerKeys = nil;
   [self addNotificationObserver];
   [self setValuesForKeysWithDictionary:loadProperties];
 
-  // TIMOB-25566: Video controls need to be handled manually as they can
-  // cause issues if the URL is set after creation
-  id showsControls = [loadProperties valueForKey:@"showsControls"];
-  if (showsControls != nil) {
-    [self setShowsControls:showsControls];
-  }
-
   // we need this code below since the player can be realized before loading
   // properties in certain cases and when we go to create it again after setting
   // url we will need to set the new controller to the already created view
