2a64293c5c80bde9a0d6af2d339a1f0c71b43a7f
Andrea Vitale <vitale@everyup.it>
2018-04-11 15:11:18 +0200

    Exposing overCurrentContext modal presentation style

--- iphone/Classes/TiUIiOSProxy.h
+++ iphone/Classes/TiUIiOSProxy.h
@@ -147,6 +147,7 @@
 @property (nonatomic, readonly) NSNumber *MODAL_PRESENTATION_PAGESHEET;
 @property (nonatomic, readonly) NSNumber *MODAL_PRESENTATION_FORMSHEET;
 @property (nonatomic, readonly) NSNumber *MODAL_PRESENTATION_CURRENT_CONTEXT;
+@property (nonatomic, readonly) NSNumber *MODAL_PRESENTATION_OVER_CURRENT_CONTEXT;
 
 @property (nonatomic, readonly) NSNumber *MODAL_TRANSITION_STYLE_COVER_VERTICAL;
 @property (nonatomic, readonly) NSNumber *MODAL_TRANSITION_STYLE_FLIP_HORIZONTAL;
