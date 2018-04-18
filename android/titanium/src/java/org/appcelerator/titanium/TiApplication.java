a23eabd183aa16f57aafc53498d0f40e5378583e
Gary Mathews <gmathews@appcelerator.com>
2018-04-18 11:01:09 -0700

    [TIMOB-25963] Handle uncaught java exceptions

--- android/titanium/src/java/org/appcelerator/titanium/TiApplication.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiApplication.java
@@ -362,20 +362,27 @@ public abstract class TiApplication extends Application implements KrollApplicat
 		super.onCreate();
 		Log.d(TAG, "Application onCreate", Log.DEBUG_MODE);
 
-		final UncaughtExceptionHandler defaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+		// handle uncaught java exceptions
 		Thread.setDefaultUncaughtExceptionHandler(new UncaughtExceptionHandler() {
+			@Override
 			public void uncaughtException(Thread t, Throwable e)
 			{
-				if (isAnalyticsEnabled()) {
-					String tiVer = buildVersion + "," + buildTimestamp + "," + buildHash;
-					Log.e(TAG,
-						  "Sending event: exception on thread: " + t.getName() + " msg:" + e.toString() + "; Titanium "
-							  + tiVer,
-						  e);
-					TiPlatformHelper.getInstance().postAnalyticsEvent(
-						TiAnalyticsEventFactory.createErrorEvent(t, e, tiVer));
+
+				// obtain java stack trace
+				String javaStack = "";
+				StackTraceElement[] frames = e.getCause() != null ? e.getCause().getStackTrace() : e.getStackTrace();
+				for (StackTraceElement frame : frames) {
+					javaStack += "\n    " + frame.toString();
 				}
-				defaultHandler.uncaughtException(t, e);
+
+				// construct exception message
+				String message = e.getMessage();
+				if (!javaStack.isEmpty()) {
+					message += javaStack;
+				}
+
+				// throw exception as KrollException
+				KrollRuntime.dispatchException("Runtime Error", message, null, 0, null, 0);
 			}
 		});
 
