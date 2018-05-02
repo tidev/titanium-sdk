db943e048ab6d82035244868872a28bfd7c709d8
Joshua Quick <jquick@axway.com>
2018-05-02 15:46:46 -0700

    Updated code format of ScrollableView unit test.

--- tests/Resources/ti.ui.scrollableview.test.js
+++ tests/Resources/ti.ui.scrollableview.test.js
@@ -4,6 +4,7 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+'use strict';
 var should = require('./utilities/assertions'),
 	utilities = require('./utilities/utilities');
 
@@ -11,11 +12,16 @@ describe('Titanium.UI.ScrollableView', function () {
 	var win;
 	this.timeout(5000);
 
-	afterEach(function () {
+	afterEach(function (finish) {
 		if (win) {
+			win.addEventListener('close', function () {
+				finish();
+			});
 			win.close();
+			win = null;
+		} else {
+			finish();
 		}
-		win = null;
 	});
 
 	it('apiName', function () {
@@ -30,7 +36,7 @@ describe('Titanium.UI.ScrollableView', function () {
 		should(bar.getViews).be.a.Function;
 		should(bar.views).be.empty;
 		should(bar.getViews()).be.empty;
-		bar.views = [Ti.UI.createView(), Ti.UI.createView()];
+		bar.views = [ Ti.UI.createView(), Ti.UI.createView() ];
 		should(bar.views.length).eql(2);
 		should(bar.getViews().length).eql(2);
 	});
@@ -42,38 +48,39 @@ describe('Titanium.UI.ScrollableView', function () {
 		should(bar.getCurrentPage).be.a.Function;
 		should(bar.currentPage).eql(0);
 		should(bar.getCurrentPage()).eql(0);
-		bar.views = [Ti.UI.createView(), Ti.UI.createView()];
+		bar.views = [ Ti.UI.createView(), Ti.UI.createView() ];
 		bar.currentPage = 1;
 		should(bar.currentPage).eql(1); // Android gives 0
 		should(bar.getCurrentPage()).eql(1);
 	});
 
 	it('moveX-scrollTo', function (finish) {
+		var testName = null,
+			nextPageIndex = 0,
+			bar = null;
 		this.slow(5000);
 		this.timeout(20000);
-		var testName = null;
-		var nextPageIndex = 0;
 		function doNextTest() {
 			try {
 				if (!testName) {
-					testName = "moveNext";
-					Ti.API.info("Testing ScrollableView.moveNext()");
+					testName = 'moveNext';
+					Ti.API.info('Testing ScrollableView.moveNext()');
 					nextPageIndex = bar.currentPage + 1;
 					should(bar.moveNext).be.a.Function;
 					bar.moveNext();
-				} else if (testName === "moveNext") {
-					testName = "movePrevious";
-					Ti.API.info("Testing ScrollableView.movePrevious()");
+				} else if (testName === 'moveNext') {
+					testName = 'movePrevious';
+					Ti.API.info('Testing ScrollableView.movePrevious()');
 					nextPageIndex = bar.currentPage - 1;
 					should(bar.movePrevious).be.a.Function;
 					bar.movePrevious();
-				} else if (testName === "movePrevious") {
-					testName = "scrollToView";
-					Ti.API.info("Testing ScrollableView.scrollToView()");
+				} else if (testName === 'movePrevious') {
+					testName = 'scrollToView';
+					Ti.API.info('Testing ScrollableView.scrollToView()');
 					nextPageIndex = 2;
 					should(bar.scrollToView).be.a.Function;
 					bar.scrollToView(nextPageIndex);
-				} else if (testName === "scrollToView") {
+				} else if (testName === 'scrollToView') {
 					finish();
 				}
 			} catch (err) {
@@ -81,8 +88,8 @@ describe('Titanium.UI.ScrollableView', function () {
 			}
 		}
 		win = Ti.UI.createWindow();
-		var bar = Ti.UI.createScrollableView();
-		bar.views = [Ti.UI.createView(), Ti.UI.createView(), Ti.UI.createView()];
+		bar = Ti.UI.createScrollableView();
+		bar.views = [ Ti.UI.createView(), Ti.UI.createView(), Ti.UI.createView() ];
 		bar.addEventListener('scrollend', function (e) {
 			try {
 				should(e.currentPage).eql(nextPageIndex);
