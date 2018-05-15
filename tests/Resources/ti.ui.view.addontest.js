d4838b8bbdb246c2fb881c407ae31bc2358f414a
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-05-15 12:52:33 +0200

    Lint unit-test

--- tests/Resources/ti.ui.view.addontest.js
+++ tests/Resources/ti.ui.view.addontest.js
@@ -10,39 +10,39 @@
 'use strict';
 var should = require('./utilities/assertions');
 
-describe('Titanium.UI.View', function() {
+describe('Titanium.UI.View', function () {
 
-  it.ios('.horizontalMotionEffect, .verticalMotionEffect', function(finish) {
-    var win = Ti.UI.createWindow({
-      backgroundColor: 'blue'
-    });
+	it.ios('.horizontalMotionEffect, .verticalMotionEffect', function (finish) {
+		var win = Ti.UI.createWindow({
+			backgroundColor: 'blue'
+		});
 
-    var view = Ti.UI.createView({
-      horizontalMotionEffect: {
-        min: -50,
-        max: 50
-      },
-      verticalMotionEffect: {
-        min: -50,
-        max: 50
-      }
-    });
+		var view = Ti.UI.createView({
+			horizontalMotionEffect: {
+				min: -50,
+				max: 50
+			},
+			verticalMotionEffect: {
+				min: -50,
+				max: 50
+			}
+		});
 
-    win.addEventListener('open', function() {
-        // horizontalMotionEffect
-        should(view.horizontalMotionEffect).be.an.Object;
-        should(view.horizontalMotionEffect.min).be.a.Number;
-        should(view.horizontalMotionEffect.max).be.a.Number;
+    	win.addEventListener('open', function () {
+			// horizontalMotionEffect
+			should(view.horizontalMotionEffect).be.an.Object;
+			should(view.horizontalMotionEffect.min).be.a.Number;
+			should(view.horizontalMotionEffect.max).be.a.Number;
 
-        // verticalMotionEffect
-        should(view.verticalMotionEffect).be.an.Object;
-        should(view.verticalMotionEffect.min).be.a.Number;
-        should(view.verticalMotionEffect.max).be.a.Number;
+			// verticalMotionEffect
+			should(view.verticalMotionEffect).be.an.Object;
+			should(view.verticalMotionEffect.min).be.a.Number;
+			should(view.verticalMotionEffect.max).be.a.Number;
+			
+			finish();
+		});
 
-        finish();
-    });
-
-    win.add(view);
-    win.open();
-  });
+		win.add(view);
+		win.open();
+	});
 });
