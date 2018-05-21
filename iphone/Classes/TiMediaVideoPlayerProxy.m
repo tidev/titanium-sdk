280086aabde5df180c06071363151c7e622fc158
vijaysingh-axway <vsingh@axway.com>
2018-05-21 19:29:03 +0800

    [TIMOB-25801] : iOS SDK 7.0.2.GA - VideoPlayer playback issues (#9890)
    * [TIMOB-25801] : iOS SDK 7.0.2.GA - VideoPlayer playback issues
    
    * [TIMOB-25801] : Unit test case added

--- iphone/Classes/TiMediaVideoPlayerProxy.m
+++ iphone/Classes/TiMediaVideoPlayerProxy.m
@@ -934,7 +934,7 @@ NSArray *moviePlayerKeys = nil;
     playing = ([[movie player] rate] == 1.0);
     if (playing) {
       _playbackState = TiVideoPlayerPlaybackStatePlaying;
-    } else if (movie.player.currentItem.duration.value == movie.player.currentItem.currentTime.value || !movie.player.currentItem.canStepBackward) {
+    } else if (movie.player.currentItem.currentTime.value == 0 || !movie.player.currentItem.canStepBackward) {
       _playbackState = TiVideoPlayerPlaybackStateStopped;
     } else {
       _playbackState = TiVideoPlayerPlaybackStatePaused;
@@ -957,7 +957,7 @@ NSArray *moviePlayerKeys = nil;
   if (movie.player.timeControlStatus == AVPlayerTimeControlStatusPlaying) {
     _playbackState = TiVideoPlayerPlaybackStatePlaying;
   } else if (movie.player.timeControlStatus == AVPlayerTimeControlStatusPaused) {
-    if (movie.player.currentItem.duration.value == movie.player.currentItem.currentTime.value) {
+    if (movie.player.currentItem.currentTime.value == 0) {
       _playbackState = TiVideoPlayerPlaybackStateStopped;
     } else {
       _playbackState = TiVideoPlayerPlaybackStatePaused;
