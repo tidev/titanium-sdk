26643f94147f7ba56dc264e3af1810764e27d39a
Andrea Vitale <vitale@everyup.it>
2018-05-28 14:21:57 +0200

    Adding fieldBackgroundColor property

--- iphone/Classes/TiUISearchBar.m
+++ iphone/Classes/TiUISearchBar.m
@@ -150,6 +150,20 @@
   }];
 }
 
+- (void)setFieldBackgroundColor_:(id)value
+{
+    // TIMOB-10368
+    // Remove this hack again once iOS exposes this as a public API
+    UIView *searchContainerView = [[[self searchBar] subviews] firstObject];
+    
+    [[searchContainerView subviews] enumerateObjectsUsingBlock:^(__kindof UIView *_Nonnull obj, NSUInteger idx, BOOL *_Nonnull stop) {
+        if ([obj isKindOfClass:[UITextField class]]) {
+            [(UITextField *)obj setBackgroundColor:[[TiUtils colorValue:value] _color]];
+            *stop = YES;
+        }
+    }];
+}
+
 - (void)setKeyboardType_:(id)value
 {
   [[self searchBar] setKeyboardType:[TiUtils intValue:value]];
