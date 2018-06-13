c2b8e5288b5df359019f0c2826968b08ff0f17b2
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-06-13 16:37:10 +0200

    Update TiViewProxy.m

--- iphone/Classes/TiViewProxy.m
+++ iphone/Classes/TiViewProxy.m
@@ -1,6 +1,6 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-2018 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
@@ -26,7 +26,7 @@
 #endif
 
 #define IGNORE_IF_NOT_OPENED                      \
-  if (!windowOpened || ![self viewAttached]) \
+  if (!windowOpened || ![self viewAttached])      \
     return;
 
 static NSArray *touchEventsArray;
