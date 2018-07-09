55cfc969b1f330cb3f6bd0914c2331ca36d3088e
Ewan Harris <ewanharris93@gmail.com>
2018-07-09 14:30:58 +0100

    [TIMOB-26115] Address review comments

--- iphone/Classes/TiConsole.m
+++ iphone/Classes/TiConsole.m
@@ -44,7 +44,7 @@
     [self logMessage:[logMessage componentsSeparatedByString:@" "] severity:@"warn"];
     return;
   }
-  double duration = [startTime timeIntervalSinceNow] * -1000;
+  NSTimeInterval duration = [startTime timeIntervalSinceNow] * -1000;
   NSString *logMessage = [NSString stringWithFormat:@"%@: %0.fms", label, duration];
   [self logMessage:[logMessage componentsSeparatedByString:@" "] severity:@"info"];
   [_times removeObjectForKey:label];
@@ -52,20 +52,17 @@
 
 - (void)timeLog:(id)args
 {
-  NSString *label = [args objectAtIndex:0];
+  NSString *label = [args objectAtIndex:0] ?: @"default";
   NSArray *logData = [args count] > 1 ? [args subarrayWithRange:NSMakeRange(1, [args count] - 1)] : nil;
-  if (label == nil) {
-    label = @"default";
-  }
   NSDate *startTime = _times[label];
   if (startTime == nil) {
     NSString *logMessage = [NSString stringWithFormat:@"Label \"%@\" does not exist", label];
     [self logMessage:[logMessage componentsSeparatedByString:@" "] severity:@"warn"];
     return;
   }
-  double duration = [startTime timeIntervalSinceNow] * -1000;
+  NSTimeInterval duration = [startTime timeIntervalSinceNow] * -1000;
   NSString *logMessage = [NSString stringWithFormat:@"%@: %0.fms ", label, duration];
-  if ([logData count]) {
+  if ([logData count] > 0) {
     logMessage = [logMessage stringByAppendingString:[logData componentsJoinedByString:@" "]];
   }
   [self logMessage:[logMessage componentsSeparatedByString:@" "] severity:@"info"];
