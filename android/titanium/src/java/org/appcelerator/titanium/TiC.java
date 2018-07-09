8ce28ff3ef4218257335c01267d2c039cb7f6842
Sergey Volkov <s.volkov@netris.ru>
2018-07-09 12:10:41 +0300

    [TIMOB-25635] add "clipViews" and "padding"

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -1281,6 +1281,11 @@ public class TiC
 	/**
 	 * @module.api
 	 */
+	public static final String PROPERTY_CLIP_VIEWS = "clipViews";
+
+	/**
+	 * @module.api
+	 */
 	public static final String PROPERTY_CODE = "code";
 
 	/**
