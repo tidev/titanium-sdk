b716f03d6afb7b1f85174716a013d5f574ef9a7d
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-07-25 11:07:32 +0200

    Remove the deprecated Ti.UI.iOS.Toolbar test

---
+++ tests/Resources/ti.ui.toolbar.test.js
@@ -0,0 +1,34 @@
+/*
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2017-Present by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+/* eslint-env mocha */
+/* global Ti */
+/* eslint no-unused-expressions: "off" */
+'use strict';
+var should = require('./utilities/assertions');
+
+describe('Titanium.UI.Toolbar', function () {
+	this.timeout(10000);
+
+	// FIXME Add to Windows API!
+	it.windowsMissing('SimpleToolbar', function () {
+		var send = Ti.UI.createButton({
+				title: 'Send',
+			}),
+			camera = Ti.UI.createButton({
+				title: 'Camera'
+			}),
+			toolbar = Ti.UI.createToolbar({
+				items: [ send, camera ],
+				bottom: 0
+			});
+
+		should(toolbar).have.readOnlyProperty('apiName').which.is.a.String;
+		should(toolbar.apiName).be.eql('Ti.UI.Toolbar');
+		should(toolbar.items).be.an.Array;
+		should(toolbar.items.length).eql(2);
+	});
+});
