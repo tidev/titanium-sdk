35b57d1e8f6f93ca9a8fab712f2a3535da7cb073
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-07-26 17:51:48 +0200

    Update ContactsApiLevel5.java

--- android/modules/contacts/src/java/ti/modules/titanium/contacts/ContactsApiLevel5.java
+++ android/modules/contacts/src/java/ti/modules/titanium/contacts/ContactsApiLevel5.java
@@ -286,6 +286,8 @@ public class ContactsApiLevel5 extends CommonContactsApi
 			return Im.PROTOCOL_QQ;
 		} else if (serviceName.equals("Skype")) {
 			return Im.PROTOCOL_SKYPE;
+		} else if (serviceName.equals("Yahoo")) {
+			return Im.PROTOCOL_YAHOO;
 		} else if (serviceName.equals("Jabber")) {
 			return Im.PROTOCOL_JABBER;
 		} else {
