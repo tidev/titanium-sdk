73dcb2fc70573bbcdf75f69cc58dcb6f8dd8a667
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-07-29 15:36:24 +0200

    Hack together backwards compatibility for old modules

--- build/ios.js
+++ build/ios.js
@@ -6,8 +6,8 @@ const path = require('path'),
 	utils = require('./utils'),
 	copyFiles = utils.copyFiles,
 	copyAndModifyFile = utils.copyAndModifyFile,
-	copyAndModifyFiles = utils.copyAndModifyFiles,
 	globCopy = utils.globCopy,
+	globCopyFlat = utils.globCopyFlat,
 	ROOT_DIR = path.join(__dirname, '..'),
 	IOS_ROOT = path.join(ROOT_DIR, 'iphone'),
 	IOS_LIB = path.join(IOS_ROOT, 'lib');
@@ -42,14 +42,26 @@ IOS.prototype.package = function (packager, next) {
 	async.parallel([
 		function (callback) {
 			async.series([
-				function (cb) {
+				// Copy legacy copies of TiBase.h, TiApp.h etc into 'include/' to retain backwards compatibility in SDK 8.0.0 
+				// TODO: Inject a deprecation warning if used and remove in SDK 9.0.0
+				function copyLegacyCoreHeaders (cb) {
+					globCopyFlat('**/*.h', path.join(IOS_ROOT, 'TitaniumKit', 'TitaniumKit', 'Sources'), path.join(DEST_IOS, 'include'), cb);
+				},
+				// Copy legacy copies of APSAnalytics.h and APSHTTPClient.h into 'include/' to retain backwards compatibility in SDK 8.0.0 
+				// TODO: Inject a deprecation warning if used and remove in SDK 9.0.0
+				function copyLegacyLibraryHeaders (cb) {
+					globCopy('**/*.h', path.join(IOS_ROOT, 'TitaniumKit', 'TitaniumKit', 'Libraries'), path.join(DEST_IOS, 'include'), cb);
+				},
+				// Copy TitaniumKit
+				function copyTitaniumKit (cb) {
 					copyFiles(IOS_ROOT, DEST_IOS, [ 'AppledocSettings.plist', 'Classes', 'cli', 'iphone', 'templates', 'TitaniumKit' ], cb);
 				},
-				function (cb) {
+				// Copy module templates (Swift & Obj-C)
+				function copyModuleTemplates (cb) {
 					copyFiles(IOS_ROOT, DEST_IOS, [ 'AppledocSettings.plist', 'Classes', 'cli', 'iphone', 'templates' ], cb);
 				},
 				// Copy and inject values for special source files
-				function (cb) {
+				function injectSDKConstants (cb) {
 					const subs = {
 						__SDK_VERSION__: this.sdkVersion,
 						__BUILD_DATE__: this.timestamp,
@@ -65,15 +77,16 @@ IOS.prototype.package = function (packager, next) {
 				}.bind(this),
 
 				// Copy Ti.Verify
-				function (cb) {
+				function copyTiVerify (cb) {
 					copyFiles(IOS_LIB, DEST_IOS, [ 'libtiverify.a' ], cb);
 				},
 				// Copy iphone/package.json, but replace __VERSION__ with our version!
-				function (cb) {
+				function copyPackageJSON (cb) {
 					copyAndModifyFile(IOS_ROOT, DEST_IOS, 'package.json', { __VERSION__: this.sdkVersion }, cb);
 				}.bind(this),
 				// Copy iphone/Resources/modules/<name>/* to this.zipSDKDir/iphone/modules/<name>/images
-				function (cb) {
+				// TODO: Pretty sure these can be removed nowadays
+				function copyModuleAssets (cb) {
 					fs.copy(path.join(IOS_ROOT, 'Resources', 'modules'), path.join(DEST_IOS, 'modules'), cb);
 				}
 			], callback);
