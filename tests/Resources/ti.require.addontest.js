8e68518faf181d3d562edf4944819bbd89cbe42d
Hans Knöchel <hansemannn@users.noreply.github.com>
2018-08-09 00:01:40 +0200

    Update ti.require.addontest.js

--- tests/Resources/ti.require.addontest.js
+++ tests/Resources/ti.require.addontest.js
@@ -12,7 +12,7 @@ var should = require('./utilities/assertions');
 
 describe('require()', function () {
 	it('JSON-based require() with single-quotes', function () {
-		var amber = require('json_files/amber');
+		var amber = require('./json_files/amber');
 		should(amber).have.property('sdk');
 		should(amber.sdk).be.eql('7.4.0.v20180627024922');
 	});
