8a338a08a2b0a62f4c503840322a2e93736621af
Sergey Volkov <drauggres@gmail.com>
2018-08-09 23:37:46 +0300

    [TIMOB-25635] Android: Multi-View Viewpager (ScrollableView) (#10172)
    * [TIMOB-25635] add "clipViews" and "padding"
    
    * Add docs
    
    * [TIMOB-25635] Specify default clipToPadding value
    
    * Add unit-tests
    
    * Update ti.ui.scrollableview.test.js

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -1291,6 +1291,11 @@ public class TiC
 	/**
 	 * @module.api
 	 */
+	public static final String PROPERTY_CLIP_VIEWS = "clipViews";
+
+	/**
+	 * @module.api
+	 */
 	public static final String PROPERTY_CODE = "code";
 
 	/**
