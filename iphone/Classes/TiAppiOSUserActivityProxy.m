7d6c98f5cee2410b059509383ee8e9ad1d2b0dd1
vijay vikram singh <vsingh@axway.com>
2018-08-28 17:01:46 +0800

    [TIMOB-26312]: iOS 12 Expose new NSUserActivity APIs for Siri Intents

--- iphone/Classes/TiAppiOSUserActivityProxy.m
+++ iphone/Classes/TiAppiOSUserActivityProxy.m
@@ -123,6 +123,19 @@
     }
   }
 
+#if IS_XCODE_10
+  if ([TiUtils isIOSVersionOrGreater:@"12.0"]) {
+    if ([props objectForKey:@"eligibleForPrediction"]) {
+      [_userActivity setEligibleForPrediction:[TiUtils boolValue:@"eligibleForPrediction" properties:props]];
+    }
+
+    if ([props objectForKey:@"persistentIdentifier"]) {
+      [_userActivity setPersistentIdentifier:[TiUtils stringValue:@"persistentIdentifier"
+                                                       properties:props]];
+    }
+  }
+#endif
+
   _userActivity.delegate = self;
 }
 
@@ -425,6 +438,73 @@
   }
   [_userActivity resignCurrent];
 }
+
+#if IS_XCODE_10
+
+- (NSNumber *)eligibleForPrediction
+{
+  if (![TiUtils isIOSVersionLower:@"12.0"]) {
+    return NUMBOOL(NO);
+  }
+
+  return NUMBOOL(_userActivity.isEligibleForPrediction);
+}
+
+- (void)setEligibleForPrediction:(NSNumber *)value
+{
+  ENSURE_TYPE(value, NSNumber);
+  ENSURE_UI_THREAD(setEligibleForSearch, value);
+  if (![TiUtils isIOSVersionLower:@"12.0"]) {
+    return;
+  }
+  [_userActivity setEligibleForPrediction:[TiUtils boolValue:value]];
+}
+
+- (NSString *)persistentIdentifier
+{
+  if (![TiUtils isIOSVersionLower:@"12.0"]) {
+    return nil;
+  }
+
+  return _userActivity.persistentIdentifier;
+}
+
+- (void)setPersistentIdentifier:(NSString *)value
+{
+  ENSURE_TYPE(value, NSString);
+  if ([TiUtils isIOSVersionLower:@"12.0"]) {
+    return;
+  }
+  [_userActivity setPersistentIdentifier:[TiUtils stringValue:value]];
+}
+
+- (void)deleteSavedUserActivitiesForPersistentIdentifiers:(id)persistentIdentifiers
+{
+  ENSURE_ARRAY(persistentIdentifiers);
+  if ([TiUtils isIOSVersionLower:@"12.0"]) {
+    return;
+  }
+  [NSUserActivity deleteSavedUserActivitiesWithPersistentIdentifiers:persistentIdentifiers
+                                                   completionHandler:^{
+                                                     if ([self _hasListeners:@"useractivitydeleted"]) {
+                                                       [self fireEvent:@"useractivitydeleted" withObject:nil];
+                                                     }
+                                                   }];
+}
+
+- (void)deleteAllSavedUserActivities:(id)unused
+{
+  if ([TiUtils isIOSVersionLower:@"12.0"]) {
+    return;
+  }
+  [NSUserActivity deleteAllSavedUserActivitiesWithCompletionHandler:^{
+    if ([self _hasListeners:@"useractivitydeleted"]) {
+      [self fireEvent:@"useractivitydeleted" withObject:nil];
+    }
+  }];
+}
+
+#endif
 @end
 
 #endif
