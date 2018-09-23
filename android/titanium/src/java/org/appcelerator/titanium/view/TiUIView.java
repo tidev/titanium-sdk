d8e1ecd5f1e9c75d49078014eda71610778c9196
Michael Gangolf <migamiga@web.de>
2018-09-23 15:41:44 +0200

    Android: anchorPoint

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -943,6 +943,14 @@ public abstract class TiUIView implements KrollProxyListener, OnFocusChangeListe
 			if (getOuterView() != null) {
 				ViewCompat.setElevation(getOuterView(), TiConvert.toFloat(newValue));
 			}
+		} else if (key.equals(TiC.PROPERTY_ANCHOR_POINT)) {
+			if (getOuterView() != null) {
+				if (newValue instanceof HashMap) {
+					setAnchor((HashMap) newValue);
+				} else {
+					Log.e(TAG, "Invalid argument type for anchorPoint property. Ignoring");
+				}
+			}
 		} else if (key.equals(TiC.PROPERTY_TRANSLATION_X)) {
 			if (getOuterView() != null) {
 				ViewCompat.setTranslationX(getOuterView(), TiConvert.toFloat(newValue));
@@ -1085,6 +1093,15 @@ public abstract class TiUIView implements KrollProxyListener, OnFocusChangeListe
 			ViewCompat.setElevation(getOuterView(), TiConvert.toFloat(d, TiC.PROPERTY_ELEVATION));
 		}
 
+		if (d.containsKey(TiC.PROPERTY_ANCHOR_POINT) && !nativeViewNull) {
+			Object value = d.get(TiC.PROPERTY_ANCHOR_POINT);
+			if (value instanceof HashMap) {
+				setAnchor((HashMap) d);
+			} else {
+				Log.e(TAG, "Invalid argument type for anchorPoint property. Ignoring");
+			}
+		}
+
 		if (d.containsKey(TiC.PROPERTY_ROTATION) && !nativeViewNull) {
 			ViewCompat.setRotation(nativeView, TiConvert.toFloat(d, TiC.PROPERTY_ROTATION));
 		}
@@ -1120,6 +1137,19 @@ public abstract class TiUIView implements KrollProxyListener, OnFocusChangeListe
 		if (LOLLIPOP_OR_GREATER && !nativeViewNull && d.containsKeyAndNotNull(TiC.PROPERTY_TRANSITION_NAME)) {
 			ViewCompat.setTransitionName(nativeView, d.getString(TiC.PROPERTY_TRANSITION_NAME));
 		}
+	}
+
+	private void setAnchor(HashMap point)
+	{
+		View outerView = getOuterView();
+		if (outerView == null) {
+			return;
+		}
+		float pivotX = TiConvert.toFloat(point.get(TiC.PROPERTY_X), 0.5f);
+		float pivotY = TiConvert.toFloat(point.get(TiC.PROPERTY_Y), 0.5f);
+
+		ViewCompat.setPivotX(outerView, pivotX * outerView.getWidth());
+		ViewCompat.setPivotY(outerView, pivotY * outerView.getHeight());
 	}
 
 	// TODO dead code?
