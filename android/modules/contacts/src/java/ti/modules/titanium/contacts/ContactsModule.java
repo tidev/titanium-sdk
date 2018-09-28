19c9c0f8fe6da9ad104c357dd5525983ae00b966
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-09-28 10:45:54 +0200

    fix: apply linting

--- android/modules/contacts/src/java/ti/modules/titanium/contacts/ContactsModule.java
+++ android/modules/contacts/src/java/ti/modules/titanium/contacts/ContactsModule.java
@@ -137,7 +137,10 @@ public class ContactsModule extends KrollModule implements TiActivityResultHandl
 	@Kroll.method
 	public PersonProxy getPersonByID(long id)
 	{
-		Log.w(TAG, "Ti.Contacts.getPersonByID() has been deprecated in favor of Ti.Contacts.getPersonByIdentifier() for cross-platform parity", Log.DEBUG_MODE);
+		Log.w(
+			TAG,
+			"Ti.Contacts.getPersonByID() has been deprecated in favor of Ti.Contacts.getPersonByIdentifier() for cross-platform parity",
+			Log.DEBUG_MODE);
 		return contactsApi.getPersonById(id);
 	}
 
