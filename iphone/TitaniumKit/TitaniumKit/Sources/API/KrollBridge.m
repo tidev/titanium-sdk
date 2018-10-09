8369638fb5e048a7139ab8931147f97166a73325
Christopher Williams <chris.a.williams@gmail.com>
2018-10-09 14:54:29 -0400

    refactor(ios): Move Ti.Gesture over to new JavaScriptCore Obj-c based API for proxies

--- iphone/TitaniumKit/TitaniumKit/Sources/API/KrollBridge.m
+++ iphone/TitaniumKit/TitaniumKit/Sources/API/KrollBridge.m
@@ -452,13 +452,11 @@ CFMutableSetRef krollBridgeRegistry = nil;
     @"Database",
     @"Filesystem",
     @"Geolocation",
-    @"Gesture",
     @"Media",
     @"Network",
     @"Stream",
     @"UI",
-    @"XML",
-    @"Yahoo" ];
+    @"XML" ];
   for (NSString *name in legacyModuleNames) {
     // We must generate the block and copy it to put it into heap or else every instance of the block shares
     // the same "name" value. See https://stackoverflow.com/questions/7750907/blocks-loops-and-local-variables
@@ -489,7 +487,7 @@ CFMutableSetRef krollBridgeRegistry = nil;
 
   // New JSExport based modules
   // Basically a whitelist of Ti.* modules to load lazily
-  NSArray *moduleNames = @[ @"Accelerometer", @"API", @"Locale", @"Platform", @"Utils" ];
+  NSArray *moduleNames = @[ @"Accelerometer", @"API", @"Gesture", @"Locale", @"Platform", @"Utils" ];
   for (NSString *name in moduleNames) {
     // We must generate the block and copy it to put it into heap or else every instance of the block shares
     // the same "name" value. See https://stackoverflow.com/questions/7750907/blocks-loops-and-local-variables
