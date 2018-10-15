ee0b11b625a754223800301d1ebff47e23b6739b
vijaysingh-axway <vsingh@axway.com>
2018-10-15 16:49:31 +0800

    [TIMOB-25048]: Deprecation message updated

--- iphone/Classes/TiUIiOSProxy.m
+++ iphone/Classes/TiUIiOSProxy.m
@@ -728,7 +728,7 @@ MAKE_SYSTEM_PROP(WEBVIEW_NAVIGATIONTYPE_OTHER, UIWebViewNavigationTypeOther);
 #ifdef USE_TI_UIIOSAPPLICATIONSHORTCUTS
 - (id)createApplicationShortcuts:(id)args
 {
-  DEPRECATED_REPLACED(@"UI.iOS.ApplicationShortcuts", @"7.1.0", @"UI.ApplicationShortcuts");
+  DEPRECATED_REPLACED(@"UI.iOS.ApplicationShortcuts", @"7.5.0", @"UI.ApplicationShortcuts");
   return [[[TiUIiOSApplicationShortcutsProxy alloc] _initWithPageContext:[self executionContext] args:args] autorelease];
 }
 #endif
