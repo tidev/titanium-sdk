86e626fb4916edbeed04fc2d5395fcdfae834f92
Yordan Banev <yordan.banev@gmail.com>
2018-10-23 15:11:15 +0300

    Connect different color states with the colors in the Android object for TiView. Replace Strings in property accessors list for Ti.View with proper constants.

--- android/titanium/src/java/org/appcelerator/titanium/util/TiUIHelper.java
+++ android/titanium/src/java/org/appcelerator/titanium/util/TiUIHelper.java
@@ -42,6 +42,7 @@ import org.appcelerator.titanium.view.TiDrawableReference;
 import org.appcelerator.titanium.view.TiUIView;
 
 import android.app.Activity;
+import android.graphics.drawable.PaintDrawable;
 import android.support.v7.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -642,15 +643,15 @@ public class TiUIHelper
 		return new LayerDrawable(layers.toArray(new Drawable[layers.size()]));
 	}
 
-	private static final int[] BACKGROUND_DEFAULT_STATE_1 = { android.R.attr.state_window_focused,
-															  android.R.attr.state_enabled };
-	private static final int[] BACKGROUND_DEFAULT_STATE_2 = { android.R.attr.state_enabled };
-	private static final int[] BACKGROUND_SELECTED_STATE = { android.R.attr.state_window_focused,
-															 android.R.attr.state_enabled,
-															 android.R.attr.state_pressed };
-	private static final int[] BACKGROUND_FOCUSED_STATE = { android.R.attr.state_focused,
-															android.R.attr.state_window_focused,
-															android.R.attr.state_enabled };
+	public static final int[] BACKGROUND_DEFAULT_STATE_1 = { android.R.attr.state_window_focused,
+															 android.R.attr.state_enabled };
+	public static final int[] BACKGROUND_DEFAULT_STATE_2 = { android.R.attr.state_enabled };
+	public static final int[] BACKGROUND_SELECTED_STATE = { android.R.attr.state_window_focused,
+															android.R.attr.state_enabled,
+															android.R.attr.state_pressed };
+	public static final int[] BACKGROUND_FOCUSED_STATE = { android.R.attr.state_focused,
+														   android.R.attr.state_window_focused,
+														   android.R.attr.state_enabled };
 	public static final int[] BACKGROUND_DISABLED_STATE = { -android.R.attr.state_enabled };
 
 	public static StateListDrawable buildBackgroundDrawable(String image, boolean tileImage, String color,
@@ -1220,5 +1221,65 @@ public class TiUIHelper
 			}
 		}
 		return mUri;
+	}
+
+	/**
+	 * Helper method for getting the actual color values for Views with defined custom backgrounds
+	 * that take advantage of color state lists.
+	 */
+	public static String getColorOfBackgroundForState(TiBackgroundDrawable backgroundDrawable, int[] state)
+	{
+		try {
+			// TiBackgroundDrawable's background can be either PaintDrawable or StateListDrawable.
+			// Handle the cases separately.
+			Drawable simpleDrawable = backgroundDrawable.getBackground();
+			if (simpleDrawable instanceof PaintDrawable) {
+				// For backwards compatibility return null if the required state is not the default one.
+				if (state != TiUIHelper.BACKGROUND_DEFAULT_STATE_1) {
+					return null;
+				}
+				return transcriptColorIntToString(((PaintDrawable) simpleDrawable).getPaint().getColor());
+			}
+			// Get the backgroundDrawable background as a StateListDrawable.
+			StateListDrawable stateListDrawable = ((StateListDrawable) simpleDrawable);
+			// Get the reflection methods.
+			Method getStateDrawableIndexMethod =
+				StateListDrawable.class.getMethod("getStateDrawableIndex", int[].class);
+			Method getStateDrawableMethod = StateListDrawable.class.getMethod("getStateDrawable", int.class);
+			// Get the disabled state's (as defined in TiUIHelper) index.
+			int index = (int) getStateDrawableIndexMethod.invoke(stateListDrawable, state);
+			// Get the drawable at the index.
+			Drawable drawable = (Drawable) getStateDrawableMethod.invoke(stateListDrawable, index);
+			// Try to get the 0 index of the result.
+			if (drawable instanceof LayerDrawable) {
+				Drawable drawableFromLayer = ((LayerDrawable) drawable).getDrawable(0);
+				// Cast it as a ColorDrawable.
+				if (drawableFromLayer instanceof ColorDrawable) {
+					// Transcript the color int to HexString.
+					String strColor = transcriptColorIntToString(((ColorDrawable) drawableFromLayer).getColor());
+					return strColor;
+				} else {
+					Log.w(TAG, "Background drawable of unexpected type. Expected - ColorDrawable. Found - "
+								   + drawableFromLayer.getClass().toString());
+					return null;
+				}
+			} else {
+				Log.w(TAG, "Background drawable of unexpected type. Expected - LayerDrawable. Found - "
+							   + drawable.getClass().toString());
+				return null;
+			}
+		} catch (NoSuchMethodException e) {
+			Log.w(TAG, "Unable to get a method for reflection.");
+		} catch (IllegalAccessException e) {
+			Log.w(TAG, "Unable to access a method for reflection.");
+		} catch (InvocationTargetException e) {
+			Log.w(TAG, "Unable to invoke a method for reflection.");
+		}
+		return null;
+	}
+
+	public static String transcriptColorIntToString(int colorInt)
+	{
+		return String.format("#%08X", 0xFFFFFFFF & colorInt);
 	}
 }
