317948519131884550a77a7996cc2ccd683081ef
Christopher Williams <chris.a.williams@gmail.com>
2018-11-01 16:03:39 -0400

    refactor(ios): Move Ti.Filesystem module over to new JavaScriptCore Obj-c based API for proxies

--- iphone/Classes/FilesystemModule.m
+++ iphone/Classes/FilesystemModule.m
@@ -9,10 +9,11 @@
 #import "FilesystemModule.h"
 #import "TiFilesystemBlobProxy.h"
 #import <CommonCrypto/CommonDigest.h>
-#import <TitaniumKit/TiBlob.h>
-#import <TitaniumKit/TiFilesystemFileProxy.h>
-#import <TitaniumKit/TiFilesystemFileStreamProxy.h>
-#import <TitaniumKit/TiHost.h>
+@import TitaniumKit.TiBlob;
+@import TitaniumKit.TiUtils;
+@import TitaniumKit.TiFilesystemFileProxy;
+@import TitaniumKit.TiFilesystemFileStreamProxy;
+@import TitaniumKit.TiHost;
 
 @implementation FilesystemModule
 
@@ -33,7 +34,7 @@
 - (NSString *)pathFromComponents:(NSArray *)args
 {
   NSString *newpath;
-  id first = [args objectAtIndex:0];
+  NSString *first = [[args objectAtIndex:0] toString];
   if ([first hasPrefix:@"file://"]) {
     NSURL *fileUrl = [NSURL URLWithString:first];
     //Why not just crop? Because the url may have some things escaped that need to be unescaped.
@@ -47,65 +48,63 @@
 
   if ([args count] > 1) {
     for (int c = 1; c < [args count]; c++) {
-      newpath = [newpath stringByAppendingPathComponent:[self resolveFile:[args objectAtIndex:c]]];
+      newpath = [newpath stringByAppendingPathComponent:[self resolveFile:[[args objectAtIndex:c] toString]]];
     }
   }
-
   return [newpath stringByStandardizingPath];
 }
 
-- (id)createTempFile:(id)args
+- (JSValue *)createTempFile
 {
-  return [TiFilesystemFileProxy makeTemp:NO];
+  return [self NativeToJSValue:[TiFilesystemFileProxy makeTemp:NO]];
 }
 
-- (id)createTempDirectory:(id)args
+- (JSValue *)createTempDirectory
 {
-  return [TiFilesystemFileProxy makeTemp:YES];
+  return [self NativeToJSValue:[TiFilesystemFileProxy makeTemp:YES]];
 }
 
-- (id)openStream:(id)args
+- (JSValue *)openStream:(TiStreamMode)mode
 {
-  NSNumber *fileMode = nil;
+  ENSURE_VALUE_RANGE(mode, TI_READ, TI_APPEND);
 
-  ENSURE_ARG_AT_INDEX(fileMode, args, 0, NSNumber);
-  ENSURE_VALUE_RANGE([fileMode intValue], TI_READ, TI_APPEND);
-
+  NSArray *args = [JSContext currentArguments];
   if ([args count] < 2) {
     [self throwException:TiExceptionNotEnoughArguments
                subreason:nil
                 location:CODELOCATION];
   }
 
-  //allow variadic file components to be passed
-  NSArray *pathComponents = [args subarrayWithRange:NSMakeRange(1, [args count] - 1)];
-  NSString *path = [self pathFromComponents:pathComponents];
+  // allow variadic file components to be passed
+  NSString *resolvedPath = [self pathFromComponents:[args subarrayWithRange:NSMakeRange(1, [args count] - 1)]];
+  NSArray *payload = [NSArray arrayWithObjects:resolvedPath, [NSNumber numberWithInt:mode], nil];
 
-  NSArray *payload = [NSArray arrayWithObjects:path, fileMode, nil];
-
-  return [[[TiFilesystemFileStreamProxy alloc] _initWithPageContext:[self executionContext] args:payload] autorelease];
+  KrollContext *context = GetKrollContext([[JSContext currentContext] JSGlobalContextRef]);
+  KrollBridge *ourBridge = (KrollBridge *)[context delegate];
+  id file = [[[TiFilesystemFileStreamProxy alloc] _initWithPageContext:ourBridge args:payload] autorelease];
+  return [self NativeToJSValue:file];
 }
 
-- (id)MODE_APPEND
+- (TiStreamMode)MODE_APPEND
 {
-  return NUMINT(TI_APPEND);
+  return TI_APPEND;
 }
 
-- (id)MODE_READ
+- (TiStreamMode)MODE_READ
 {
-  return NUMINT(TI_READ);
+  return TI_READ;
 }
 
-- (id)MODE_WRITE
+- (TiStreamMode)MODE_WRITE
 {
-  return NUMINT(TI_WRITE);
+  return TI_WRITE;
 }
 
-- (id)isExternalStoragePresent:(id)unused
+- (BOOL)isExternalStoragePresent
 {
   //IOS treats the camera connection kit as just that, and does not allow
   //R/W access to it, which is just as well as it'd mess up cameras.
-  return NUMBOOL(NO);
+  return NO;
 }
 
 #define fileURLify(foo) [[NSURL fileURLWithPath:foo isDirectory:YES] path]
@@ -114,39 +113,44 @@
 {
   return [NSString stringWithFormat:@"%@/", fileURLify([TiHost resourcePath])];
 }
+GETTER_IMPL(NSString *, resourcesDirectory, ResourcesDirectory);
 
 - (NSString *)applicationDirectory
 {
   return [NSString stringWithFormat:@"%@/", fileURLify([NSSearchPathForDirectoriesInDomains(NSApplicationDirectory, NSUserDomainMask, YES) objectAtIndex:0])];
 }
+GETTER_IMPL(NSString *, applicationDirectory, ApplicationDirectory);
 
 - (NSString *)applicationSupportDirectory
 {
   return [NSString stringWithFormat:@"%@/", fileURLify([NSSearchPathForDirectoriesInDomains(NSApplicationSupportDirectory, NSUserDomainMask, YES) objectAtIndex:0])];
 }
+GETTER_IMPL(NSString *, applicationSupportDirectory, ApplicationSupportDirectory);
 
 - (NSString *)applicationDataDirectory
 {
   return [NSString stringWithFormat:@"%@/", fileURLify([NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0])];
 }
+GETTER_IMPL(NSString *, applicationDataDirectory, ApplicationDataDirectory);
 
 - (NSString *)applicationCacheDirectory
 {
   return [NSString stringWithFormat:@"%@/", fileURLify([NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) objectAtIndex:0])];
 }
+GETTER_IMPL(NSString *, applicationCacheDirectory, ApplicationCacheDirectory);
 
 - (NSString *)tempDirectory
 {
   return [NSString stringWithFormat:@"%@/", fileURLify(NSTemporaryDirectory())];
 }
+GETTER_IMPL(NSString *, tempDirectory, TempDirectory);
 
-- (id)directoryForSuite:(id)args
+- (NSString *)directoryForSuite:(NSString *)suiteName
 {
-  ENSURE_SINGLE_ARG(args, NSString);
-  NSURL *groupURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:args];
+  NSURL *groupURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:suiteName];
   if (!groupURL) {
-    NSLog(@"[ERROR] Directory not found for suite: %@ check the com.apple.security.application-groups entitlement.", args);
-    return [NSNull null];
+    NSLog(@"[ERROR] Directory not found for suite: %@ check the com.apple.security.application-groups entitlement.", suiteName);
+    return nil;
   }
   return [NSString stringWithFormat:@"%@/", fileURLify([groupURL path])];
 }
@@ -155,29 +159,33 @@
 {
   return @"/";
 }
+GETTER_IMPL(NSString *, separator, Separator);
 
 - (NSString *)lineEnding
 {
   return @"\n";
 }
+GETTER_IMPL(NSString *, lineEnding, LineEnding);
 
-- (id)getFile:(id)args
+- (JSValue *)getFile
 {
+  NSArray *args = [JSContext currentArguments];
   NSString *newpath = [self pathFromComponents:args];
 
   if ([newpath hasPrefix:[self resourcesDirectory]] && ([newpath hasSuffix:@".html"] || [newpath hasSuffix:@".js"] || [newpath hasSuffix:@".css"] || [newpath hasSuffix:@".json"])) {
     NSURL *url = [NSURL fileURLWithPath:newpath];
     NSData *data = [TiUtils loadAppResource:url];
     if (data != nil) {
-      return [[[TiFilesystemBlobProxy alloc] initWithURL:url data:data] autorelease];
+      return [self NativeToJSValue:[[[TiFilesystemBlobProxy alloc] initWithURL:url data:data] autorelease]];
     }
   }
 
-  return [[[TiFilesystemFileProxy alloc] initWithFile:newpath] autorelease];
+  return [self NativeToJSValue:[[[TiFilesystemFileProxy alloc] initWithFile:newpath] autorelease]];
 }
 
-- (id)getAsset:(id)args
+- (TiBlob *)getAsset
 {
+  NSArray *args = [JSContext currentArguments];
   NSString *newpath = [self pathFromComponents:args];
 
   if ([newpath hasPrefix:[self resourcesDirectory]] && ([newpath hasSuffix:@".jpg"] || [newpath hasSuffix:@".png"])) {
@@ -209,7 +217,7 @@
     }
     return [[[TiBlob alloc] initWithImage:image] autorelease];
   }
-  return [NSNull null];
+  return nil;
 }
 
 - (NSString *)IOS_FILE_PROTECTION_NONE
