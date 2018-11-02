ee69a8b8c366b7da1bdb4719b9bd6e51370a1aff
Christopher Williams <chris.a.williams@gmail.com>
2018-11-02 11:14:07 -0400

    refactor(ios): Move Ti.Database module over to new JavaScriptCore Obj-c based API for proxies

--- iphone/Classes/DatabaseModule.h
+++ iphone/Classes/DatabaseModule.h
@@ -1,12 +1,12 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-Present by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-#import <TitaniumKit/TiModule.h>
-
 #ifdef USE_TI_DATABASE
+@import JavaScriptCore;
+@import TitaniumKit.ObjcProxy;
 
 typedef enum {
   FieldTypeUnknown = -1,
@@ -16,9 +16,26 @@ typedef enum {
   FieldTypeDouble
 } DatabaseFieldType;
 
-@interface DatabaseModule : TiModule {
-}
+@protocol DatabaseModuleExports <JSExport>
 
+// Constants
+CONSTANT(DatabaseFieldType, FIELD_TYPE_DOUBLE);
+CONSTANT(DatabaseFieldType, FIELD_TYPE_FLOAT);
+CONSTANT(DatabaseFieldType, FIELD_TYPE_INT);
+CONSTANT(DatabaseFieldType, FIELD_TYPE_STRING);
+
+// Methods
+// TODO: Change JSValue* return type to TiDatabaseProxy* when it's moved to obj-c API
+JSExportAs(install,
+           -(JSValue *)install
+           : (NSString *)path withName
+           : (NSString *)dbName);
+// TODO: Change JSValue* return type to TiDatabaseProxy* when it's moved to obj-c API
+- (JSValue *)open:(NSString *)dbName;
+
+@end
+
+@interface DatabaseModule : ObjcProxy <DatabaseModuleExports>
 @end
 
 #endif
