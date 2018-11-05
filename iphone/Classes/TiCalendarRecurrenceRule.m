5eae0850c199e0b9e39501aa9eed2400a166e1b8
Christopher Williams <chris.a.williams@gmail.com>
2018-11-05 15:00:20 -0500

    refactor(ios): Move Ti.Calendar.* APIs over to new JavaScriptCore Obj-c based API for proxies * Update docs to fix incorrect info regarding properties that should be read-only, or incorrect types. Also update example code in docs. * Remove methods from Ti.Calendar.Calendar that were deprecated in 7.0.0. This includes #getEventsInDate(), despite Android/docs not noting it as deprecated - iOS did report it as deprecated. * Remove unused Ti.Calendar.Reminder type from iOS * Return begin/end properties as Date objects on Ti.Calendar.Event, and Ti.Calendar.RecurrenceRule.end's endDate property. Docs/Android used Date, but iOS used String.

--- iphone/Classes/TiCalendarRecurrenceRule.m
+++ iphone/Classes/TiCalendarRecurrenceRule.m
@@ -1,24 +1,23 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-Present by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-
 #ifdef USE_TI_CALENDAR
 
-#import "TiCalendarCalendar.h"
-#import <TitaniumKit/TiUtils.h>
-
 #import "TiCalendarRecurrenceRule.h"
+#import "TiCalendarCalendar.h"
+@import TitaniumKit.TiUtils;
+@import EventKit;
 
 @implementation TiCalendarRecurrenceRule
 
 #pragma mark - Internals
 
-- (id)_initWithPageContext:(id<TiEvaluator>)context rule:(EKRecurrenceRule *)rule_
+- (id)initWithRule:(EKRecurrenceRule *)rule_
 {
-  if (self = [super _initWithPageContext:context]) {
+  if (self = [super init]) {
     rule = [rule_ retain];
   }
   return self;
@@ -42,48 +41,95 @@
 
 #pragma mark - Public API's
 
-- (id)valueForUndefinedKey:(NSString *)key
+#define ENSURE_RULE(DEFAULT)                             \
+  EKRecurrenceRule *currRule = [self ruleForRecurrence]; \
+  if (currRule == NULL) {                                \
+    return DEFAULT;                                      \
+  }
+
+- (NSString *)calendarID
 {
-  EKRecurrenceRule *currRule = [self ruleForRecurrence];
-  if (currRule == NULL) {
-    return NULL;
-  }
-  if ([key isEqualToString:@"calendarId"]) {
-    return currRule.calendarIdentifier;
-  } else if ([key isEqualToString:@"end"]) {
-    EKRecurrenceEnd *end = currRule.recurrenceEnd;
-    NSDictionary *recurrenceEnd = [NSDictionary dictionaryWithObjectsAndKeys:[TiUtils UTCDateForDate:end.endDate], @"endDate",
-                                                NUMUINTEGER(end.occurrenceCount), @"occurrenceCount", nil];
-    return recurrenceEnd;
-  } else if ([key isEqualToString:@"frequency"]) {
-    return NUMINT(currRule.frequency);
-  } else if ([key isEqualToString:@"interval"]) {
-    return NUMINTEGER(currRule.interval);
-  } else if ([key isEqualToString:@"firstDayOfTheWeek"]) {
-    return NUMINTEGER(currRule.firstDayOfTheWeek);
-  } else if ([key isEqualToString:@"daysOfTheWeek"]) {
-    NSArray *value = currRule.daysOfTheWeek;
-    NSMutableArray *result = [NSMutableArray arrayWithCapacity:[value count]];
-    for (EKRecurrenceDayOfWeek *dayofWeek in value) {
-      NSDictionary *props = [NSDictionary dictionaryWithObjectsAndKeys:NUMINTEGER(dayofWeek.dayOfTheWeek), @"dayOfWeek",
-                                          NUMINTEGER(dayofWeek.weekNumber), @"week", nil];
-      [result addObject:props];
-    }
-    return result;
-  } else if ([key isEqualToString:@"daysOfTheMonth"]) {
-    return [NSArray arrayWithArray:currRule.daysOfTheMonth];
-  } else if ([key isEqualToString:@"daysOfTheYear"]) {
-    return [NSArray arrayWithArray:currRule.daysOfTheYear];
-  } else if ([key isEqualToString:@"weeksOfTheYear"]) {
-    return [NSArray arrayWithArray:currRule.weeksOfTheYear];
-  } else if ([key isEqualToString:@"monthsOfTheYear"]) {
-    return [NSArray arrayWithArray:currRule.monthsOfTheYear];
-  } else if ([key isEqualToString:@"setPositions"]) {
-    return [NSArray arrayWithArray:currRule.setPositions];
-  } else {
-    return [super valueForUndefinedKey:key];
-  }
+  ENSURE_RULE(NULL);
+  return currRule.calendarIdentifier;
 }
+GETTER_IMPL(NSString *, calendarID, CalendarID);
+
+- (NSArray<NSNumber *> *)daysOfTheMonth
+{
+  ENSURE_RULE(NULL);
+  return [NSArray arrayWithArray:currRule.daysOfTheMonth];
+}
+GETTER_IMPL(NSArray<NSNumber *> *, daysOfTheMonth, DaysOfTheMonth);
+
+- (NSArray<NSDictionary *> *)daysOfTheWeek
+{
+  ENSURE_RULE(NULL);
+  NSArray *value = currRule.daysOfTheWeek;
+  NSMutableArray *result = [NSMutableArray arrayWithCapacity:[value count]];
+  for (EKRecurrenceDayOfWeek *dayofWeek in value) {
+    NSDictionary *props = @{
+      @"dayOfWeek" : NUMINTEGER(dayofWeek.dayOfTheWeek),
+      @"week" : NUMINTEGER(dayofWeek.weekNumber)
+    };
+    [result addObject:props];
+  }
+  return result;
+}
+GETTER_IMPL(NSArray<NSDictionary *> *, daysOfTheWeek, DaysOfTheWeek);
+
+- (NSArray<NSNumber *> *)daysOfTheYear
+{
+  ENSURE_RULE(NULL);
+  return [NSArray arrayWithArray:currRule.daysOfTheYear];
+}
+GETTER_IMPL(NSArray<NSNumber *> *, daysOfTheYear, DaysOfTheYear);
+
+- (NSDictionary *)end
+{
+  ENSURE_RULE(NULL);
+  EKRecurrenceEnd *end = currRule.recurrenceEnd;
+  NSDictionary *recurrenceEnd = @{
+    @"endDate" : end.endDate,
+    @"occurrenceCount" : NUMUINTEGER(end.occurrenceCount)
+  };
+  return recurrenceEnd;
+}
+GETTER_IMPL(NSDictionary *, end, End);
+
+- (EKRecurrenceFrequency)frequency
+{
+  ENSURE_RULE(EKRecurrenceFrequencyDaily);
+  return currRule.frequency;
+}
+GETTER_IMPL(EKRecurrenceFrequency, frequency, Frequency);
+
+- (NSUInteger)interval
+{
+  ENSURE_RULE(1);
+  return currRule.interval;
+}
+GETTER_IMPL(NSUInteger, interval, Interval);
+
+- (NSArray<NSNumber *> *)monthsOfTheYear
+{
+  ENSURE_RULE(NULL);
+  return [NSArray arrayWithArray:currRule.monthsOfTheYear];
+}
+GETTER_IMPL(NSArray<NSNumber *> *, monthsOfTheYear, MonthsOfTheYear);
+
+- (NSArray<NSNumber *> *)setPositions
+{
+  ENSURE_RULE(NULL);
+  return [NSArray arrayWithArray:currRule.setPositions];
+}
+GETTER_IMPL(NSArray<NSNumber *> *, setPositions, SetPositions);
+
+- (NSArray<NSNumber *> *)weeksOfTheYear
+{
+  ENSURE_RULE(NULL);
+  return [NSArray arrayWithArray:currRule.weeksOfTheYear];
+}
+GETTER_IMPL(NSArray<NSNumber *> *, weeksOfTheYear, WeeksOfTheYear);
 
 @end
 
