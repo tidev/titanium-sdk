ff219251a87bc42f6d72f86dadb7a32e4db90b6e
Joshua Quick <jquick@axway.com>
2018-11-06 19:29:02 -0800

    iOS: Updated [TIMOB-25839] based on feedback.

--- iphone/TitaniumKit/TitaniumKit/Sources/API/TiUtils.m
+++ iphone/TitaniumKit/TitaniumKit/Sources/API/TiUtils.m
@@ -1203,12 +1203,14 @@ If the new path starts with / and the base url is app://..., we have to massage 
 
 + (NSDictionary *)touchPropertiesToDictionary:(UITouch *)touch andView:(UIView *)view
 {
+  CGPoint point = [touch locationInView:view];
+  point.x = convertDipToDefaultUnit(point.x);
+  point.y = convertDipToDefaultUnit(point.y);
+
   if ([self forceTouchSupported] || [self validatePencilWithTouch:touch]) {
-    CGFloat xValue = convertDipToDefaultUnit([touch locationInView:view].x);
-    CGFloat yValue = convertDipToDefaultUnit([touch locationInView:view].y);
     NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithObjectsAndKeys:
-                                                         [NSNumber numberWithFloat:xValue], @"x",
-                                                     [NSNumber numberWithFloat:yValue], @"y",
+                                                         [NSNumber numberWithFloat:point.x], @"x",
+                                                     [NSNumber numberWithFloat:point.y], @"y",
                                                      [NSNumber numberWithFloat:touch.force], @"force",
                                                      [NSNumber numberWithFloat:touch.maximumPossibleForce], @"maximumPossibleForce",
                                                      [NSNumber numberWithDouble:touch.timestamp], @"timestamp",
@@ -1226,7 +1228,7 @@ If the new path starts with / and the base url is app://..., we have to massage 
     return dict;
   }
 
-  return [self pointToDictionary:[touch locationInView:view]];
+  return [self pointToDictionary:point];
 }
 
 + (CGRect)contentFrame:(BOOL)window
