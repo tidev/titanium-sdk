d6bf5d1f03342edfed4bf4e8e91134f8d765be0e
Sergey Volkov <s.volkov@netris.ru>
2018-11-23 22:45:02 +0300

    [TIMOB-24549] Android: fix promise fulfillment

--- android/runtime/v8/src/java/org/appcelerator/kroll/runtime/v8/V8Promise.java
+++ android/runtime/v8/src/java/org/appcelerator/kroll/runtime/v8/V8Promise.java
@@ -1,10 +1,21 @@
 package org.appcelerator.kroll.runtime.v8;
 
+import android.os.Message;
+
 import org.appcelerator.kroll.KrollDict;
 import org.appcelerator.kroll.KrollPromise;
+import org.appcelerator.kroll.KrollRuntime;
+import org.appcelerator.kroll.common.AsyncResult;
+import org.appcelerator.kroll.common.TiMessenger;
 
 public class V8Promise extends V8Object implements KrollPromise
 {
+	private static final String TAG = "V8Promise";
+
+	protected static final int MSG_RESOLVE = V8Object.MSG_LAST_ID + 100;
+	protected static final int MSG_REJECT = V8Object.MSG_LAST_ID + 101;
+	protected static final int MSG_LAST_ID = MSG_REJECT;
+
 	public V8Promise(long resolver)
 	{
 		super(resolver);
@@ -13,13 +24,44 @@ public class V8Promise extends V8Object implements KrollPromise
 	@Override
 	public void resolve(Object value)
 	{
-		nativeResolve(getPointer(), value);
+		if (KrollRuntime.getInstance().isRuntimeThread()) {
+			nativeResolve(getPointer(), value);
+		} else {
+			TiMessenger.sendBlockingRuntimeMessage(handler.obtainMessage(MSG_RESOLVE), value);
+		}
 	}
 
 	@Override
 	public void reject(Object value)
 	{
-		nativeReject(getPointer(), value);
+		if (KrollRuntime.getInstance().isRuntimeThread()) {
+			nativeReject(getPointer(), value);
+		} else {
+			TiMessenger.sendBlockingRuntimeMessage(handler.obtainMessage(MSG_REJECT), value);
+		}
+	}
+
+	@Override
+	public boolean handleMessage(Message message)
+	{
+		switch (message.what) {
+			case MSG_RESOLVE: {
+				AsyncResult asyncResult = ((AsyncResult) message.obj);
+				Object value = asyncResult.getArg();
+				nativeResolve(getPointer(), value);
+				asyncResult.setResult(null);
+				return true;
+			}
+			case MSG_REJECT: {
+				AsyncResult asyncResult = ((AsyncResult) message.obj);
+				Object value = asyncResult.getArg();
+				nativeReject(getPointer(), value);
+				asyncResult.setResult(null);
+				return true;
+			}
+		}
+
+		return super.handleMessage(message);
 	}
 
 	@Override
