e4622d59dae3479e2fc44dc95527d63a0cea8707
Christopher Williams <chris.a.williams@gmail.com>
2018-12-10 15:21:29 -0500

    feat(api): add node-compatible os core module
    Add cpu information for iOS via lookup table based on underlying iphone/ipad model string
    
    Fixes TIMOB-18584

--- common/Resources/ti.internal/extensions/process.js
+++ common/Resources/ti.internal/extensions/process.js
@@ -5,7 +5,9 @@ const process = {
 	cwd: function () {
 		return __dirname;
 	},
-	platform: Ti.Platform.name
+	// FIXME: Should we try and adopt 'windowsphone'/'windowsstore' to 'win32'?
+	// FIXME: Should we try and adopt 'ipad'/'iphone' to 'darwin'? or 'ios'?
+	platform: Ti.Platform.osname
 };
 global.process = process;
 module.exports = process;
