ca15db02e503dd7ea0e392d016d5da5fa19b0ab9
Yordan Banev <yordan.banev@gmail.com>
2018-12-10 17:17:14 +0200

    [TIMOB-26480] Android: Titanium.UI.ProgressBar 'color' setting portion of progressBar - should be text only (#10478)
    * Fix color, tintColor and add parity for trackTintColor.
    
    * Prevent unnecessary initialization of ColorStateList.

--- android/titanium/src/java/org/appcelerator/titanium/TiC.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiC.java
@@ -3225,6 +3225,11 @@ public class TiC
 	public static final String PROPERTY_SOUND_EFFECTS_ENABLED = "soundEffectsEnabled";
 
 	/**
+	 * module.api
+	 */
+	public static final String PROPERTY_TRACK_TINT_COLOR = "trackTintColor";
+
+	/**
 	 * @module.api
 	 */
 	public static final String PROPERTY_TRANSFORM = "transform";
