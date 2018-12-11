f6644d1adab35cc7f3f9a19aae3a12efbcbdba76
vijay vikram singh <vsingh@axway.com>
2018-12-11 12:57:33 +0800

    fix(ios) : Unit test added

---
+++ tests/Resources/ti.ui.textfield.addontest.js
@@ -0,0 +1,50 @@
+/*
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2011-Present by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+/* eslint-env mocha */
+/* global Ti, Titanium */
+/* eslint no-unused-expressions: "off" */
+'use strict';
+var should = require('./utilities/assertions'),
+	utilities = require('./utilities/utilities');
+
+describe('Titanium.UI.TextField', function () {
+	var win;
+
+	afterEach(function () {
+		if (win) {
+			win.close();
+		}
+		win = null;
+	});
+
+	it.ios('hasText', function (finish) {
+		win = Ti.UI.createWindow();
+		 
+		var textFieldA = Ti.UI.createTextField({
+		  top: '60dip',
+		  value: 0
+		});
+		 
+		win.add(textFieldA);
+		 
+		var textFieldB = Ti.UI.createTextField({
+		  top: '120dip',
+		  value: 0
+		});
+		 
+		win.add(textFieldB);
+		 
+		textFieldA.addEventListener('change', function() {
+		  should(textFieldA.hasText()).be.true;
+		  should(textFieldB.hasText()).be.true;
+		  finish();
+		});
+		 
+		win.open();
+	});
+
+});
