e1c57e64c86900148b2458ded9c22e40e7f87955
Sergey Volkov <s.volkov@netris.ru>
2018-12-14 15:52:10 +0300

    fix(android): tableViewRow height update

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/tableview/TiTableViewRowProxyItem.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/tableview/TiTableViewRowProxyItem.java
@@ -552,10 +552,6 @@ public class TiTableViewRowProxyItem extends TiBaseTableViewItem
 				} else {
 					h = Math.max(minRowHeight, height.getAsPixels(this));
 				}
-				// Make sure the height is greater than 1 (not 0 since image views default to 1)
-				if (hasChildView && h > 1) {
-					content.getLayoutParams().height = h;
-				}
 
 				if (Log.isDebugModeEnabled()) {
 					Log.d(TAG, "Row content measure (" + adjustedWidth + "x" + h + ")", Log.DEBUG_MODE);
