ab2ccd5e6ab22f3a0b8c72564cf0d0a81776facf
Christopher Williams <chris.a.williams@gmail.com>
2018-12-19 13:05:20 -0500

    fix: standardize os.arch values to Node.js equivalents

--- common/Resources/ti.internal/extensions/process.js
+++ common/Resources/ti.internal/extensions/process.js
@@ -1,7 +1,53 @@
 'use strict';
 
+/**
+ * This function 'standardizes' the reported architectures to the equivalents reported by Node.js
+ * node values: 'arm', 'arm64', 'ia32', 'mips', 'mipsel', 'ppc', 'ppc64', 's390', 's390x', 'x32', and 'x64'.
+ * iOS values: "arm64", "armv7", "x86_64", "i386", "Unknown"
+ * Android values: "armeabi", "armeabi-v7a", "arm64-v8a", "x86", "x86_64", "mips", "mips64", "unknown"
+ * Windows values: "x64", "ia64", "ARM", "x86", "unknown"
+ * @param {string} original original architecture reported by Ti.Platform
+ * @returns {string}
+ */
+function standardizeArch(original) {
+	switch (original) {
+		// coerce 'armv7', 'armeabi', 'armeabi-v7a', 'ARM' -> 'arm'
+		// 'armeabi' is a dead ABI for Android, removed in NDK r17
+		case 'armv7':
+		case 'armeabi':
+		case 'armeabi-v7a':
+		case 'ARM':
+			return 'arm';
+
+		// coerce 'arm64-v8a' -> 'arm64'
+		case 'arm64-v8a':
+			return 'arm64';
+
+		// coerce 'i386', 'x86' -> 'ia32'
+		case 'i386':
+		case 'x86':
+			return 'ia32';
+
+		// coerce 'x86_64', 'ia64', 'x64' -> 'x64'
+		case 'x86_64':
+		case 'ia64':
+			return 'x64';
+
+		// coerce 'mips64' -> 'mips' // 'mips' and 'mips64' are dead ABIs for Android, removed in NDK r17
+		case 'mips64':
+			return 'mips';
+
+		// coerce 'Unknown' -> 'unknown'
+		case 'Unknown':
+			return 'unknown';
+
+		default:
+			return original;
+	}
+}
+
 const process = {
-	arch: Ti.Platform.architecture,
+	arch: standardizeArch(Ti.Platform.architecture),
 	cwd: function () {
 		return __dirname;
 	},
