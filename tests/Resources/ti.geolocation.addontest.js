59999d2e63322eca2b48d4bfbc29f64d8cef61a8
Sergey Volkov <s.volkov@netris.ru>
2019-01-15 12:12:15 +0300

    [TIMOB-24549] Update Geolocation tests

--- tests/Resources/ti.geolocation.addontest.js
+++ tests/Resources/ti.geolocation.addontest.js
@@ -8,35 +8,34 @@
 /* global Ti */
 /* eslint no-unused-expressions: "off" */
 'use strict';
-var should = require('./utilities/assertions');
+const should = require('./utilities/assertions');
 
 describe.android('Titanium.Geolocation', function () {
 	// Methods
 
-	it('async #forwardGeocoder()', function (finish) {
+	it('async #forwardGeocoder()', () => {
 		this.timeout(6e4); // 60 sec
 
 		should(Ti.Geolocation.forwardGeocoder).be.a.Function;
-		Ti.Geolocation.forwardGeocoder('440 N Bernardo Ave, Mountain View')
-			.then(function (data) {
+		return Ti.Geolocation.forwardGeocoder('440 N Bernardo Ave, Mountain View')
+			.then((data) => {
 				should(data).have.property('success').which.is.a.Boolean;
 				should(data.success).be.eql(true);
 				should(data).have.property('code').which.is.a.Number;
 				should(data.code).be.eql(0);
 				should(data.latitude).be.approximately(37.387, 0.002); // iOS gives: 37.38605, Windows does 37.3883645
 				should(data.longitude).be.approximately(-122.065, 0.02); // WIndows gives: -122.0512682, iOS gives -122.08385
-				finish();
-			})
-			.catch(finish);
+				return;
+			});
 	});
 
 	// FIXME The address object is different from platform to platform! https://jira.appcelerator.org/browse/TIMOB-23496
-	it('async #reverseGeocoder()', function (finish) {
+	it('async #reverseGeocoder()', () => {
 		this.timeout(6e4); // 60 sec
 
 		should(Ti.Geolocation.reverseGeocoder).be.a.Function;
-		Ti.Geolocation.reverseGeocoder(37.3883645, -122.0512682)
-			.then(function (data) {
+		return Ti.Geolocation.reverseGeocoder(37.3883645, -122.0512682)
+			.then((data) => {
 				should(data).have.property('success').which.is.a.Boolean;
 				should(data.success).be.eql(true);
 				should(data).have.property('code').which.is.a.Number;
@@ -54,9 +53,7 @@ describe.android('Titanium.Geolocation', function () {
 				should(data.places[0].country_code).be.eql('US');
 				should(data.places[0]).have.property('city').which.is.a.String;
 				should(data.places[0]).have.property('address').which.is.a.String;
-
-				finish();
-			})
-			.catch(finish);
+				return;
+			});
 	});
 });
