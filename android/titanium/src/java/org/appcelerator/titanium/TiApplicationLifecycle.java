74398ba51bdc353153a39480d35aeee963339e7e
Joshua Quick <jquick@axway.com>
2019-01-17 20:17:15 -0800

    [TIMOB-26746] Android: Fixed bug where Ti.App wrongly fires pause/resume events when opening/closing child windows
    - Updated Ti.App docs to indicate Android supported these events since 7.5.0

--- android/titanium/src/java/org/appcelerator/titanium/TiApplicationLifecycle.java
+++ android/titanium/src/java/org/appcelerator/titanium/TiApplicationLifecycle.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import com.appcelerator.aps.APSAnalytics;
 
 import org.appcelerator.kroll.common.Log;
+import org.appcelerator.kroll.KrollModule;
 
 public class TiApplicationLifecycle implements Application.ActivityLifecycleCallbacks
 {
@@ -29,19 +30,49 @@ public class TiApplicationLifecycle implements Application.ActivityLifecycleCall
 	@Override
 	public void onActivityStarted(Activity activity)
 	{
-		if (activityCount == 0 && tiApp != null && tiApp.isAnalyticsEnabled()) {
-			APSAnalytics.getInstance().sendAppForegroundEvent();
+		// If no activities have been started, then app is going to be put into the foreground.
+		if (this.activityCount == 0) {
+			// Fire Ti.App resume events.
+			KrollModule appModule = this.tiApp.getModuleByName("App");
+			if (appModule != null) {
+				appModule.fireEvent(TiC.EVENT_RESUME, null);
+				appModule.fireEvent(TiC.EVENT_RESUMED, null);
+			}
+
+			// Post analytics for this event, if enabled.
+			if (this.tiApp.isAnalyticsEnabled()) {
+				APSAnalytics.getInstance().sendAppForegroundEvent();
+			}
 		}
-		activityCount++;
+
+		// Increment number of "started" activities. These are activities that are currently in the foreground.
+		// Note: Should never be more than 1, unless some of these activities are fragments.
+		this.activityCount++;
 	}
 
 	@Override
 	public void onActivityStopped(Activity activity)
 	{
-		if (activityCount == 1 && tiApp != null && tiApp.isAnalyticsEnabled()) {
-			APSAnalytics.getInstance().sendAppBackgroundEvent();
+		// If this is the last activity being stopped, then the app is going to be put into the background.
+		if (activityCount == 1) {
+			// Fire Ti.App pause events.
+			KrollModule appModule = this.tiApp.getModuleByName("App");
+			if (appModule != null) {
+				appModule.fireEvent(TiC.EVENT_PAUSE, null);
+				appModule.fireEvent(TiC.EVENT_PAUSED, null);
+			}
+
+			// Post analytics for this event, if enabled.
+			if (this.tiApp.isAnalyticsEnabled()) {
+				APSAnalytics.getInstance().sendAppBackgroundEvent();
+			}
 		}
-		activityCount--;
+
+		// Decrement count of started activities.
+		this.activityCount--;
+		if (this.activityCount < 0) {
+			this.activityCount = 0;
+		}
 	}
 
 	@Override
