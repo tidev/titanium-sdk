758975202609b071fa54e5d6b26552c3aac59253
Chris Williams <chris.a.williams@gmail.com>
2019-01-23 13:43:00 -0500

    build(ios): disallow commit of static values for sdk version/date/hash

--- build/scons.js
+++ build/scons.js
@@ -16,4 +16,5 @@ commander
 	.command('ssri [urls]', 'generates ssri integrity hashes for URLs')
 	.command('modules-integrity', 'Regenerates ssri integrity hashes for all the modules in our pre-packaged listing under support/module/packged/modules.json given the current url values')
 	.command('xcode-test', 'Hacks the XCode project for iOS to copy in the unit test suite so it can be run under XCode\'s debugger')
+	.command('check-ios-toplevel', 'Ensures we don\'t check in prefilled values for version/hash/timestamp')
 	.parse(process.argv);
