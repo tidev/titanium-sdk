79ec3d3d40679c4e3d62b6e7e45a3cbf5e4d4699
vijay vikram singh <vsingh@axway.com>
2019-03-29 15:06:45 -0700

    fix(ios): Give a time delay to scroll if search view is hiding on selection

--- iphone/Classes/TiUITableView.m
+++ iphone/Classes/TiUITableView.m
@@ -287,7 +287,7 @@
 
 @implementation TiUITableView
 #pragma mark Internal
-@synthesize searchString, viewWillDetach;
+@synthesize searchString, viewWillDetach, searchResultIndexes;
 
 #ifdef TI_USE_AUTOLAYOUT
 - (void)initializeTiLayoutView
@@ -1005,11 +1005,15 @@
   // see the behavior of, say, Contacts. If users want to hide search, they can do so
   // in an event callback.
 
+  if (viaSearch) {
+    self.isLoaded = NO;
+  }
   if ([target _hasListeners:name]) {
     [target fireEvent:name withObject:eventObject];
   }
 
   if (viaSearch) {
+    self.isLoaded = NO;
     if (hideOnSearch) {
       [self hideSearchScreen:nil];
     } else {
