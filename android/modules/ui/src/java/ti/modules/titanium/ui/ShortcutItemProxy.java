e95049532ec2c6ae7c3bee4804090778197bdf62
Gary Mathews <gmathews@appcelerator.com>
2019-06-05 14:56:24 -0700

    fix(android): missing import

--- android/modules/ui/src/java/ti/modules/titanium/ui/ShortcutItemProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/ShortcutItemProxy.java
@@ -14,6 +14,7 @@ import org.appcelerator.titanium.TiApplication;
 import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.util.TiUIHelper;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ShortcutInfo;
