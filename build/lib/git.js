dff19e6d3a2d93ad07d48993182d06e3e5b8b30e
hansemannn <hans.knoechel@hs-osnabrueck.de>
2019-07-28 10:59:52 +0200

    fix: discard V8Snapshots.h changes after packaging

--- build/lib/git.js
+++ build/lib/git.js
@@ -9,8 +9,18 @@ const exec = promisify(require('child_process').exec); // eslint-disable-line se
  * @returns {Promise<string>} sha of current git commit for HEAD
  */
 async function getHash(cwd) {
-	const { stdout } = await exec('git rev-parse --short --no-color HEAD', { cwd: cwd });
+	const { stdout } = await exec('git rev-parse --short --no-color HEAD', { cwd });
 	return stdout.trim(); // drop leading 'commit ', just take 7-character sha
 }
 
-module.exports = { getHash };
+/**
+ * Discards a local Git change.
+ * @param {string} cwd current working directory path
+ * @param {string} file the file to discard
+ * @returns {Promise<string>} sha of current git commit for HEAD
+ */
+function discardLocalChange(cwd, file) {
+	return exec(`git checkout -- ${file}`, { cwd });
+}
+
+module.exports = { getHash, discardLocalChange };
