a73ebca8c56f054d4eaf083cdb47e85fc8df4176
Ewan Harris <ewanharris93@gmail.com>
2019-08-19 18:50:30 +0100

    fix(ios): fix compatability with iOS 9 (#11091)
    * fix(ios): fix compatability with iOS 9
    
    Fixes TIMOB-27292
    
    * build(ios): correct property used for minimum ios version
    
    * refactor: pull minIosVersion off package.json object
    
    * fix(ios): add back ios 11 constants to code
    
    Rename from IS_XCODE_9 to IS_IOS_SDK_11 per the discussion in https://github.com/appcelerator/titanium_mobile/pull/10932
    
    * fix(ios): correct minimum xcode version

--- iphone/Classes/GeolocationModule.m
+++ iphone/Classes/GeolocationModule.m
@@ -316,10 +316,11 @@ extern NSString *const TI_APPLICATION_GUID;
 
     locationManager.allowsBackgroundLocationUpdates = allowsBackgroundLocationUpdates;
 
+#if IS_SDK_IOS_11
     if ([TiUtils isIOSVersionOrGreater:@"11.0"]) {
       locationManager.showsBackgroundLocationIndicator = showBackgroundLocationIndicator;
     }
-
+#endif
     locationManager.activityType = activityType;
     locationManager.pausesLocationUpdatesAutomatically = pauseLocationUpdateAutomatically;
 
@@ -603,19 +604,23 @@ READWRITE_IMPL(CLLocationDegrees, headingFilter, HeadingFilter);
 
 - (BOOL)showBackgroundLocationIndicator
 {
+#if IS_SDK_IOS_11
   if ([TiUtils isIOSVersionOrGreater:@"11.0"]) {
     return showBackgroundLocationIndicator;
   }
+#endif
   DebugLog(@"[ERROR] The showBackgroundLocationIndicator property is only available on iOS 11.0+. Returning \"false\" ...");
   return NO;
 }
 
 - (void)setShowBackgroundLocationIndicator:(BOOL)value
 {
+#if IS_SDK_IOS_11
   if ([TiUtils isIOSVersionOrGreater:@"11.0"]) {
     showBackgroundLocationIndicator = value;
     return;
   }
+#endif
   DebugLog(@"[ERROR] The showBackgroundLocationIndicator property is only available on iOS 11.0+. Ignoring call ...");
 }
 
