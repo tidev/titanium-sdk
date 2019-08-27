5b008b237092db9cd0aef7f398f98a6d1fbde0ee
vijay vikram singh <vsingh@axway.com>
2019-08-27 10:30:51 -0700

    fix(ios): updated unit test

--- tests/Resources/ti.watchsession.addontest.js
+++ tests/Resources/ti.watchsession.addontest.js
@@ -9,56 +9,56 @@
 'use strict';
 var should = require('./utilities/assertions');
 
-describe('Titanium.WatchSession', function () {
+describe.ios('Titanium.WatchSession', function () {
 
-	it.ios('apiName', function () {
+	it('apiName', function () {
 		var watchSession = Ti.WatchSession;
 		should(watchSession).have.readOnlyProperty('apiName').which.is.a.String;
 		should(watchSession.apiName).be.eql('Ti.WatchSession');
 	});
 
 	// constants
-	it.ios('ACTIVATION_STATE_NOT_ACTIVATED', function () {
+	it('ACTIVATION_STATE_NOT_ACTIVATED', function () {
 		should(Ti.WatchSession).have.constant('ACTIVATION_STATE_NOT_ACTIVATED').which.is.a.Number;
 	});
 
-	it.ios('ACTIVATION_STATE_INACTIVE', function () {
+	it('ACTIVATION_STATE_INACTIVE', function () {
 		should(Ti.WatchSession).have.constant('ACTIVATION_STATE_INACTIVE').which.is.a.Number;
 	});
 
-	it.ios('ACTIVATION_STATE_ACTIVATED', function () {
+	it('ACTIVATION_STATE_ACTIVATED', function () {
 		should(Ti.WatchSession).have.constant('ACTIVATION_STATE_ACTIVATED').which.is.a.Number;
 	});
 
-	it.ios('#isSupported', function () {
+	it('#isSupported', function () {
 		should(Ti.WatchSession.isSupported).be.a.Boolean;
 	});
 
-	it.ios('#isPaired', function () {
+	it('#isPaired', function () {
 		should(Ti.WatchSession.isPaired).be.a.Boolean;
 	});
 
-	it.ios('#isWatchAppInstalled', function () {
+	it('#isWatchAppInstalled', function () {
 		should(Ti.WatchSession.isWatchAppInstalled).be.a.Boolean;
 	});
 
-	it.ios('#isComplicationEnabled', function () {
+	it('#isComplicationEnabled', function () {
 		should(Ti.WatchSession.isComplicationEnabled).be.a.Boolean;
 	});
 
-	it.ios('#isReachable', function () {
+	it('#isReachable', function () {
 		should(Ti.WatchSession.isReachable).be.a.Boolean;
 	});
 
-	it.ios('#isActivated', function () {
+	it('#isActivated', function () {
 		should(Ti.WatchSession.isActivated).be.a.Boolean;
 	});
 
-	it.ios('#hasContentPending', function () {
+	it('#hasContentPending', function () {
 		should(Ti.WatchSession.hasContentPending).be.a.Boolean;
 	});
 
-	it.ios('#remainingComplicationUserInfoTransfers', function () {
+	it('#remainingComplicationUserInfoTransfers', function () {
 		should(Ti.WatchSession.remainingComplicationUserInfoTransfers).be.a.Number;
 	});
 });
