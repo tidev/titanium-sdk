e5999226436dcdb74a8ebce312c6af7cd1a553a7
Ewan Harris <ewanharris93@gmail.com>
2019-09-09 15:20:46 +0100

    fix(ios): correctly decode device token for ios 13 compatability
    Fixes TIMOB-27386

--- iphone/Classes/TiApp+Addons.m
+++ iphone/Classes/TiApp+Addons.m
@@ -154,11 +154,8 @@
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
 {
-  NSString *token = [[[[deviceToken description] stringByReplacingOccurrencesOfString:@"<" withString:@""]
-      stringByReplacingOccurrencesOfString:@">"
-                                withString:@""]
-      stringByReplacingOccurrencesOfString:@" "
-                                withString:@""];
+
+  NSString *token = [NSString hexStringFromData:deviceToken];
 
   RELEASE_TO_NIL(remoteDeviceUUID);
   remoteDeviceUUID = [token copy];
