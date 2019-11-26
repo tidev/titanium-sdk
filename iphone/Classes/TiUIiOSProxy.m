87903e5efaf3f01122faf34aabcb3ff8a675fa32
vijay <vsingh@axway.com>
2019-11-26 12:52:50 -0800

    fix(ios): status bar background color crash fix ios13

--- iphone/Classes/TiUIiOSProxy.m
+++ iphone/Classes/TiUIiOSProxy.m
@@ -130,9 +130,23 @@
   ENSURE_UI_THREAD(setStatusBarBackgroundColor, value);
   ENSURE_SINGLE_ARG(value, NSString);
 
-  UIView *statusBar = [[[UIApplication sharedApplication] valueForKey:@"statusBarWindow"] valueForKey:@"statusBar"];
-  if ([statusBar respondsToSelector:@selector(setBackgroundColor:)]) {
-    statusBar.backgroundColor = [[TiUtils colorValue:value] _color];
+  if ([TiUtils isIOSVersionOrGreater:@"13.0"]) {
+#if IS_SDK_IOS_13
+    CGRect frame = [[[[[UIApplication sharedApplication] keyWindow] windowScene] statusBarManager] statusBarFrame];
+    UIView *view = [[[UIApplication sharedApplication] keyWindow] viewWithTag:TI_STATUSBAR_TAG];
+    if (!view) {
+      view = [[UIView alloc] initWithFrame:frame];
+      view.tag = 11101;
+      [[[UIApplication sharedApplication] keyWindow] addSubview:view];
+    }
+    view.frame = frame;
+    view.backgroundColor = [[TiUtils colorValue:value] _color];
+#endif
+  } else {
+    UIView *statusBar = [[[UIApplication sharedApplication] valueForKey:@"statusBarWindow"] valueForKey:@"statusBar"];
+    if ([statusBar respondsToSelector:@selector(setBackgroundColor:)]) {
+      statusBar.backgroundColor = [[TiUtils colorValue:value] _color];
+    }
   }
 }
 
