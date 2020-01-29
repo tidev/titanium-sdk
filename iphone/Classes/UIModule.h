8f59561a748691ca31661b07fa3b7cddec262e6b
Chris Williams <chris.a.williams@gmail.com>
2020-01-29 15:36:29 -0500

    feat(ios): support use of named system colors baked into ios

--- iphone/Classes/UIModule.h
+++ iphone/Classes/UIModule.h
@@ -30,6 +30,7 @@
   TiProxy *clipboard;
 #endif
   NSNumber *lastEmittedMode;
+  NSDictionary *systemColors;
 }
 
 //TODO: review these, maybe they need to go on iPhone Animation Style - however, they are platform generic
