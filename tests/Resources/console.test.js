7010b507d6ba037fd05302c06fc4985e3e3a127a
Jan Vennemann <jan.vennemann@gmx.net>
2020-03-16 16:27:42 +0100

    test: add tests for console.trace

--- tests/Resources/console.test.js
+++ tests/Resources/console.test.js
@@ -39,6 +39,10 @@ describe('console', function () {
 		should(console.warn).be.a.Function;
 	});
 
+	it('#trace', () => {
+		should(console.trace).be.a.Function;
+	});
+
 	it('#time', () => {
 		should(console.time).be.a.Function;
 	});
@@ -294,13 +298,15 @@ describe('console', function () {
 			console.dirxml('dirxml'); // stdout
 			console.warn('warn'); // stderr
 			console.error('error'); // stderr
+			console.trace('trace'); // stderr
 			stdout.logs.length.should.eql(3);
 			stdout.logs[0].should.eql('log');
 			stdout.logs[1].should.eql('info');
 			stdout.logs[2].should.eql('dirxml');
-			stderr.logs.length.should.eql(2);
+			stderr.logs.length.should.eql(3);
 			stderr.logs[0].should.eql('warn');
 			stderr.logs[1].should.eql('error');
+			stderr.logs[2].should.eql('trace');
 		});
 
 		it('squashes sync errors on stdout write by default (ignoreErrors = true)', () => {
