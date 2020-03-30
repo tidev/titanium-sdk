01f9307a73cd0d4d26da7a72825cfd13336fd37c
Jan Vennemann <jan.vennemann@gmx.net>
2020-03-30 15:04:20 +0200

    fix(ios): manually evaluate all error properties

--- iphone/TitaniumKit/TitaniumKit/Sources/API/KrollBridge.m
+++ iphone/TitaniumKit/TitaniumKit/Sources/API/KrollBridge.m
@@ -314,9 +314,8 @@ CFMutableSetRef krollBridgeRegistry = nil;
     }
   }
   if (exception != NULL) {
-    id excm = [KrollObject toID:context value:exception];
     evaluationError = YES;
-    [[TiExceptionHandler defaultExceptionHandler] reportScriptError:[TiUtils scriptErrorValue:excm]];
+    [TiExceptionHandler.defaultExceptionHandler reportScriptError:exception inKrollContext:context];
   }
 
   JSStringRelease(jsCode);
@@ -692,8 +691,7 @@ CFMutableSetRef krollBridgeRegistry = nil;
   [eval release];
 
   if (exception != NULL) {
-    id excm = [KrollObject toID:context value:exception];
-    [[TiExceptionHandler defaultExceptionHandler] reportScriptError:[TiUtils scriptErrorValue:excm]];
+    [TiExceptionHandler.defaultExceptionHandler reportScriptError:exception inKrollContext:context];
     return nil;
   }
   /*
