78363d7431de0b5d7fcb3cf1ec773552e2197c73
Chris Williams <chris.a.williams@gmail.com>
2020-05-21 15:41:19 -0400

    refactor(common): use common library to handle semantic colors
    * force alpha as float in iOS Contents.json

--- common/Resources/ti.internal/extensions/ti/ti.ui.js
+++ common/Resources/ti.internal/extensions/ti/ti.ui.js
@@ -5,6 +5,7 @@
  * Please see the LICENSE included with this distribution for details.
  */
 /* globals OS_ANDROID,OS_IOS */
+import Color from '../../../../lib/color';
 const isIOS13Plus = OS_IOS && parseInt(Ti.Platform.version.split('.')[0]) >= 13;
 
 // As Android passes a new instance of Ti.UI to every JS file we can't just
@@ -37,30 +38,6 @@ Object.defineProperty(UI, 'semanticColorType', {
 // on Android/iOS < 13, we need to roll our own fetchSemanticColor impl
 // on iOS 13+, we have a native version
 if (!isIOS13Plus) {
-	const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-	const fallbackColor = 'black'; // To match iphone/Classes/TiUIiOSProxy.m#fetchSemanticColor
-
-	/**
- 	 * Converts an entry from semantic.colors.json to an 'rgba()' string
- 	 * @param {object} entry entry from the semantic.colors.json file
- 	 * @param {string} entry.color the base color hex (must be 3 or 6 digit hexadecimal string)
- 	 * @param {string|number} [entry.alpha=100.0] the alpha value (as a percent, 0.0 - 100.0)
- 	 * @returns {string|null}
- 	 */
-	function hexToRgb(entry) {
-		let alpha = 1.0;
-		let color = entry.color;
-		if (entry.alpha) { // FIMXE: What if alpha is 0?
-			alpha = parseFloat(entry.alpha) / 100.0; // convert from 0-100 range to 0-1 range
-		}
-		// TODO: if no alpha, assume 1, and skip conversion!
-		// Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-		color = color.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
-
-		const r = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
-		return r ? `rgba(${parseInt(r[1], 16)}, ${parseInt(r[2], 16)}, ${parseInt(r[3], 16)}, ${alpha.toFixed(3)})` : null;
-	}
-
 	let colorset;
 	UI.fetchSemanticColor = function fetchSemanticColor (colorName) {
 		if (!colorset) {
@@ -72,32 +49,24 @@ if (!isIOS13Plus) {
 			} catch (error) {
 				// We should probably throw an Error here (or return a fallback color!)
 				console.error('Failed to load colors file \'semantic.colors.json\'');
-				return;
+				return Color.fallback().toRGBAString();
 			}
 		}
 
 		try {
 			if (!colorset[colorName]) {
-				return fallbackColor;
+				return Color.fallback().toRGBAString();
 			}
 
-			const entry = colorset[colorName][Ti.UI.semanticColorType];
+			const entry = colorset[colorName][UI.semanticColorType];
+			const colorObj = Color.fromSemanticColorsEntry(entry);
 			// For now, return a string on iOS < 13, Android so we can pass the result directly to the UI property we want to set
 			// Otherwise we need to modify the Android APIs to accept this faked Ti.UI.Color instance and convert it to it's own internal
 			// Color representation
-			if (typeof entry === 'string') {
-				return entry; // should be a hex string (hopefully!)
-			}
-			if (!entry || typeof entry !== 'object') {  // it's not a string, nor an object, fail
-				return fallbackColor;
-			}
-			const result = hexToRgb(entry);
-			if (result) {
-				return result;
-			}
+			return colorObj.toRGBAString(); // rgba is standard across iOS/Android. Hex on Android ia ARGB vs RGBA on iOS.
 		} catch (error) {
 			console.error(`Failed to lookup color for ${colorName}`);
 		}
-		return fallbackColor;
+		return Color.fallback().toRGBAString();
 	};
 }
