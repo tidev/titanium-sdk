ff1dc343fc5ed83560c8473f6b1f83b9027ca7c2
Chris Williams <chris.a.williams@gmail.com>
2020-06-02 09:53:43 -0400

    test(buffer): add tests for Buffer .buffer and #set

--- tests/Resources/buffer.addontest.js
+++ tests/Resources/buffer.addontest.js
@@ -9,7 +9,7 @@
 'use strict';
 const should = require('./utilities/assertions');
 
-describe('buffer ext', () => {
+describe('buffer', () => {
 	it('#concat() sliced Buffer', () => {
 		const buf = Buffer.from('buffer');
 		const slice = buf.slice(1, -2); // return a slice pointing at 'uff' portion
@@ -25,5 +25,40 @@ describe('buffer ext', () => {
 	it('passes constructor.BYTES_PER_ELEMENT === 1 sniff test', () => {
 		const buf = Buffer.from(Ti.createBuffer({ length: 5 }));
 		should(buf.constructor).have.property('BYTES_PER_ELEMENT').which.is.equal(1);
+	});
+
+	it('.buffer property', () => {
+		const buf = Buffer.from([ 0xFF, 0xAB, 0x80 ]);
+		const arrayBuffer = buf.buffer;
+		should.exist(arrayBuffer);
+		should(ArrayBuffer.isView(buf.buffer)).be.false(); // true for Buffer, false for ArrayBuffer itself
+		should(arrayBuffer).have.property('byteLength').which.equals(3);
+	});
+
+	describe('#set()', () => {
+		it('is a Function', () => {
+			const buf = Buffer.from([ 0xFF, 0xAB, 0x80 ]);
+			should(buf).have.property('set').which.is.a.Function();
+		});
+
+		it('with array argument', () => {
+			const buf = Buffer.from([ 0xFF, 0xAB, 0x80 ]);
+			buf.set([ 1, 2, 3 ]);
+			should(buf.equals(Buffer.from([ 1, 2, 3 ]))).be.true();
+		});
+
+		it('with array argument, offset', () => {
+			const buf = Buffer.from([ 0xFF, 0xAB, 0x80 ]);
+			buf.set([ 1, 2 ], 1);
+			should(buf.equals(Buffer.from([ 0xFF, 1, 2 ]))).be.true();
+		});
+
+		it('with bad offset throws Error', () => {
+			const buf = Buffer.from([ 0xFF, 0xAB, 0x80 ]);
+			(() => buf.set([ 1, 2 ], 3)).should.throw();
+		});
+
+		// TODO: set with Typed Array
+		// TODO: set with another Buffer
 	});
 });
