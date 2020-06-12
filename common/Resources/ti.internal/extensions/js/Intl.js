3167d9a717e73ed13195e282b3603c6cb6040b16
Joshua Quick <jquick@axway.com>
2020-06-11 21:36:19 -0700

    chore(android): resolve JS linting for TIMOB-27240

--- common/Resources/ti.internal/extensions/js/Intl.js
+++ common/Resources/ti.internal/extensions/js/Intl.js
@@ -4,6 +4,7 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+/* globals OS_ANDROID */
 
 /**
  * Generates a "creation" properties dictionary for Titanium's Collator, DateTimeFormat, and NumberFormat proxies
--- common/Resources/ti.internal/extensions/js/Number.js
+++ common/Resources/ti.internal/extensions/js/Number.js
@@ -4,6 +4,7 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+/* globals OS_ANDROID */
 
 if (OS_ANDROID) {
 	Number.prototype.toLocaleString = function () {
