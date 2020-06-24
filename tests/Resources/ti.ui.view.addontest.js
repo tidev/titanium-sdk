2a0b1bea925c9cc1eefb29535e7a33ef724adc09
Joshua Quick <jquick@axway.com>
2020-06-24 15:32:15 -0700

    feat(android): support touch feedback for all backgrounds
    - Used to only be supported when using "backgroundColor" property.
    - Now applied to all background types such as:
      * backgroundImage
      * backgroungGradient
      * No background
    
    Fixes TIMOB-26315

--- tests/Resources/ti.ui.view.addontest.js
+++ tests/Resources/ti.ui.view.addontest.js
@@ -1,6 +1,6 @@
 /*
  * Appcelerator Titanium Mobile
- * Copyright (c) 2015-Present by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2015-Present by Axway, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
@@ -173,5 +173,79 @@ describe('Titanium.UI.View', function () {
 			win.add(outerView);
 			win.open();
 		});
+	});
+
+	it.android('touchFeedback', finish => {
+		win = Ti.UI.createWindow({ layout: 'horizontal' });
+		win.add(Ti.UI.createLabel({
+			text: 'View 1',
+			touchFeedback: true,
+			touchFeedbackColor: 'yellow'
+		}));
+		win.add(Ti.UI.createLabel({
+			text: 'View 2',
+			backgroundColor: 'gray',
+			touchFeedback: true,
+			touchFeedbackColor: 'yellow'
+		}));
+		win.add(Ti.UI.createLabel({
+			text: 'View 3',
+			backgroundImage: '/Logo.png',
+			touchFeedback: true,
+			touchFeedbackColor: 'yellow'
+		}));
+		win.add(Ti.UI.createLabel({
+			text: 'View 4',
+			backgroundGradient: {
+				type: 'linear',
+				startPoint: { x: '0%', y: '50%' },
+				endPoint: { x: '100%', y: '50%' },
+				colors: [ { color: 'red', offset: 0.0 }, { color: 'blue', offset: 1.0 } ]
+			},
+			touchFeedback: true,
+			touchFeedbackColor: 'yellow'
+		}));
+		win.add(Ti.UI.createLabel({
+			text: 'View 5',
+			borderRadius: 20,
+			borderColor: 'red',
+			borderWidth: '8dp',
+			touchFeedback: true,
+			touchFeedbackColor: 'yellow'
+		}));
+		win.add(Ti.UI.createLabel({
+			text: 'View 6',
+			backgroundColor: 'gray',
+			borderRadius: 20,
+			borderColor: 'red',
+			borderWidth: '8dp',
+			touchFeedback: true,
+			touchFeedbackColor: 'yellow'
+		}));
+		win.add(Ti.UI.createLabel({
+			text: 'View 7',
+			backgroundImage: '/Logo.png',
+			borderRadius: 20,
+			borderColor: 'red',
+			borderWidth: '8dp',
+			touchFeedback: true,
+			touchFeedbackColor: 'yellow'
+		}));
+		win.add(Ti.UI.createLabel({
+			text: 'View 8',
+			backgroundGradient: {
+				type: 'linear',
+				startPoint: { x: '0%', y: '50%' },
+				endPoint: { x: '100%', y: '50%' },
+				colors: [ { color: 'red', offset: 0.0 }, { color: 'blue', offset: 1.0 } ]
+			},
+			borderRadius: 20,
+			borderColor: 'red',
+			borderWidth: '8dp',
+			touchFeedback: true,
+			touchFeedbackColor: 'yellow'
+		}));
+		win.addEventListener('open', () => finish());
+		win.open();
 	});
 });
