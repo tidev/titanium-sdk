97e60e70400ee3da172843623e3d9a4d7345cde5
Chris Williams <chris.a.williams@gmail.com>
2020-07-06 15:50:32 -0400

    test: expand Ti.UI.View.borderRadius testing

--- tests/Resources/ti.ui.view.addontest.js
+++ tests/Resources/ti.ui.view.addontest.js
@@ -27,22 +27,10 @@ describe('Titanium.UI.View', function () {
 		rootWindow.close();
 	});
 
-	afterEach(function (done) {
-		if (win) {
-			// If `win` is already closed, we're done.
-			let t = setTimeout(function () {
-				if (win) {
-					win = null;
-					done();
-				}
-			}, 3000);
-
+	afterEach(done => { // fires after every test in sub-suites too...
+		if (win && !win.closed) {
 			win.addEventListener('close', function listener () {
-				clearTimeout(t);
-
-				if (win) {
-					win.removeEventListener('close', listener);
-				}
+				win.removeEventListener('close', listener);
 				win = null;
 				done();
 			});
@@ -53,46 +41,133 @@ describe('Titanium.UI.View', function () {
 		}
 	});
 
-	it.android('borderRadius corners (string)', finish => {
-		win = Ti.UI.createWindow({ backgroundColor: 'blue' });
-		const view = Ti.UI.createView({
-			width: 100,
-			height: 100,
-			borderRadius: '20px 20 20dp 20',
+	describe.only('borderRadius corners', () => {
+		// NOTE: We're very careful to use exact pixel values that are divisble by 1, 2 and 3
+		// so that varying device dpi don't alter expectations for snapshot image comparisons
+
+		it('4 values in String', finish => {
+			win = Ti.UI.createWindow({ backgroundColor: 'blue' });
+			const outerView = Ti.UI.createView({
+				width: '90px',
+				height: '90px',
+				backgroundColor: 'green'
+			});
+			const view = Ti.UI.createView({
+				width: '60px',
+				height: '60px',
+				borderRadius: '12px 12 12dp 12', // NOTE: Will this ruin image comparison when we vary device resolution?
+				backgroundColor: 'yellow'
+			});
+
+			win.addEventListener('postlayout', function postlayout() { // FIXME: Support once!
+				win.removeEventListener('postlayout', postlayout); // only run once
+				try {
+					should(view.borderRadius).be.a.String();
+					should(view.borderRadius).eql('12px 12 12dp 12');
+					should(outerView).matchImage('snapshots/borderRadius12p_x12_12dp_12.png');
+				} catch (e) {
+					return finish(e);
+				}
+				finish();
+			});
+			outerView.add(view);
+			win.add(outerView);
+			win.open();
 		});
 
-		win.addEventListener('focus', () => {
-			try {
-				should(view.borderRadius).be.a.String();
-			} catch (err) {
-				return finish(err);
-			}
-			finish();
+		it('4 values in Array', finish => {
+			win = Ti.UI.createWindow({ backgroundColor: 'blue' });
+			const outerView = Ti.UI.createView({
+				width: '90px',
+				height: '90px',
+				backgroundColor: 'green'
+			});
+			const view = Ti.UI.createView({
+				width: '60px',
+				height: '60px',
+				borderRadius: [ '12px', 12, '12dp', '12' ],
+				backgroundColor: 'yellow'
+			});
+
+			win.addEventListener('postlayout', function postlayout() {
+				win.removeEventListener('postlayout', postlayout); // only run once
+				try {
+					should(view.borderRadius).be.an.Array();
+					should(view.borderRadius.length).eql(4);
+					// should be the exact same as above
+					should(outerView).matchImage('snapshots/borderRadius12px_12_12dp_12.png');
+				} catch (err) {
+					return finish(err);
+				}
+				finish();
+			});
+
+			outerView.add(view);
+			win.add(outerView);
+			win.open();
 		});
 
-		win.add(view);
-		win.open();
-	});
+		it('2 values in String', finish => {
+			win = Ti.UI.createWindow({ backgroundColor: 'blue' });
+			const outerView = Ti.UI.createView({
+				width: '90px',
+				height: '90px',
+				backgroundColor: 'green'
+			});
+			const view = Ti.UI.createView({
+				width: '60px',
+				height: '60px',
+				borderRadius: '12px 12',
+				backgroundColor: 'yellow'
+			});
 
-	it.android('borderRadius corners (array)', finish => {
-		win = Ti.UI.createWindow({ backgroundColor: 'blue' });
-		const view = Ti.UI.createView({
-			width: 100,
-			height: 100,
-			borderRadius: [ '20px', 20, '20dp', '20' ],
+			win.addEventListener('postlayout', function postlayout() {
+				win.removeEventListener('postlayout', postlayout); // only run once
+				try {
+					should(view.borderRadius).be.a.String();
+					should(view.borderRadius).eql('12px 12');
+					should(outerView).matchImage('snapshots/borderRadius12px_12.png');
+				} catch (e) {
+					return finish(e);
+				}
+				finish();
+			});
+			outerView.add(view);
+			win.add(outerView);
+			win.open();
 		});
 
-		win.addEventListener('focus', () => {
-			try {
-				should(view.borderRadius).be.an.Array();
-				should(view.borderRadius.length).eql(4);
-			} catch (err) {
-				return finish(err);
-			}
-			finish();
-		});
+		it('2 values in Array', finish => {
+			win = Ti.UI.createWindow({ backgroundColor: 'blue' });
+			const outerView = Ti.UI.createView({
+				width: '90px',
+				height: '90px',
+				backgroundColor: 'green'
+			});
+			const view = Ti.UI.createView({
+				width: '60px',
+				height: '60px',
+				borderRadius: [ '12px', 12 ],
+				backgroundColor: 'yellow'
+			});
 
-		win.add(view);
-		win.open();
+			win.addEventListener('postlayout', function postlayout() {
+				win.removeEventListener('postlayout', postlayout); // only run once
+				try {
+					should(view.borderRadius).be.an.Array();
+					should(view.borderRadius.length).eql(2);
+					should(view.borderRadius).eql([ '12px', 12 ]);
+					// should be the exact same as above
+					should(outerView).matchImage('snapshots/borderRadius12px_12.png');
+				} catch (err) {
+					return finish(err);
+				}
+				finish();
+			});
+
+			outerView.add(view);
+			win.add(outerView);
+			win.open();
+		});
 	});
 });
