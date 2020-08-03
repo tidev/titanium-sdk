e1140ccc90d613ad12230674c772b7fe1fde1ef1
Jan Vennemann <jan.vennemann@gmx.net>
2020-08-04 00:22:22 +0200

    chore: update readme and use shared post-create hook

--- templates/app/angular-default/hooks/post-create.js
+++ templates/app/angular-default/hooks/post-create.js
@@ -1,62 +1,6 @@
 'use strict';
 
-const fs = require('fs-extra');
-const path = require('path');
-const spawn = require('child_process').spawn; // eslint-disable-line security/detect-child-process
+const postAppCreate = require('../../../../cli/lib/webpack-post-create');
 
-exports.id = 'com.appcelerator.angular.post-create';
-
-exports.init = (logger, config, cli) => {
-	cli.on('create.post.app', async (creator, next) => {
-		const projectName = cli.argv.name;
-		const projectPath = path.join(cli.argv['workspace-dir'], projectName);
-		try {
-			await Promise.all([
-				copyGitIgnore(projectPath),
-				updatePackageJson(projectPath, { name: projectName }),
-				installDependencies(projectPath, logger)
-			])
-			next();
-		} catch (e) {
-			next(e);
-		}
-	});
-};
-
-async function updatePackageJson(projectPath, data) {
-	const packageJsonPath = path.join(projectPath, 'package.json');
-	if (await fs.exists(packageJsonPath)) {
-		let pkg = await fs.readJson(packageJsonPath);
-		Object.assign(pkg, data);
-		return fs.writeJson(packageJsonPath, pkg);
-	}
-}
-
-async function installDependencies(projectPath, logger) {
-	logger.info('Installing Angular project dependencies');
-	let npmExecutable = 'npm';
-	const spawnOptions = {
-		cwd: projectPath,
-		stdio: 'inherit'
-	};
-	if (process.platform === 'win32') {
-		spawnOptions.shell = true;
-		npmExecutable += '.cmd';
-	}
-	return new Promise((resolve, reject) => {
-		const child = spawn(npmExecutable, [ 'i' ], spawnOptions);
-		child.on('close', code => {
-			if (code !== 0) {
-				return reject(new Error('Failed to install project dependencies.'));
-			}
-
-			resolve();
-		});
-	});
-}
-
-async function copyGitIgnore(projectPath) {
-	const sourcePath = path.resolve(__dirname, '..', 'template', '.gitignore');
-	const destPath = path.join(projectPath, '.gitignore');
-	return fs.copyFile(sourcePath, destPath);
-}
+exports.id = 'template.angular.post-create';
+exports.init = postAppCreate(__dirname);
--- templates/app/angular-default/template/README.md
+++ templates/app/angular-default/template/README.md
@@ -1,11 +1,21 @@
-# Welcome to your Titanium Angular project
+# <%= name %>
 
 > ⚠️ Titanium Angular is currently only available as a preview and under ongoing development. Expect things to be broken or APIs to change as this project matures. DO NOT USE IN PRODUCTION!
 
-This project was created using the default Angular project template. It is a dead simple app that demonstrates basic concepts of Titanium Angular, including:
+## Development
 
-- Using Titanium views in templates
-- Property and event binding
-- Routing
+Run `<%= buildCmd %> -p <android|ios> --liveview` to build the app for development and enable live reload using LiveView.
 
-For more examples check out the [sample app](https://github.com/appcelerator/titanium-angular/tree/master/ti-angular-example) in the [titanium-angular](https://github.com/appcelerator/titanium-angular) repository.
+## Build
+
+Run `<%= buildCmd %> -p <android|ios> -T <dist-adhoc|dist-appstore|dist-playstore>` to build the app for production.
+
+## Linting
+
+Run `npm run lint` to lint your project via [eslint](https://eslint.org/).
+
+## Further help
+
+To get more help on Titanium Angular go check out the [Titanium Angular Guide](https://docs.appcelerator.com/platform/latest/#!/guide/Titanium_and_Angular). For more examples check out the [sample app](https://github.com/appcelerator/titanium-angular/tree/master/ti-angular-example) in the [titanium-angular](https://github.com/appcelerator/titanium-angular) repository.
+
+To get more help on Titanium in general go check out the [How-tos](https://docs.appcelerator.com/platform/latest/#!/guide/Titanium_SDK_How-tos) and [API Docs](https://docs.appcelerator.com/platform/latest/#!/api).
