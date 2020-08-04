49a14b180a7a43b8e8f15376b03915fdc2d11154
Joshua Quick <jquick@axway.com>
2020-08-04 15:27:27 -0700

    feat: add OS_ANDROID/OS_IOS for non-transpiled builds

--- common/Resources/ti.internal/extensions/globals.js
+++ common/Resources/ti.internal/extensions/globals.js
@@ -4,9 +4,13 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+/* eslint-disable quote-props */
+/* globals OS_ANDROID, OS_IOS */
 
 // Add global constants.
 Object.defineProperties(global, {
+	'OS_ANDROID': { value: OS_ANDROID, writable: false },
+	'OS_IOS': { value: OS_IOS, writable: false },
 	OS_VERSION_MAJOR: { value: Ti.Platform.versionMajor, writable: false },
 	OS_VERSION_MINOR: { value: Ti.Platform.versionMinor, writable: false }
 });
