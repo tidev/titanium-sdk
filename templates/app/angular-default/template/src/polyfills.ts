fb5e2000cdc59ff8e043646ea443956f53b2287b
Jan Vennemann <jan.vennemann@gmx.net>
2020-08-12 22:09:50 +0200

    feat: update template for angular 9 (#11852)
    * update readme and use shared post-create hook
    * split dev dependencies
    * unify app icon and launch image
    * vscode settings
    
    Fixes TIMOB-27856

---
+++ templates/app/angular-default/template/src/polyfills.ts
@@ -0,0 +1,8 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * Note that zone.js will already be loaded by Titanium Angular itself.
+ */
+
+import 'core-js/es7/reflect';
