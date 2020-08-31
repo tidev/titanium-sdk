2d9bfce4c9741bade20d4453c57b306288f81b57
Chris Williams <chris.a.williams@gmail.com>
2020-08-31 13:56:12 -0400

    fix(android): fix Ti.Blob.arrayBuffer definition

--- common/Resources/ti.internal/extensions/ti/ti.blob.js
+++ common/Resources/ti.internal/extensions/ti/ti.blob.js
@@ -4,23 +4,29 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-/* globals OS_IOS, OS_VERSION_MAJOR */
+/* globals OS_ANDROID, OS_IOS, OS_VERSION_MAJOR */
 const buffer = Ti.createBuffer({ value: '' });
 const blob = buffer.toBlob();
 const BlobPrototype = Object.getPrototypeOf(blob);
-// Web Blob has an arrayBuffer() method that returns a Promise
-// https://developer.mozilla.org/en-US/docs/Web/API/Blob/arrayBuffer
-BlobPrototype.arrayBuffer = function () {
-	return new Promise((resolve, reject) => {
-		let buf;
-		try {
-			buf = this.toArrayBuffer();
-		} catch (err) {
-			return reject(err);
-		}
-		resolve(buf);
+if (OS_ANDROID) {
+	// This doesn't "stick" for iOS. It is implemented natively.
+	// Web Blob has an arrayBuffer() method that returns a Promise
+	// https://developer.mozilla.org/en-US/docs/Web/API/Blob/arrayBuffer
+	Object.defineProperty(BlobPrototype, 'arrayBuffer', {
+		value: function () {
+			return new Promise((resolve, reject) => {
+				let buf;
+				try {
+					buf = this.toArrayBuffer();
+				} catch (err) {
+					return reject(err);
+				}
+				resolve(buf);
+			});
+		},
+		enumerable: true
 	});
-};
+}
 
 if (OS_IOS) {
 	if (OS_VERSION_MAJOR < 13) {
