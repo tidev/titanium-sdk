40ab20d50ca5ceeb8441555dc159abdc701c9e75
Chris Williams <chris.a.williams@gmail.com>
2020-09-03 09:40:48 -0400

    refactor(ios): guard APIs for mac catalyst target

--- iphone/Classes/ContactsModule.m
+++ iphone/Classes/ContactsModule.m
@@ -4,7 +4,7 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
-#ifdef USE_TI_CONTACTS
+#if defined(USE_TI_CONTACTS) && (!TARGET_OS_MACCATALYST || IS_SDK_IOS_14)
 
 #import "ContactsModule.h"
 #import "TiContactsGroup.h"
