df093845e0d19805cbd4c851b159407fdd34e6e0
Chris Williams <chris.a.williams@gmail.com>
2020-09-14 15:00:23 -0400

    test(mac): re-enable test involving ti.map on macos

--- tests/Resources/ti.ui.tabgroup.test.js
+++ tests/Resources/ti.ui.tabgroup.test.js
@@ -57,8 +57,7 @@ describe('Titanium.UI.TabGroup', function () {
 		should(tabGroup.activeTintColor).eql('red');
 	});
 
-	// FIXME: macOS doesn't have ti.map module yet!
-	it.macMissingAndWindowsBroken('add Map.View to TabGroup', function (finish) {
+	it.windowsBroken('add Map.View to TabGroup', function (finish) {
 		this.slow(5000);
 		this.timeout(15000);
 
