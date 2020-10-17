ce51835442ebc7b6b45b17ee508713bd3b3e39c6
hansemannn <hans.knoechel@hs-osnabrueck.de>
2020-10-17 17:42:18 +0200

    refactor(ios): remove deprecated framework usage

--- iphone/Classes/TiContactsPerson.h
+++ iphone/Classes/TiContactsPerson.h
@@ -4,11 +4,10 @@
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
+
+#if defined(USE_TI_CONTACTS)
+
 #import <TitaniumKit/TiProxy.h>
-
-#if defined(USE_TI_CONTACTS) && (!TARGET_OS_MACCATALYST || IS_SDK_IOS_14)
-
-#import <AddressBook/AddressBook.h>
 #import <Contacts/Contacts.h>
 
 @class TiContactsPerson, ContactsModule;
