087f73b5cb92bf3e11c5860db3cac0c19b2825a7
Joshua Quick <jquick@axway.com>
2020-12-09 20:48:28 -0800

    fix(android): date.toLocaleString() to default to numeric date/time

--- common/Resources/ti.internal/extensions/js/Date.js
+++ common/Resources/ti.internal/extensions/js/Date.js
@@ -37,5 +37,34 @@ if (OS_ANDROID) {
 		return formatter.format(this);
 	};
 
-	Date.prototype.toLocaleString = Date.prototype.toLocaleDateString;
+	Date.prototype.toLocaleString = function () {
+		const properties = Intl.DateTimeFormat._makeTiCreationPropertiesFrom(arguments);
+		const oldOptions = properties.options;
+		let hasOption = false;
+		if (oldOptions) {
+			hasOption
+				=  !!oldOptions.dateStyle
+				|| !!oldOptions.timeStyle
+				|| !!oldOptions.weekday
+				|| !!oldOptions.month
+				|| !!oldOptions.day
+				|| !!oldOptions.year
+				|| !!oldOptions.hour
+				|| !!oldOptions.minute
+				|| !!oldOptions.second;
+		}
+		if (!hasOption) {
+			const defaultOptions = {
+				month: 'numeric',
+				day: 'numeric',
+				year: 'numeric',
+				hour: 'numeric',
+				minute: 'numeric',
+				second: 'numeric'
+			};
+			properties.options = Object.assign(defaultOptions, oldOptions);
+		}
+		const formatter = new Intl.DateTimeFormat(properties.locale, properties.options);
+		return formatter.format(this);
+	};
 }
