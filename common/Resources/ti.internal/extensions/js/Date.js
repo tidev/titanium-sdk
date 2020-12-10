e51d597f827b7786a8c58e4090972f726cd7b791
Joshua Quick <jquick@axway.com>
2020-12-09 19:19:34 -0800

    fix(android): localized date/time format should default to numeric
    Fixes TIMOB-28278

--- common/Resources/ti.internal/extensions/js/Date.js
+++ common/Resources/ti.internal/extensions/js/Date.js
@@ -9,15 +9,31 @@
 if (OS_ANDROID) {
 	Date.prototype.toLocaleDateString = function () {
 		const properties = Intl.DateTimeFormat._makeTiCreationPropertiesFrom(arguments);
-		const mergedOptions = Object.assign({ dateStyle: 'short' }, properties.options);
-		const formatter = new Intl.DateTimeFormat(properties.locale, mergedOptions);
+		const oldOptions = properties.options;
+		if (!oldOptions || (!oldOptions.dateStyle && !oldOptions.month && !oldOptions.day && !oldOptions.year)) {
+			const defaultOptions = {
+				month: 'numeric',
+				day: 'numeric',
+				year: 'numeric'
+			};
+			properties.options = Object.assign(defaultOptions, oldOptions);
+		}
+		const formatter = new Intl.DateTimeFormat(properties.locale, properties.options);
 		return formatter.format(this);
 	};
 
 	Date.prototype.toLocaleTimeString = function () {
 		const properties = Intl.DateTimeFormat._makeTiCreationPropertiesFrom(arguments);
-		const mergedOptions = Object.assign({ timeStyle: 'short' }, properties.options);
-		const formatter = new Intl.DateTimeFormat(properties.locale, mergedOptions);
+		const oldOptions = properties.options;
+		if (!oldOptions || (!oldOptions.timeStyle && !oldOptions.hour && !oldOptions.minute && !oldOptions.second)) {
+			const defaultOptions = {
+				hour: 'numeric',
+				minute: 'numeric',
+				second: 'numeric'
+			};
+			properties.options = Object.assign(defaultOptions, oldOptions);
+		}
+		const formatter = new Intl.DateTimeFormat(properties.locale, properties.options);
 		return formatter.format(this);
 	};
 
