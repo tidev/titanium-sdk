10c1a25f7a2bc48194aad00a52b50be3155dee67
Vijay Vikram Singh <vsingh@axway.com>
2021-01-15 12:08:10 -0800

    fix(ios): set parent view controller for detached view controllers (#12233)
    * set parent for UITabController, UINavigationController, AVPlayerViewController
    * fixed memory leak in listview and tableview
    * remove AVPlayerViewController from parent
    
    Fixes TIMOB-28209, TIMOB-27865

--- iphone/Classes/TiUIListView.m
+++ iphone/Classes/TiUIListView.m
@@ -2417,6 +2417,7 @@ static TiViewProxy *FindViewProxyWithBindIdContainingPoint(UIView *view, CGPoint
     dimmingView.alpha = .2;
     UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissSearchController)];
     [dimmingView addGestureRecognizer:tapGesture];
+    [tapGesture release];
   }
 }
 
