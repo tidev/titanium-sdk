e41f17e6fd9281d74c807f0641a86b7614d94397
Christopher Williams <chris.a.williams@gmail.com>
2021-01-21 12:44:42 -0500

    fix(ios): convert KrollPromise to JSValueRef for return types of old proxies

--- iphone/TitaniumKit/TitaniumKit/Sources/API/TiBindingTiValue.m
+++ iphone/TitaniumKit/TitaniumKit/Sources/API/TiBindingTiValue.m
@@ -1,6 +1,6 @@
 /**
  * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2018 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright (c) 2009-2021 by Appcelerator, Inc. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
  */
@@ -10,6 +10,7 @@
 #import "KrollContext.h"
 #import "KrollMethod.h"
 #import "KrollObject.h"
+#import "KrollPromise.h"
 #import <objc/runtime.h>
 
 /*
@@ -361,6 +362,9 @@ JSValueRef TiBindingTiValueFromNSObject(JSContextRef jsContext, NSObject *obj)
   if ([obj isKindOfClass:[JSValue class]]) {
     JSValue *jsValue = (JSValue *)obj;
     return jsValue.JSValueRef;
+  }
+  if ([obj isKindOfClass:[KrollPromise class]]) {
+    return ((KrollPromise *)obj).JSValue.JSValueRef;
   }
   return TiBindingTiValueFromProxy(jsContext, (TiProxy *)obj);
 }
