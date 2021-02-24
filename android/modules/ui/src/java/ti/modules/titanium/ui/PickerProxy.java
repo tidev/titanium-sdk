983fe1346cc63e0f6aa7548f7c33ce26c4b78281
Joshua Quick <jquick@axway.com>
2021-02-23 21:34:20 -0800

    chore(android): showDatePickerDialog() now throws error if min >= max

--- android/modules/ui/src/java/ti/modules/titanium/ui/PickerProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/PickerProxy.java
@@ -507,6 +507,9 @@ public class PickerProxy extends TiViewProxy implements PickerColumnListener
 		} else if (hasProperty(TiC.PROPERTY_MAX_DATE)) {
 			maxDate = TiConvert.toDate(getProperty(TiC.PROPERTY_MAX_DATE));
 		}
+		if ((minDate != null) && (maxDate != null) && !minDate.before(maxDate)) {
+			throw new IllegalArgumentException("showDatePickerDialog() property 'minDate' must be less than 'maxDate'");
+		}
 		if ((minDate != null) || (maxDate != null)) {
 			CalendarConstraints.Builder constraintsBuilder = new CalendarConstraints.Builder();
 			ArrayList<CalendarConstraints.DateValidator> validatorList = new ArrayList<>();
