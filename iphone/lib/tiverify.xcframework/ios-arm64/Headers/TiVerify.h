ad651a660621826e0b37e7cbfffc676baa356962
Gary Mathews <contact@garymathews.com>
2021-03-01 14:35:34 -0800

    chore(ios): remove module verification

--- iphone/lib/tiverify.xcframework/ios-arm64/Headers/TiVerify.h
+++ iphone/lib/tiverify.xcframework/ios-arm64/Headers/TiVerify.h
@@ -4,20 +4,3 @@
 // This code is proprietary and confidential to Appcelerator
 // and not for redistribution.
 //
-
-#import "UIKit/UIKit.h"
-
-@interface ApplicationMods : NSObject
-+ (NSArray*) compiledMods;
-@end
-
-@interface TiApp : NSObject
-+(TiApp*)app;
--(void)showModalController:(UIViewController*)modalController animated:(BOOL)animated;
-@end
-
-
-@interface TiVerify : NSObject {
-}
-
-@end
