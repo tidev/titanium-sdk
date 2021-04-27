2624c8142b59570908182a78e7da997987774b73
vijay <vsingh@axway.com>
2021-04-27 12:39:34 -0700

    fix(ios): fixed large title rendering issue

--- iphone/Classes/TiUIListView.m
+++ iphone/Classes/TiUIListView.m
@@ -2252,6 +2252,7 @@ static TiViewProxy *FindViewProxyWithBindIdContainingPoint(UIView *view, CGPoint
     }
     if (!controller.navigationItem.searchController) {
       controller.navigationItem.searchController = searchController;
+      [controller.navigationController.navigationBar sizeToFit];
     }
   }
 
@@ -2355,7 +2356,7 @@ static TiViewProxy *FindViewProxyWithBindIdContainingPoint(UIView *view, CGPoint
     searchController.delegate = self;
     searchController.searchResultsUpdater = self;
     searchController.hidesNavigationBarDuringPresentation = NO;
-    searchController.dimsBackgroundDuringPresentation = NO;
+    searchController.obscuresBackgroundDuringPresentation = NO;
 
     searchController.searchBar.frame = CGRectMake(searchController.searchBar.frame.origin.x, searchController.searchBar.frame.origin.y, 0, 44.0);
     searchController.searchBar.autoresizingMask = UIViewAutoresizingFlexibleWidth;
