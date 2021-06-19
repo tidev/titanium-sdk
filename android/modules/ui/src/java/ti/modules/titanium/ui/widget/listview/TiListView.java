4b4fd9fded0a4bd8e363797f7768fc557c69deab
Joshua Quick <jquick@axway.com>
2021-06-18 21:14:29 -0700

    fix(android): improve ListView scroll performance
    Fixes TIMOB-28439

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/listview/TiListView.java
@@ -20,8 +20,6 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
-import android.os.Handler;
-import android.os.SystemClock;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -44,12 +42,9 @@ public class TiListView extends TiSwipeRefreshLayout implements OnSearchChangeLi
 {
 	private static final String TAG = "TiListView";
 
-	private static final int CACHE_SIZE = 32;
-	private static final int PRELOAD_INTERVAL = 800;
-
 	private final ListViewAdapter adapter;
 	private final DividerItemDecoration decoration;
-	private final List<ListItemProxy> items = new ArrayList<>(CACHE_SIZE);
+	private final List<ListItemProxy> items = new ArrayList<>(128);
 	private final ListViewProxy proxy;
 	private final TiNestedRecyclerView recyclerView;
 	private final SelectionTracker tracker;
@@ -143,7 +138,7 @@ public class TiListView extends TiSwipeRefreshLayout implements OnSearchChangeLi
 		this.recyclerView.setItemAnimator(null);
 
 		// Optimize scroll performance.
-		recyclerView.setItemViewCacheSize(CACHE_SIZE);
+		recyclerView.setItemViewCacheSize(0);
 
 		// Set list separator.
 		decoration = new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL);
@@ -469,9 +464,7 @@ public class TiListView extends TiSwipeRefreshLayout implements OnSearchChangeLi
 	 */
 	public void update()
 	{
-
 		final KrollDict properties = this.proxy.getProperties();
-		final boolean shouldPreload = this.items.size() == 0;
 		int filterResultsCount = 0;
 
 		final boolean hasHeader = properties.containsKeyAndNotNull(TiC.PROPERTY_HEADER_TITLE)
@@ -571,38 +564,6 @@ public class TiListView extends TiSwipeRefreshLayout implements OnSearchChangeLi
 		// If filtered and no results, fire `noresult` event.
 		if (isFiltered() && filterResultsCount == 0) {
 			this.proxy.fireEvent(TiC.EVENT_NO_RESULTS, null);
-		}
-
-		// Pre-load items of empty list.
-		if (shouldPreload) {
-			final Handler handler = new Handler();
-			final long startTime = SystemClock.elapsedRealtime();
-
-			for (int i = 0; i < Math.min(this.items.size(), PRELOAD_INTERVAL / 8); i++) {
-				final ListItemProxy item = this.items.get(i);
-
-				// Fill event queue with pre-load attempts.
-				handler.postDelayed(new Runnable()
-				{
-					@Override
-					public void run()
-					{
-						final long currentTime = SystemClock.elapsedRealtime();
-						final long delta = currentTime - startTime;
-
-						// Only pre-load views for a maximum period of time.
-						// This prevents over-taxing older devices.
-						if (delta <= PRELOAD_INTERVAL
-							&& recyclerView.getLastTouchX() == 0
-							&& recyclerView.getLastTouchY() == 0) {
-
-							// While there is no user interaction;
-							// pre-load views for smooth initial scroll.
-							item.getOrCreateView();
-						}
-					}
-				}, 8); // Pre-load at 120Hz to prevent noticeable UI blocking.
-			}
 		}
 
 		// Notify adapter of changes on UI thread.
