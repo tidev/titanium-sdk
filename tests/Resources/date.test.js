9a01815a30e893711f11b232a6714d068b7cfd63
Ewan Harris <ewanharris93@gmail.com>
2021-06-30 22:17:55 +0100

    style: fix lint warning

--- tests/Resources/date.test.js
+++ tests/Resources/date.test.js
@@ -95,7 +95,7 @@ describe('Date', function () {
 		should(date.toLocaleTimeString([ 'en-US' ], options)).be.a.String();
 		should(date.toLocaleTimeString([ 'en-US', 'de-DE' ], options)).be.a.String();
 
-		should(date.toLocaleTimeString('en-US', options)).be.equalOneOf(['8:02:05 PM', '8:02:05 in the evening']);
+		should(date.toLocaleTimeString('en-US', options)).be.equalOneOf([ '8:02:05 PM', '8:02:05 in the evening' ]);
 		should(date.toLocaleTimeString('de-DE', options)).be.equalOneOf([ '8:02:05 PM', '8:02:05 nachm.', '8:02:05 abends' ]);
 
 		should(date.toLocaleTimeString('en-US')).be.equalOneOf([ '8:02:05 PM', '20:02:05' ]);
