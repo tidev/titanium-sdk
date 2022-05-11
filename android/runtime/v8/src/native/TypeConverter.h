0d18315f7cba289506f4c6abd8419003dc37da32
Sebastian Klaus <sebastian@lightapps.de>
2022-05-11 12:10:31 +0200

    fix: changed copyrights and legal stuff

--- android/runtime/v8/src/native/TypeConverter.h
+++ android/runtime/v8/src/native/TypeConverter.h
@@ -1,5 +1,5 @@
 /*
- * Appcelerator Titanium Mobile
+ * TiDev Titanium Mobile
  * Copyright TiDev, Inc. 04/07/2022-Present
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
