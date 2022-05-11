27cbe93e5ab596f0864bdc390f2297f2cb492aaf
Sebastian Klaus <sebastian@lightapps.de>
2022-05-11 15:12:08 +0200

    Fix legal information and copyrights (#13436)
    * fix: changed copyrights and legal stuff
    
    * fix: changed copyrights and legal stuff
    
    * fix: changed copyrights and legal stuff
    
    * fix: changed copyrights and legal stuff
    
    * fix: changed copyrights and legal stuff
    
    * feat: added tidev
    
    * fix: replace copyright in iOS libs
    
    * fix: package github url
    
    * fix: package.json cleaned up and copyrights
    
    * fix: android readme

--- cli/lib/creator.js
+++ cli/lib/creator.js
@@ -5,7 +5,7 @@
  * access build properties.
  *
  * @copyright
- * Copyright (c) 2014-2018 by Appcelerator, Inc. All Rights Reserved.
+ * Copyright TiDev, Inc. 04/07/2022-Present
  *
  * @license
  * Licensed under the terms of the Apache Public License
