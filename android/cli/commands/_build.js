7ed7856cf56d3169da4b2dec4d1ff62199ae3db3
Sebastian Klaus <sebastian@lightapps.de>
2022-05-11 08:56:24 +0200

    fix: changed copyrights and legal stuff

--- android/cli/commands/_build.js
+++ android/cli/commands/_build.js
@@ -4,7 +4,7 @@
  * @module cli/_build
  *
  * @copyright
- * Copyright (c) 2009-2021 by Axway, Inc. All Rights Reserved.
+ * Copyright TiDev, Inc. 04/07/2022-Present
  *
  * @license
  * Licensed under the terms of the Apache Public License
