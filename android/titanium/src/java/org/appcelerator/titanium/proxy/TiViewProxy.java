6c6d4ba7a5ebe45cf447eae9cd620e78b585bf4b
m1ga <migamiga@web.de>
2022-06-19 12:23:56 +0200

    fix version warning

--- android/titanium/src/java/org/appcelerator/titanium/proxy/TiViewProxy.java
+++ android/titanium/src/java/org/appcelerator/titanium/proxy/TiViewProxy.java
@@ -1376,23 +1376,27 @@ public abstract class TiViewProxy extends KrollProxy
 		handleHideKeyboard();
 	}
 
-	@RequiresApi(api = Build.VERSION_CODES.S)
+
 	@Kroll.method
 	public void blur(KrollDict values)
 	{
-		TiUIView v = peekView();
-		if (v != null) {
-			View nv = v.getNativeView();
-			if (nv != null) {
-				int blurX = values.getInt(TiC.PROPERTY_X);
-				int blurY = values.getInt(TiC.PROPERTY_Y);
-				try {
-					RenderEffect re = RenderEffect.createBlurEffect(blurX, blurY, Shader.TileMode.MIRROR);
-					nv.setRenderEffect(re);
-				} catch (Exception e){
-					//
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+			TiUIView v = peekView();
+			if (v != null) {
+				View nv = v.getNativeView();
+				if (nv != null) {
+					int blurX = values.getInt(TiC.PROPERTY_X);
+					int blurY = values.getInt(TiC.PROPERTY_Y);
+					try {
+						RenderEffect re = RenderEffect.createBlurEffect(blurX, blurY, Shader.TileMode.MIRROR);
+						nv.setRenderEffect(re);
+					} catch (Exception e) {
+						//
+					}
 				}
 			}
+		} else {
+			Log.w(TAG, "Only available for Android 12 or later.");
 		}
 	}
 
