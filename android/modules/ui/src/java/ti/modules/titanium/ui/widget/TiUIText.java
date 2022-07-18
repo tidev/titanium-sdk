639ecedf1b33502764a18ef5aa4251193ee7cf12
Hans Knöchel <hansemannn@users.noreply.github.com>
2022-07-18 19:02:29 +0200

    Revert "fix(android): textfield padding (#13279)"
    This reverts commit dcdfce50f823b90dd7fff4dc92a74e798acf86e7.

--- android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIText.java
@@ -310,10 +310,10 @@ public class TiUIText extends TiUIView implements TextWatcher, OnEditorActionLis
 
 	private void setTextPadding(HashMap<String, Object> d)
 	{
-		int paddingLeft = tv.getPaddingLeft();
-		int paddingRight = tv.getPaddingRight();
-		int paddingTop = tv.getPaddingTop();
-		int paddingBottom = tv.getPaddingBottom();
+		int paddingLeft = textInputLayout.getPaddingLeft();
+		int paddingRight = textInputLayout.getPaddingRight();
+		int paddingTop = textInputLayout.getPaddingTop();
+		int paddingBottom = textInputLayout.getPaddingBottom();
 
 		if (d.containsKey(TiC.PROPERTY_LEFT)) {
 			paddingLeft = TiConvert.toInt(d.get(TiC.PROPERTY_LEFT), 0);
@@ -330,7 +330,8 @@ public class TiUIText extends TiUIView implements TextWatcher, OnEditorActionLis
 		if (d.containsKey(TiC.PROPERTY_BOTTOM)) {
 			paddingBottom = TiConvert.toInt(d.get(TiC.PROPERTY_BOTTOM), 0);
 		}
-		tv.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+
+		textInputLayout.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
 	}
 
 	@Override
