2f9a32e38aa78d02d1d82c1c37845036006cc390
Hans Knöchel <h.knoechel@lambus.com>
2022-10-07 16:07:14 +0200

    chore: clean up default app.js code

--- templates/app/default/template/Resources/app.js
+++ templates/app/default/template/Resources/app.js
@@ -1,47 +1,43 @@
 /**
- * Create a new `Ti.UI.TabGroup`.
+ * Create a new tab group.
+ * @see https://titaniumsdk.com/api/titanium/ui/tabgroup.html
  */
-var tabGroup = Ti.UI.createTabGroup();
+const tabGroup = Ti.UI.createTabGroup();
 
 /**
  * Add the two created tabs to the tabGroup object.
  */
-tabGroup.addTab(createTab("Tab 1", "I am Window 1", "assets/images/tab1.png"));
-tabGroup.addTab(createTab("Tab 2", "I am Window 2", "assets/images/tab2.png"));
+tabGroup.addTab(createTab('Tab 1', 'I am Window 1', 'assets/images/tab1.png'));
+tabGroup.addTab(createTab('Tab 2', 'I am Window 2', 'assets/images/tab2.png'));
 
 /**
- * Open the tabGroup
+ * Open the tab group
  */
 tabGroup.open();
 
 /**
- * Creates a new Tab and configures it.
+ * Creates a new tab and configures it
  *
  * @param  {String} title The title used in the `Ti.UI.Tab` and it's included `Ti.UI.Window`
  * @param  {String} message The title displayed in the `Ti.UI.Label`
  * @return {String} icon The icon used in the `Ti.UI.Tab`
  */
 function createTab(title, message, icon) {
-    var win = Ti.UI.createWindow({
-        title: title,
-        backgroundColor: '#fff'
+    const window = Ti.UI.createWindow({
+        backgroundColor: 'backgroundColor', // This is a semantic color defined in the "semantic.colors.json"
+        title: title
     });
 
-    var label = Ti.UI.createLabel({
+    const label = Ti.UI.createLabel({
         text: message,
-        color: "#333",
-        font: {
-            fontSize: 20
-        }
+        color: 'textColor' // This is a semantic color defined in the "semantic.colors.json"
     });
 
-    win.add(label);
+    window.add(label);
 
-    var tab = Ti.UI.createTab({
-        title: title,
-        icon: icon,
-        window: win
-    });
-
-    return tab;
+    return Ti.UI.createTab({
+        title,
+        icon,
+        window
+    });;
 }
