559f86887dc382ec48c37a811ae603b7ff12b11f
m1ga <migamiga@web.de>
2022-10-30 19:28:35 +0100

    feat(android): add FAB

---
+++ android/modules/ui/src/java/ti/modules/titanium/ui/android/FabProxy.java
@@ -0,0 +1,35 @@
+/**
+ * TiDev Titanium Mobile
+ * Copyright TiDev, Inc. 04/07/2022-Present
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package ti.modules.titanium.ui.android;
+
+import android.app.Activity;
+
+import org.appcelerator.kroll.annotations.Kroll;
+import org.appcelerator.titanium.proxy.TiViewProxy;
+import org.appcelerator.titanium.view.TiUIView;
+
+import ti.modules.titanium.ui.widget.TiUIFab;
+
+@Kroll.proxy(creatableInModule = AndroidModule.class)
+public class FabProxy extends TiViewProxy
+{
+	private static final String TAG = "FabProxy";
+	private TiUIFab fab;
+
+	@Override
+	public TiUIView createView(Activity activity)
+	{
+		fab = new TiUIFab(this);
+		return fab;
+	}
+/*
+	@Kroll.method
+	public void show()
+	{
+		fab.show();
+	}*/
+}
---
+++ android/modules/ui/src/java/ti/modules/titanium/ui/widget/TiUIFab.java
@@ -0,0 +1,66 @@
+/**
+ * TiDev Titanium Mobile
+ * Copyright TiDev, Inc. 04/07/2022-Present
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package ti.modules.titanium.ui.widget;
+
+import android.content.res.ColorStateList;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.appcelerator.kroll.KrollDict;
+import org.appcelerator.kroll.KrollProxy;
+import org.appcelerator.kroll.common.Log;
+import org.appcelerator.titanium.TiApplication;
+import org.appcelerator.titanium.TiC;
+import org.appcelerator.titanium.proxy.TiViewProxy;
+import org.appcelerator.titanium.util.TiConvert;
+import org.appcelerator.titanium.view.TiDrawableReference;
+import org.appcelerator.titanium.view.TiUIView;
+
+public class TiUIFab extends TiUIView
+{
+	private static final String TAG = "TiUIFab";
+	FloatingActionButton fab;
+
+	public TiUIFab(TiViewProxy proxy)
+	{
+		super(proxy);
+		fab = new FloatingActionButton(TiApplication.getAppCurrentActivity());
+		setNativeView(fab);
+	}
+
+	@Override
+	public void processProperties(KrollDict d)
+	{
+		super.processProperties(d);
+
+		if (fab == null) {
+			return;
+		}
+
+		if (d.containsKey(TiC.PROPERTY_BACKGROUND_COLOR)) {
+			fab.setBackgroundTintList(ColorStateList.valueOf(
+				TiConvert.toColor(d, TiC.PROPERTY_BACKGROUND_COLOR, proxy.getActivity())));
+		}
+
+		if (d.containsKey(TiC.PROPERTY_IMAGE)) {
+			TiDrawableReference source = TiDrawableReference.fromObject(getProxy(), d.get(TiC.PROPERTY_IMAGE));
+			fab.setImageDrawable(source.getDrawable());
+		}
+	}
+
+	@Override
+	public void propertyChanged(String key, Object oldValue, Object newValue, KrollProxy proxy)
+	{
+		Log.i("---", key);
+		if (key.equals(TiC.PROPERTY_BACKGROUND_COLOR)) {
+			fab.setBackgroundTintList(ColorStateList.valueOf(TiConvert.toColor(newValue, proxy.getActivity())));
+		} else if (key.equals(TiC.PROPERTY_IMAGE)) {
+			TiDrawableReference source = TiDrawableReference.fromObject(getProxy(), newValue);
+			fab.setImageDrawable(source.getDrawable());
+		}
+	}
+}
