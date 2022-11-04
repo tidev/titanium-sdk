bc31a55e7ecfd21bd8c218ae945024189083d31f
Hans Knöchel <h.knoechel@lambus.com>
2022-11-04 23:10:13 +0100

    chore: move files to main target
    So it can be better included via the widget (still will require some CLI changes)

--- iphone/Classes/TiAppiOSActivityAttributesProxy.swift
+++ iphone/Classes/TiAppiOSActivityAttributesProxy.swift
@@ -10,7 +10,7 @@ import TitaniumKit
 import ActivityKit
 
 @objc(TiAppiOSActivityAttributesProxy)
-public class TiAppiOSActivityAttributesProxy : TiProxy {
+public class TiAppiOSActivityAttributesProxy: TiProxy {
 
   @objc(startActivity:)
   @available(iOS 16.1, *)
@@ -18,13 +18,13 @@ public class TiAppiOSActivityAttributesProxy : TiProxy {
     guard let params = args.first as? [String: String] else {
       fatalError("Missing required parameters")
     }
-    
+
     let attributes = TiActivityAttributes()
     let contentState = TiActivityAttributes.Status(value: params)
 
     do {
-      let _ = try Activity<TiActivityAttributes>.request(attributes: attributes, contentState: contentState)
-    } catch (let error) {
+      _ = try Activity<TiActivityAttributes>.request(attributes: attributes, contentState: contentState)
+    } catch let error {
       NSLog("[ERROR] Cannot start activity: \(error.localizedDescription)")
     }
   }
