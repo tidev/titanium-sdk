51c7c98afd69282c216c7a4fdfde1f5227032248
Marc Bender <79178770+mbender74@users.noreply.github.com>
2023-09-04 20:19:34 +0200

    fix(ios): building for MacOS (Catalyst) now works with that fix (#13903)
    * fix(ios): building for MacOS (Catalyst) now works with that fix
    
    Building was not working because of not longer supported removeFromSuperView in MacOSCatalyst
    Updated Titanium.xcodeproj to correct destination settings for using same bundleId for iOS and MacOS
    
    * fix(ios): building for MacOS (Catalyst)  finding all provisioning profiles
    
    * fix(ios): tab/spaces correction and  m1ga hint correted about the WWDR cert
    
    * fix(macos): fix layout issue

--- iphone/Classes/TiUIScrollableViewProxy.m
+++ iphone/Classes/TiUIScrollableViewProxy.m
@@ -6,8 +6,12 @@
  */
 #ifdef USE_TI_UISCROLLABLEVIEW
 
-#import "TiUIScrollableViewProxy.h"
+#if TARGET_OS_MACCATALYST
+#import <AppKit/AppKit.h>
+#endif
+
 #import "TiUIScrollableView.h"
+#import "TiUIScrollableViewProxy.h"
 
 @implementation TiUIScrollableViewProxy
 @synthesize viewProxies;
@@ -88,7 +92,7 @@
 #else
     TiThreadPerformOnMainThread(
         ^{
-          [[oldViewProxy view] removeFromSuperview];
+          [[oldViewProxy view] performSelector:@selector(removeFromSuperview)];
         },
         YES);
 #endif
