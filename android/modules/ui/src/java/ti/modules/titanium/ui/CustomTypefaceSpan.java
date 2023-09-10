f613ff636899f780709add528e86b8827b4056e3
m1ga <migamiga@web.de>
2023-09-10 19:49:49 +0200

    revert google copyright

--- android/modules/ui/src/java/ti/modules/titanium/ui/CustomTypefaceSpan.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/CustomTypefaceSpan.java
@@ -1,5 +1,5 @@
 /**
- * Copyright TiDev, Inc. 04/07/2022-Present. All Rights Reserved.
+ * Copyright (c) 2013, Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
