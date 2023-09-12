730d6238990a2e04d6ccd0642e0884167e43b04b
m1ga <migamiga@web.de>
2023-09-12 16:44:36 +0200

    chore: replace 'Axway Titanium' with 'Titanium SDK'

--- android/templates/module/generated/KrollGeneratedBindings.h
+++ android/templates/module/generated/KrollGeneratedBindings.h
@@ -1,5 +1,5 @@
 /**
- * Axway Titanium SDK
+ * Titanium SDK SDK
  * Copyright TiDev, Inc. 04/07/2022-Present
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
--- iphone/templates/module/objc/Xcode Templates/TiUIView.xctemplate/___FILEBASENAME___.h
+++ iphone/templates/module/objc/Xcode Templates/TiUIView.xctemplate/___FILEBASENAME___.h
@@ -1,5 +1,5 @@
 /**
- * Axway Titanium
+ * Titanium SDK
  * Copyright TiDev, Inc. 04/07/2022-Present. All Rights Reserved.
  * Licensed under the terms of the Apache Public License
  * Please see the LICENSE included with this distribution for details.
