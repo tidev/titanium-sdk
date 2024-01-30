5ecce41654a8a02b097dab8fbbd3c2598c7f0474
Hans Knöchel <h.knoechel@lambus.com>
2024-01-30 21:51:02 +0100

    Revert "Revert "feat(ios): support multi-scene applications (#13941)""
    This reverts commit 4a1d20f43c82296dd21bfd760fa63b65927e98fd.

--- iphone/TitaniumKit/TitaniumKit/Sources/API/TiApp.m
+++ iphone/TitaniumKit/TitaniumKit/Sources/API/TiApp.m
@@ -33,11 +33,6 @@ extern void UIColorFlushCache(void);
 
 #define SHUTDOWN_TIMEOUT_IN_SEC 3
 
-BOOL applicationInMemoryPanic = NO; // TODO: Remove in SDK 9.0+
-
-// TODO: Remove in SDK 9.0+
-TI_INLINE void waitForMemoryPanicCleared(void); //WARNING: This must never be run on main thread, or else there is a risk of deadlock!
-
 @interface TiApp ()
 - (void)checkBackgroundServices;
 - (void)appBoot;
@@ -327,14 +322,8 @@ TI_INLINE void waitForMemoryPanicCleared(void); //WARNING: This must never be ru
     [[TiLogServer defaultLogServer] start];
   }
 
-  // Initialize the root-window
-  window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-
   // Initialize the launch options to be used by the client
   launchOptions = [[NSMutableDictionary alloc] initWithDictionary:launchOptions_];
-
-  // Initialize the root-controller
-  [self initController];
 
   // If we have a APNS-UUID, assign it
   NSString *apnsUUID = [[NSUserDefaults standardUserDefaults] stringForKey:@"APNSRemoteDeviceUUID"];
@@ -1233,6 +1222,22 @@ TI_INLINE void waitForMemoryPanicCleared(void); //WARNING: This must never be ru
 - (KrollBridge *)krollBridge
 {
   return kjsBridge;
+}
+
+#pragma mark UIWindowSceneDelegate
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options
+{
+  return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+- (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions
+{
+  // Initialize the root-window
+  window = [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
+
+  // Initialize the root-controller
+  [self initController];
 }
 
 #pragma mark Background Tasks
