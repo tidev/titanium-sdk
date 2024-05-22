73bd156e3954cec2b0cfa3159177e7f69c614d1b
Marian Kucharcik <marian.kucharcik@maxapp.sk>
2024-05-22 12:54:41 +0200

    feat(ios): feat(ios): add overrideUserInterfaceStyle to Picker

--- iphone/Classes/TiUIPicker.m
+++ iphone/Classes/TiUIPicker.m
@@ -224,7 +224,14 @@ USE_PROXY_FOR_VERIFY_AUTORESIZING
     [[self picker] setBackgroundColor:[[TiUtils colorValue:value] _color]];
   }
 }
-
+- (void)setOverrideUserInterfaceStyle_:(id)args
+{
+  ENSURE_SINGLE_ARG(args, NSNumber);
+  if (picker != nil) {
+    int style = [TiUtils intValue:args def:UIUserInterfaceStyleUnspecified];
+    ((UIDatePicker *)[self picker]).overrideUserInterfaceStyle = style;
+  }
+}
 - (void)setDateTimeColor_:(id)value
 {
   // Guard date picker and iOS 14+ date picker style
