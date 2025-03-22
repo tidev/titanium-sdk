0ccdd0cc9ade80767fac1cb6a520697a286d5857
Michael Gangolf <migamiga@web.de>
2025-03-22 17:40:54 +0100

    feat(android): new widget: Ti.UI.RadioButtons

--- android/modules/ui/src/java/ti/modules/titanium/ui/RadioButtonsProxy.java
+++ android/modules/ui/src/java/ti/modules/titanium/ui/RadioButtonsProxy.java
@@ -7,27 +7,28 @@
 package ti.modules.titanium.ui;
 
 import android.app.Activity;
+
 import org.appcelerator.kroll.annotations.Kroll;
 import org.appcelerator.titanium.TiC;
 import org.appcelerator.titanium.proxy.TiViewProxy;
 import org.appcelerator.titanium.view.TiUIView;
-import ti.modules.titanium.ui.widget.TiUIButtonBar;
+
+import ti.modules.titanium.ui.widget.TiUIRadioButtons;
 
 @Kroll.proxy(creatableInModule = UIModule.class, propertyAccessors = {
-	TiC.PROPERTY_LABELS, TiC.PROPERTY_SELECTED_INDEX
+	TiC.PROPERTY_LABELS,
 })
-public class ButtonBarProxy extends TiViewProxy
+public class RadioButtonsProxy extends TiViewProxy
 {
 	@Override
 	public TiUIView createView(Activity activity)
 	{
-		defaultValues.put(TiC.PROPERTY_SELECTED_INDEX, -1);
-		return new TiUIButtonBar(this);
+		return new TiUIRadioButtons(this);
 	}
 
 	@Override
 	public String getApiName()
 	{
-		return "Ti.UI.ButtonBar";
+		return "Ti.UI.RadioButtons";
 	}
 }
