9aed300aed8599b502d697f8b0657c45b7ea525d
Hans Knöchel <h.knoechel@lambus.com>
2025-08-18 17:07:11 +0200

    feat(ios): add support for modern button configuration

---
+++ iphone/Classes/TiUIiOSButtonConfigurationProxy.m
@@ -0,0 +1,120 @@
+/**
+ * Titanium SDK
+ * Copyright TiDev, Inc. 04/07/2022-Present. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+#ifdef USE_TI_UIIOSBUTTONCONFIGURATION
+
+#import "TiUIiOSButtonConfigurationProxy.h"
+#import <TitaniumKit/TiBase.h>
+#import <TitaniumKit/TiUtils.h>
+
+@implementation TiUIiOSButtonConfigurationProxy
+
+- (NSString *)apiName
+{
+  return @"Ti.UI.iOS.ButtonConfiguration";
+}
+
+- (NSArray *)keySequence
+{
+  return @[ @"type" ];
+}
+
+- (void)setType:(NSString *)type
+{
+  if ([type isEqualToString:@"plain"]) {
+    _configuration = UIButtonConfiguration.plainButtonConfiguration;
+  } else if ([type isEqualToString:@"tinted"]) {
+    _configuration = UIButtonConfiguration.tintedButtonConfiguration;
+  } else if ([type isEqualToString:@"filled"]) {
+    _configuration = UIButtonConfiguration.filledButtonConfiguration;
+  } else if ([type isEqualToString:@"gray"]) {
+    _configuration = UIButtonConfiguration.grayButtonConfiguration;
+  } else if ([type isEqualToString:@"borderless"]) {
+    _configuration = UIButtonConfiguration.borderlessButtonConfiguration;
+  } else if ([type isEqualToString:@"bordered"]) {
+    _configuration = UIButtonConfiguration.borderedButtonConfiguration;
+  } else if ([type isEqualToString:@"borderedTinted"]) {
+    _configuration = UIButtonConfiguration.borderedTintedButtonConfiguration;
+  } else if ([type isEqualToString:@"borderedProminent"]) {
+    _configuration = UIButtonConfiguration.borderedProminentButtonConfiguration;
+  } else if (@available(iOS 26.0, *)) {
+#if IS_SDK_IOS_26
+    if ([type isEqualToString:@"glass"]) {
+      _configuration = UIButtonConfiguration.glassButtonConfiguration;
+    } else if ([type isEqualToString:@"prominentGlass"]) {
+      _configuration = UIButtonConfiguration.prominentGlassButtonConfiguration;
+    } else if ([type isEqualToString:@"clearGlass"]) {
+      _configuration = UIButtonConfiguration.clearGlassButtonConfiguration;
+    } else if ([type isEqualToString:@"prominentClearGlass"]) {
+      _configuration = UIButtonConfiguration.prominentClearGlassButtonConfiguration;
+    }
+#endif
+  }
+
+  if (_configuration == nil) {
+    _configuration = UIButtonConfiguration.plainButtonConfiguration;
+  }
+
+  [_configuration retain];
+}
+
+- (void)setTitle:(NSString *)title
+{
+  _configuration.title = title;
+}
+
+- (void)setSubtitle:(NSString *)subtitle
+{
+  _configuration.subtitle = subtitle;
+}
+
+- (void)setBackgroundColor:(id)backgroundColor
+{
+  _configuration.baseBackgroundColor = [TiUtils colorValue:backgroundColor].color;
+}
+
+- (void)setColor:(id)color
+{
+  _configuration.baseForegroundColor = [TiUtils colorValue:color].color;
+}
+
+- (void)setImage:(id)image
+{
+  _configuration.image = [TiUtils image:image proxy:self];
+}
+
+- (void)setPadding:(NSDictionary *)padding
+{
+  UIEdgeInsets insets = [TiUtils contentInsets:padding];
+  _configuration.contentInsets = NSDirectionalEdgeInsetsMake(insets.top, insets.left, insets.bottom, insets.right);
+}
+
+- (void)setImagePlacement:(NSString *)imagePlacement
+{
+  if ([imagePlacement isEqualToString:@"leading"]) {
+    _configuration.imagePlacement = NSDirectionalRectEdgeLeading;
+  } else if ([imagePlacement isEqualToString:@"trailing"]) {
+    _configuration.imagePlacement = NSDirectionalRectEdgeTrailing;
+  } else if ([imagePlacement isEqualToString:@"top"]) {
+    _configuration.imagePlacement = NSDirectionalRectEdgeTop;
+  } else if ([imagePlacement isEqualToString:@"bottom"]) {
+    _configuration.imagePlacement = NSDirectionalRectEdgeBottom;
+  }
+}
+
+- (void)setImagePadding:(NSNumber *)imagePadding
+{
+  _configuration.imagePadding = imagePadding.floatValue;
+}
+
+- (void)setTitlePadding:(NSNumber *)titlePadding
+{
+  _configuration.titlePadding = titlePadding.floatValue;
+}
+
+@end
+
+#endif
