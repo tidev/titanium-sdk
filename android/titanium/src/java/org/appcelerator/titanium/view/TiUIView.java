e7ec9f244dbab6ac0fccc42e43fd0061b1c5b546
Michael Gangolf <migamiga@web.de>
2026-01-06 10:20:38 +0100

    feat(android): keepHardwareMode property in Ti.UI.View

--- android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
+++ android/titanium/src/java/org/appcelerator/titanium/view/TiUIView.java
@@ -2210,7 +2210,9 @@ public abstract class TiUIView implements KrollProxyListener, OnFocusChangeListe
 
 	protected void disableHWAcceleration()
 	{
-		if (this.borderView != null) {
+		if (this.borderView != null && !(proxy.hasProperty("keepHardwareMode")
+			&& TiConvert.toBoolean(proxy.getProperty("keepHardwareMode"), false))
+		) {
 			this.borderView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
 		}
 	}
