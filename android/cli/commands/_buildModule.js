55ff8464fd8101fb5926bcdea0ad41c0a23d801d
Michael Gangolf <migamiga@web.de>
2026-07-18 19:53:33 +0200

    update times

--- android/cli/commands/_buildModule.js
+++ android/cli/commands/_buildModule.js
@@ -525,7 +525,7 @@ export class AndroidModuleBuilder extends Builder {
 		gradleProperties.push({ key: 'android.nonTransitiveRClass', value: 'false' });
 		gradleProperties.push({ key: 'android.builtInKotlin', value: 'false' });
 		gradleProperties.push({ key: 'android.newDsl', value: 'false' });
-		gradleProperties.push({ key: 'org.gradle.daemon.idletimeout', value: '300000' });
+		gradleProperties.push({ key: 'org.gradle.daemon.idletimeout', value: '600000' });
 		gradleProperties.push({
 			key: 'org.gradle.jvmargs',
 			value: `-Xmx${this.javacMaxMemory} -Dkotlin.daemon.jvm.options="-Xmx${this.javacMaxMemory}"`
