236c9a0704199d46dbfac926281edf8be712075e
Hans Knöchel <h.knoechel@lambus.com>
2026-07-28 08:40:00 +0200

    fix: remove debug-only selector for Ti.App._restart() method

--- iphone/Classes/AppModule.m
+++ iphone/Classes/AppModule.m
@@ -30,8 +30,6 @@ extern NSString *const TI_APPLICATION_GUID;
 
 @implementation AppModule
 
-#if defined(DEBUG) || defined(DEVELOPER)
-
 - (void)_restart:(id)unused
 {
   TiThreadPerformOnMainThread(
@@ -82,8 +80,6 @@ extern NSString *const TI_APPLICATION_GUID;
   [appDelegate applicationDidBecomeActive:app];
   /* End foregrounding simulation */
 }
-
-#endif
 
 - (void)dealloc
 {
